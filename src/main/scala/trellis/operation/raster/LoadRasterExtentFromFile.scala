371530464cfc6c3ca43d8f2fcee6ffb40f61c3ca
Josh Marcus <jmarcus@azavea.com>
2012-01-23 15:23:53 -0500

    Updating 4 operations to new operation syntax.

--- src/main/scala/trellis/operation/raster/LoadRasterExtentFromFile.scala
+++ src/main/scala/trellis/operation/raster/LoadRasterExtentFromFile.scala
@@ -7,18 +7,14 @@ import trellis.process._
 /**
   * Load the [[trellis.geoattrs.RasterExtent]] from the raster in the specified file.
   */
-class LoadRasterExtentFromFile(path:String) extends SimpleOperation[RasterExtent] { 
-
-  def _value(context:Context) = {
-    //val rdr = server.getReader(path)
-    //rdr.readMetadata
-    //rdr.getRasterExtent()
+class LoadRasterExtentFromFile(path:String) extends Op1(path)({
+  (path) => {
     val i = path.lastIndexOf(".")
     val jsonPath = (if (i == -1) path else path.substring(0, i)) + ".json"
     val layer = RasterLayer.fromPath(jsonPath)
-    layer.rasterExtent
+    Result(layer.rasterExtent)
   }
-}
+})
 
 object LoadRasterExtentFromFile {
   def apply(path:String) = new LoadRasterExtentFromFile(path)
--- src/main/scala/trellis/operation/raster/ResampleRaster.scala
+++ src/main/scala/trellis/operation/raster/ResampleRaster.scala
@@ -7,15 +7,13 @@ import trellis.data.IntRasterReader
 /**
  * This uses a nearest-neighbor algorithm to resample a raster.
  */
-case class ResampleRaster(r:Op[IntRaster], cols:Op[Int], rows:Op[Int]) extends SimpleOp[IntRaster] {
-  def _value(context:Context) = {
-    val raster = context.run(r)
-    val ncols = context.run(cols)
-    val nrows = context.run(rows)
+case class ResampleRaster(r:Op[IntRaster], cols:Op[Int], rows:Op[Int]) 
+extends Op3(r,cols,rows)({
+  (raster,cols,rows) => {
     val extent = raster.rasterExtent.extent
-    val cw = extent.width / ncols
-    val ch = extent.height / nrows
-    val re = RasterExtent(extent, cw, ch, ncols, nrows)
-    IntRasterReader.read(raster, Option(re))
+    val cw = extent.width / cols
+    val ch = extent.height / rows
+    val re = RasterExtent(extent, cw, ch, cols, rows)
+    Result(IntRasterReader.read(raster, Option(re)))
   }
-}
+})
