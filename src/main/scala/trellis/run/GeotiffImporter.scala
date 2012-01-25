1c5abd4f2ec98fc529ec2237632320b2af7a24bc
Josh Marcus <jmarcus@azavea.com>
2012-01-25 16:39:47 -0500

    Add runnable Geotiff importer.

---
+++ src/main/scala/trellis/run/GeotiffImporter.scala
@@ -0,0 +1,47 @@
+package trellis.run
+
+import java.util.Calendar
+
+import trellis.data.GeoTiffReader
+import trellis.data.Arg32Writer
+import trellis.process.{Server}
+import trellis.operation.LoadFile
+import trellis.process._
+
+object GeotiffImporter {
+  def error(msg:String) {
+    if (msg.length > 0) {
+      Console.printf("ERROR: %s\n\n", msg)
+    }
+    Console.printf("usage: geotif-to-arg32.scala INPATH OUTPATH\n")
+    sys.exit(1)
+  }
+
+  def main(args:Array[String]) {
+    if (args.length != 2) error("wrong number of arguments (%d)".format(args.length));
+
+    val inpath  = args(0)
+    val outpath = args(1)
+    
+    val server = Server("script", Catalog.empty("script"))
+
+    println("Loading file: " + inpath)
+    val raster = server.run(LoadFile(inpath))
+    server.shutdown()
+    //val reader = new GeoTiffReader(inpath, server)
+    //val raster = reader.loadRaster
+    
+    val version = "1.0"
+    
+    val name = "Generated from " + inpath
+    
+    
+    println("Converting file to ARG32 format: " + inpath)
+    val writer = Arg32Writer.write(outpath, raster, name)
+
+    println("ARG file generated: " + outpath )
+
+  }
+}
+
+// vim: set ts=4 sw=4 et:
