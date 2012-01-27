24ecd34ec501b5817a7ccff0c467204ea72bf8b9
Erik Osheim <eosheim@azavea.com>
2012-01-27 10:30:07 -0500

    removed unused HelloWorld servlet

--- src/main/scala/trellis/rest/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-package trellis.rest
-
-import javax.servlet.http._
-
-class HelloWorld extends HttpServlet {
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) =
-    resp.getWriter().print("Hello Worldddddd!")
-}
