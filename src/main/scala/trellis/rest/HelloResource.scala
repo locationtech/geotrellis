f8973b3e6213231ddb3548693b6b5f609277de8f
Josh Marcus <jmarcus@azavea.com>
2012-01-27 10:25:00 -0500

    Add embedded jetty runner and default rest services.

---
+++ src/main/scala/trellis/rest/HelloResource.scala
@@ -0,0 +1,16 @@
+package trellis.rest
+
+import javax.ws.rs._
+
+/**
+ * Simple hello world rest service that responds to "/hello"
+ */
+@Path("/hello")
+class HelloResource {
+  @GET
+  def hello() = {
+    "<h2>Hello Worldd!</h2>"
+  }
+
+}
+
