818651c539b7f5acc738f2fc7d36c535eb22f255
Erik Osheim <eosheim@azavea.com>
2012-02-14 11:41:01 -0500

    add some useful operations, mainly around building/parsing

--- src/main/scala/trellis/operation/raster/BuildExtent.scala
+++ src/main/scala/trellis/operation/raster/BuildExtent.scala
@@ -9,7 +9,7 @@ import trellis.process._
 case class BuildExtent(xmin:Op[Double], ymin:Op[Double], xmax:Op[Double], ymax:Op[Double])
 extends Op4 (xmin, ymin, xmax, ymax) ((x1,y1,x2,y2) => Result(Extent(x1,y1,x2,y2)))
 
-case class ParseExtent(s:String) extends Op1(s)({
+case class ParseExtent(s:Op[String]) extends Op1(s)({
   s => try {
     val Array(x1, y1, x2, y2) = s.split(",").map(_.toDouble)
     Result(Extent(x1, y1, x2, y2))
