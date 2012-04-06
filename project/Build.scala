8ff542519f89da73ff3f8b10caf32fcff8d617de
Josh Marcus <jmarcus@azavea.com>
2012-04-06 09:53:27 -0400

    Exclude jars no longer available from repo.

--- project/Build.scala
+++ project/Build.scala
@@ -32,8 +32,16 @@ object MyBuild extends Build {
       "javax.media" % "jai_core" % "1.1.3",
       "postgresql" % "postgresql" % "8.4-701.jdbc4",
       "net.liftweb" %% "lift-json" % "2.4-M5",
-      "com.typesafe.akka" % "akka-kernel" % "2.0-M2",
-      "com.typesafe.akka" % "akka-remote" % "2.0-M2",
+      "com.typesafe.akka" % "akka-kernel" % "2.0-M2" excludeAll(
+      ExclusionRule(organization = "com.sun.jdmk"),
+      ExclusionRule(organization = "com.sun.jmx"),
+      ExclusionRule(organization = "javax.jms")
+      ),
+      "com.typesafe.akka" % "akka-remote" % "2.0-M2" excludeAll(
+      ExclusionRule(organization = "com.sun.jdmk"),
+      ExclusionRule(organization = "com.sun.jmx"),
+      ExclusionRule(organization = "javax.jms")
+      ),
       "com.typesafe.akka" % "akka-actor"  % "2.0-M2",
       "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
       "com.google.code.caliper" % "caliper" % "1.0-SNAPSHOT" from "http://n0d.es/jars/caliper-1.0-SNAPSHOT.jar",
