cb4105b7c2ebcdaa4356b0c5bbb98b90e5ef6975
Josh Marcus <jmarcus@azavea.com>
2012-04-24 14:08:05 -0400

    Add new png rendering operations.

--- src/main/scala/geotrellis/operation/render/png/RenderPNG.scala
+++ src/main/scala/geotrellis/operation/render/png/RenderPNG.scala
@@ -14,15 +14,25 @@ extends Op4(r,colorBreaks,noDataColor, transparent)({
   (r, breaks, noDataColor, transparent) => {
     val mapper = ColorMapper(breaks, noDataColor)
     val ndc = if (transparent) 0 else noDataColor
-    val r2 = r.map(n => if (n == NODATA) ndc else (mapper(n) << 8) + 25)
+    val r2 = r.map(n => if (n == NODATA) ndc else (mapper(n) << 8) + 255)
     Result(RgbaEncoder().writeByteArray(r2))
   }
 })
 
-//TODO: documentation, rename
-case class RenderPNG3(r:Op[IntRaster], mapper:Op[ColorMapper]) extends Op2(r, mapper) ({
-  (r, mapper) => {
-    val r2 = r.map(n => if (n == NODATA) 0 else (mapper(n) << 8) + 255)
-    Result(RgbaEncoder().writeByteArray(r2))
-  }
+object RenderPNG3 {
+  def apply(r:Op[IntRaster], mapper:Op[ColorMapper]) = 
+    RenderPng32(r, mapper)
+}
+
+object RenderPng32 {
+  def apply(r:Op[IntRaster], mapper:Op[ColorMapper]) =
+    RenderPngRgba(ReclassifyColorsRgba(r, mapper))
+}
+
+case class RenderPngRgba(r:Op[IntRaster]) extends Op1(r) ({
+  r => Result(RgbaEncoder().writeByteArray(r))
+})
+
+case class ReclassifyColorsRgba(r:Op[IntRaster], mapper:Op[ColorMapper]) extends Op2(r, mapper) ({
+  (r, mapper) => Result(r.map(n => if (n == NODATA) 0 else (mapper(n) << 8) + 255))
 })
