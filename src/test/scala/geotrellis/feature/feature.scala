5db9384ecd5c7a1f0b84eabdb42c1d5cb7f6194c
Josh Marcus <jmarcus@azavea.com>
2012-08-22 17:18:46 -0400

    Add new feature and feature set classes, plus initial operations.
    This checkin includes the results of our feature spike, which creates
    some initial classes for features and featuresets and some sample
    operations.  This is still a work in progress.

---
+++ src/test/scala/geotrellis/feature/feature.scala
@@ -0,0 +1,46 @@
+package geotrellis.feature
+
+import geotrellis._
+import geotrellis.feature.op.geometry._
+import geotrellis.process._
+import geotrellis.feature._
+
+import math.{max,min,round}
+
+import org.scalatest.Spec
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.matchers.ShouldMatchers
+
+@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+class FeatureSpec extends Spec with MustMatchers with ShouldMatchers {
+  describe("Buffer") {
+    it("should buffer") {
+      val s = TestServer()
+
+      val p = Point(1.0,2.0,"hi")
+      val b = Buffer(p, 5.0, 8)
+      val r1a = s.run(b)
+
+      val p2 = Point (3.0,2.0,"goodbye")
+      val b2 = Buffer(p2, 5.0, 8)
+      val r1b = s.run(b2)
+ 
+      val c = GetCentroid(b)
+      val r2 = s.run(c)
+
+      val e = GetEnvelope(b)
+      val r3 = s.run(e)
+      println("envelope: " + r3)
+
+      val i = Intersect(Literal(p),b) 
+      println("i is: " + i)
+      val r4 = s.run(i)
+      println("interesect1: " + r4)
+
+      //val i2 = Intersect(p,b, (_:String) + (_:String))
+      //val r5 = s.run(i2)
+
+      s.shutdown()
+    }
+  }
+}
