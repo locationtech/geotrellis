f753d15f1a36973622de69f81553f4f0961062fd
Josh Marcus <jmarcus@azavea.com>
2012-10-02 10:04:12 -0400

    Add documentation to ArgWriter.

--- src/main/scala/geotrellis/data/arg/ArgWriter.scala
+++ src/main/scala/geotrellis/data/arg/ArgWriter.scala
@@ -7,11 +7,24 @@ import geotrellis.data._
 import geotrellis.util._
 import geotrellis.process._
 
+/**
+ * ArgWriter will write a raster to disk in ARG format.
+ * 
+ * Each instance of ArgWriter is provided a data type (e.g. int or float) to 
+ * use for output files.
+ */
 case class ArgWriter(typ:RasterType) extends Writer {
   def width = typ.bits / 8
   def rasterType = typ.name
   def dataType = "arg"
 
+  /**
+   * Write a raster in ARG format to the specified path.
+   * 
+   * @param path: Path to write arg files including base filename but not extension
+   * @param raster: Raster to write to disk
+   * @param name: Name to be included in json metadata as 'layer', used in catalog
+   */
   def write(path:String, raster:Raster, name:String) {
     val base = path.substring(0, path.lastIndexOf("."))
     writeMetadataJSON(base + ".json", name, raster.rasterExtent)
