9384ddb503f01b977fa49e14dead226960e7dd40
Josh Marcus <jmarcus@azavea.com>
2012-12-05 09:35:16 -0500

    Add zonal max for double values.

--- src/main/scala/geotrellis/raster/op/zonal/Min.scala
+++ src/main/scala/geotrellis/raster/op/zonal/Min.scala
@@ -65,3 +65,59 @@ case class Min[DD] (r:Op[Raster], zonePolygon:Op[Polygon[DD]], tileResults:Map[R
 }
 
 
+object MinDouble {
+  def createTileResults(trd:TiledRasterData, re:RasterExtent) = {
+    val tiles = trd.getTileList(re)
+    tiles map { r => (r.rasterExtent, minRaster(r))} toMap
+  }
+
+  def minRaster (r:Raster):Double = {
+    var min = Double.PositiveInfinity
+    r.foreach( (x) => if (x != NODATA && x < min) min = x )
+    min
+  }
+}
+
+/**
+ * Perform a zonal summary that calculates the sum of all raster cells within a geometry.
+ *
+ * @param   r             Raster to summarize
+ * @param   zonePolygon   Polygon that defines the zone
+ * @param   tileResults   Cached results of full tiles created by createTileResults
+ */
+case class MinDouble[DD] (r:Op[Raster], zonePolygon:Op[Polygon[DD]], tileResults:Map[RasterExtent,Double]) 
+  (implicit val mB: Manifest[Double], val mD: Manifest[DD]) extends TiledPolygonalZonalSummary[Double] {
+ 
+  type B = Double
+  type D = DD
+  
+  def handlePartialTileIntersection(rOp: Op[Raster], gOp: Op[Geometry[D]]) = {
+    rOp.flatMap ( r => gOp.flatMap ( g => {
+      var min = Double.PositiveInfinity
+      val f = (col:Int, row:Int, g:Geometry[_]) => {
+        val z = r.get(col,row)
+        if (z != NODATA && z < min) { min = z }
+      }
+      geotrellis.feature.rasterize.Rasterizer.foreachCellByFeature(
+        g,
+        r.rasterExtent,
+        f)
+      min
+    }))
+  }
+
+  def handleFullTile(rOp:Op[Raster]) = rOp.map (r =>
+    tileResults.get(r.rasterExtent).getOrElse({
+      var min = Double.PositiveInfinity
+      r.force.foreach((x:Int) => if (x != NODATA && x < min) min = x )
+      min
+   }))
+  
+ 
+  def handleNoDataTile = Double.PositiveInfinity
+  def handleDisjointTile = Double.PositiveInfinity
+
+  def reducer(mapResults: List[Double]):Double = mapResults.foldLeft(Double.PositiveInfinity)(math.min(_, _)) 
+}
+
+
