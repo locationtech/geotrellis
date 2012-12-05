4f64aa885983892f2f33f346cfc7014edacbe1a1
Josh Marcus <jmarcus@azavea.com>
2012-12-05 09:31:55 -0500

    Add zonal max for double values operation (MaxDouble).

--- src/main/scala/geotrellis/raster/op/zonal/Max.scala
+++ src/main/scala/geotrellis/raster/op/zonal/Max.scala
@@ -64,4 +64,59 @@ case class Max[DD] (r:Op[Raster], zonePolygon:Op[Polygon[DD]], tileResults:Map[R
   def reducer(mapResults: List[Int]):Int = mapResults.foldLeft(Int.MinValue)(math.max(_, _)) 
 }
 
+object MaxDouble {
+  def createTileResults(trd:TiledRasterData, re:RasterExtent) = {
+    val tiles = trd.getTileList(re)
+    tiles map { r => (r.rasterExtent, maxRaster(r))} toMap
+  }
+
+  def maxRaster (r:Raster):Double = {
+    var max = Double.NegativeInfinity
+    r.foreach( (x) => if (x != NODATA && x > max) max = x )
+    max
+  }
+}
+
+/**
+ * Perform a zonal summary that calculates the max value of all raster cells within a geometry.
+ *
+ * @param   r             Raster to summarize
+ * @param   zonePolygon   Polygon that defines the zone
+ * @param   tileResults   Cached results of full tiles created by createTileResults
+ */
+case class MaxDouble[DD] (r:Op[Raster], zonePolygon:Op[Polygon[DD]], tileResults:Map[RasterExtent,Double]) 
+  (implicit val mB: Manifest[Double], val mD: Manifest[DD]) extends TiledPolygonalZonalSummary[Double] {
+ 
+  type B = Double
+  type D = DD
+  
+  def handlePartialTileIntersection(rOp: Op[Raster], gOp: Op[Geometry[D]]) = {
+    rOp.flatMap ( r => gOp.flatMap ( g => {
+      var max = Double.NegativeInfinity
+      val f = (col:Int, row:Int, g:Geometry[_]) => {
+        val z = r.get(col,row)
+        if (z != NODATA && z > max) { max = z }
+      }
+      geotrellis.feature.rasterize.Rasterizer.foreachCellByFeature(
+        g,
+        r.rasterExtent,
+        f)
+      max
+    }))
+  }
+
+  def handleFullTile(rOp:Op[Raster]) = rOp.map (r =>
+    tileResults.get(r.rasterExtent).getOrElse({
+      var max = Double.NegativeInfinity
+      r.force.foreach((x:Int) => if (x != NODATA && x > max) max = x )
+      max
+   }))
+  
+ 
+  def handleNoDataTile = Double.NegativeInfinity
+  def handleDisjointTile = Double.NegativeInfinity
+
+  def reducer(mapResults: List[Double]):Double = mapResults.foldLeft(Double.NegativeInfinity)(math.max(_, _)) 
+}
+
 
