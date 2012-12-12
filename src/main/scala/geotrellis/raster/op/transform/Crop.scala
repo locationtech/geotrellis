b4179355f6831793c36008d7105d365f2e9ffe36
Rob Emanuele <rdemanuele@gmail.com>
2012-12-12 16:32:05 -0500

    Updated doc for Crop operation.

--- src/main/scala/geotrellis/raster/op/transform/Crop.scala
+++ src/main/scala/geotrellis/raster/op/transform/Crop.scala
@@ -4,7 +4,10 @@ import geotrellis._
 import geotrellis.raster._
 
 /**
- * Operation to crop a raster.
+ * Create a new raster from the data in a sub-extent of an existing raster.
+ * 
+ * @param      r         Raster to crop
+ * @param      extent    Subextent of r that will be the resulting cropped raster's extent.
  */
 case class Crop(r:Op[Raster], extent:Extent) 
      extends Op2(r,extent)((r,extent) => Result(CroppedRaster(r,extent))) { }
