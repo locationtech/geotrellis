288bd36c381b7f0fb8d91b250402df62f6d0bbe7
lossyrob <rdemanuele@gmail.com>
2013-02-11 11:23:59 -0500

    Added additional doc for RasterExtent

--- src/main/scala/geotrellis/RasterExtent.scala
+++ src/main/scala/geotrellis/RasterExtent.scala
@@ -12,6 +12,32 @@ case class GridBounds(colMin:Int,rowMin:Int,colMax:Int,rowMax:Int)
 
 /**
  * RasterExtent objects represent the geographic extent (envelope) of a raster.
+ *
+ * The Raster extent has two coordinate concepts involved: map coordinates and grid
+ * coordinates. Map coordinates are what the [[Extent]] class uses, and specifies points
+ * using an X coordinate and a Y coordinate. The X coordinate is oriented along west to east
+ * such that the larger the X coordinate, the more eastern the point. The Y coordinate is
+ * along south to north such that the larger the Y coordinate, the more Northern the point.
+ *
+ * This contrasts with the grid coordinate system. The grid coordinate system does not
+ * actually reference points on the map, but instead a cell of the raster that represents
+ * values for some square area of the map. The column axis is similar in that the number
+ * gets larger as one goes from west to east; however, the row axis is inverted from map coordinates:
+ * as the row number increases, the cell is heading south. The top row is labeled as 0, and the next
+ * 1, so that the highest indexed row is the southern most row of the raster.
+ * A cell has a height and a width that is in terms of map units. You can think of it as each cell
+ * is itself an extent, with width cellwidth and height cellheight. When a cell needs
+ * to be represented or thought of as a point, the center of the cell will be used.
+ * So when gridToMap is called, what is returned is the center point, in map coordinates.
+ *
+ * Map points are considered to be 'inside' the cell based on these rules:
+ *  - If the point is inside the area of the cell, it is included in the cell.
+ *  - If the point lies on the north or west border of the cell, it is included in the cell.
+ *  - If the point lies on the south or east border of the cell, it is not included in the cell,
+ *    it is included in the next southern or eastern cell, respectively.
+ *
+ * Note that based on these rules, the eastern and southern borders of an Extent are not actually
+ * considered to be part of the RasterExtent.
  */
 case class RasterExtent(extent:Extent, cellwidth:Double, cellheight:Double, cols:Int, rows:Int) {
 
