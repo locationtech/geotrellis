93b556120464ebdc28e32d011d0c1dc56dfb21d3
Josh Marcus <jmarcus@azavea.com>
2013-02-26 23:07:50 -0500

    Update Min to use new Reducer abstract class.

--- src/main/scala/geotrellis/statistics/op/stat/Min.scala
+++ src/main/scala/geotrellis/statistics/op/stat/Min.scala
@@ -3,13 +3,16 @@ package geotrellis.statistics.op.stat
 import scala.math.{min, max}
 
 import geotrellis._
-import geotrellis._
 
-case class Min(r:Op[Raster]) extends logic.Reducer1(r)({
-  r =>
-  var zmin = Int.MaxValue
-  r.foreach(z => if (z != NODATA) zmin = min(z, zmin))
-  zmin
-})({
-  zs => zs.reduceLeft(min)
-})
+case class Min(r:Op[Raster]) extends logic.TileReducer1[Int] {
+  type B = Int
+
+  def mapper(r: Op[Raster]) = {
+    println("in mapper")
+    logic.AsList(Min(r))
+  }
+  def reducer(mapResults:List[Int]) = {
+    println("in reducer")
+    mapResults.reduceLeft(min)
+  }
+}
