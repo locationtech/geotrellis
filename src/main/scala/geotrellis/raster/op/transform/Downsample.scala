0a40f9372bff5cc2907b014ded4d1b6737aceed2
lossyrob <rdemanuele@gmail.com>
2013-03-03 15:58:02 -0500

    Added Downsample operation.

---
+++ src/main/scala/geotrellis/raster/op/transform/Downsample.scala
@@ -0,0 +1,65 @@
+package geotrellis.raster.op.transform
+
+import geotrellis._
+import geotrellis.raster.op.focal.CellSet
+
+/**
+ * Downsamples the given raster to the given number of columns and rows
+ * by applying the given function to the cells of the original raster
+ * that compose a cell in the resulting raster. If the original RasterExtent
+ * dimensions are not divisible by the given cols and rows, the south and/or
+ * east borders of the resulting raster will be values of the function
+ * applied to the remainder cell values of the original raster. This may change the
+ * extent of the raster to accommodate for the remainder.
+ */
+case class Downsample(r: Raster, cols: Int, rows: Int)(f:CellSet=>Int)
+     extends Op4(r,cols,rows,f)({
+       (r,cols,rows,f) =>
+         val colsPerBlock = math.ceil(r.cols / cols.toDouble).toInt
+         val rowsPerBlock = math.ceil(r.rows / rows.toDouble).toInt
+         val cellwidth = colsPerBlock * r.rasterExtent.cellwidth
+         val cellheight = rowsPerBlock * r.rasterExtent.cellheight
+         val xmin = r.rasterExtent.extent.xmin
+         val ymin = r.rasterExtent.extent.ymin
+         val ext = Extent(xmin,ymin,xmin + cellwidth*cols,ymin + cellheight*rows)
+         val re = RasterExtent(ext,cellwidth,cellheight, cols, rows)
+         
+         val data = IntArrayRasterData.empty(cols,rows)
+
+         //Traverse the raster by blocks according to the given sizes,
+         val cellSet = new DownsampleCellSet(colsPerBlock,rowsPerBlock)
+         var col = 0
+         while(col < cols) {
+           var row = 0
+           while(row < rows) {
+             cellSet.focusOn(col,row)
+             data.set(col,row, f(cellSet))
+             row += 1
+           }
+           col += 1
+         }
+         Result(Raster(data,re))
+})
+
+class DownsampleCellSet(val colsPerBlock:Int, val rowsPerBlock:Int) extends CellSet {
+  private var focusCol = 0
+  private var focusRow = 0
+
+  def focusOn(col:Int,row:Int) = {
+    focusCol = col
+    focusRow = row
+  }
+  
+  def foreach(f:(Int,Int)=>Unit):Unit = {
+    var col = 0
+    while(col < colsPerBlock) {
+      var row = 0      
+      while(row < rowsPerBlock) {
+        f(focusCol*colsPerBlock + col, focusRow*rowsPerBlock + row)
+        row += 1
+      }
+      col += 1
+    }
+  }
+}
+
