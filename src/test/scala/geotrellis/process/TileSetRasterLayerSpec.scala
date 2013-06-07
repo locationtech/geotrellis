0111e8fb483da23819bc1d93e6b79b4f49699026
lossyrob <rdemanuele@gmail.com>
2013-06-07 13:13:44 -0400

    Made test tile rasters smaller.

--- src/test/scala/geotrellis/process/TileSetRasterLayerSpec.scala
+++ src/test/scala/geotrellis/process/TileSetRasterLayerSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 
 import geotrellis._
+import geotrellis.raster._
 import geotrellis.testutil._
 
 class TileSetRasterLayerSpec extends FunSpec 
@@ -14,9 +15,12 @@ describe("TileSetRasterLayer") {
     it("can cache") {
       println("Loading tiles...") 
       val start = System.nanoTime
-      val uncached = run(io.LoadRaster("sbn_tiled"))
-      val cached = run(io.LoadRaster("sbn_tiled_cached"))
+      val uncached = run(io.LoadRaster("mtsthelens_tiled"))
+      val cached = run(io.LoadRaster("mtsthelens_tiled_cached"))
 
+      // Make sure it's cached.
+      cached.data.asInstanceOf[TileSetRasterData].loader.asInstanceOf[CacheTileLoader]
+      
       cached.toArray should be (uncached.toArray)
       val duration = System.nanoTime - start
       println(s"Cache test took ${duration / 1000000000} seconds.")
