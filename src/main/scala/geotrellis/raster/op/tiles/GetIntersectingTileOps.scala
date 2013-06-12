f8eaa0bb20ebc88b466170af5cd1b86d2a9608b9
Josh Marcus <jmarcus@azavea.com>
2013-06-12 10:13:02 -0400

    Add new operations that return raster tile loading operations.
    These operations for data parallization on a raster return a list of operations
    that each load a raster tile, and are designed to replace Raster.getTileOpList().

---
+++ src/main/scala/geotrellis/raster/op/tiles/GetIntersectingTileOps.scala
@@ -0,0 +1,20 @@
+package geotrellis.raster.op.tiles
+
+import geotrellis._
+import geotrellis.raster._
+import geotrellis.feature._
+
+/** Get operations to load the tiles of this raster. 
+  *
+  * Note that the returned tiles are lazily loaded, so that they
+  * can still be dispatched across the network without passing data. 
+  */
+case class GetTileOps(r: Op[Raster]) extends Op1(r)({
+  r => { 
+    val tileOps = r.data match { 
+      case t:TiledRasterData => t.getTileOpList(r.rasterExtent)
+      case _ => Literal(r) :: Nil
+    }
+    Result(tileOps)
+  }
+})
