b98f5096f911206bfdecc7cf7123106172e81112
lossyrob <rdemanuele@gmail.com>
2013-06-25 16:43:04 -0400

    Second version of ToVector.
    Still only works with 4-neighborhoods. Now properly creates polygon for convex shapes. Abandoned using JTS's Convex Hull algorithm since we need to support convex shapes, instead use an algorithm that walks the parimeter.

--- src/main/scala/geotrellis/raster/op/ToVector.scala
+++ src/main/scala/geotrellis/raster/op/ToVector.scala
@@ -12,42 +12,276 @@ import scala.collection.mutable
 import spire.syntax._
 
 object ToVector {
-  def apply(r:Op[Raster]):Op[List[Geometry[Int]]] = {
+  def apply(r:Op[Raster]):Op[List[Polygon[Int]]] = {
     RegionGroup(r).flatMap { rgr =>
-      ToVector(rgr.raster, { i => rgr.regionMap(i) })
+      val p = new Polygonizer(rgr.raster)
+      (for(v <- rgr.regionMap.keys) yield {
+        p.getPolygon(v, rgr.regionMap(v))
+      }).toList
     }
   }
 }
 
-/**
- * Converts a raster to a vector.
- * Currently does not work with polygons with holes.
+/* Rules:
+ * Start with top left. Mark point on Top Left corner
+ * Check adjacent cells in counter clockwise starting from left:
+ *   - left, down, right, up
+ * 
+ *  On move, if the previous move is in the same direction, make no marks.
+ *  If the previous move and this move make a right hand turn,
+ *  a mark is made on the border of this cell and the previous cell
+ *  at the corner of the turn.
+ *  If they make a left hand turn, a mark is made on the edge opposite of
+ *  the border of this cell and the previous cell at the corner of the turn.
+ *  If the previous move was in the opposite direction, mark the two
+ *  corners of the edge opposite of the border of this cell and the
+ *  previous cell.
+ * 
+ *  Marks are signified by TL,TR,BL,BR based on previous cell.
+ * 
+ *  TL = Top Left  TR = Top Right  BL = Bottom Left   BR = Bottom Right 
+ *  D = Down  U = Up  L = Left  R = Right
+ *  PM = Previous Move   LHT = Left Hand Turn   RHT = Right Hand Turn
+ *                        RT = Reverse Turn
+ * 
+ *  Move   PM RHT  Mark   PM LHT  Mark    PM RT  Mark
+ * ------ -------------- --------------- -------------
+ *  D        R      BL      L      TL       U    TL,TR 
+ *  R        U      BR      D      BL       L    TL,BL
+ *  U        L      TR      R      BR       D    BL,BR
+ *  L        D      TL      U      TR       R    BR,TR
+ * 
  */
-case class ToVector(r:Op[Raster], f:Int=>Int) extends Op2(r,f)({
-  (r,f) =>
-    val re = r.rasterExtent
-    val cols = re.cols
-    val rows = re.rows
+class Polygonizer(val r:Raster) {
+  val re = r.rasterExtent
+  val cols = re.cols
+  val rows = re.rows
+  val halfCellWidth = re.cellwidth / 2.0
+  val halfCellHeight = re.cellheight / 2.0
 
-    val pointMap = mutable.Map[Int,mutable.ArrayBuffer[geom.Coordinate]]()
+  // Directions
+  val NOTFOUND = -1
+  val LEFT = 0
+  val DOWN = 1
+  val RIGHT = 2
+  val UP = 3
 
-    cfor(0)(_ < cols, _ + 1) { col =>
-      cfor(0)(_ < rows, _ + 1) { row =>
-        val v = r.get(col,row)
-        if(v != NODATA) {
-          if(!pointMap.contains(v)) {
-            pointMap(v) = mutable.ArrayBuffer[geom.Coordinate]()
-          }
-          pointMap(v) += new geom.Coordinate(re.gridColToMap(col),re.gridRowToMap(row))
+  // Marks
+  val TOPLEFT = 0
+  val BOTTOMLEFT = 1
+  val BOTTOMRIGHT = 2
+  val TOPRIGHT = 3
+
+  def sd(d:Int) = 
+    d match {
+      case NOTFOUND => "NOTFOUND"
+      case LEFT => "LEFT"
+      case DOWN => "DOWN"
+      case RIGHT => "RIGHT"
+      case UP => "UP"
+      case _ => "BAD"
+    }
+
+  def sm(d:Int) = 
+    d match {
+      case TOPLEFT => "TOPLEFT"
+      case BOTTOMLEFT => "BOTTOMLEFT"
+      case BOTTOMRIGHT => "BOTTOMRIGHT"
+      case TOPRIGHT => "TOPRIGHT"
+      case _ => "BAD"
+    }
+
+  def mark(col:Int,row:Int,m:Int):geom.Coordinate = {
+    val mapX = re.gridColToMap(col)
+    val mapY = re.gridRowToMap(row)
+    if(m == TOPLEFT) {
+      new geom.Coordinate(mapX - halfCellWidth, mapY + halfCellHeight)
+    } else if(m == BOTTOMLEFT) {
+      new geom.Coordinate(mapX - halfCellWidth, mapY - halfCellHeight)
+    } else if(m == BOTTOMRIGHT) {
+      new geom.Coordinate(mapX + halfCellWidth, mapY - halfCellHeight)
+    } else if(m == TOPRIGHT) {
+      new geom.Coordinate(mapX + halfCellWidth, mapY + halfCellHeight)
+    } else {
+      sys.error("Bad Mark Integer")
+    }
+  }
+
+  /*  Move   PM RHT  Mark   PM LHT  Mark    PM RT  Mark
+   * ------ -------------- --------------- -------------
+   *  D        R      BL      L      TL       U    TL,TR 
+   *  R        U      BR      D      BL       L    BL,TL
+   *  U        L      TR      R      BR       D    BR,BL
+   *  L        D      TL      U      TR       R    TR,BR
+   */
+  def makeMarks(points:mutable.ArrayBuffer[geom.Coordinate],
+                col:Int,row:Int,d:Int,pd:Int) = {
+    if(d == DOWN) {
+      if(pd != DOWN) {
+        if(pd == RIGHT) {                //RHT
+          points += mark(col,row-1,BOTTOMLEFT)
+        } else if(pd == LEFT) {          //LHT
+          points += mark(col,row-1,TOPLEFT)
+        } else {                         //RT
+          points += mark(col,row-1,TOPLEFT)
+          points += mark(col,row-1,TOPRIGHT)
         }
+      }
+    } else if(d == RIGHT) {
+      if(pd != RIGHT) {
+        if(pd == UP) {                   //RHT
+          points += mark(col-1,row,BOTTOMRIGHT)
+        } else if(pd == DOWN) {          //LHT
+          points += mark(col-1,row,BOTTOMLEFT)
+        } else {                         //RT
+          points += mark(col-1,row,BOTTOMLEFT)
+          points += mark(col-1,row,TOPLEFT)
+        }
+      }
+    } else if(d == UP) {
+      if(pd != UP) {
+        if(pd == LEFT) {                 //RHT
+          points += mark(col,row+1,TOPRIGHT)
+        } else if(pd == RIGHT) {         //LHT
+          points += mark(col,row+1,BOTTOMRIGHT)
+        } else {                         //RT
+          points += mark(col,row+1,BOTTOMRIGHT)
+          points += mark(col,row+1,BOTTOMLEFT)
+        }
+      }
+    } else if(d == LEFT) {
+      if(pd != LEFT) {
+        if(pd == DOWN) {                 //RHT
+          points += mark(col+1,row,TOPLEFT)
+        } else if(pd == UP) {            //LHT
+          points += mark(col+1,row,TOPRIGHT)
+        } else {                         //RT
+          points += mark(col+1,row,TOPRIGHT)
+          points += mark(col+1,row,BOTTOMRIGHT)
+        }
+      }
+    } else { sys.error(s"Unknown direction $d") }
+  }
+
+  def findNextDirection(col:Int,row:Int,d:Int,v:Int):Int = {
+    var i = d + 3
+    while(i < d + 7) {
+      val m = i % 4
+      if(m == 0) {
+        // Check left
+        if(col > 0) {
+          if(r.get(col-1,row) == v) {
+            return LEFT
+          }
+        }
+      }
+      else if(m == 1) {
+        // Check down
+        if(row < rows) {
+          if(r.get(col,row+1) == v) {
+            return DOWN
+          }
+        }
+      }
+      else if(m == 2) {
+        // Check right
+        if(col < cols) {
+          if(r.get(col+1,row) == v) {
+            return RIGHT
+          }
+        }
+      }
+      else if(m == 3) {
+        // Check up
+        if(row > 0) {
+          if(r.get(col,row-1) == v) {
+            return UP
+          }
+        }
+      }
+      i += 1
+    }
+
+    return NOTFOUND
+  }
+
+  def getPolygon[T](v:Int, data:T) = {
+    val points = mutable.ArrayBuffer[geom.Coordinate]()
+
+    // Find upper left start point.
+    var sc = 0
+    var sr = 0
+    var found = false
+    while(!found && sc < cols) {
+      sr = 0
+      while(!found && sr < rows) {
+        if(r.get(sc,sr) == v) { found = true }
+        if(!found) { sr += 1 }
+      }
+      if(!found) { sc += 1 }
+    }
+
+    if(!found) { sys.error(s"This raster does not contain value $v") }
+
+    val startCol = sc
+    val startRow = sr
+    points += mark(startCol,startRow,TOPLEFT)
+
+    // First check down and right of first. 
+    var direction = NOTFOUND
+    if(startRow < rows) {
+      if(r.get(startCol,startRow+1) == v) {
+        direction = DOWN
       }
     }
 
-    Result(pointMap.keys
-                   .map { v =>
-                     val jtsMP = Feature.factory.createMultiPoint( pointMap(v).toArray )
-                     val geom = jtsMP.convexHull
-                     Feature(geom, f(v))
-                    }
-                   .toList)
-})
+    if(direction == NOTFOUND && startCol < cols) {
+      if(r.get(startCol+1,startRow) == v) {
+        direction = RIGHT
+      }
+    }
+
+    if(direction == NOTFOUND) {
+      // Single cell polygon.
+      points += mark(startCol,startRow,BOTTOMLEFT)
+      points += mark(startCol,startRow,BOTTOMRIGHT)
+      points += mark(startCol,startRow,TOPRIGHT)
+      points += mark(startCol,startRow,TOPLEFT)
+    } else {
+      var previousDirection = direction
+      var col = startCol
+      var row = startRow
+
+      var break = false
+      while(!break) {
+        // Move in the direction.
+        if(direction == DOWN) {
+          row += 1
+        } else if(direction == RIGHT) {
+          col += 1
+        } else if(direction == UP) {
+          row -= 1
+        } else if(direction == LEFT) {
+          col -= 1
+        }
+
+        makeMarks(points, col,row,direction,previousDirection)
+        previousDirection = direction
+        direction = findNextDirection(col,row,direction,v)
+        if(col == startCol && row == startRow) {
+          if(previousDirection == LEFT) {
+            break = true
+          } else if(previousDirection == UP && direction == DOWN) {
+            break = true
+          }
+        }
+      }
+
+      // Make end marks
+      if(direction == UP) { mark(col,row, TOPRIGHT) }
+      points += mark(startCol,startRow,TOPLEFT) // Completes the polygon
+    }
+
+    Polygon(points.toArray, data)
+  }
+}
--- src/test/scala/geotrellis/raster/op/ToVectorSpec.scala
+++ src/test/scala/geotrellis/raster/op/ToVectorSpec.scala
@@ -35,16 +35,100 @@ class ToVectorSpec extends FunSpec
 
       val geoms = run(ToVector(r))
 
-      val onesCoords = Set( (1.5,-5.0),  (2.5,-5.0),
-                           (1.5,-15.0),(2.5,-15.0),
-                                                    (3.5,-25.0), (4.5,-25.0),
-                                                    (3.5,-35.0), (4.5,-35.0))
+      val onesCoords = Set(   (1.0,0.0), (3.0,-0.0),
+                            (1.0,-20.0),(3.0,-20.0),
+                                                    (3.0,-20.0), (5.0,-20.0),
+                                                    (3.0,-40.0), (5.0,-40.0))
       val ones = geoms.filter(_.data == 1).map(_.asInstanceOf[Polygon[Int]]).toList
       ones.length should be (2)
       ones.map { polygon =>
         polygon.data should be (1)
         val coordinates = polygon.geom.getCoordinates.sortBy(_.x).map(c => (c.x,c.y))
         coordinates.length should be (5)
+
+        coordinates.map { c => 
+          withClue (s"$c in expected coordinate set:") { onesCoords.contains(c) should be (true) }
+        }
+      }
+    }
+
+    it("should vectorize an off shape.") {
+      val n = NODATA
+      val arr = 
+        Array(
+//             0  1  2  3  4  5  6  7  8  9  0  1
+               n, n, n, n, n, n, n, n, n, n, n, n, // 0
+               n, n, n, n, n, n, n, n, n, n, n, n, // 1
+               n, n, n, n, n, n, n, n, n, n, n, n, // 2
+               n, n, n, n, 1, 1, n, n, n, 1, 1, n, // 3
+               n, n, n, 1, 1, 1, n, n, n, 1, 1, n, // 4
+               n, n, 1, 1, 5, 1, n, n, 1, 1, n, n, // 5
+               n, n, 1, 5, 5, 1, n, 1, 1, n, n, n, // 6 
+               n, n, 5, 5, 1, 1, n, 1, n, n, n, n, // 7
+               n, n, 5, 5, 1, 1, 1, 1, n, n, n, n, // 8
+               n, n, n, n, n, n, n, n, n, n, n, n, // 9
+               n, n, n, n, n, n, n, n, n, n, n, n, // 0
+               n, n, n, n, n, n, n, n, n, n, n, n, // 1
+               n, n, n, n, n, n, n, n, n, n, n, n, // 2
+               n, n, n, n, n, n, n, n, n, n, n, n) // 3
+
+      val cols = 12
+      val rows = 14
+      val cw = 1
+      val ch = 10
+      val xmin = 0
+      val xmax = 12
+      val ymin = -140
+      val ymax = 0
+
+      val r = Raster(arr,RasterExtent(Extent(xmin,ymin,xmax,ymax),cw,ch,cols,rows))
+
+
+      val geoms = run(ToVector(r))
+
+      def d(i:Int) = i.toDouble
+      def tl(col:Int,row:Int) = (d(col),d(row)* -10)        // Top left
+      def tr(col:Int,row:Int) = (d(col+1),(d(row))* -10)    // Top right
+      def bl(col:Int,row:Int) = (d(col),d(row+1)* -10)        // Bottom left
+      def br(col:Int,row:Int) = (d(col+1),(d(row)+1)* -10)    // Bottom right
+
+      val onesCoords = Set(  tl(4,3), tr(5,3),                       tl(9,3), tr(10,3),
+                    tl(3,4), tl(4,4),                                
+                            br(3,4),  bl(5,4),              tl(8,5), tl(9,5),
+            tl(2,5),tl(3,5),br(3,5),               tl(7,6), tl(8,6), br(9,4), br(10,4),
+                    br(2,5),          br(5,7),     tl(7,8), br(8,5), br(9,5),
+            bl(2,6),br(2,6),          bl(5,6),     br(7,6), br(8,6),
+                               tl(4,7),
+                               bl(4,8),            br(7,8),
+         //To complete polygon:
+              tl(2,5))
+
+
+      geoms.length should be (2)
+
+      val ones = geoms.filter(_.data == 1).map(_.asInstanceOf[Polygon[Int]]).toList
+      ones.length should be (1)
+      ones.map { polygon =>
+        polygon.data should be (1)
+        polygon.geom.getCoordinates.map(_.y).min should be (-90.0)
+        polygon.geom.getCoordinates.map(_.y).max should be (-30.0)
+        polygon.geom.getCoordinates.map(_.x).min should be (2.0)
+        polygon.geom.getCoordinates.map(_.x).max should be (11.0)
+        val coordinates = polygon.geom.getCoordinates.sortBy(_.x).map(c => (c.x,c.y))
+        def coordString = {
+          val sortedActual = coordinates.toSeq.sorted.toList
+          val sortedExpected = onesCoords.toSeq.sorted.toList
+          var s = "Values: (Actual,Expected)\n"
+          for(x <- 0 until math.max(sortedActual.length,sortedExpected.length)) {
+            if(x < sortedActual.length) { s += s" ${sortedActual(x)}  " }
+            if(x < sortedExpected.length) { s += s" ${sortedExpected(x)}  \n" }
+            else { s += "\n" }
+          }
+          s
+        }
+        withClue (s"$coordString\n\t\t") {
+          coordinates.length should be (onesCoords.size + 1)
+        }
 
         coordinates.map { c => 
           withClue (s"$c in expected coordinate set:") { onesCoords.contains(c) should be (true) }
