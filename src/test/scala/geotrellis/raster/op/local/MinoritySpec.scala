8d49fa4fca90d4fdc51f6c25091ffa6ebce9a81f
lossyrob <rdemanuele@gmail.com>
2013-08-01 15:57:20 -0400

    Added Minority operation

--- src/test/scala/geotrellis/raster/op/local/MinoritySpec.scala
+++ src/test/scala/geotrellis/raster/op/local/MinoritySpec.scala
@@ -7,11 +7,11 @@ import org.scalatest.matchers.ShouldMatchers
 
 import geotrellis.testutil._
 
-class MajoritySpec extends FunSpec 
+class MinoritySpec extends FunSpec 
                   with ShouldMatchers 
                   with TestServer 
                   with RasterBuilders {
-  describe("Majority") {
+  describe("Minority") {
     it("takes majority on rasters of all one value") {
       val r1 = createRaster(Array.fill(7*8)(1), 7, 8)
       val r2 = createRaster(Array.fill(7*8)(5), 7, 8)
@@ -21,14 +21,14 @@ class MajoritySpec extends FunSpec
       val r6 = createRaster(Array.fill(7*8)(7), 7, 8)
       val r7 = createRaster(Array.fill(7*8)(NODATA), 7, 8)
 
-      assertEqual(Majority(r1,r2,r3,r4,r5,r6,r7), Array.fill(7*8)(1))
-      assertEqual(Majority(1,r1,r2,r3,r4,r5,r6), Array.fill(7*8)(7))
-      assertEqual(Majority(Array(r1,r2,r3,r4,r5,r6),2), Array.fill(7*8)(5))
-      assertEqual(Majority(0,r1,r1,r2), Array.fill(7*8)(1))
-      assertEqual(Majority(1,r1,r1,r2), Array.fill(7*8)(5))
-      assertEqual(Majority(2,r1,r1,r2), Array.fill(7*8)(NODATA))
-      assertEqual(Majority(3,r1,r2,r3), Array.fill(7*8)(NODATA))
-      assertEqual(Majority(4,r1,r2,r3), Array.fill(7*8)(NODATA))
+      assertEqual(Minority(r1,r2,r3,r4,r5,r6,r7), Array.fill(7*8)(5))
+      assertEqual(Minority(1,r1,r2,r3,r4,r5,r6), Array.fill(7*8)(7))
+      assertEqual(Minority(Array(r1,r2,r3,r4,r5,r6),2), Array.fill(7*8)(1))
+      assertEqual(Minority(0,r1,r1,r2), Array.fill(7*8)(5))
+      assertEqual(Minority(1,r1,r1,r2), Array.fill(7*8)(1))
+      assertEqual(Minority(2,r1,r1,r2), Array.fill(7*8)(NODATA))
+      assertEqual(Minority(3,r1,r2,r3), Array.fill(7*8)(NODATA))
+      assertEqual(Minority(4,r1,r2,r3), Array.fill(7*8)(NODATA))
     }
   }
 }
