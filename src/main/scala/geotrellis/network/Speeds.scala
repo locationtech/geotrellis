b4d84b2af7cbafef7c45593fe32777333243b316
lossyrob <rdemanuele@gmail.com>
2013-08-13 21:44:48 -0400

    Removed comments from speeds.

--- src/main/scala/geotrellis/network/Speeds.scala
+++ src/main/scala/geotrellis/network/Speeds.scala
@@ -6,12 +6,4 @@ package geotrellis.network
 object Speeds {
   val walking = 1.33 // m/s
   val biking = 4.9  // m/s
-
-  // def walkDuration(start:Location,end:Location):Duration = {
-  //   val d = Distance.distance(start,end)
-  //   Duration(math.ceil(d/WALKING_SPEED).toInt)
-  // }
-
-  // def walkDistance(duration:Duration) =
-  //   duration.toInt / WALKING_SPEED
 }
