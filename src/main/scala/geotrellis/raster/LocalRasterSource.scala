e3da0a7438d304ac6f8b6a3622118b763f8466ae
Josh Marcus <jmarcus@azavea.com>
2013-08-20 14:40:58 -0400

    Implement LocalRasterSource and DistributedRasterSource.

--- src/main/scala/geotrellis/raster/LocalRasterSource.scala
+++ src/main/scala/geotrellis/raster/LocalRasterSource.scala
@@ -8,10 +8,20 @@ object LocalRasterSource {
     def apply() = new LocalRasterSourceBuilder
     def apply(rasterSrc:LocalRasterSource) = LocalRasterSourceBuilder(rasterSrc)
   }
+
+  def apply(raster:Op[Raster]) = {
+    raster.map { r =>
+      RasterDefinition(
+        r.rasterExtent,
+        TileLayout(1, 1, r.cols, r.rows),
+        Seq(r)
+      )
+    }
+  }
 }
 
-case class LocalRasterSource(val rasterDefinition:Op[RasterDefinition]) extends RasterSource
-  with RasterSourceLike[LocalRasterSource] {
+case class LocalRasterSource(val rasterDefinition:Op[RasterDefinition]) extends RasterSource  with RasterSourceLike[LocalRasterSource] {
   def partitions = rasterDefinition.map(_.tiles)
   def converge = ???
 }
+
--- src/test/scala/geotrellis/DataSourceSpec.scala
+++ src/test/scala/geotrellis/DataSourceSpec.scala
@@ -15,19 +15,13 @@ class DataSourceSpec extends FunSpec
   describe("DistributedRasterData") {
     it("should return a DistributedRasterData from map when possible") { 
       val r = byteRaster
-      val d1 = new DistributedRasterSource(null)
-      val d2 = d1.localAdd(3)
+      val d1 = DistributedRasterSource("mtsthelens_tiled_cached")
+      val d2:DistributedRasterSource = d1.localAdd(3)
+      val d3:DistributedRasterSource  = d1.map(local.Add(_, 3))
 
-      d2 should be (DistributedRasterSource)
-
-      val d3  = d1.map(local.Add(_, 3))
-
-      val l1 = new LocalRasterSource(null)
-      val l2 = l1.map(local.Add(_, 3))
-      l2 should be (LocalRasterSource)
-      
-      val l3 = l1.localAdd(2)
-      l3 should be (LocalRasterSource)
-    }
+     // val l1 = new LocalRasterSource(null)
+    //  val l2:LocalRasterSource = l1.map(local.Add(_, 3))
+    //  val l3:LocalRasterSource = l1.localAdd(2)
+     }
   }
 }
