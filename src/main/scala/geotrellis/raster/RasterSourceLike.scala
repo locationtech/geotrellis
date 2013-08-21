149c50494dce7104866fc8b79441e71a48e9f325
Josh Marcus <jmarcus@azavea.com>
2013-08-21 15:25:31 -0400

    Update Add and Subtract for new API.
    Add and Subtract no longer use the basic logic raster operations --
    appropriate handling of tiles is done through the RasterSource.localAdd()
    operation.

--- src/main/scala/geotrellis/raster/RasterSourceLike.scala
+++ src/main/scala/geotrellis/raster/RasterSourceLike.scala
@@ -4,7 +4,6 @@ import geotrellis._
 import geotrellis.raster.op._
 
 trait RasterSourceLike[+Repr <: DataSource[Raster]] extends DataSourceLike[Raster, Repr] with DataSource[Raster] { self: Repr =>
-  //type BF:CBSF[Repr, Raster, That]
   type CBF[A] = ({ type BF = CBSF[Repr, Raster, A]})#BF
   def tiles = self.partitions
   def rasterDefinition:Op[RasterDefinition]
@@ -22,14 +21,10 @@ def converge = LocalRasterSource.fromRaster(
       logic.Collect(rd.tiles).map(s => Raster(TileArrayRasterData(s.toArray,rd.tileLayout,re), re))
     })
  
-  def localAdd[That](i: Int)(implicit cbf: CBF[That]) = {
-    this.map(MyAddConstant(_, i))
-  }
+  def localAdd[That](i: Int)(implicit cbf: CBF[That]) = this.map(local.Add(_, i))
+  def localSubtract[That](i: Int)(implicit cbf: CBF[That]) = this.map(local.Subtract(_, i))
+
 }
 
-case class MyAddConstant(r:Op[Raster], i:Op[Int]) extends Op2(r,i) ({
-  (r,i) => Result(r.mapIfSet(z => z + i))
-})
 
 
-//case class StitchRasterDefinition
