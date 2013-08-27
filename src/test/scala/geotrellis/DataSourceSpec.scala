a533c1f00691ef1da82324677a97d3ea45bded18
Josh Marcus <jmarcus@azavea.com>
2013-08-27 10:10:56 -0400

    Add DistributedSeqSource test.

--- src/test/scala/geotrellis/DataSourceSpec.scala
+++ src/test/scala/geotrellis/DataSourceSpec.scala
@@ -6,16 +6,19 @@ import org.scalatest.matchers.ShouldMatchers
 
 import geotrellis.raster._
 import geotrellis.raster.op._
+import geotrellis.statistics.Histogram
 
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class DataSourceSpec extends FunSpec 
                         with ShouldMatchers 
                         with TestServer 
                         with RasterBuilders {
-  describe("DistributedRasterData") {
-    it("should return a DistributedRasterData from map when possible") { 
-      val r = byteRaster
-      val d1 = DistributedRasterSource("mtsthelens_tiled_cached")
+  def getDistributedRasterSource = 
+    DistributedRasterSource("mtsthelens_tiled_cached")
+
+  describe("DistributedRasterSource") {
+    it("should return a DistributedRasterSource when possible") { 
+      val d1 = getDistributedRasterSource
       val d2:DistributedRasterSource = d1.localAdd(3)
       val d3:DistributedRasterSource  = d2.map(local.Add(_, 3))
 
@@ -26,7 +29,16 @@ class DataSourceSpec extends FunSpec
       result1.get(100,100) should be (3233)
       result2.get(100,100) should be (3236)
       result3.get(100,100) should be (3239)
+    }
+    it ("should return a DistributedSeqSource when appropriate") {
+      val d = getDistributedRasterSource
+      val s:DistributedSeqSource[Histogram] = d.histogram
+      println(s)
+    }
+  }
 
+  describe("LocalRasterSource") {
+    it("should return a LocalRasterSource when possible") {
       val l1 = LocalRasterSource.fromRaster(createOnesRaster(5))
       val l2:LocalRasterSource = l1.localAdd(2)
       val l3:LocalRasterSource = l2.localAdd(5)
@@ -42,5 +54,9 @@ class DataSourceSpec extends FunSpec
       result6.get(0,0) should be (8)
       result7.get(0,0) should be (-2)
     }
+
+    
   }
+
+ 
 }
