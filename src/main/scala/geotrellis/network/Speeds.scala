50f0de0ea70f79a9abc2e863017cb5f0bc06d10c
Josh Marcus <jmarcus@azavea.com>
2013-09-11 14:38:30 -0400

    Increase biking speed.

--- src/main/scala/geotrellis/network/Speeds.scala
+++ src/main/scala/geotrellis/network/Speeds.scala
@@ -5,7 +5,6 @@ package geotrellis.network
  */
 object Speeds {
   val walking = 1.33 // m/s
-  // biking speed from: 
-  // http://arxiv.org/abs/1011.6266
-  val biking = 2.7  // m/s
+  // biking speed based on http://arxiv.org/abs/1011.6266
+  val biking = 3.2  // m/s
 }
