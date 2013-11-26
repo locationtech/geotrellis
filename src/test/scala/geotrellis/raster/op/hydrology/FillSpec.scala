0c5bc388943ae9d0fbba18c51f43bd0fca25f22e
lossyrob <rdemanuele@gmail.com>
2013-11-26 15:34:51 -0500

    Fixes to syntax and other things.

--- src/test/scala/geotrellis/raster/op/hydrology/FillSpec.scala
+++ src/test/scala/geotrellis/raster/op/hydrology/FillSpec.scala
@@ -10,7 +10,6 @@ import org.junit.Assert._
 
 import geotrellis.testutil._
 
-@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class FillSpec extends FunSpec 
                  with ShouldMatchers 
                  with TestServer 
@@ -20,7 +19,7 @@ class FillSpec extends FunSpec
     it("Returns a new raster with sinks removed"){
       var ncols = 6
       var nrows = 6
-      val r_extent = RasterExtent(Extent(0,0,1,1),1,1,ncols,nrows)
+      val re = RasterExtent(Extent(0,0,1,1),1,1,ncols,nrows)
       val m = IntArrayRasterData(Array[Int](
             2,2,2,4,4,8,
             1,1,1,4,8,4,
@@ -30,7 +29,7 @@ class FillSpec extends FunSpec
             1,1,1,1,4,16),
             ncols,nrows)
 
-      val in_raster = Raster(m, r_extent)
+      val inRaster = Raster(m, re)
       val o = IntArrayRasterData(Array[Int](
             2,2,2,4,4,8,
             1,1,1,4,8,4,
@@ -39,8 +38,8 @@ class FillSpec extends FunSpec
             1,2,3,4,5,6,
             1,1,1,1,4,16),
             ncols,nrows)
-      val out_raster = Raster(o, r_extent)
-      assertEqual(Fill(in_raster),out_raster )
+      val outRaster = Raster(o, re)
+      assertEqual(Fill(inRaster),outRaster )
     } 
   }
 }
