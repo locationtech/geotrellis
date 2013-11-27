26e7a7dfaeb70884d10e3bf771fe7a0202e064ef
lossyrob <rdemanuele@gmail.com>
2013-11-27 17:22:39 -0500

    Fix to io.LoadTile for loading raster extent

--- src/main/scala/geotrellis/io/LoadTile.scala
+++ src/main/scala/geotrellis/io/LoadTile.scala
@@ -11,10 +11,10 @@ object LoadTile {
 
 case class LoadTile(n:Op[String],col:Op[Int],row:Op[Int],targetExtent:Op[Option[RasterExtent]]) 
     extends Op[Raster] {
-  def _run(context:Context) = runAsync(List(n, col, row, context))
+  def _run(context:Context) = runAsync(List(n, col, row, targetExtent, context))
   val nextSteps:Steps = {
-    case (n:String) :: (col:Int) :: (row:Int) :: (context:Context) :: Nil =>
+    case (n:String) :: (col:Int) :: (row:Int) :: (te:Option[RasterExtent]) :: (context:Context) :: Nil =>
       val layer = context.getRasterLayer(n)
-      Result(layer.getTile(col,row))
+      Result(layer.getTile(col,row,te))
   }
 }
