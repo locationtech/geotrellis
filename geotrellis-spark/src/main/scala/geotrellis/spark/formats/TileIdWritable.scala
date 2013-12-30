3e60aa1d4911977e03dc261a0b9a553f4fba58ee
Ameet Kini <ameetkini@gmail.com>
2013-12-30 17:47:16 -0500

    Override writeObject/readObject in TileIdWritable Using the defaults is wrong since it results in an uninitialized member "0" on the read side

--- geotrellis-spark/src/main/scala/geotrellis/spark/formats/TileIdWritable.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/formats/TileIdWritable.scala
@@ -1,8 +1,28 @@
 package geotrellis.spark.formats
 
 import org.apache.hadoop.io.LongWritable
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
-class TileIdWritable extends LongWritable with Serializable 
+class TileIdWritable extends LongWritable with Serializable {
+  override def equals(that: Any) : Boolean = {
+    //println(s"called equals on ${get}, isInstanceOf = ${that.isInstanceOf[TileIdWritable]} and [${get},${that.asInstanceOf[TileIdWritable].get}")
+    that.isInstanceOf[TileIdWritable] &&
+    (this.get == that.asInstanceOf[TileIdWritable].get)
+  }
+  
+  private def writeObject(out: ObjectOutputStream) {
+    //println("called TileIdWritable writeObject " + get)
+    out.defaultWriteObject()
+    out.writeLong(get)
+  }
+
+  private def readObject(in: ObjectInputStream) {
+    in.defaultReadObject()
+    set(in.readLong)
+    //println("called TileIdWritable readObject " + get)
+  }
+} 
 
 object TileIdWritable {
   def apply(value: Long): TileIdWritable = {
