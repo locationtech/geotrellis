f44be208ae106b431b2f30764ad96236f295dd54
Ameet Kini <ameetkini@gmail.com>
2013-12-30 14:45:59 -0500

    update LoadImageRDD to extend HadoopRDD from the old HadoopFileRDD

--- geotrellis-spark/src/main/scala/geotrellis/spark/rdd/LoadImageRDD.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/rdd/LoadImageRDD.scala
@@ -1,28 +1,29 @@
 package geotrellis.spark.rdd
 
 import org.apache.spark.SparkContext._
- 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.mapred.SequenceFileInputFormat
 import org.apache.spark.SerializableWritable
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.rdd.HadoopFileRDD
 import geotrellis.spark.formats.ArgWritable
 import geotrellis.spark.formats.TileIdWritable
 import geotrellis.spark.utils.GeotrellisSparkUtils
 import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.rdd.HadoopRDD
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapred.FileInputFormat
 
 class LoadImageRDD(
   sc: SparkContext,
   path: String,
   broadcastedConf: Broadcast[SerializableWritable[Configuration]],
   minSplits: Int)
-  extends HadoopFileRDD[TileIdWritable, ArgWritable](
+  extends HadoopRDD[TileIdWritable, ArgWritable](
 		  	sc,
-		  	path,
 		  	broadcastedConf,
+		  	Some((jobConf: JobConf) => FileInputFormat.setInputPaths(jobConf, path)),
 		  	classOf[SequenceFileInputFormat[TileIdWritable, ArgWritable]],
 		  	classOf[TileIdWritable],
 		  	classOf[ArgWritable],
