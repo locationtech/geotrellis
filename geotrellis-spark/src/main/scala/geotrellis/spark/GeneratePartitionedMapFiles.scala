13d02a2c97193b8a1dccfd52276d030065275163
Ameet Kini <ameetkini@gmail.com>
2014-01-02 16:41:19 -0500

    rewrote from imperative to functional style

--- geotrellis-spark/src/main/scala/geotrellis/spark/GeneratePartitionedMapFiles.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/GeneratePartitionedMapFiles.scala
@@ -16,17 +16,17 @@ import scala.util.Random
 
 /**
  * @author akini
- * 
+ *
  * A simple example demonstrating reading and writing of (tileId, Array) out to sequence file
  * Run command: GeneratePartitionedMapFiles [HDFS-OR-LOCAL-PATH-TO-DIR] [NUMTILES] [NUMFILES]
- *  		e.g.,	GeneratePartitionedMapFiles file:///tmp/argtest/ 100 5 
+ *  		e.g.,	GeneratePartitionedMapFiles file:///tmp/argtest/ 100 5
  * This would give 5 partitions, each having 20 tiles
- * 
- * 
+ *
+ *
  */
 object GeneratePartitionedMapFiles {
   val defaultTileSize = TmsTiling.DefaultTileSize
-  
+
   def fill(value: Int) = {
     //val arr = Array.fill(defaultTileSize * defaultTileSize)(value)
     val arr = (Seq.fill(defaultTileSize * defaultTileSize)(Random.nextInt)).toArray
@@ -37,39 +37,40 @@ object GeneratePartitionedMapFiles {
     val dirPath = new Path(args(0)) // full path to directory
     val numTiles = args(1).toInt
     val numFiles = args(2).toInt
-    
+
     val tilesPerFile = numTiles / numFiles
     val conf = GeotrellisSparkUtils.createHadoopConfiguration
     conf.set("io.map.index.interval", "1");
     val fs = dirPath.getFileSystem(conf)
 
     val key = new TileIdWritable()
-    
-    var writer: Option[MapFile.Writer] = None
 
-    for (i <- 0 until numTiles) {
-      if(i % tilesPerFile == 0) {
-    	writer.foreach(_.close)  
-        val mapFilePath = new Path(dirPath, "part-%05d".format(i))
-        println("writing to " + mapFilePath)
-        writer = Some(new MapFile.Writer(conf, fs, mapFilePath.toUri.toString, 
-        							classOf[TileIdWritable], classOf[ArgWritable],
-    								SequenceFile.CompressionType.RECORD))         
+    for ((indices, fileIndex) <- 0 until numTiles grouped tilesPerFile zipWithIndex) {
+      val mapFilePath = new Path(dirPath, f"part-${fileIndex * tilesPerFile}%05d")
+      println(s"writing to $mapFilePath")
+
+      val writer = new MapFile.Writer(conf, fs, mapFilePath.toUri.toString,
+        classOf[TileIdWritable], classOf[ArgWritable],
+        SequenceFile.CompressionType.RECORD)
+      try {
+        for (i <- indices) {
+          key.set(i)
+          val array = fill(i)
+          val value = ArgWritable.toWritable(array)
+          writer.append(key, value)
+        }
+      } finally {
+        writer.close
       }
-      key.set(i)
-      val array = fill(i)
-      val value = ArgWritable.toWritable(array)
-      writer.foreach(_.append(key, value))
+
     }
-    writer.foreach(_.close)  
 
     val generator = new SplitGenerator {
-      def getSplits = (for(i <- 0 until numTiles by tilesPerFile) yield i.toLong).drop(1).map(_ - 1)
+      def getSplits = (for (i <- 0 until numTiles by tilesPerFile) yield i.toLong).drop(1).map(_ - 1)
       println(getSplits)
     }
     val splitFile = new Path(dirPath, TileIdPartitioner.SplitFile).toUri.toString
     TileIdPartitioner(generator, splitFile, conf)
   }
-
 
 }
