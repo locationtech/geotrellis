634a2dc324803ab2621b211384dc35be3da94547
Ameet Kini <ameetkini@gmail.com>
2014-01-10 18:27:11 -0500

    add listFiles to recurse down a directory and fetch all file paths

--- geotrellis-spark/src/main/scala/geotrellis/spark/utils/HdfsUtils.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/utils/HdfsUtils.scala
@@ -1,22 +1,37 @@
 package geotrellis.spark.utils
-
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs._
 
-import java.io._
+import org.apache.hadoop.fs.FileStatus
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.LocalFileSystem
+import org.apache.hadoop.fs.Path
+
+import java.io.BufferedReader
+import java.io.Closeable
+import java.io.File
+import java.io.FileReader
+import java.io.InputStreamReader
 import java.util.Scanner
+
+import scala.collection.mutable.ListBuffer
 
 abstract class LineScanner extends Iterator[String] with Closeable
 
 object HdfsUtils {
-  def getLineScanner(path:String, conf:Configuration): Option[LineScanner] =
+  def listFiles(path: Path, conf: Configuration): List[Path] = {
+    val fs = path.getFileSystem(conf)
+    val files = new ListBuffer[Path]
+    addFiles(fs.listStatus(path), fs, conf, files)
+    files.toList
+  }
+  def getLineScanner(path: String, conf: Configuration): Option[LineScanner] =
     getLineScanner(new Path(path), conf)
 
-  def getLineScanner(path:Path, conf:Configuration): Option[LineScanner] = {
+  def getLineScanner(path: Path, conf: Configuration): Option[LineScanner] = {
     path.getFileSystem(conf) match {
       case localFS: LocalFileSystem =>
         val localFile = new File(path.toUri.getPath)
-        if(!localFile.exists)
+        if (!localFile.exists)
           return None
         else {
           val scanner =
@@ -32,9 +47,10 @@ object HdfsUtils {
           Some(lineScanner)
         }
       case fs =>
-        if(!fs.exists(path)) {
+        if (!fs.exists(path)) {
           return None
-        } else {
+        }
+        else {
           val fdis = fs.open(path)
           val scanner = new Scanner(new BufferedReader(new InputStreamReader(fdis)))
 
@@ -42,11 +58,25 @@ object HdfsUtils {
             new LineScanner {
               def hasNext = scanner.hasNextLine
               def next = scanner.nextLine
-              def close = { scanner.close ; fdis.close }
+              def close = { scanner.close; fdis.close }
             }
 
           Some(lineScanner)
         }
+    }
+  }
+
+  private def addFiles(fileStatuses: Array[FileStatus],
+                       fs: FileSystem,
+                       conf: Configuration,
+                       files: ListBuffer[Path]): Unit = {
+    for (fst <- fileStatuses) {
+      println("adding " + fst.getPath)
+      if (fst.isDir())
+        addFiles(fs.listStatus(fst.getPath()), fs, conf, files)
+      else
+        files += fst.getPath()
+
     }
   }
 }
