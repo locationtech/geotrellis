30eacbc4ee97ff6e1e1782457b664ed5cb3015b9
Ameet Kini <ameetkini@gmail.com>
2014-01-13 13:49:28 -0500

    update deprecated API calls in JacksonWrapper

--- geotrellis-spark/src/main/scala/geotrellis/spark/metadata/JacksonWrapper.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/metadata/JacksonWrapper.scala
@@ -19,21 +19,23 @@ object JacksonWrapper {
     writer.toString
   }
 
-  def deserialize[T: Manifest](value: String) : T =
+  def deserialize[T: Manifest](value: String): T =
     mapper.readValue(value, typeReference[T])
 
   def prettyPrint(value: Any): String = {
     val writer = mapper.writer().withDefaultPrettyPrinter()
     writer.writeValueAsString(value)
   }
+  
   private [this] def typeReference[T: Manifest] = new TypeReference[T] {
     override def getType = typeFromManifest(manifest[T])
   }
 
   private [this] def typeFromManifest(m: Manifest[_]): Type = {
-    if (m.typeArguments.isEmpty) { m.erasure }
+    if (m.typeArguments.isEmpty) 
+      m.runtimeClass 
     else new ParameterizedType {
-      def getRawType = m.erasure
+      def getRawType = m.runtimeClass
       def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
       def getOwnerType = null      
     }
