d8f5a95a07b38b2ac25663ec4ce5b30f930f14ca
lossyrob <rdemanuele@gmail.com>
2014-01-16 13:45:52 -0500

    Added implicits to convert jts Geometries to features.

--- src/main/scala/geotrellis/feature/package.scala
+++ src/main/scala/geotrellis/feature/package.scala
@@ -16,4 +16,25 @@ package object feature {
 
     def asPolygonSet() = flattenToPolygon(geom)
   }
+
+  implicit def pointToFeature(p:jts.Point): Point[Int] = 
+    Point(p, 0)
+
+  implicit def lineStringToFeature(l:jts.LineString): LineString[Int] =
+    LineString(l, 0)
+
+  implicit def polyToFeature(p:jts.Polygon): Polygon[Int] =
+    Polygon(p, 0)
+
+  implicit def multiPointToFeature(mp:jts.MultiPoint): MultiPoint[Int] =
+    MultiPoint(mp, 0)
+
+  implicit def multiLineStringToFeature(mls:jts.MultiLineString): MultiLineString[Int] =
+    MultiLineString(mls, 0)
+
+  implicit def mutliPolygonToFeature(mp:jts.MultiPolygon): MultiPolygon[Int] =
+    MultiPolygon(mp, 0)
+
+  implicit def geometryCollectionToFeature(gc:jts.GeometryCollection): GeometryCollection[Int] =
+    GeometryCollection(gc, 0)
 }
