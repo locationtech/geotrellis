6fae95871b9d3f2d02f85cbfe7c240834591465e
lossyrob <rdemanuele@gmail.com>
2014-01-16 13:46:25 -0500

    Added SeqSource, HistogramSource, removed DS type aliases.

--- src/main/scala/geotrellis/source/package.scala
+++ src/main/scala/geotrellis/source/package.scala
@@ -1,6 +1,9 @@
 package geotrellis
 
 package object source {
+  type SeqSource[+T] = DataSource[T,Seq[T]]
+  type HistogramSource = DataSource[statistics.Histogram,statistics.Histogram]
+
   implicit def seqRasterSourceToRasterSourceSeq(seq:Seq[RasterSource]):RasterSourceSeq =
     RasterSourceSeq(seq)
 }
