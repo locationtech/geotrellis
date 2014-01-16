5f3dd7b7d87dab0a30ab51c9f86b87b6566ee567
lossyrob <rdemanuele@gmail.com>
2014-01-16 15:14:19 -0500

    Create a DataSource with the converge results of a seq of DataSources

--- src/main/scala/geotrellis/source/package.scala
+++ src/main/scala/geotrellis/source/package.scala
@@ -4,6 +4,9 @@ package object source {
   type SeqSource[+T] = DataSource[T,Seq[T]]
   type HistogramSource = DataSource[statistics.Histogram,statistics.Histogram]
 
-  implicit def seqRasterSourceToRasterSourceSeq(seq:Seq[RasterSource]):RasterSourceSeq =
-    RasterSourceSeq(seq)
+  implicit def iterableRasterSourceToRasterSourceSeq(iterable:Iterable[RasterSource]):RasterSourceSeq =
+    RasterSourceSeq(iterable.toSeq)
+
+  implicit def dataSourceSeqToSeqSource[T](iterable:Iterable[DataSource[_,T]]): SeqSource[T] =
+    DataSource.fromSources(iterable.toSeq)
 }
