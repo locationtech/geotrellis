b48bacfdb6454b8d72076ce32a0069182f5532fb
Ameet Kini <ameetkini@gmail.com>
2014-01-24 11:00:39 -0500

    NoDataHandler: simplified logic

--- geotrellis-spark/src/main/scala/geotrellis/spark/cmd/NoDataHandler.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/cmd/NoDataHandler.scala
@@ -5,37 +5,27 @@ import geotrellis.raster.MutableRasterData
 import geotrellis.raster.RasterData
 
 object NoDataHandler {
+
   def removeUserNodata(rd: MutableRasterData, userNodata: Double): MutableRasterData = {
-
-    def removeInt(rd: MutableRasterData, gtNodata: Int) = {
-      var i = 0
-      while (i < rd.length) {
-        if (rd(i) == userNodata) rd(i) = gtNodata
-        i += 1
-      }
-      rd
+    /* 
+     * This handles all types of RasterData - e.g., FloatArrayRasterData, ByteArrayRasterData
+     * because the apply/update methods handle conversion of NODATA to the appropriate types
+     * via macros i2f, i2b, respectively 
+     */
+    var i = 0
+    while (i < rd.length) {
+      if (rd(i) == userNodata) rd(i) = NODATA
+      i += 1
     }
-    def removeDouble(rd: MutableRasterData, gtNodata: Double) = {
-      var i = 0
-      while (i < rd.length) {
-        if (rd(i) == userNodata) rd.updateDouble(i, gtNodata)
-        i += 1
-      }
-      rd
-    }
-
-    val trd = rd.getType match {
-      case TypeBit    => removeInt(rd, 0)
-      case TypeByte   => removeInt(rd, byteNODATA)
-      case TypeShort  => removeInt(rd, shortNODATA)
-      case TypeInt    => removeInt(rd, NODATA)
-      case TypeFloat  => removeDouble(rd, Float.NaN)
-      case TypeDouble => removeDouble(rd, Double.NaN)
-    }
-    trd
+    rd
   }
 
   def removeGtNodata(rd: MutableRasterData, userNodata: Double): RasterData = {
+    /* 
+     * This handles all types of RasterData - e.g., FloatArrayRasterData, ByteArrayRasterData
+     * because the scala will convert the raw types to either Double or Int as per the argument
+     * of the anonymous function
+     */
     if (rd.isFloat)
       rd.mapDouble((i: Double) => if (isNoData(i)) userNodata else i)
     else
