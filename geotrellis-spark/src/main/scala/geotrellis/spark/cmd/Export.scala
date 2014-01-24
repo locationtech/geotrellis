706f5303f066e673263799744d647a0a250668a0
Ameet Kini <ameetkini@gmail.com>
2014-01-24 14:39:26 -0500

    Export: delete and create output directory

--- geotrellis-spark/src/main/scala/geotrellis/spark/cmd/Export.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/cmd/Export.scala
@@ -11,33 +11,32 @@ import geotrellis.spark.metadata.PyramidMetadata
 import geotrellis.spark.rdd.RasterHadoopRDD
 import geotrellis.spark.tiling.TmsTiling
 import geotrellis.spark.utils.SparkUtils
-
 import org.apache.hadoop.fs.Path
 import org.apache.spark.Logging
-
 import com.quantifind.sumac.ArgMain
+import java.io.File
 
 /**
  * @author akini
- * 
+ *
  * Export a raster as GeoTIFF. Each tile is saved as a separate TIFF file. Uses Spark to read the tiles
- * from the underlying raster  
+ * from the underlying raster
  *
  * Export --input <path-to-raster> --zoom <zoom> --output <path-to-dir> --sparkMaster <spark-master-ip>
- * 
+ *
  * e.g., Export --input file:///tmp/all-ones --zoom 10 --output /tmp/all-ones-ingested --sparkMaster "local"
- * 
+ *
  * Constraints:
- * 
- * --input <path-to-raster> - this can be either on hdfs (hdfs://) or local fs (file://) and is a fully 
- * qualified path to the raster pyramid 
- * 
+ *
+ * --input <path-to-raster> - this can be either on hdfs (hdfs://) or local fs (file://) and is a fully
+ * qualified path to the raster pyramid
+ *
  * --output <path-to-dir> - the output directory has to exist on the local file system. If the directory
  * doesn't exist, an error woud be thrown
- * 
- * --sparkMaster <spark-master-ip> - this is the conventional spark cluster url 
+ *
+ * --sparkMaster <spark-master-ip> - this is the conventional spark cluster url
  * 	(e.g. spark://host:port, local, local[4])
- *     
+ *
  */
 object Export extends ArgMain[CommandArguments] with Logging {
 
@@ -47,6 +46,11 @@ object Export extends ArgMain[CommandArguments] with Logging {
     val rasterPathWithZoom = new Path(rasterPath, zoom.toString)
     val outputDir = args.output
     val sparkMaster = args.sparkMaster
+
+    logInfo(s"Deleting and creating output directory: $outputDir")
+    val dir = new File(outputDir)
+    dir.delete()
+    dir.mkdirs()
 
     val sc = SparkUtils.createSparkContext(sparkMaster, "Export")
     val meta = PyramidMetadata(rasterPath, sc.hadoopConfiguration)
