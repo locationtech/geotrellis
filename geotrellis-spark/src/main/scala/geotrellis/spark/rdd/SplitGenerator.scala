0010bbe34cad7ee2855035a81b0130aa45aac936
Ameet Kini <ameetkini@gmail.com>
2014-01-24 22:10:56 -0500

    fix bug in computeIncrement and account for compression

--- geotrellis-spark/src/main/scala/geotrellis/spark/rdd/SplitGenerator.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/rdd/SplitGenerator.scala
@@ -17,13 +17,16 @@ case class RasterSplitGenerator(
   increment: Int = -1)
   extends SplitGenerator {
   // if increment is -1 getSplits return an empty sequence
-  def getSplits = for (i <- tileBounds.s until tileBounds.n by increment) yield TmsTiling.tileId(tileBounds.e, i, zoom)
+  // also, we start with s+(i-1) as the first split point needs to be there, not at s
+  def getSplits = for (i <- tileBounds.s+(increment-1) until tileBounds.n by increment) yield TmsTiling.tileId(tileBounds.e, i, zoom)
 }
 
 object RasterSplitGenerator {
   def apply(tileBounds: TileBounds, zoom: Int, tileSizeBytes: Int, blockSizeBytes: Long) = {
     new RasterSplitGenerator(tileBounds, zoom, computeIncrement(tileBounds, tileSizeBytes, blockSizeBytes))
   }
+  // the goal is to come up with an increment such that tiles are partitioned roughly by hdfs block size
+  // so if the block size is 64MB, we want each partition to have at most that much. 
   def computeIncrement(tileBounds: TileBounds, tileSizeBytes: Int, blockSizeBytes: Long) = {
     val tilesPerBlock = (blockSizeBytes / tileSizeBytes).toLong
     val tileCount = tileBounds.width * tileBounds.height
@@ -32,8 +35,8 @@ object RasterSplitGenerator {
     val increment =
       if (blockSizeBytes <= 0 || tilesPerBlock >= tileCount)
         -1
-      else
-        (tilesPerBlock / tileBounds.width).toInt
+      else // assume compression ratio of 20%. If it is more than that, well, we'll have empty space in that block
+        ((tilesPerBlock / tileBounds.width) * 1.2).toInt 
     increment
   }
 }
