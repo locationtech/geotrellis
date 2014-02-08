d4c457f6cd7328fc4d7d14bc4a089dd45e00e2cb
Mitch Tee <mitch.tee@gmail.com>
2014-02-08 11:34:11 -0800

    Add unit test for local Acos.

--- src/main/scala/geotrellis/raster/op/local/Acos.scala
+++ src/main/scala/geotrellis/raster/op/local/Acos.scala
@@ -6,8 +6,12 @@ import geotrellis._
  * Operation to get the arc cosine of values.
  */
 object Acos extends Serializable {
-  /** Takes the arc cosine of each raster cell value. */
-  def apply(r:Op[Raster]) = 
-    r.map(_.dualMap(z => z)(z => math.acos(z))) 
-     .withName("ArcCos")
+  /** 
+  * Takes the arc cosine of each raster cell value. 
+  * Always returns a double tiled raster.
+  * If the absolute value of the cell value is > 1, it will be NaN.
+  */
+  def apply(r:Op[Raster]) =
+    r.map(y => y.convert(TypeDouble).mapDouble(z => math.acos(z)))
+     .withName("Acos")
 }
