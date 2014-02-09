317b6edd08bcae4d9b04b931443ff22fc461972a
Mitch Tee <mitch.tee@gmail.com>
2014-02-09 10:43:18 -0800

    Add tests for local sin.

--- src/main/scala/geotrellis/raster/op/local/Sin.scala
+++ src/main/scala/geotrellis/raster/op/local/Sin.scala
@@ -6,8 +6,12 @@ import geotrellis._
  * Operation to get the sine of values.
  */
 object Sin extends Serializable {
-  /** Takes the sine of each raster cell value. */
+  /**
+    * Takes the sine of each raster cell value.
+    * Always returns a double raster.
+    */
   def apply(r:Op[Raster]) = 
-    r.map(_.dualMap(z => z)(z => math.sin(z))) 
+    r.map(_.convert(TypeDouble)
+           .mapDouble(z => math.sin(z)))
      .withName("Sin")
 }
