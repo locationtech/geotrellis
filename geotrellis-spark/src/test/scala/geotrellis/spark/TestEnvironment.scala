5eaa042dc3dae6cfb62f7ce7cddf997859e067d3
Ameet Kini <ameetkini@gmail.com>
2014-02-18 11:19:58 -0500

    Cleanup TestEnvironment

--- geotrellis-spark/src/test/scala/geotrellis/spark/TestEnvironment.scala
+++ geotrellis-spark/src/test/scala/geotrellis/spark/TestEnvironment.scala
@@ -11,9 +11,21 @@ import org.scalatest.BeforeAndAfterAll
 import java.io.File
 
 /*
- * This trait sets up the test directories on the local fs and hdfs 
+ * These set of traits handle the creation and deletion of test directories on the local fs and hdfs,
+ * and creation/freeing of SparkContext.  
  * 
  * It uses commons-io in at least one case (recursive directory deletion)
+ * 
+ * It is split into three traits   
+ * TestEnvironmentBase - which handles the creation/deletion of various test directories
+ * TestEnvironment - which inherits from FunSpec 
+ * TestEnvironmentFixture - which inherits from fixture.FunSpec, and provides withFixture implementation 
+ * to handle creation/freeing of SparkContext for tests that use Spark. 
+ * 
+ * The reason it is split into the three traits is because there wasn't a clear way to get 
+ * a single trait to inherit from either/both FunSpec and fixture.FunSpec. This was the best 
+ * way to reduce as much as possible code duplication. The only duplication is the definition 
+ * of beforeAll/afterAll
  */
 
 trait TestEnvironmentBase {
@@ -23,20 +35,26 @@ trait TestEnvironmentBase {
   // a hadoop configuration
   val conf = SparkUtils.createHadoopConfiguration
 
+  // cache the local file system, no tests should have to call getFileSystem
   val localFS = getLocalFS
 
   // e.g., root directory on local file system for source data (e.g., tiffs)
   // localFS.getWorkingDirectory is for e.g., /home/jdoe/git/geotrellis
-  final val inputHome = new Path(localFS.getWorkingDirectory, "geotrellis-spark/src/test/resources")
-
-  // root directory name on both local file system and hdfs for all tests
-  private final val outputHome = "testFiles"
+  val inputHome = new Path(localFS.getWorkingDirectory, "geotrellis-spark/src/test/resources")
 
   // test directory paths on local and hdfs 
   // outputHomeLocal - root directory of all tests on the local file system (e.g., file:///tmp/testFiles)
   // outputHomeHdfs - root directory of all tests on hdfs (e.g., hdfs:///tmp)
   // outputLocal - directory of this particular test (e.g., file:///tmp/testFiles/geotrellis.spark.cmd.IngestSpec)
   val (outputHomeLocal, outputHomeHdfs, outputLocal) = setupTestDirs
+
+  // clean up the test directory after the test
+  // note that this afterAll is not inherited from BeforeAndAfterAll, its callers are
+  protected def afterAll() = FileUtils.deleteDirectory(new File(outputLocal.toUri()))
+
+  // root directory name on both local file system and hdfs for all tests
+  private final val outputHome = "testFiles"
+
   private def getLocalFS: FileSystem = new Path(System.getProperty("java.io.tmpdir")).getFileSystem(conf)
 
   private def setupTestDirs: (Path, Path, Path) = {
@@ -58,11 +76,10 @@ trait TestEnvironmentBase {
 }
 
 trait TestEnvironment extends TestEnvironmentBase with FunSpec with BeforeAndAfterAll {
-  //override def beforeAll {
-  //}
 
-  override def afterAll =
-    FileUtils.deleteDirectory(new File(outputLocal.toUri()))
+  // clean up the test directory after the test
+  override def afterAll() = super[TestEnvironmentBase].afterAll()
+
 }
 
 trait TestEnvironmentFixture extends TestEnvironmentBase with fixture.FunSpec with BeforeAndAfterAll {
@@ -71,6 +88,7 @@ trait TestEnvironmentFixture extends TestEnvironmentBase with fixture.FunSpec wi
 
   def withFixture(test: OneArgTest) {
     val sc = SparkUtils.createSparkContext("local", "some name")
+
     try {
       test(sc)
     } finally {
@@ -80,9 +98,6 @@ trait TestEnvironmentFixture extends TestEnvironmentBase with fixture.FunSpec wi
     }
   }
 
-  //override def beforeAll {
-  //}
-
-  override def afterAll =
-    FileUtils.deleteDirectory(new File(outputLocal.toUri()))
+  // clean up the test directory after the test
+  override def afterAll() = super[TestEnvironmentBase].afterAll()
 }
