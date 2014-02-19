716bd5c547e3135158026bccacd28328de89828a
Ameet Kini <ameetkini@gmail.com>
2014-02-19 17:43:12 -0500

    Added a more functional RasterReader - Supports range and points lookups - this is called RasterReader, the older one moved to SimpleRasterReader - Added a test

--- geotrellis-spark/src/test/scala/geotrellis/spark/storage/RasterReaderSpec.scala
+++ geotrellis-spark/src/test/scala/geotrellis/spark/storage/RasterReaderSpec.scala
@@ -1,11 +1,36 @@
 package geotrellis.spark.storage
 import geotrellis.spark.TestEnvironment
-
 import geotrellis.spark.utils.SparkUtils
+import org.scalatest.matchers.ShouldMatchers
+import geotrellis.spark.formats.TileIdWritable
+import org.apache.hadoop.fs.Path
 
-class RasterReaderSpec extends TestEnvironment {
+class RasterReaderSpec extends TestEnvironment with ShouldMatchers {
+
+  val allOnes = new Path(inputHome, "all-ones/10")
+
+  private def read(start: Long, end: Long): Int = {
+    val reader = RasterReader(allOnes, conf, TileIdWritable(start), TileIdWritable(end))
+    val numEntries = reader.count(_ => true)
+    reader.close()
+    numEntries
+  }
 
   describe("RasterReader") {
-	// nothing here for now, IngestSpec covers a basic test  
+    it("should retrieve all entries") {
+      read(208787, 211861) should be(12)
+    }
+    it("should retrieve all entries when no range is specified") {
+      read(Long.MinValue, Long.MaxValue) should be(12)
+    }
+    it("should handle a non-existent start and end") {
+      read(0, 209810) should be(3)
+    }
+    it("should be able to skip a partition") {
+      read(210838, Long.MaxValue) should be(3)
+    }
+    it("should be handle start=end") {
+      read(209811,209811) should be(1)
+    }
   }
 }
