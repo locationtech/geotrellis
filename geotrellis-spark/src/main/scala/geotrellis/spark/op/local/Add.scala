ca3df40c2ce279d7be33688a5563f896cedd340b
Ameet Kini <ameetkini@gmail.com>
2014-02-20 17:32:44 -0500

    Add to Add the ability to add two rasters :)

--- geotrellis-spark/src/main/scala/geotrellis/spark/op/local/Add.scala
+++ geotrellis-spark/src/main/scala/geotrellis/spark/op/local/Add.scala
@@ -13,26 +13,34 @@ import org.apache.spark.Logging
 import geotrellis.spark.rdd.RasterRDD
 trait LocalBinaryOpMethods[+Repr <: RasterRDD] extends Logging { self: Repr =>
 
-  def mapOp(d: Double, f: (Raster, Double) => Raster) =
-    self.mapPartitions(_.map(tr => (tr._1, f(tr._2, d))), true)
+  // map over a rdd with a function f that takes a raster and something and produces a raster
+  def mapOp[T](d: T)(f: ((Long, Raster), T) => (Long, Raster)) =
+    self.mapPartitions(_.map(f(_, d)), true)
+
+  // function takes two rasters and produces a third raster  
+  def combineOp(other: RasterRDD)(f: (((Long, Raster), (Long, Raster))) => (Long, Raster)) =
+    self.zipPartitions(other, true)(_.zip(_).map(f))
 
 }
 
 trait AddOpMethods[+Repr <: RasterRDD] extends LocalBinaryOpMethods[Repr] { self: Repr =>
   /** Add a constant Int value to each cell. */
-  //def localAdd(i: Int) = self.mapOp(Add(_, i))
+  def localAdd(i: Int) = 
+    self.mapOp[Int](i)({ case ((t, r), i) => (t, Add(r, i)) }).withMetadata(self.meta)
   /** Add a constant Int value to each cell. */
-  //def +(i:Int) = localAdd(i)
+  def +(i: Int) = localAdd(i)
   /** Add a constant Int value to each cell. */
-  //def +:(i:Int) = localAdd(i)
+  def +:(i: Int) = localAdd(i)
   /** Add a constant Double value to each cell. */
-  def localAdd(d: Double) = self.mapOp(d, (r, d) => Add(r, d)).withMetadata(self.meta)
+  def localAdd(d: Double) = 
+    self.mapOp[Double](d)({ case ((t, r), d) => (t, Add(r, d)) }).withMetadata(self.meta)
   /** Add a constant Double value to each cell. */
   def +(d: Double) = localAdd(d)
   /** Add a constant Double value to each cell. */
   def +:(d: Double) = localAdd(d)
   /** Add the values of each cell in each raster.  */
-  //def localAdd(rs:RasterSource) = self.combineOp(rs)(Add(_,_))
+  def localAdd(rdd: RasterRDD) =
+    self.combineOp(rdd)({ case ((t1, r1), (t2, r2)) => (t1, Add(r1, r2)) }).withMetadata(self.meta)
   /** Add the values of each cell in each raster. */
-  //def +(rs:RasterHadoopRDD) = localAdd(rs)
+  def +(rdd: RasterRDD) = localAdd(rdd)
 }
--- geotrellis-spark/src/test/scala/geotrellis/spark/op/local/AddSpec.scala
+++ geotrellis-spark/src/test/scala/geotrellis/spark/op/local/AddSpec.scala
@@ -7,25 +7,40 @@ import org.apache.spark.SparkContext
 import geotrellis.spark.utils.SparkUtils
 import org.scalatest.fixture.FunSpec
 import geotrellis.spark.TestEnvironmentFixture
+import org.scalatest.Ignore
 
 class AddSpec extends TestEnvironmentFixture with ShouldMatchers {
 
   describe("Add Operation") {
+    val onesPath = new Path(inputHome, "all-ones/10")
+
     it("should add a constant to a raster") { sc =>
 
-      val allOnes = new Path(inputHome, "all-ones/10")
-      val allOnesRDD = RasterHadoopRDD.toRasterRDD(allOnes, sc)
+      val ones = RasterHadoopRDD.toRasterRDD(onesPath, sc)
 
       val d = 1
-      val allTwosRDD = allOnesRDD + d
+      val twos = ones + d
 
       // TODO - test doesn't have to save all twos
       /*val allTwos = new Path(outputLocal, "10")
       println(s"allTwos=$allTwos, and outputLocal=${outputLocal.toUri().toString()}")
       allTwosRDD.save(allTwos)*/
 
-      allTwosRDD.map { case (tileId, raster) => raster.findMinMaxDouble }.collect.foreach(_ should be(2, 2))
+      val res = twos.map { case (tileId, raster) => raster.findMinMaxDouble }.collect
+      res.foreach(_ should be(2, 2))
+      res.length should be(ones.count)
 
+    }
+
+    it("should add multiple rasters") { sc =>
+
+      val left = RasterHadoopRDD.toRasterRDD(onesPath, sc)
+
+      val threes = left + left + left 
+
+      val res = threes.map { case (tileId, raster) => raster.findMinMaxDouble }.collect
+      res.foreach(_ should be(3, 3))
+      res.length should be(left.count)
     }
   }
 }
