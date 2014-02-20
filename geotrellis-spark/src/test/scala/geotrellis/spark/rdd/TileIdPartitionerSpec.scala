47395f3f845b2dba2d5d4733d11de526807a339b
Ameet Kini <ameetkini@gmail.com>
2014-02-20 17:31:11 -0500

    Add range() method to TileIdPartitioner

--- geotrellis-spark/src/test/scala/geotrellis/spark/rdd/TileIdPartitionerSpec.scala
+++ geotrellis-spark/src/test/scala/geotrellis/spark/rdd/TileIdPartitionerSpec.scala
@@ -1,6 +1,7 @@
 package geotrellis.spark.rdd
 import geotrellis.spark.TestEnvironment
 import geotrellis.spark.formats.TileIdWritable
+
 import org.scalatest.matchers.ShouldMatchers
 
 class TileIdPartitionerSpec extends TestEnvironment with ShouldMatchers {
@@ -55,6 +56,23 @@ class TileIdPartitionerSpec extends TestEnvironment with ShouldMatchers {
     it("should assign all tiles to partition 0") {
       partitioner.getPartition(TileIdWritable(0)) should be(0)
       partitioner.getPartition(TileIdWritable(Long.MaxValue)) should be(0)
+    }
+  }
+
+  /* range tests - if we have three partitions (0, 1, 2) corresponding to the two split points 
+   * 10 and 20, then these are the valid ranges for those partitions
+   */
+  describe("range") {
+    val partitioner = getPartitioner(Seq(10, 20))
+
+    it("should handle range of partition 0") {
+      partitioner.range(0) should be(TileIdWritable(Long.MinValue), TileIdWritable(10))
+    }
+    it("should handle range of partition 1") {
+      partitioner.range(1) should be(TileIdWritable(11), TileIdWritable(20))
+    }
+    it("should handle range of partition 2") {
+      partitioner.range(2) should be(TileIdWritable(21), TileIdWritable(Long.MaxValue))
     }
   }
 }
