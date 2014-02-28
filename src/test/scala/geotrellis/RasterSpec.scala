locationtech/geotrellis

commit 672aec002f5d2e5b53f78e0a2e836e1da8996ec6

Focal Operations
- Created raster.op.global for global operations that need the whole
(untiled) raster in order to function. These operations cannot be
distributed against tiles. That is not to say they will never be
able to; once the execution parameters change (e.g. iterative
algorithms over tiles becomes a possibility).

src/main/scala/geotrellis/Raster.scala
@@ -20,28 +20,64 @@ object Raster {
 /**
  * Base trait for the Raster data type.
  */
-trait Raster extends RasterLike {
+trait Raster {
+  val rasterExtent:RasterExtent
+  lazy val cols = rasterExtent.cols
+  lazy val rows = rasterExtent.rows
+
   /** This function will force any deferred operations to happen
    *  at the time it is called. This is necessary so that lazy maps
    *  don't get passed between machines.
    */
-  def force():Raster
+//  def force():Raster
 
-  def toArray:Array[Int]
-  def toArrayDouble:Array[Double]
+  val rasterType:RasterType
+  def isFloat:Boolean = rasterType.float
+
+
+
+  /**
+   * Get value at given coordinates.
+   */
+  def get(col:Int, row:Int):Int
+
+  /**
+   * Get value at given coordinates.
+   */
+  def getDouble(col:Int, row:Int):Double
+
+  def toArrayRaster():ArrayRaster
+  def toArray():Array[Int]
+  def toArrayDouble():Array[Double]
 
   /**
    * Clone this raster.
    */
-  def copy():Raster
+//  def copy():Raster
   def convert(typ:RasterType):Raster
+
+  def dualForeach(f:Int => Unit)(g:Double => Unit):Unit =
+    if (isFloat) foreachDouble(g) else foreach(f)
+
+  def foreach(f:Int=>Unit):Unit =
+    for(col <- 0 until cols optimized) {
+      for(row <- 0 until rows optimized) {
+        f(get(col,row))
+      }
+    }
+
+  def foreachDouble(f:Double=>Unit):Unit =
+    for(col <- 0 until cols optimized) {
+      for(row <- 0 until rows optimized) {
+        f(getDouble(col,row))
+      }
+    }
 
   def map(f:Int => Int):Raster
   def combine(r2:Raster)(f:(Int, Int) => Int):Raster
 
   def mapDouble(f:Double => Double):Raster
   def combineDouble(r2:Raster)(f:(Double, Double) => Double):Raster
-
 
   def combine(rs:Seq[Raster])(f:Seq[Int] => Int):Raster = {
     if(Set(rs.map(_.rasterExtent)).size != 1) {
@@ -176,9 +212,106 @@ trait Raster extends RasterLike {
    */
   def compare(other:Raster) = this.rasterExtent.compare(other.rasterExtent)
 
-  def normalize(zmin:Int, zmax:Int, gmin:Int, gmax:Int): Raster = {
-    val dg = gmax - gmin
-    val dz = zmax - zmin
-    if (dz > 0) mapIfSet(z => ((z - zmin) * dg) / dz + gmin) else copy()
+  /**
+   * Normalizes the values of this raster, given the current min and max, to a new min and max.
+   * 
+   *   @param oldMin    Old mininum value
+   *   @param oldMax    Old maximum value
+   *   @param newMin     New minimum value
+   *   @param newMax     New maximum value
+   */
+  def normalize(oldMin:Int, oldMax:Int, newMin:Int, newMax:Int): Raster = {
+    val dnew = newMax - newMin
+    val dold = oldMax - oldMin
+    if(dold <= 0 || dnew <= 0) { sys.error(s"Invalid parameters: $oldMin,$oldMax,$newMin,$newMax") }
+    mapIfSet(z => ( ((z - oldMin) / dold) * dnew ) + newMin)
   }
+
+  /**
+   * Return tuple of highest and lowest value in raster.
+   *
+   * @note   Currently does not support double valued raster data types
+   *         (TypeFloat,TypeDouble). Calling findMinMax on rasters of those
+   *         types will give the integer min and max of the rounded values of
+   *         their cells.
+   */
+  def findMinMax = {
+    var zmin = Int.MaxValue
+    var zmax = Int.MinValue
+
+    foreach { 
+      z => if (z != NODATA) {
+        zmin = math.min(zmin, z)
+        zmax = math.max(zmax, z)
+      }
+    }
+
+    if(zmin == Int.MaxValue) { zmin = NODATA }
+    (zmin, zmax)
+  } 
+
+  /**
+   * Return tuple of highest and lowest value in raster.
+   */
+  def findMinMaxDouble = {
+    var zmin = Double.NaN
+    var zmax = Double.NaN
+
+    foreachDouble {
+      z => if (!java.lang.Double.isNaN(z)) {
+        if(java.lang.Double.isNaN(zmin)) {
+          zmin = z
+          zmax = z
+        } else {
+          zmin = math.min(zmin, z)
+          zmax = math.max(zmax, z)
+        }
+      }
+    }
+
+    (zmin, zmax)
+  }
+
+
+  /**
+   * Return ascii art of this raster.
+   */
+  def asciiDraw():String = { 
+    val sb = new StringBuilder
+    for(row <- 0 until rows) {
+      for(col <- 0 until cols) {
+        val v = get(col,row)
+        val s = if(v == NODATA) {
+          "ND"
+        } else {
+          s"$v"
+        }
+        val pad = " " * math.max(6 - s.length,0) 
+        sb.append(s"$pad$s")
+      }
+      sb += '\n'
+    }      
+    sb += '\n'
+    sb.toString
+  }
+
+  /**
+   * Return ascii art of a range from this raster.
+   */
+  def asciiDrawRange(colMin:Int, colMax:Int, rowMin:Int, rowMax:Int) = {
+    var s = "";
+    for (row <- rowMin to rowMax) {
+      for (col <- colMin to colMax) {
+        val z = this.get(row, col)
+        if (z == NODATA) {
+          s += ".."
+        } else {
+          s += "%02X".format(z)
+        }
+      }
+      s += "\n"
+    }
+    s
+  }
+
 }

commit 3bdbd6692e396daed8e26c8e60743d8db94fc755

Fix normalization bug.
There is a bug in normalization that was introduced with 672aec002f5d2e5b53f78e0a2e836e1da8996ec6.

Fixes #747

src/main/scala/geotrellis/Raster.scala
@@ -112,7 +112,7 @@ trait Raster extends local.AddMethods
     val dnew = newMax - newMin
     val dold = oldMax - oldMin
     if(dold <= 0 || dnew <= 0) { sys.error(s"Invalid parameters: $oldMin,$oldMax,$newMin,$newMax") }
-    mapIfSet(z => ( ((z - oldMin) / dold) * dnew ) + newMin)
+    mapIfSet(z => ( ((z - oldMin) * dnew) / dold ) + newMin)
   }
 
   def warp(target:RasterExtent):Raster

src/test/scala/geotrellis/RasterSpec.scala
@@ -1,9 +1,10 @@
 package geotrellis
 
 import org.scalatest.FunSpec
-import org.scalatest.matchers.MustMatchers
+import org.scalatest.matchers._
 
-class RasterSpec extends FunSpec with MustMatchers {
+class RasterSpec extends FunSpec with MustMatchers 
+                                 with ShouldMatchers {
   val e = Extent(0.0, 0.0, 10.0, 10.0)
   val g = RasterExtent(e, 1.0, 1.0, 10, 10)
   describe("A Raster") {
@@ -40,6 +41,18 @@ class RasterSpec extends FunSpec with MustMatchers {
       r1 must not be r2
       r1 must be === r3
       r1 must be === r4
+    }
+
+    it("should normalize from 500-999 to 1-100") {
+      val arr = (for(i <- 500 to 999) yield { i }).toArray
+      val r = Raster(arr, RasterExtent(Extent(0.0,0.0,50.0,10.0), 1, 1, 50, 10))
+      val (oldMin, oldMax) = r.findMinMax
+      val nr = r.normalize(oldMin, oldMax, 1, 100)
+      val (newMin, newMax) = nr.findMinMax
+
+      newMin should be (1)
+      newMax should be (100)
+      nr.toArray.toSet should be ((for(i <- 1 to 100) yield { i }).toSet)
     }
   }
 