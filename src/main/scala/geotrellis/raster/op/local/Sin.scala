5c1655c460b746810c125815aaa0328ffb0bfc4c
Mitch Tee <mitch.tee@gmail.com>
2014-03-10 03:59:55 -0700

    Add localSin.

--- src/main/scala/geotrellis/raster/op/local/Sin.scala
+++ src/main/scala/geotrellis/raster/op/local/Sin.scala
@@ -8,10 +8,22 @@ import geotrellis._
 object Sin extends Serializable {
   /**
     * Takes the sine of each raster cell value.
-    * Always returns a double raster.
+    * @info Always returns a double raster.
     */
   def apply(r:Op[Raster]) = 
     r.map(_.convert(TypeDouble)
            .mapDouble(z => math.sin(z)))
      .withName("Sin")
+}
+
+/**
+ * Operation to get the sine of values.
+ */
+trait SinMethods { self: Raster =>
+  /**
+    * Takes the sine of each raster cell value.
+    * @info Always returns a double raster.
+    */
+  def localSin() = 
+    Sin(self)
 }
