6e0ac5554bd826b5dd7d236c639c0148cf212891
Eugene Cheipesh <echeipesh@gmail.com>
2014-03-11 17:13:50 -0400

    more stars in copyright comments

--- scripts/insert-copyright-header.py
+++ scripts/insert-copyright-header.py
@@ -30,7 +30,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-max_width = max(map(lambda x: len(x), license))
+max_width = max(map(lambda x: len(x), license.split('\n')))
 
 def c_style_comment(text):
   commented = [ ]
--- spark/src/main/scala/geotrellis/spark/utils/HdfsUtils.scala
+++ spark/src/main/scala/geotrellis/spark/utils/HdfsUtils.scala
@@ -1,4 +1,4 @@
-/***
+/**************************************************************************
  * Copyright (c) 2014 Digital Globe.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ***/
+ **************************************************************************/
 
 package geotrellis.spark.utils
 import org.apache.hadoop.conf.Configuration
