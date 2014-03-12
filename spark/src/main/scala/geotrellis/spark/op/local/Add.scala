3121925118050735606dacc303faf971b5ed35d1
Ameet Kini <ameetkini@gmail.com>
2014-03-12 11:12:53 -0400

    replace 'Digital Globe' with 'DigitalGlobe'

--- spark/src/main/scala/geotrellis/spark/op/local/Add.scala
+++ spark/src/main/scala/geotrellis/spark/op/local/Add.scala
@@ -1,5 +1,5 @@
 /**************************************************************************
- * Copyright (c) 2014 Digital Globe.
+ * Copyright (c) 2014 DigitalGlobe.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
