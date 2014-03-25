ccadc15aba242f353fe29825c4db97013aa160cf
Eugene Cheipesh <echeipesh@gmail.com>
2014-03-25 19:02:05 -0400

    CRS insertion

--- feature/src/main/scala/geotrellis/feature/json/GeoJsonSupport.scala
+++ feature/src/main/scala/geotrellis/feature/json/GeoJsonSupport.scala
@@ -15,28 +15,24 @@ import spray.httpx.unmarshalling.Unmarshaller
  * You'll need to provide the appropriate *spray-json* artifacts yourself.
  */
 trait GeoJsonSupport extends FeatureFormats {
-  implicit def sprayJsonUnmarshallerConverter[T](reader: RootJsonReader[T]) =
-    sprayJsonUnmarshaller(reader)
-  implicit def sprayJsonUnmarshaller[T: RootJsonReader] =
+  implicit def sprayJsonUnmarshaller[T :RootJsonReader] =
     Unmarshaller[T](MediaTypes.`application/json`) {
       case x: HttpEntity.NonEmpty ⇒
         val json = JsonParser(x.asString(defaultCharset = HttpCharsets.`UTF-8`))
         jsonReader[T].read(json)
     }
-  implicit def sprayJsonMarshallerConverter[T](writer: RootJsonWriter[T])(implicit printer: JsonPrinter = PrettyPrinter) =
-    sprayJsonMarshaller[T](writer, printer)
-  implicit def sprayJsonMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter, csr: CSR = NoCSR()) =
-    Marshaller.delegate[T, String](ContentTypes.`application/json`) { value =>
+
+  implicit def sprayJsonMarshaller[T](implicit
+    writer: RootJsonWriter[T],
+    printer: JsonPrinter = PrettyPrinter,
+    crs: CRS = GeoJsonSupport.defaultCrs
+  ) = Marshaller.delegate[T, String](ContentTypes.`application/json`) { value =>
       val json = writer.write(value)
-//      val csrJson = json match {
-//        case JsObject(fields) =>
-//          JsObject(fields + ("CSR" -> JsString("MY CSR WILL GO WITH ME")))
-//      }
-      printer(json)
-    }
+      printer(crs.addTo(json))
+  }
 }
 
-object GeoJsonSupport extends GeoJsonSupport {
-  implicit val defaultCSR = NoCSR
+object GeoJsonSupport extends GeoJsonSupport{
+  implicit val defaultCrs = BlankCRS
 }
 
