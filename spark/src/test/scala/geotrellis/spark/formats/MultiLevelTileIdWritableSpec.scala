33df8dd5227a7292aa1fda6db6667fe21cb41833
Ameet Kini <ameetkini@gmail.com>
2014-05-06 14:50:46 -0400

    Refactor SerializerTester into its own file

--- spark/src/test/scala/geotrellis/spark/formats/MultiLevelTileIdWritableSpec.scala
+++ spark/src/test/scala/geotrellis/spark/formats/MultiLevelTileIdWritableSpec.scala
@@ -1,15 +1,6 @@
 package geotrellis.spark.formats
-import geotrellis.spark.TestEnvironment
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
-import java.io.DataOutputStream
-import java.io.DataInputStream
-import org.apache.hadoop.io.Writable
-import scala.reflect.ClassTag
 
 class MultiLevelTileIdWritableSpec extends FunSpec with ShouldMatchers with SerializerTester {
   describe("java serialization") {
