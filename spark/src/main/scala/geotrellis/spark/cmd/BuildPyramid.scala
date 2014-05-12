e5cd75ec0a31073fa4668c17a6ed06583e9bce4d
Ameet Kini <ameetkini@gmail.com>
2014-05-12 16:23:58 -0400

    BuildPyramid: cleanup

--- spark/src/main/scala/geotrellis/spark/cmd/BuildPyramid.scala
+++ spark/src/main/scala/geotrellis/spark/cmd/BuildPyramid.scala
@@ -15,47 +15,96 @@
  */
 package geotrellis.spark.cmd
 import geotrellis._
+import geotrellis.raster.RasterData
 import geotrellis.spark.Tile
 import geotrellis.spark.cmd.args.HadoopArgs
 import geotrellis.spark.cmd.args.SparkArgs
 import geotrellis.spark.formats.ArgWritable
+import geotrellis.spark.formats.PayloadArgWritable
 import geotrellis.spark.formats.TileIdWritable
-import geotrellis.spark.ingest.IngestInputFormat
-import geotrellis.spark.ingest.TiffTiler
+import geotrellis.spark.formats.TileIdZoomWritable
 import geotrellis.spark.metadata.PyramidMetadata
+import geotrellis.spark.metadata.RasterMetadata
+import geotrellis.spark.rdd.MultiLevelTileIdPartitioner
+import geotrellis.spark.rdd.RasterRDD
 import geotrellis.spark.rdd.RasterSplitGenerator
-import geotrellis.spark.rdd.TileIdPartitioner
+import geotrellis.spark.rdd.SplitGenerator
 import geotrellis.spark.tiling.TmsTiling
 import geotrellis.spark.utils.HdfsUtils
-import geotrellis.spark.utils.SparkUtils
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.FileSystem
+
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.MapFile
 import org.apache.hadoop.io.SequenceFile
 import org.apache.spark.Logging
 import org.apache.spark.SerializableWritable
 import org.apache.spark.SparkContext._
-import org.geotools.coverage.grid.GridCoverage2D
+
 import com.quantifind.sumac.ArgMain
-import com.quantifind.sumac.FieldArgs
 import com.quantifind.sumac.validation.Required
-import geotrellis.spark.rdd.RasterRDD
-import geotrellis.spark.rdd.SplitGenerator
-import geotrellis.spark.metadata.RasterMetadata
-import geotrellis.spark.rdd.MultiLevelTileIdPartitioner
-import geotrellis.spark.formats.TileIdZoomWritable
-import geotrellis.spark.formats.PayloadArgWritable
-import geotrellis.raster.RasterData
-import geotrellis.data.GeoTiffWriter
 
 class BuildPyramidArgs extends SparkArgs with HadoopArgs {
   @Required var pyramid: String = _
 }
 
 object BuildPyramid extends ArgMain[BuildPyramidArgs] with Logging {
+  def main(args: BuildPyramidArgs) {
+    val sc = args.sparkContext("BuildPyramid")
+    val pyramid = new Path(args.pyramid)
+    val conf = args.hadoopConf
 
-  private def fillRasterMetadata(meta: PyramidMetadata): PyramidMetadata = {
+    // fill metadata with info on all higher levels
+    val meta = fillMetadata(PyramidMetadata(pyramid, conf))
+
+    // make splits for each higher level
+    // TODO - revisit the "saving" of splits 
+    // 1. Splits shouldn't be saved before the job is complete - needs refactoring of TileIdPartitioner's apply that takes gen
+    // 2. The base zoom level shouldn't be rewritten. This will be made easier once #1 is done. 
+    // Addendum for #2: base zoom is no longer part of mp, so #2 can be ignored
+    val splits = getSplits(meta, HdfsUtils.defaultBlockSize(pyramid, conf))
+    val mp = MultiLevelTileIdPartitioner(splits, pyramid, conf)
+    println(mp)
+
+    val rasterPath = new Path(pyramid, meta.maxZoomLevel.toString)
+    val rdd = RasterRDD(rasterPath, sc)
+    
+    // broadcast configuration and metadata
+    val broadCastedConf = sc.broadcast(new SerializableWritable(conf))
+    val broadCastedMeta = sc.broadcast(meta)
+    val pyramidStr = pyramid.toUri().toString()
+    
+    val res = rdd
+      .mapPartitions({ partition =>
+        val meta = broadCastedMeta.value
+        partition.map(MultiLevelWarper.warp(_, meta)).flatten
+      }, true)
+      .groupByKey(mp)
+      .mapPartitions({ partition =>
+        val meta = broadCastedMeta.value
+        val conf = broadCastedConf.value.value
+        val buf = partition.toArray.sortWith((x, y) => x._1.get() < y._1.get())
+        val (zoom, index) = (buf.head._1.zoom, mp.getPartitionForZoom(buf.head._1))
+        val outPath = new Path(pyramidStr, zoom.toString)
+        val mapFilePath = new Path(outPath, f"part-${index}%05d")
+        val fs = mapFilePath.getFileSystem(conf)
+        val fsRep = fs.getDefaultReplication()
+        conf.set("io.map.index.interval", "1")
+        logInfo(s"Working on partition ${index} with rep = (${conf.getInt("dfs.replication", -1)}, ${fsRep})")
+        val writer = new MapFile.Writer(conf, fs, mapFilePath.toUri.toString,
+          classOf[TileIdWritable], classOf[ArgWritable], SequenceFile.CompressionType.RECORD)
+
+        buf.foreach { tiles =>
+          val (tw, aw) = Stitcher.stitch(tiles, meta)
+          writer.append(tw, aw)
+        }
+        writer.close()
+        partition
+      }, true)
+
+    meta.save(pyramid, conf)
+    println(s"Result has ${res.count} tuples")
+  }
+
+  private def fillMetadata(meta: PyramidMetadata): PyramidMetadata = {
     val newRasterMetadata = meta.rasterMetadata ++
       ((1 until meta.maxZoomLevel) map { zoom =>
         val tileExtent = TmsTiling.extentToTile(meta.extent, zoom, meta.tileSize)
@@ -78,9 +127,14 @@ object BuildPyramid extends ArgMain[BuildPyramidArgs] with Logging {
     math.max(1, (tileSize / math.pow(2, maxZoom - zoom)).toInt)
 
   // this object encapsulates the actual warping work of warping a single tile into multiple tiles, one for each zoom levels 
-  object MultiLevelWarper {
+  private object MultiLevelWarper {
     // warp the input tile to each of the higher pyramid levels
     def warp(tile: Tile, meta: PyramidMetadata): Seq[(TileIdZoomWritable, PayloadArgWritable)] = {
+      // Nomenclature -           
+      // par or parent is the input tile, typically from base zoom, 
+      // ch or child is the tile being warped into, which is one for each zoom level, and differing sizes 
+      // prev is the tile from the "previous" zoom level. Since each input tile is warped into one tile  
+      // at each higher zoom level, "previous" is always a unique tile
       val (maxZoom, tileSize) = (meta.maxZoomLevel, meta.tileSize)
 
       val parTzw = TileIdZoomWritable(tile.id, maxZoom)
@@ -89,6 +143,7 @@ object BuildPyramid extends ArgMain[BuildPyramidArgs] with Logging {
       val parExtent2 = TmsTiling.tileToExtent(tile.id, maxZoom, tileSize)
       assert(parExtent.extent == parExtent2)
 
+      // initialize the map with info on the input tile, we'll remove it later before returning from warp 
       val initCond = collection.mutable.Map[Int, (TileIdZoomWritable, RasterData, RasterExtent)](maxZoom -> (parTzw, parRd, parExtent))
       val tileMap = (meta.maxZoomLevel - 1 to 1 by -1).foldLeft(initCond) { (m, zoom) =>
         val chTs = tileSizeForZoomLevel(tileSize, maxZoom, zoom)
@@ -108,7 +163,7 @@ object BuildPyramid extends ArgMain[BuildPyramidArgs] with Logging {
   }
 
   // this object encapsulates the stitching of multiple tiles from one zoom level onto a single tile of a higher zoom level 
-  object Stitcher {
+  private object Stitcher {
     def stitch(tiles: (TileIdZoomWritable, Seq[PayloadArgWritable]), meta: PyramidMetadata): (TileIdWritable, ArgWritable) = {
       // Nomenclature -           
       // ch or child is the tile being written to, so 512x512, 
@@ -149,57 +204,5 @@ object BuildPyramid extends ArgMain[BuildPyramidArgs] with Logging {
       }
       (TileIdWritable(chTileId), ArgWritable.fromRasterData(chRd))
     }
-  }
-  def main(args: BuildPyramidArgs) {
-    val sc = args.sparkContext("BuildPyramid")
-    val pyramid = new Path(args.pyramid)
-    val conf = args.hadoopConf
-    val meta = PyramidMetadata(pyramid, conf)
-    val newMeta = fillRasterMetadata(meta)
-
-    val splits = getSplits(newMeta, HdfsUtils.defaultBlockSize(pyramid, conf))
-
-    // TODO - revisit the "saving" of splits 
-    // 1. Splits shouldn't be saved before the job is complete - needs refactoring of TileIdPartitioner's apply that takes gen
-    // 2. The base zoom level shouldn't be rewritten. This will be made easier once #1 is done. 
-    // Addendum for #2: base zoom is no longer part of mp 
-    val mp = MultiLevelTileIdPartitioner(splits, pyramid, conf)
-    println(mp)
-
-    val rasterPath = new Path(pyramid, meta.maxZoomLevel.toString)
-    val rdd = RasterRDD(rasterPath, sc)
-    val broadCastedConf = sc.broadcast(new SerializableWritable(conf))
-    val broadCastedMeta = sc.broadcast(newMeta)
-    val pyramidStr = pyramid.toUri().toString()
-    val res = rdd
-      .mapPartitions({ partition =>
-        val meta = broadCastedMeta.value
-        partition.map(MultiLevelWarper.warp(_, meta)).flatten
-      }, true)
-      .groupByKey(mp)
-      .mapPartitions({ partition =>
-        val meta = broadCastedMeta.value
-        val conf = broadCastedConf.value.value
-        val buf = partition.toArray.sortWith((x, y) => x._1.get() < y._1.get())
-        val (zoom, index) = (buf.head._1.zoom, mp.getPartitionForZoom(buf.head._1))
-        val outPath = new Path(pyramidStr, zoom.toString)
-        val mapFilePath = new Path(outPath, f"part-${index}%05d")
-        val fs = mapFilePath.getFileSystem(conf)
-        val fsRep = fs.getDefaultReplication()
-        conf.set("io.map.index.interval", "1")
-        logInfo(s"Working on partition ${index} with rep = (${conf.getInt("dfs.replication", -1)}, ${fsRep})")
-        val writer = new MapFile.Writer(conf, fs, mapFilePath.toUri.toString,
-          classOf[TileIdWritable], classOf[ArgWritable], SequenceFile.CompressionType.RECORD)
-
-        buf.foreach { tiles =>
-          val (tw, aw) = Stitcher.stitch(tiles, meta)
-          writer.append(tw, aw)
-        }
-        writer.close()
-        partition
-      }, true)
-
-    newMeta.save(pyramid, conf)
-    println(s"Result has ${res.count} tuples and partitioner = ${res.partitioner}")
   }
 }
