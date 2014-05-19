22e15028790d99d02159b7764ec685ebdb302bd7
Jonathan Chien <jchien10@gmail.com>
2014-05-19 14:12:22 -0400

    Switch x/y and i/j to follow col/row convention for ApproxViewshed

--- core-test/src/test/scala/geotrellis/raster/op/global/ApproxViewshedSpec.scala
+++ core-test/src/test/scala/geotrellis/raster/op/global/ApproxViewshedSpec.scala
@@ -16,75 +16,51 @@ class ApproxViewshedSpec extends FunSpec
                             with TestServer
                             with RasterBuilders {
   describe("Viewshed") {
-    //    it("computes the viewshed of a flat int plane") {
-    //      val r = createRaster(Array.fill(7*8)(1), 7, 8)
-    //      assertEqual(Raster(BitConstant(true, 7, 8), r.rasterExtent), ApproxViewshed.approxComputeMinHeightViewable(3, 4, r))
-    //    }
-    //
-    //    it("computes the viewshed of a flat double plane") {
-    //      val r = createRaster(Array.fill(7*8)(1.5), 7, 8)
-    //      assertEqual(Raster(BitConstant(true, 7, 8), r.rasterExtent), ApproxViewshed.approxComputeMinHeightViewable(3, 4, r))
-    //    }
-    //
-    //    it("computes the viewshed of a double line") {
-    //      val rasterData = Array (
-    //        300.0, 1.0, 99.0, 0.0, 10.0, 200.0, 137.0
-    //      )
-    //      val viewable = Array (
-    //        1, 0, 1, 1, 1, 1, 0
-    //      )
-    //      val r = createRaster(rasterData, 7, 1)
-    //      val viewRaster = createRaster(viewable, 7, 1)
-    //      assertEqual(viewRaster, ApproxViewshed.approxComputeMinHeightViewable(0, 3, r))
-    //    }
-    //
-    //    it("computes the viewshed of a double plane") {
-    //      val rasterData = Array (
-    //        999.0, 1.0,   1.0,   1.0,   1.0,   1.0,   999.0,
-    //        1.0,   1.0,   1.0,   1.0,   1.0,   499.0, 1.0,
-    //        1.0,   1.0,   1.0,   1.0,   99.0,  1.0,   1.0,
-    //        1.0,   1.0,   999.0, 1.0,   1.0,   1.0,   1.0,
-    //        1.0,   1.0,   1.0,   1.0,   100.0, 1.0,   1.0,
-    //        1.0,   1.0,   1.0,   1.0,   1.0,   101.0, 1.0,
-    //        1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   102.0
-    //      )
-    //      val viewable = Array (
-    //          1,     1,     1,     1,     0,     0,     1,
-    //          0,     1,     1,     1,     0,     1,     0,
-    //          0,     0,     1,     1,     1,     0,     0,
-    //          0,     0,     1,     1,     1,     1,     1,
-    //          0,     0,     1,     1,     1,     0,     0,
-    //          0,     1,     1,     1,     0,     0,     0,
-    //          1,     1,     1,     1,     0,     0,     0
-    //      )
-    //      val r = createRaster(rasterData, 7, 7)
-    //      val viewRaster = createRaster(viewable, 7, 7)
-    //      assertEqual(viewRaster, ApproxViewshed.approxComputeMinHeightViewable(3, 3, r))
-    //    }
-    //
-    //    it("computes the viewshed of a int plane") {
-    //      val rasterData = Array (
-    //        999, 1,   1,   1,   1,   499, 999,
-    //        1,   1,   1,   1,   1,   499, 250,
-    //        1,   1,   1,   1,   99,  1,   1,
-    //        1,   999, 1,   1,   1,   1,   1,
-    //        1,   1,   1,   1,   1,   1,   1,
-    //        1,   1,   1,   0,   1,   1,   1,
-    //        1,   1,   1,   1,   1,   1,   1
-    //      )
-    //      val viewable = Array (
-    //        1,     1,     1,     1,     0,     1,     1,
-    //        1,     1,     1,     1,     0,     1,     1,
-    //        0,     1,     1,     1,     1,     0,     0,
-    //        0,     1,     1,     1,     1,     1,     1,
-    //        0,     1,     1,     1,     1,     1,     1,
-    //        1,     1,     1,     0,     1,     1,     1,
-    //        1,     1,     1,     1,     1,     1,     1
-    //      )
-    //      val r = createRaster(rasterData, 7, 7)
-    //      val viewRaster = createRaster(viewable, 7, 7)
-    //      assertEqual(viewRaster, ApproxViewshed.approxComputeMinHeightViewable(3, 3, r))
-    //    }
+    it("computes the viewshed of a flat int plane") {
+      val r = createRaster(Array.fill(7*8)(1), 7, 8)
+      assertEqual(Raster(BitConstant(true, 7, 8), r.rasterExtent), ApproxViewshed.approxComputeViewable(4, 3, r))
+    }
+
+    it("computes the viewshed of a flat double plane") {
+      val r = createRaster(Array.fill(7*8)(1.5), 7, 8)
+      assertEqual(Raster(BitConstant(true, 7, 8), r.rasterExtent), ApproxViewshed.approxComputeViewable(4, 3, r))
+    }
+
+    it("computes the viewshed of a double line") {
+      val rasterData = Array (
+        300.0, 1.0, 99.0, 0.0, 10.0, 200.0, 137.0
+      )
+      val viewable = Array (
+        1, 0, 1, 1, 1, 1, 0
+      )
+      val r = createRaster(rasterData, 7, 1)
+      val viewRaster = createRaster(viewable, 7, 1)
+      assertEqual(viewRaster.localEqual(createRaster(Array.fill(7*1)(1), 7, 1)), ApproxViewshed.approxComputeViewable(3, 0, r))
+    }
+
+    it("computes the viewshed of a double plane") {
+      val rasterData = Array (
+        999.0, 1.0,   1.0,   1.0,   1.0,   1.0,   999.0,
+        1.0,   1.0,   1.0,   1.0,   1.0,   499.0, 1.0,
+        1.0,   1.0,   1.0,   1.0,   99.0,  1.0,   1.0,
+        1.0,   1.0,   999.0, 1.0,   1.0,   1.0,   1.0,
+        1.0,   1.0,   1.0,   1.0,   100.0, 1.0,   1.0,
+        1.0,   1.0,   1.0,   1.0,   1.0,   101.0, 1.0,
+        1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   102.0
+      )
+      val viewable = Array (
+        1,     1,     1,     1,     0,     0,     1,
+        0,     1,     1,     1,     0,     1,     0,
+        0,     0,     1,     1,     1,     0,     0,
+        0,     0,     1,     1,     1,     1,     1,
+        0,     0,     1,     1,     1,     0,     0,
+        0,     1,     1,     1,     0,     0,     0,
+        1,     1,     1,     1,     0,     0,     0
+      )
+      val r = createRaster(rasterData, 7, 7)
+      val viewRaster = createRaster(viewable, 7, 7)
+      assertEqual(viewRaster.localEqual(createRaster(Array.fill(7*7)(1), 7, 7)), ApproxViewshed.approxComputeViewable(3, 3, r))
+    }
 
     it("computes the approx viewshed of a int plane") {
       val rasterData = Array (
@@ -116,7 +92,7 @@ class ApproxViewshedSpec extends FunSpec
 
       val (x,y) = (-93.63300872055451407, 30.54649743277299123) // create overload
       val (col, row) = elevation.rasterExtent.mapToGrid(x, y)
-      val actual = ApproxViewshed.approxComputeViewable(row, col, elevation)
+      val actual = ApproxViewshed.approxComputeViewable(col, row, elevation)
 
       def countDiff(a:Raster, b:Raster):Int = {
         var ans = 0
@@ -128,7 +104,7 @@ class ApproxViewshedSpec extends FunSpec
         ans
       }
       System.out.println(countDiff(expected,actual))
-      System.out.println(countDiff(Viewshed.computeViewable(row,col,elevation),actual))
+      System.out.println(countDiff(Viewshed.computeViewable(col,row,elevation),actual))
      // assertEqual(ApproxViewshed.approxComputeViewable(row, col, elevation), expected)
     }
   }
--- core/src/main/scala/geotrellis/raster/op/global/ApproxViewshed.scala
+++ core/src/main/scala/geotrellis/raster/op/global/ApproxViewshed.scala
@@ -24,71 +24,71 @@ object ApproxViewshed extends Serializable {
     val re = r.rasterExtent
     val rows = re.rows
     val cols = re.cols
-    def inBounds(x:Int, y:Int):Boolean = (x >= 0 && x < rows && y >= 0 && y < cols)
+    def inBounds(x:Int, y:Int):Boolean = (y >= 0 && y < rows && x >= 0 && x < cols)
 
     if(! inBounds(i,j)) {
       sys.error("Point indices out of bounds")
     } else {
-      val k = getRasterVal(j,i,r)
+      val k = getRasterVal(i,j,r)
       val data = RasterData.allocByType(TypeDouble,cols,rows)
-      data.setDouble(j, i, k)
-      val maxLayer = List((rows - i), (cols - j), i + 1, j + 1).max
+      data.setDouble(i, j, k)
+      val maxLayer = List((rows - j), (cols - i), j + 1, i + 1).max
       for(layer <- 1 until maxLayer) {
-        var xPointsPossible = List.empty[Pair[Int, Int]]
         var yPointsPossible = List.empty[Pair[Int, Int]]
+        var xPointsPossible = List.empty[Pair[Int, Int]]
         for(ii <- 0 until (2*layer)) {
-          xPointsPossible = Pair(i - layer,j - layer + ii) :: Pair(i + layer,j + layer - ii) :: xPointsPossible
-          yPointsPossible = Pair(i + layer - ii,j - layer) :: Pair(i - layer + ii,j + layer) :: yPointsPossible
-        }
-
-        val xPoints = xPointsPossible.filter {case (x,y) => inBounds(x,y)}
-        xPoints.map {
-          case (x,y) =>
-            val z = getRasterVal(y,x,r)
-            if (layer == 1) {
-              data.setDouble(y,x,z)
-            }else {
-              val yVal = math.abs(1.0/(i-x))*(j-y) + y
-              val yInt = yVal.toInt
-              val closestHeight = {
-                if (i == x) {
-                  data.getDouble(y, x - math.signum(x-i))
-                }else if (yVal.isValidInt) {
-                  data.getDouble(yInt, x - math.signum(x-i))
-                }else { // need linear interpolation
-                  (yInt + 1 - yVal) * data.getDouble(yInt, x - math.signum(x-i)) + (yVal - yInt) *  data.getDouble(yInt + 1, x - math.signum(x-i))
-                }
-              }
-              if (x > i) {
-                data.setDouble(y,x,math.max(z, 1.0/(i-(x-1))*(k-closestHeight)+closestHeight))
-              }else {
-                data.setDouble(y,x,math.max(z, -1.0/(i-(x+1))*(k-closestHeight) + closestHeight))
-              }
-            }
+          yPointsPossible = Pair(i - layer + ii,j - layer) :: Pair(i + layer - ii,j + layer) :: yPointsPossible
+          xPointsPossible = Pair(i - layer,j + layer - ii) :: Pair(i + layer,j - layer + ii) :: xPointsPossible
         }
 
         val yPoints = yPointsPossible.filter {case (x,y) => inBounds(x,y)}
         yPoints.map {
           case (x,y) =>
-            val z = getRasterVal(y,x,r)
+            val z = getRasterVal(x,y,r)
             if (layer == 1) {
-              data.setDouble(y,x,z)
+              data.setDouble(x,y,z)
             }else {
               val xVal = math.abs(1.0/(j-y))*(i-x) + x
               val xInt = xVal.toInt
               val closestHeight = {
                 if (j == y) {
-                  data.getDouble(y - math.signum(y-j), x)
+                  data.getDouble(x, y - math.signum(y-j))
                 }else if (xVal.isValidInt) {
-                  data.getDouble(y - math.signum(y-j), xInt)
-                } else { // need linear interpolation
-                  (xInt + 1 - xVal) *  data.getDouble(y - math.signum(y-j), xInt) + (xVal - xInt) * data.getDouble(y - math.signum(y-j), xInt + 1)
+                  data.getDouble(xInt, y - math.signum(y-j))
+                }else { // need linear interpolation
+                  (xInt + 1 - xVal) * data.getDouble(xInt, y - math.signum(y-j)) + (xVal - xInt) *  data.getDouble(xInt + 1, y - math.signum(y-j))
                 }
               }
               if (y > j) {
-                data.setDouble(y,x,math.max(z, 1.0/(j-(y-1))*(k-closestHeight) + closestHeight))
+                data.setDouble(x,y,math.max(z, 1.0/(j-(y-1))*(k-closestHeight) + closestHeight))
               }else {
-                data.setDouble(y,x,math.max(z, -1.0/(j-(y+1))*(k-closestHeight) + closestHeight))
+                data.setDouble(x,y,math.max(z, -1.0/(j-(y+1))*(k-closestHeight) + closestHeight))
+              }
+            }
+        }
+
+        val xPoints = xPointsPossible.filter {case (x,y) => inBounds(x,y)}
+        xPoints.map {
+          case (x,y) =>
+            val z = getRasterVal(x,y,r)
+            if (layer == 1) {
+              data.setDouble(x,y,z)
+            }else {
+              val yVal = math.abs(1.0/(i-x))*(j-y) + y
+              val yInt = yVal.toInt
+              val closestHeight = {
+                if (i == x) {
+                  data.getDouble(x - math.signum(x-i), y)
+                }else if (yVal.isValidInt) {
+                  data.getDouble(x - math.signum(x-i), yInt)
+                } else { // need linear interpolation
+                  (yInt + 1 - yVal) *  data.getDouble(x - math.signum(x-i), yInt) + (yVal - yInt) * data.getDouble(x - math.signum(x-i), yInt + 1)
+                }
+              }
+              if (x > i) {
+                data.setDouble(x,y,math.max(z, 1.0/(i-(x-1))*(k-closestHeight) + closestHeight))
+              }else {
+                data.setDouble(x,y,math.max(z, -1.0/(i-(x+1))*(k-closestHeight) + closestHeight))
               }
             }
         }
