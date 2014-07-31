573d57fa100cd6a68a1e06016bc0241af5674401
deshapriya <ggk.deshapriya@gmail.com>
2014-07-31 20:45:07 +0530

    Rebuild build MultiBandArg read and write for MultiBandTile

---
+++ raster/src/main/scala/geotrellis/raster/io/arg/MultiBandArgReader.scala
@@ -0,0 +1,72 @@
+package geotrellis.raster.io.arg
+
+import geotrellis.raster._
+import geotrellis.vector.Extent
+
+import com.typesafe.config.ConfigFactory
+
+import java.io.File
+import java.nio.ByteBuffer
+
+object MultibandArgReader {
+
+  /** Reads multiband arg from the jsom metadata file. */
+  final def read(path: String): MultiBandTile =
+    readBands(path, None)
+    
+  /** Reads multiband arg from the jsom metadata file. */
+  final def read(path: String, targetRasterExtent: RasterExtent): MultiBandTile =
+    readBands(path, Some(targetRasterExtent))
+    
+  private final def readBands(path: String, targetRasterExtent: Option[RasterExtent]): MultiBandTile = {
+    val json = ConfigFactory.parseString(Filesystem.readText(path))
+    val layerType = json.getString("type").toLowerCase
+    if(layerType != "arg") { sys.error(s"Cannot read raster layer type $layerType, must be arg") }
+    
+    val noOfBands: Int = json.getInt("bands")
+
+    val argPath = getPaths()
+    
+    def getPaths(): Array[String] = {
+      val paths: Array[String] = Array("0")
+      if(json.hasPath("path")) {
+        val p = json.getString("path")
+        for(i <- 0 until noOfBands) yield { paths(i)= p+"-band"+i+".arg" }
+      } else {
+        val layerName = json.getString("layer")
+        // Default to a .arg file with the same name as the layer name.
+        for(i <- 0 until noOfBands) yield { paths(i)= layerName+"-band"+i+".arg" }
+      }
+      paths
+    }
+
+    val cellType =
+      json.getString("datatype") match {
+        case "bool" => TypeBit
+        case "int8" => TypeByte
+        case "int16" => TypeShort
+        case "int32" => TypeInt
+        case "float32" => TypeFloat
+        case "float64" => TypeDouble
+        case s => sys.error("unsupported datatype '%s'" format s)
+      }
+
+    val cols = json.getInt("cols")
+    val rows = json.getInt("rows")
+
+    targetRasterExtent match {
+      case Some(te) =>
+
+        val xmin = json.getDouble("xmin")
+        val ymin = json.getDouble("ymin")
+        val xmax = json.getDouble("xmax")
+        val ymax = json.getDouble("ymax")
+        val extent = Extent(xmin, ymin, xmax, ymax)
+
+        MultiBandTile(argPath.map(f => ArgReader.read(f, cellType, RasterExtent(extent, cols, rows), te)))
+      case None =>
+        MultiBandTile(argPath.map(f => ArgReader.read(f, cellType, cols, rows)))
+    } 
+  }
+  
+}
---
+++ raster/src/main/scala/geotrellis/raster/io/arg/MultiBandArgWriter.scala
@@ -0,0 +1,79 @@
+package geotrellis.raster.io.arg
+
+import java.io.{BufferedOutputStream, DataOutputStream, FileOutputStream}
+
+import geotrellis.raster._
+import geotrellis.vector.Extent
+
+/**
+ * MultiBandArgWriter will write a MultiBandRaster to disk in ARG format.
+ * 
+ * Each instance of MultiBandArgWriter is provided a data type (e.g. int or float) to 
+ * use for output files.
+ */
+
+case class MultibandArgWriter(typ: CellType){
+  def width = typ.bits / 8
+  def cellType = typ.name
+  def dataType = "arg"
+    
+  /**
+   * Write a MultiBandRaster in ARG format to the specified path.
+   *
+   * The outputFilePath argument should be the full file path (including file name and extension)
+   * to which the MultiBandRaster file should be written.  For example, "/var/data/geotrellis/myraster.arg".
+   *
+   * The metadataName argument is a logical name for the MultiBandRaster that will be included in the MultiBandRaster's
+   * metadata.  It does not have to match the output filename.  This filename is used by the catalog
+   * when a MultiBandRaster is loaded by name as opposed to filepath.
+   *
+   * @param outputFilePath: File path to which to write out arg files (can include .arg extension)
+   * @param multiBandTile: MultiBandRaster to write to disk
+   * @param metadataName: Name to be included in json metadata as 'layer', used in catalog
+   */
+    
+  def write(outputFilePath: String, multiBandTile: MultiBandTile, extent: Extent, metadataName: String){
+    val path = outputFilePath
+    val base: String = if (path.endsWith(".arg") || path.endsWith(".json") || path.endsWith(".")) {
+      path.substring(0, path.lastIndexOf("."))
+    } else {
+      path 
+    }
+    
+    writeMetadataJSON(base, metadataName,multiBandTile.bands, RasterExtent(extent, multiBandTile.cols, multiBandTile.rows))
+    
+    for(i <- 0 until multiBandTile.bands) yield { writeData(base+"-band"+i+".arg", multiBandTile.data(i)) }
+  }
+    
+  private def writeMetadataJSON(path: String, name: String, noOfBands: Int, re: RasterExtent) {
+    val metadata = s"""{
+        |  "layer": "$name",
+        |  "datatype": "$cellType",
+        |  "type": "$dataType",
+        |  "bands": "$noOfBands",
+        |  "path": "$path",
+        |  "xmin": ${re.extent.xmin},
+        |  "xmax": ${re.extent.xmax},
+        |  "ymin": ${re.extent.ymin},
+        |  "ymax": ${re.extent.ymax},
+        |  "cols": ${re.cols},
+        |  "rows": ${re.rows},
+        |  "cellwidth": ${re.cellwidth},
+        |  "cellheight": ${re.cellheight},
+        |  "epsg": 3785,
+        |  "yskew": 0.0,
+        |  "xskew": 0.0
+        |}""".stripMargin
+
+    val bos = new BufferedOutputStream(new FileOutputStream(path+".json"))
+    bos.write(metadata.getBytes)
+    bos.close
+  }
+  
+  def writeData(path: String, raster: Tile) {
+    val dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(path)))
+    CellWriter.byType(typ).writeCells(raster, dos)
+    dos.close()
+  }
+
+}
