75c6f898918002770dc22920e3be31b089923e65
deshapriya <ggk.deshapriya@gmail.com>
2014-08-11 14:18:54 +0530

    add some operations on MultiBandTile

--- raster/src/main/scala/geotrellis/raster/MultiBandArrayTile.scala
+++ raster/src/main/scala/geotrellis/raster/MultiBandArrayTile.scala
@@ -1,6 +1,15 @@
 package geotrellis.raster
 
+import geotrellis.vector.Extent
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ConcatCall
+
 case class MultiBandArrayTile(multiBandData: Array[Tile]) extends MultiBandTile with Serializable {
+
+  /**
+   * check whether there is at least two bands
+   */
+  if (multiBandData.length < 2)
+    sys.error("There should be at least two Tiles to be MultiBandTile")
 
   val cols: Int = multiBandData(0).cols
   val rows: Int = multiBandData(0).rows
@@ -37,25 +46,63 @@ case class MultiBandArrayTile(multiBandData: Array[Tile]) extends MultiBandTile 
   }
 
   def combine(other: MultiBandTile)(f: (Int, Int) => Int): MultiBandTile = {
-    if (this.bands != other.bands){
+    if (this.bands != other.bands) {
       throw new IndexOutOfBoundsException("MultiBandTile.bands")
-    }else if (this.dimensions != other.dimensions){
-      throw new Exception("MultiBandTile.dimensions") 
-    }else {
-      val output = (for (i <- 0 until this.bands) yield { this.getBand(i).combine(other.getBand(i))(f)}).toArray
+    } else if (this.dimensions != other.dimensions) {
+      throw new Exception("MultiBandTile dimensions of bands are not Equal")
+    } else {
+      val output = (for (i <- 0 until this.bands) yield { this.getBand(i).combine(other.getBand(i))(f) }).toArray
       MultiBandTile(output)
     }
   }
 
   def combineDouble(other: MultiBandTile)(f: (Double, Double) => Double): MultiBandTile = {
-    if (this.bands != other.bands){
+    if (this.bands != other.bands) {
       throw new IndexOutOfBoundsException("MultiBandTile.bands")
-    }else if (this.dimensions != other.dimensions){
-      throw new Exception("MultiBandTile.dimensions") 
-    }else {
-      val output = (for (i <- 0 until this.bands) yield { this.getBand(i).combineDouble(other.getBand(i))(f)}).toArray
+    } else if (this.dimensions != other.dimensions) {
+      throw new Exception("MultiBandTile dimensions of bands are not Equal")
+    } else {
+      val output = (for (i <- 0 until this.bands) yield { this.getBand(i).combineDouble(other.getBand(i))(f) }).toArray
       MultiBandTile(output)
     }
+  }
+
+  def combine(first: Int, last: Int)(f: (Int, Int) => Int): Tile = {
+    if (first < bands || last < bands) {
+      var result = getBand(first)
+      for (i <- first + 1 to last) yield { result = result.combine(getBand(i))(f) }
+      result
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def combineDouble(first: Int, last: Int)(f: (Double, Double) => Double): Tile = {
+    if (first < bands || last < bands) {
+      var result = getBand(first)
+      for (i <- first + 1 to last) yield { result = result.combineDouble(getBand(i))(f) }
+      result
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def warp(source: Extent, target: RasterExtent): MultiBandTile = {
+    val outPutData = Array.ofDim[Tile](bands)
+    for (i <- 0 until bands) yield { outPutData(i) = getBand(i).warp(source, target) }
+    MultiBandTile(outPutData)
+  }
+
+  def warp(source: Extent, target: Extent): MultiBandTile = {
+    val outPutData = Array.ofDim[Tile](bands)
+    for (i <- 0 until bands) yield { outPutData(i) = getBand(i).warp(source, target) }
+    MultiBandTile(outPutData)
+  }
+
+  def warp(source: Extent, targetCols: Int, targetRows: Int): MultiBandTile = {
+    val outPutData = Array.ofDim[Tile](bands)
+    for (i <- 0 until bands) yield { outPutData(i) = getBand(i).warp(source, targetCols, targetRows) }
+    MultiBandTile(outPutData)
   }
 
   override def equals(other: Any): Boolean = other match {
@@ -72,43 +119,129 @@ case class MultiBandArrayTile(multiBandData: Array[Tile]) extends MultiBandTile 
     }
     case _ => false
   }
-  
-  def combine(first: Int, last: Int)(f: (Int, Int) => Int): Tile = {
-    if(bands < 2){
-      throw new IndexOutOfBoundsException("MultiBandTile.band")
-    }else if(bands <= first || bands <= last){
-      throw new IndexOutOfBoundsException("MultiBandTile.band with Index")
-    }else{
-      getBand(first).combine(getBand(last))(f)
+
+  /**
+   * implementation of Local sequence methods on single MultiBandTile
+   */
+
+  def min(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combine(first, last)((i, j) => math.min(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
     }
   }
 
-  def combineDouble(first: Int, last: Int)(f: (Double, Double) => Double): Tile = {
-    if(bands < 2){
-      throw new IndexOutOfBoundsException("MultiBandTile.band")
-    }else if(bands <= first || bands <= last){
-      throw new IndexOutOfBoundsException("MultiBandTile.band with Index")
-    }else{
-      getBand(first).combineDouble(getBand(last))(f)
+  def minDouble(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combineDouble(first, last)((i, j) => math.min(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
     }
   }
-  
+
+  def dualMin(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      dualCombine(first, last)((i, j) => math.min(i, j))((i, j) => math.min(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def max(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combine(first, last)((i, j) => math.max(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def maxDouble(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combineDouble(first, last)((i, j) => math.max(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def dualMax(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      dualCombine(first, last)((i, j) => math.max(i, j))((i, j) => math.max(i, j))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def mean(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combine(first, last)((i, j) => i + j).map(k => k / (last - first + 1))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def meanDouble(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      combineDouble(first, last)((i, j) => i + j).map(k => k / (last - first + 1))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
+  def dualMean(first: Int, last: Int): Tile = {
+    if (first < bands || last < bands) {
+      dualCombine(first, last)((i, j) => i + j)((i, j) => i + j).map(k => k / (last - first + 1))
+    } else {
+      throw new IndexOutOfBoundsException("MultiBandTile.bands")
+    }
+  }
+
   def ndvi(redBandIndex: Int, greenBandIndex: Int): Tile = {
-    if(bands < 2)
-      throw new IndexOutOfBoundsException("MultiBandTile.bands < 2")
-    else if(redBandIndex >= bands || greenBandIndex >= bands)
+    if (redBandIndex >= bands || greenBandIndex >= bands)
       throw new IndexOutOfBoundsException("MultiBandTile.bands < (redBandIndex or greenBandIndex)")
     else
-      combine(redBandIndex, greenBandIndex)((r,g) => (r-g)/(r+g))
+      combine(redBandIndex, greenBandIndex)((r, g) => (r - g) / (r + g))
   }
-  
+
   def ndviDouble(redBandIndex: Int, greenBandIndex: Int): Tile = {
-    if(bands < 2)
-      throw new IndexOutOfBoundsException("MultiBandTile.bands < 2")
-    else if(redBandIndex >= bands || greenBandIndex >= bands)
+    if (redBandIndex >= bands || greenBandIndex >= bands)
       throw new IndexOutOfBoundsException("MultiBandTile.bands < (redBandIndex or greenBandIndex)")
     else
-      combineDouble(redBandIndex, greenBandIndex)((r,g) => (r-g)/(r+g))
+      combineDouble(redBandIndex, greenBandIndex)((r, g) => (r - g) / (r + g))
   }
-  
+
+  /**
+   * Implementation of Local specific operations on single MultiBandTile
+   */
+  def localAdd(constant: Int): MultiBandTile = {
+    this.map(a => a + constant)
+  }
+
+  def localAdd(): Tile = {
+    this.combine(0, this.bands - 1)((a, b) => a + b)
+  }
+
+  def localSubtract(constant: Int): MultiBandTile = {
+    this.map(a => a - constant)
+  }
+
+  def localSubtract(): Tile = {
+    this.combine(0, this.bands - 1)((a, b) => a - b)
+  }
+
+  def localMultiply(constant: Int): MultiBandTile = {
+    this.map(a => a * constant)
+  }
+
+  def localMultiply(): Tile = {
+    this.combine(0, this.bands - 1)((a, b) => a * b)
+  }
+
+  def localDivide(constnt: Int): MultiBandTile = {
+    this.map(a => a / constnt)
+  }
+
+  def localDivide(): Tile = {
+    this.combine(0, this.bands - 1)((a, b) => a / b)
+  }
 }
--- raster/src/main/scala/geotrellis/raster/MultiBandTile.scala
+++ raster/src/main/scala/geotrellis/raster/MultiBandTile.scala
@@ -1,14 +1,24 @@
 package geotrellis.raster
 
+import geotrellis.vector.Extent
+
 object MultiBandTile {
   def apply(arr: Array[Tile]): MultiBandTile =
     MultiBandArrayTile(arr)
+
+  def empty(cellType: CellType, noOfBands: Int, cols: Int, rows: Int): MultiBandTile =
+    if (noOfBands < 2) {
+      sys.error("There should be at least two Tiles to be MultiBandTile")
+    } else {
+      val output = Array.ofDim[Tile](noOfBands)
+      for (i <- 0 until noOfBands) yield { output(i) = ArrayTile.empty(cellType, cols, rows) }
+      MultiBandTile(output)
+    }
 }
 
 /**
  * Base trait for a MultiBandTile.
  */
-
 trait MultiBandTile {
 
   val cols: Int
@@ -22,7 +32,6 @@ trait MultiBandTile {
   def getBand(bandNo: Int): Tile
 
   def map(f: Int => Int): MultiBandTile
-
   def mapDouble(f: Double => Double): MultiBandTile
 
   def dualMap(f: Int => Int)(g: Double => Double): MultiBandTile =
@@ -32,7 +41,6 @@ trait MultiBandTile {
   def convert(cellType: CellType): MultiBandTile
 
   def combine(other: MultiBandTile)(f: (Int, Int) => Int): MultiBandTile
-
   def combineDouble(other: MultiBandTile)(f: (Double, Double) => Double): MultiBandTile
 
   def dualCombine(other: MultiBandTile)(f: (Int, Int) => Int)(g: (Double, Double) => Double): MultiBandTile =
@@ -56,20 +64,48 @@ trait MultiBandTile {
     else mapIfSet(f)
 
   def combine(first: Int, last: Int)(f: (Int, Int) => Int): Tile
-
   def combineDouble(first: Int, last: Int)(f: (Double, Double) => Double): Tile
 
   def dualCombine(first: Int, last: Int)(f: (Int, Int) => Int)(g: (Double, Double) => Double): Tile =
     if (cellType.isFloatingPoint) combineDouble(first, last)(g)
     else combine(first, last)(f)
 
-  def ndvi(redBandIndex: Int, greenBandIndex: Int): Tile
+  def warp(source: Extent, target: RasterExtent): MultiBandTile
+  def warp(source: Extent, target: Extent): MultiBandTile
+  def warp(source: Extent, targetCols: Int, targetRows: Int): MultiBandTile
 
+  /**
+   * implementation of Local sequence methods on single MultiBandTile
+   */
+  def min(first: Int, last: Int): Tile
+  def minDouble(first: Int, last: Int): Tile
+  def dualMin(first: Int, last: Int): Tile
+
+  def max(first: Int, last: Int): Tile
+  def maxDouble(first: Int, last: Int): Tile
+  def dualMax(first: Int, last: Int): Tile
+
+  def mean(first: Int, last: Int): Tile
+  def meanDouble(first: Int, last: Int): Tile
+  def dualMean(first: Int, last: Int): Tile
+
+  def ndvi(redBandIndex: Int, greenBandIndex: Int): Tile
   def ndviDouble(redBandIndex: Int, greenBandIndex: Int): Tile
 
   def dualndvi(redBandIndex: Int, greenBandIndex: Int): Tile = {
     if (cellType.isFloatingPoint) ndviDouble(redBandIndex, greenBandIndex)
     else ndvi(redBandIndex, greenBandIndex)
   }
-  
+
+  /**
+   * Implementation of Local specific operations on single MultiBandTile
+   */
+  def localAdd(constant: Int): MultiBandTile
+  def localAdd(): Tile
+  def localSubtract(constant: Int): MultiBandTile
+  def localSubtract(): Tile
+  def localMultiply(constant: Int): MultiBandTile
+  def localMultiply(): Tile
+  def localDivide(constnt: Int): MultiBandTile
+  def localDivide(): Tile
 }
