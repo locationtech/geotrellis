38f02ca03ac9b0ffdbb31b7de7e0ec6a239d58a9
deshapriya <ggk.deshapriya@gmail.com>
2014-09-02 11:14:27 +0530

    add test to fix 871 with 2 tiles

--- raster-test/src/test/scala/geotrellis/raster/op/local/LessSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/op/local/LessSpec.scala
@@ -22,20 +22,20 @@ import org.scalatest._
 
 import geotrellis.testkit._
 
-class LessSpec extends FunSpec 
-                  with Matchers 
-                  with TestEngine 
-                  with TileBuilders {
+class LessSpec extends FunSpec
+  with Matchers
+  with TestEngine
+  with TileBuilders {
   describe("Less") {
     it("checks int valued raster against int constant") {
       val r = positiveIntegerRaster
       val result = (r < 5)
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.get(col, row)
           val rz = result.get(col, row)
-          if(z < 5) rz should be (1)
-          else rz should be (0)
+          if (z < 5) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -43,12 +43,12 @@ class LessSpec extends FunSpec
     it("checks int valued raster against int constant (right associative)") {
       val r = positiveIntegerRaster
       val result = (5 <<: r)
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.get(col, row)
           val rz = result.get(col, row)
-          if(5 < z) rz should be (1)
-          else rz should be (0)
+          if (5 < z) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -56,12 +56,12 @@ class LessSpec extends FunSpec
     it("checks int valued raster against double constant") {
       val r = probabilityRaster.map(_ * 100).convert(TypeInt)
       val result = r < 69.0
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.get(col, row)
           val rz = result.get(col, row)
-          if(z < 69) rz should be (1)
-          else rz should be (0)
+          if (z < 69) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -69,12 +69,12 @@ class LessSpec extends FunSpec
     it("checks double valued raster against int constant") {
       val r = positiveIntegerRaster.convert(TypeDouble).mapDouble(_.toDouble)
       val result = r < 5
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.getDouble(col, row)
           val rz = result.get(col, row)
-          if(z < 5.0) rz should be (1)
-          else rz should be (0)
+          if (z < 5.0) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -82,12 +82,12 @@ class LessSpec extends FunSpec
     it("checks double valued raster against int constant (right associative)") {
       val r = positiveIntegerRaster.convert(TypeDouble).mapDouble(_.toDouble)
       val result = (5 <<: r)
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.getDouble(col, row)
           val rz = result.get(col, row)
-          if(5.0 < z) rz should be (1)
-          else rz should be (0)
+          if (5.0 < z) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -95,12 +95,12 @@ class LessSpec extends FunSpec
     it("checks double valued raster against double constant") {
       val r = probabilityRaster
       val result = r < 0.69
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
           val z = r.getDouble(col, row)
           val rz = result.getDouble(col, row)
-          if(z < 0.69) rz should be (1)
-          else rz should be (0)
+          if (z < 0.69) rz should be(1)
+          else rz should be(0)
         }
       }
     }
@@ -108,9 +108,9 @@ class LessSpec extends FunSpec
     it("checks an integer raster against itself") {
       val r = positiveIntegerRaster
       val result = r < r
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
-          result.get(col, row) should be (0)
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
+          result.get(col, row) should be(0)
         }
       }
     }
@@ -119,9 +119,9 @@ class LessSpec extends FunSpec
       val r = positiveIntegerRaster
       val r2 = positiveIntegerRaster.map(_ * 2)
       val result = r < r2
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
-          result.get(col, row) should be (1)
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
+          result.get(col, row) should be(1)
         }
       }
     }
@@ -129,9 +129,9 @@ class LessSpec extends FunSpec
     it("checks a double raster against itself") {
       val r = probabilityRaster
       val result = r < r
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
-          result.get(col, row) should be (0)
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
+          result.get(col, row) should be(0)
         }
       }
     }
@@ -140,13 +140,13 @@ class LessSpec extends FunSpec
       val r = probabilityRaster
       val r2 = positiveIntegerRaster.mapDouble(_ * 2.3)
       val result = r < r2
-      for(col <- 0 until r.cols) {
-        for(row <- 0 until r.rows) {
-          result.get(col, row) should be (1)
+      for (col <- 0 until r.cols) {
+        for (row <- 0 until r.rows) {
+          result.get(col, row) should be(1)
         }
       }
     }
-    
+
     it("checks double valued raster against int constant right associative(compare 6 against 6.9, 6 < 6.9 should be true)") {
 
       val a = Array(0.0, -1.0, 2.5, -3.2,
@@ -166,6 +166,34 @@ class LessSpec extends FunSpec
 
       for (col <- 0 until r.cols) {
         for (row <- 0 until r.rows) {
+          result.get(col, row) should be(expected.get(col, row))
+        }
+      }
+    }
+
+    it("checks int valued raster against double valued raster") {
+
+      val a1 = Array(0, -2, 4, 3,
+        10, 6, -9, -20,
+        18, 5, 7, 3,
+        -1, -9, 9, 1)
+      val r1 = IntArrayTile(a1, 4, 4)
+
+      val a2 = Array(0.0, -1.0, 2.5, -3.2,
+        4.8, -5.67, 6.90, -7.4,
+        8.7, 5.7, 10.3, -11.6,
+        12.4, -13.9, 14.5, -15.1)
+      val r2 = DoubleArrayTile(a2, 4, 4)
+
+      val a3 = Array[Byte](0, 1, 0, 0,
+        0, 0, 1, 1,
+        0, 1, 1, 0,
+        1, 0, 1, 0)
+      val expected = ByteArrayTile(a3, 4, 4)
+      val result = r1 < r2
+      
+      for (col <- 0 until result.cols) {
+        for (row <- 0 until result.rows) {
           result.get(col, row) should be(expected.get(col, row))
         }
       }
