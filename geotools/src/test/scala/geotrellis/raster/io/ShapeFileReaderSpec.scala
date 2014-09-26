049d4d28fb154eaea20f261e913d902b1ccbda90
lossyrob <rdemanuele@gmail.com>
2014-09-26 15:29:56 -0400

    Added functionality to ShapeFileReader

---
+++ geotools/src/test/scala/geotrellis/raster/io/ShapeFileReaderSpec.scala
@@ -0,0 +1,18 @@
+package geotrellis.geotools
+
+import geotrellis.vector._
+
+import org.scalatest._
+
+class ShapeFileReaderSpec extends FunSpec with Matchers {
+  describe("ShapeFileReader") {
+    it("should read multipolygons feature attribute") {
+      val path = "geotools/data/shapefiles/demographics/demographics.shp"
+      val features = ShapeFileReader.readMultiPolygonFeatures(path)
+      features.size should be (160)
+      for(MultiPolygonFeature(polygon, data) <- features) {
+        data.keys.toSeq should be (Seq("LowIncome", "gbcode", "ename", "WorkingAge", "TotalPop", "Employment"))
+      }
+    }
+  }
+}
