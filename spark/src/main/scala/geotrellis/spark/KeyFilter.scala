fbe4e65a35bf460560c2a1aeec36da5c341fcdc3
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-06 18:00:40 -0400

    KeyFilter works around dates now

--- spark/src/main/scala/geotrellis/spark/KeyFilter.scala
+++ spark/src/main/scala/geotrellis/spark/KeyFilter.scala
@@ -2,6 +2,7 @@ package geotrellis.spark
 
 import geotrellis.spark.tiling.TileCoordScheme
 import geotrellis.spark._
+import org.joda.time.DateTime
 
 import scala.annotation.implicitNotFound
 import scala.collection.mutable
@@ -12,7 +13,7 @@ trait KeyFilter
 trait Filterable[K, F]
 
 case class SpaceFilter(bounds: TileBounds, scheme: TileCoordScheme) extends KeyFilter
-case class TimeFilter(startTime: Double, endTime: Double) extends KeyFilter
+case class TimeFilter(startTime: DateTime, endTime: DateTime) extends KeyFilter
 
 class FilterSet[K] {
   private var _filters = mutable.ListBuffer[KeyFilter]()
