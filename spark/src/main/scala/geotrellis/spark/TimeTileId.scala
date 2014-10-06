4ac546da02079cdbc38017967366fa7fc52d80c4
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-06 17:02:36 -0400

    fix: removing useless implicits

--- spark/src/main/scala/geotrellis/spark/TimeTileId.scala
+++ spark/src/main/scala/geotrellis/spark/TimeTileId.scala
@@ -1,8 +1,5 @@
 package geotrellis.spark
 
-object TimeTileId {
-  implicit object v1 extends Filterable[TimeTileId, TimeFilter]
-  implicit object v2 extends Filterable[TimeTileId, SpaceFilter]
-}
+import org.joda.time.DateTime
 
-case class TimeTileId(tileId: TileId, time: Double)
+case class TimeTileId(tileId: TileId, time: DateTime)
