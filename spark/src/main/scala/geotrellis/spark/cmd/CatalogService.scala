26d945ac5eefe4e080cc6568c64432ef1c85134d
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-14 13:43:37 -0400

    fix: core support for catalog route

--- spark/src/main/scala/geotrellis/spark/cmd/CatalogService.scala
+++ spark/src/main/scala/geotrellis/spark/cmd/CatalogService.scala
@@ -5,11 +5,12 @@ import com.quantifind.sumac.{ArgApp, ArgMain}
 import geotrellis.proj4.LatLng
 import geotrellis.raster.GridBounds
 import geotrellis.spark._
+import geotrellis.spark.cmd.args.{AccumuloArgs, SparkArgs, HadoopArgs}
 import geotrellis.spark.io.accumulo.{Layer, AccumuloInstance}
 import geotrellis.spark.tiling.{GridCoordScheme, TmsCoordScheme}
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
 import org.joda.time.DateTime
-import spray.http.MediaTypes
+import spray.http.{AllOrigins, MediaTypes}
 import spray.httpx.SprayJsonSupport
 import spray.routing.SimpleRoutingApp
 import geotrellis.raster.render._
@@ -28,33 +29,18 @@ import spray.http.HttpHeaders._
 import spray.http.HttpMethods._
 import spray.routing._
 
-//TODO source: https://coderwall.com/p/0izzta
-// see also https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
-trait CORSSupport {
-  this: HttpService =>
+trait CORSSupport { self: HttpService =>
+  val corsHeaders = List(`Access-Control-Allow-Origin`(AllOrigins),
+    `Access-Control-Allow-Methods`(GET, POST, OPTIONS, DELETE),
+    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"))
 
-  private val allowOriginHeader = `Access-Control-Allow-Origin`(AllOrigins)
-  private val optionsCorsHeaders = List(
-    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"),
-    `Access-Control-Max-Age`(1728000))
-
-  def cors[T]: Directive0 = mapRequestContext { ctx => ctx.withRouteResponseHandling({
-    //It is an option requeset for a resource that responds to some other method
-    case Rejected(x) if (ctx.request.method.equals(HttpMethods.OPTIONS) && !x.filter(_.isInstanceOf[MethodRejection]).isEmpty) => {
-      val allowedMethods: List[HttpMethod] = x.filter(_.isInstanceOf[MethodRejection]).map(rejection=> {
-        rejection.asInstanceOf[MethodRejection].supported
-      })
-      ctx.complete(HttpResponse().withHeaders(
-        `Access-Control-Allow-Methods`(OPTIONS, allowedMethods :_*) ::  allowOriginHeader ::
-          optionsCorsHeaders
-      ))
-    }
-  }).withHttpResponseHeadersMapped { headers =>
-    allowOriginHeader :: headers
-
-  }
+  def cors: Directive0 = {
+    val rh = implicitly[RejectionHandler]
+    respondWithHeaders(corsHeaders) & handleRejections(rh)
   }
 }
+
+class TmsArgs extends SparkArgs with AccumuloArgs with HadoopArgs
 
 /**
  * Catalog and TMS service for TimeRaster layers only
@@ -92,7 +78,7 @@ object CatalogService extends ArgApp[TmsArgs] with SimpleRoutingApp with SprayJs
       }
     }
 
-  def catalogRoute =
+  def catalogRoute = cors {
     path("") {
       get {
         // get the entire catalog
@@ -106,26 +92,30 @@ object CatalogService extends ArgApp[TmsArgs] with SimpleRoutingApp with SprayJs
         }
       }
     } ~
-    pathPrefix(Segment / IntNumber / "bands") { (layer, zoom) =>
-      get {
-        complete {
-          import DefaultJsonProtocol._
+    pathPrefix(Segment / IntNumber / "bands") {
+      (layer, zoom) =>
+        get {
+          complete {
+            import DefaultJsonProtocol._
 
-          accumulo.metaDataCatalog.get(Layer(layer, zoom)).flatMap { case (table, md) =>
-            val GridBounds(col, row, _, _) = md.gridBounds
-            val filters = new FilterSet[TimeTileId]() withFilter SpaceFilter(GridBounds(col, row, col, row), GridCoordScheme)
-            catalog.load(layer, zoom, filters).map(_.map{ case (key, tile) => key.time.toString })
-          }.get.collect
+            accumulo.metaDataCatalog.get(Layer(layer, zoom)).flatMap {
+              case (table, md) =>
+                val GridBounds(col, row, _, _) = md.gridBounds
+                val filters = new FilterSet[TimeTileId]() withFilter SpaceFilter(GridBounds(col, row, col, row), GridCoordScheme)
+                catalog.load(layer, zoom, filters).map(_.map {
+                  case (key, tile) => key.time.toString
+                })
+            }.get.collect
+          }
         }
-      }
     }
-
+  }
   def root = {
     pathPrefix("catalog") { catalogRoute } ~
     pathPrefix("tms") { tmsRoute }
   }
   
   startServer(interface = "localhost", port = 8080) {
-    get(pingPong ~ cors {root})
+    get(pingPong ~  root)
   }
 }
