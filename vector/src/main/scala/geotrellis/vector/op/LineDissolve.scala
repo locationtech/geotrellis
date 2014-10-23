4882427f81b94f28eae275034d6d0d50b2c2892d
lossyrob <rdemanuele@gmail.com>
2014-10-23 19:36:21 -0400

    More optimizations.

--- vector/src/main/scala/geotrellis/vector/op/LineDissolve.scala
+++ vector/src/main/scala/geotrellis/vector/op/LineDissolve.scala
@@ -12,15 +12,15 @@ import GeomFactory._
 
 object LineDissolve {
 
-  class LineSegment(val p1: Point, val p2: Point) {
+  class LineSegment(val p1: jts.Coordinate, val p2: jts.Coordinate) {
     def toLine: Line = 
-      Line(factory.createLineString(Array(new Coordinate(p1.x, p1.y), new Coordinate(p2.x, p2.y))))
+      Line(factory.createLineString(Array(p1, p2)))
 
     override val hashCode = {
-      var x1 = 0
-      var y1 = 0
-      var x2 = 0
-      var y2 = 0
+      var x1 = 0.0
+      var y1 = 0.0
+      var x2 = 0.0
+      var y2 = 0.0
 
       if(p1.x < p2.x) { x1 = p1.x ; y1 = p1.y ; x2 = p2.x ; y2 = p2.y }
       else if(p1.x > p2.x) { x1 = p2.x ; y1 = p2.y ; x2 = p1.x ; y2 = p1.y }
@@ -53,12 +53,12 @@ object LineDissolve {
 
     cfor(0)(_ < lineArray.size, _ + 1) { i =>
       val line = lineArray(i)
-      val points = line.points
+      val points = line.jtsGeom.getCoordinates
 
       cfor(0)(_ < points.size - 1, _ + 1) { j =>
         val p1 = points(j)
         val p2 = points(j + 1)
-        if(p1 != p2) {
+        if(p1.x != p2.x || p1.y != p2.y) {
           hashSet.add(new LineSegment(p1, p2))
         }
       }
