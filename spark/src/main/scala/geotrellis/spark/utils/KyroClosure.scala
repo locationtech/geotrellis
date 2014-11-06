a4843f5de9c7acc99fcd36f3f624c066542cb0c8
Eugene Cheipesh <echeipesh@gmail.com>
2014-11-06 18:01:57 -0500

    cleanup type names to match FunctionX notations

--- spark/src/main/scala/geotrellis/spark/utils/KyroClosure.scala
+++ spark/src/main/scala/geotrellis/spark/utils/KyroClosure.scala
@@ -1,15 +1,15 @@
 package geotrellis.spark.utils
 
-class KryoClosure1[A, B](f: A=>B) extends KryoWrapper[A=>B] with Function1[A, B] with Serializable {
+class KryoClosure1[T1, R](f: T1=>R) extends KryoWrapper[T1=>R] with (T1=>R) {
   assert(f != null)
   value = f
 
-  def apply(in: A): B = {
+  def apply(in: T1): R = {
     assert(value != null)
     value.apply(in)
   }
 }
-class KryoClosure2[T1, T2, R](f: (T1,T2)=>R) extends KryoWrapper[(T1, T2)=>R] with Function2[T1, T2, R] with Serializable {
+class KryoClosure2[T1, T2, R](f: (T1,T2)=>R) extends KryoWrapper[(T1, T2)=>R] with ((T1,T2)=>R) {
   assert(f != null)
   value = f
 
@@ -20,10 +20,10 @@ class KryoClosure2[T1, T2, R](f: (T1,T2)=>R) extends KryoWrapper[(T1, T2)=>R] wi
 }
 
 object KryoClosure {
-  import com.esotericsoftware.minlog.Log
-  import com.esotericsoftware.minlog.Log._
-  Log.set(LEVEL_TRACE)
+//  import com.esotericsoftware.minlog.Log
+//  import com.esotericsoftware.minlog.Log._
+//  Log.set(LEVEL_TRACE)
 
-  def apply[A, B](f: A => B)  = new KryoClosure1[A, B](f)
+  def apply[T1, R](f: T1 => R)  = new KryoClosure1[T1, R](f)
   def apply[T1, T2, R](f: (T1,T2) => R)  = new KryoClosure2[T1, T2, R](f)
 }
