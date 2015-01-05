551ecb88217edb37208d389988b4111e21423098
Eugene Cheipesh <echeipesh@gmail.com>
2015-01-05 09:07:41 -0500

    AttributeCatalogs use a timeout map

--- spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloAttributeCatalog.scala
+++ spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloAttributeCatalog.scala
@@ -2,6 +2,7 @@ package geotrellis.spark.io.accumulo
 
 import geotrellis.spark._
 import geotrellis.spark.io._
+import geotrellis.spark.utils.TimedCache
 
 import spray.json._
 
@@ -14,41 +15,61 @@ import org.apache.accumulo.core.data._
 import org.apache.hadoop.io.Text
 
 class AccumuloAttributeCatalog(connector: Connector, val attributeTable: String) extends AttributeCatalog with Logging {
-  type ReadableWritable[T] = RootJsonFormat[T]
+  type ReadableWritable[T] = JsonFormat[T]
+  
+  val cache = new TimedCache[(LayerId, String), Any](1000*30)
 
-  //create the attribute table if it does not exist
   {
     val ops = connector.tableOperations()
     if (!ops.exists(attributeTable))
       ops.create(attributeTable)
   }
 
+  def getScanner = connector.createScanner(attributeTable, new Authorizations())
+  
   private def fetch(layerId: LayerId, attributeName: String): List[Value] = {
-    val scanner  = connector.createScanner(attributeTable, new Authorizations())
+    val scanner  = getScanner
     scanner.setRange(new Range(new Text(layerId.toString)))
     scanner.fetchColumnFamily(new Text(attributeName))
     scanner.iterator.toList.map(_.getValue)
   }
 
-  def load[T: RootJsonFormat](layerId: LayerId, attributeName: String): T = {
-    val values = fetch(layerId, attributeName)
+  def load[T: JsonFormat](layerId: LayerId, attributeName: String): T = {
+    cache get (layerId -> attributeName) match {
+      case Some(value) =>    
+        value.asInstanceOf[T]
+      case None =>
+        val values = fetch(layerId, attributeName)
 
-    if(values.size == 0) {
-      sys.error(s"Attribute $attributeName not found for layer $layerId")
-    } else if(values.size > 1) {
-      sys.error(s"Multiple attributes found for $attributeName for layer $layerId")
-    } else {
-      values.head.toString.parseJson.convertTo[T]
+        if(values.size == 0) {
+          sys.error(s"Attribute $attributeName not found for layer $layerId")
+        } else if(values.size > 1) {
+          sys.error(s"Multiple attributes found for $attributeName for layer $layerId")
+        } else {
+          val value = values.head.toString.parseJson.convertTo[T]
+          cache put (layerId -> attributeName, value)
+          value
+        }
     }
   }
 
-  def save[T: RootJsonFormat](layerId: LayerId, attributeName: String, value: T): Unit = {
-    val mutation = new Mutation(layerId.toString)
+  def save[T: JsonFormat](layerId: LayerId, attributeName: String, value: T): Unit = {
+    val mutation = new Mutation(layerId.toString)  
+    
     mutation.put(
       new Text(attributeName), new Text(), System.currentTimeMillis(),
       new Value(value.toJson.compactPrint.getBytes)
     )
 
     connector.write(attributeTable, mutation)
+    cache put (layerId -> attributeName, value)
+  }
+
+  def listLayers: List[(LayerId, String)] = {
+    val scanner = getScanner
+    scanner.fetchColumnFamily(AttributeCatalog.KEYCLASS_FIELD)
+    scanner.iterator.toList.map{ row => 
+      LayerId.fromString(row.getKey.toString) -> row.getValue.toString
+    }
   }
 }
