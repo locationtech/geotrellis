9532efa098273f5601208aa5d8475d4b60a96a95
Kenny Shepard <kshepard@gmail.com>
2015-02-11 10:28:56 -0500

    Allow file filtering in hadoopGeoTiffRDD
    Fixes #990

--- spark/src/test/scala/geotrellis/spark/io/hadoop/HadoopCatalogSpec.scala
+++ spark/src/test/scala/geotrellis/spark/io/hadoop/HadoopCatalogSpec.scala
@@ -99,6 +99,22 @@ with OnlyIfCanRunSpark
         cat.save(LayerId("onesSpecial", level.zoom), onesRdd)
         assert(fs.exists(new Path(catalogPath, "special/onesSpecial")))
       }
+
+      it("should allow filtering files in hadoopGeoTiffRDD") {
+        val tilesDir = new Path(localFS.getWorkingDirectory, "../raster-test/data/one-month-tiles/")
+        val source = sc.hadoopGeoTiffRDD(tilesDir)
+
+        // Raises exception if the bogus file isn't properly filtered out
+        Ingest(source, LatLng, layoutScheme)
+      }
+
+      it("should allow overriding tiff file extensions in hadoopGeoTiffRDD") {
+        val tilesDir = new Path(localFS.getWorkingDirectory, "../raster-test/data/one-month-tiles-tiff/")
+        val source = sc.hadoopGeoTiffRDD(tilesDir, ".tiff")
+
+        // Raises exception if the ".tiff" extension override isn't provided
+        Ingest(source, LatLng, layoutScheme)
+      }
     }
   }
 }
