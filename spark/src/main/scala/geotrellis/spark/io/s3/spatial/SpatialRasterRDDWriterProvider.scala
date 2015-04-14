3e9441ae4b59ee22930ec98b5b827252755966d0
Eugene Cheipesh <echeipesh@gmail.com>
2015-04-14 16:47:24 -0400

    testing s3client for back-off compliance

--- spark/src/main/scala/geotrellis/spark/io/s3/spatial/SpatialRasterRDDWriterProvider.scala
+++ spark/src/main/scala/geotrellis/spark/io/s3/spatial/SpatialRasterRDDWriterProvider.scala
@@ -7,13 +7,13 @@ import geotrellis.spark.io.s3._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import java.io.ByteArrayInputStream
+import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.auth.{AWSCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
 import geotrellis.index.zcurve.Z2
 import com.typesafe.scalalogging.slf4j._
 import scala.collection.mutable.ArrayBuffer
-import com.amazonaws.services.s3.transfer.{Upload, TransferManager}
 
 object SpatialRasterRDDWriterProvider extends RasterRDDWriterProvider[SpatialKey] with LazyLogging {
   def writer(credentialsProvider: AWSCredentialsProvider, bucket: String, layerPath: String, clobber: Boolean = true)(implicit sc: SparkContext) =
@@ -28,21 +28,18 @@ object SpatialRasterRDDWriterProvider extends RasterRDDWriterProvider[SpatialKey
         
         rdd
           .foreachPartition { partition =>
-            val tx = new TransferManager(bcCredentials.value);
+            val s3client = new AmazonS3Client(bcCredentials.value);
 
-            partition.sliding(128,128).foreach{ iter =>
-              var uploads = ArrayBuffer.empty[Upload]
-            
-              iter.foreach { case (key: SpatialKey, tile: Tile) =>
-                val index = Z2(key.col, key.row).z
-                val bytes = tile.toBytes
-                val metadata = new ObjectMetadata()
-                metadata.setContentLength(bytes.length);              
-                val is = new ByteArrayInputStream(bytes)
-                uploads += tx.upload(catalogBucket, f"$path/${index}%019d", is, metadata)              
-              }
-              uploads.foreach(_.waitForUploadResult) // block
+            val requests = partition.map{ case (key: SpatialKey, tile: Tile) =>
+              val index = Z2(key.col, key.row).z
+              val bytes = tile.toBytes
+              val metadata = new ObjectMetadata()
+              metadata.setContentLength(bytes.length);              
+              val is = new ByteArrayInputStream(bytes)
+              new PutObjectRequest(catalogBucket, f"$path/${index}%019d", is, metadata)
             }
+
+            requests.foreach(s3client.putObject)
           }
 
         logger.info(s"Finished saving tiles to ${layerPath}")
