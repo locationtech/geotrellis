a79fdf2efdcf6c22bbe233aa52b4fbe9d0de54c1
Eugene Cheipesh <echeipesh@gmail.com>
2015-04-15 10:49:04 -0400

    add s3 query command

---
+++ spark/src/main/scala/geotrellis/spark/cmd/S3QueryCommand.scala
@@ -0,0 +1,47 @@
+package geotrellis.spark.cmd
+
+import geotrellis.spark._
+import com.quantifind.sumac.FieldArgs
+import geotrellis.spark.io.s3._
+import geotrellis.spark.tiling._
+import geotrellis.spark.utils.SparkUtils
+import geotrellis.vector._
+import geotrellis.proj4._
+import geotrellis.raster.{Tile, GridBounds}
+import org.apache.spark._
+import com.quantifind.sumac.ArgMain
+import com.quantifind.sumac.validation.Required
+
+import scala.reflect.ClassTag
+
+class S3QueryArgs extends FieldArgs {
+  @Required var bucket: String = _  
+  @Required var key: String = _ 
+  @Required var layer: String = _
+  @Required var bbox: String = _
+
+  def layerId: LayerId = {
+    val c = layer.split(":")
+    LayerId(c(0), c(0).toInt)
+  }
+
+  def bounds: GridBounds = {
+    val c = layer.split(",")
+    GridBounds(c(0).toInt, c(1).toInt, c(2).toInt, c(3).toInt)
+  }
+}
+
+object S3QueryCommand extends ArgMain[S3QueryArgs] with Logging {
+  def main(args: S3QueryArgs): Unit = {
+    implicit val sc = SparkUtils.createSparkContext("S3 Query")
+    
+    implicit val wProv = geotrellis.spark.io.s3.spatial.SpatialRasterRDDWriterProvider
+    implicit val rProv = geotrellis.spark.io.s3.spatial.SpatialRasterRDDReaderProvider
+    val catalog = S3RasterCatalog(args.bucket, args.key)
+    val reader = catalog.reader[SpatialKey]()
+    
+    val rdd = reader.read(args.layerId, FilterSet(SpaceFilter(args.bounds)))
+    println(s"Expected Count: ${args.bounds.coords.length}")
+    println(s"Record Count: ${rdd.count}")
+  }
+}
