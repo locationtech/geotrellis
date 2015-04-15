d119fb670b2e1e356d358b62c2296b5c9da48d1d
Eugene Cheipesh <echeipesh@gmail.com>
2015-04-15 18:21:01 -0400

    fix: splitting s3 ranges evenly

--- spark/src/main/scala/geotrellis/spark/io/s3/spatial/SpatialRasterRDDReadedProvider.scala
+++ spark/src/main/scala/geotrellis/spark/io/s3/spatial/SpatialRasterRDDReadedProvider.scala
@@ -52,10 +52,11 @@ object SpatialRasterRDDReaderProvider extends RasterRDDReaderProvider[SpatialKey
 
         val rdd: RDD[(SpatialKey, Tile)] = sc
           .parallelize(bins)
-          .flatMap(ranges => ranges)
-          .mapPartitions{ iter =>
+          .mapPartitions{ rangeList =>
             val s3Client = new AmazonS3Client(bcCredentials.value)
-            iter
+            
+            rangeList
+              .flatMap( ranges => ranges)            
               .flatMap{ range => 
                 listKeys(s3Client, bucket, dir, range) 
               }
@@ -125,17 +126,35 @@ object SpatialRasterRDDReaderProvider extends RasterRDDReaderProvider[SpatialKey
    * Will attemp to bin ranges into buckets, each containing at least the average number of elements.
    * Trailing bins may be empty if the count is too high for number of ranges.
    */
-  private def ballancedBin(ranges: Seq[(Long, Long)], count: Int ): Seq[Seq[(Long, Long)]] = {
-    val total = ranges.foldLeft(0l){ (s,r) => s + r._2 - r._1 + 1l }
-    val step = total / count
+  def ballancedBin(ranges: Seq[(Long, Long)], count: Int ): Seq[Seq[(Long, Long)]] = {
+    var stack = ranges.toList
+
+    def len(r: (Long, Long)) = r._2 - r._1 + 1l
+    val total = ranges.foldLeft(0l){ (s,r) => s + len(r) }
+    val binWidth = total / count + 1
+    
+    def splitRange(range: (Long, Long), take: Long): ((Long, Long), (Long, Long)) = {
+      assert(len(range) > take)
+      (range._1, range._1 + take - 1) -> (range._1 + take, range._2)
+    }
 
     val arr = Array.fill(count)(Nil: List[(Long, Long)])
     var sum = 0l
     var i = 0
-    for (r <- ranges) {
-      arr(i) = r :: arr(i)
-      sum += r._2 - r._1  + 1
-      if (sum > step) {
+    while (! stack.isEmpty) {      
+      if (len(stack.head)+sum <= binWidth){
+        val take = stack.head
+        arr(i) = take :: arr(i) 
+        sum += len(take)
+        stack = stack.tail
+      }else{
+        val (take, left) = splitRange(stack.head, binWidth - sum)
+        stack = left :: stack.tail
+        arr(i) = take :: arr(i)
+        sum += len(take)
+      }
+
+      if (sum >= binWidth) {
         sum = 0l
         i += 1
       }
