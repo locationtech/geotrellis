f4df9c5b46d09499dab2119219bc7482d6db99a3
Eugene Cheipesh <echeipesh@gmail.com>
2015-04-21 17:38:46 -0400

    s3 client gains utility and overload methods

--- spark/src/main/scala/geotrellis/spark/io/s3/S3Client.scala
+++ spark/src/main/scala/geotrellis/spark/io/s3/S3Client.scala
@@ -1,12 +1,16 @@
 package geotrellis.spark.io.s3
 
 import com.amazonaws.auth.AWSCredentialsProvider
-import java.io.InputStream
+import java.io.{InputStream, ByteArrayInputStream, DataInputStream, ByteArrayOutputStream}
 import com.amazonaws.services.s3.model._
+import com.typesafe.scalalogging.slf4j._
 
-trait S3Client {
+trait S3Client extends LazyLogging {
 
   def listObjects(listObjectsRequest: ListObjectsRequest): ObjectListing
+  
+  def listObjects(bucketName: String, prefix: String): ObjectListing =          
+      listObjects(new ListObjectsRequest(bucketName, prefix, null, null, null));
 
   def getObject(getObjectRequest: GetObjectRequest): S3Object
 
@@ -15,8 +19,16 @@ trait S3Client {
   def getObject(bucketName: String, key: String): S3Object = 
     getObject(new GetObjectRequest(bucketName, key))
 
-  def putObject(bucketName: String, key: String, input: InputStream, metadata: ObjectMetadata): PutObjectResult =
+  def putObject(bucketName: String, key: String, input: InputStream, metadata: ObjectMetadata): PutObjectResult = 
     putObject(new PutObjectRequest(bucketName, key, input, metadata))
+
+  def putObject(bucketName: String, key: String, bytes: Array[Byte], metadata: ObjectMetadata): PutObjectResult = {    
+    metadata.setContentLength(bytes.length)
+    putObject(bucketName, key, new ByteArrayInputStream(bytes), metadata)
+  }
+
+  def putObject(bucketName: String, key: String, bytes: Array[Byte]): PutObjectResult =
+    putObject(bucketName, key, bytes, new ObjectMetadata())
 
   def putObjectWithBackoff(putObjectRequest: PutObjectRequest): PutObjectResult = {
     var ret: PutObjectResult = null
@@ -27,10 +39,29 @@ trait S3Client {
         ret = putObject(putObjectRequest)
       } catch {
         case e: AmazonS3Exception =>
-          backoff = math.max(8, backoff*2)
+          if (e.getErrorCode == 503 && e.getErrorType == "SlowDown") {
+            backoff = math.max(8, backoff*2)
+            logger.info(s"Got $e, Backing off for $backoff ms")
+          }else{
+            throw e
+          }
       }
     } while (ret == null)
     ret
+  }
+}
+
+object S3Client {
+  implicit class S3ObjectBytes(obj: S3Object) {
+    def toBytes: Array[Byte] = {
+      val is = obj.getObjectContent
+      val len = obj.getObjectMetadata.getContentLength.toInt
+      val bytes = new Array[Byte](len)
+      val read = is.read(bytes, 0, len)
+      is.close()
+      assert(read == len, s"$read bytes read, $len expected.")      
+      bytes
+    }
   }
 }
 
