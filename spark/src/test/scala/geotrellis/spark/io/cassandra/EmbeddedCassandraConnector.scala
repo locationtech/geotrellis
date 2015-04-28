402d1ecc4150cb081476b7a8bc7de8db132f7fbb
Kevin Yeh <kevinyeah@utexas.edu>
2015-04-28 18:47:10 -0500

    Update Cassandra Specs

--- spark/src/test/scala/geotrellis/spark/io/cassandra/EmbeddedCassandraConnector.scala
+++ spark/src/test/scala/geotrellis/spark/io/cassandra/EmbeddedCassandraConnector.scala
@@ -1,33 +1,24 @@
 package geotrellis.spark.io.cassandra
 
-import com.datastax.driver.core.Session
 import com.datastax.spark.connector.cql._
+import org.apache.spark.SparkContext
 
 import java.net.InetAddress
-import org.apache.spark.SparkConf
 
-class EmbeddedCassandraConnector(conf: CassandraConnectorConf) extends CassandraConnector(conf) {
+object EmbeddedCassandra {
 
   val deleteKeyspaceCql = "DROP KEYSPACE IF EXISTS test"
   val createKeyspaceCql = "CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 }"
-
-  // Recreate keyspace on creation to start with clean state
-  {
-    withSessionDo { session =>
-      session.execute(deleteKeyspaceCql)
-      session.execute(createKeyspaceCql)
-    }
-  }
-}
-
-object EmbeddedCassandraConnector {
-
-  def apply(conf: SparkConf): EmbeddedCassandraConnector = {
-    new EmbeddedCassandraConnector(CassandraConnectorConf(conf))
-  }
-
-  def apply(hosts: Set[InetAddress]) = {
-    val config = CassandraConnectorConf(hosts)
-    new EmbeddedCassandraConnector(config)
+  
+  // Recreate the keyspace with each session, and prevent premature closure of session.
+  def withSession(host: String, keySpace: String)(f: CassandraSession => Unit)(implicit sc: SparkContext): Unit = {
+    val sparkConf = sc.getConf
+    val hadoopConf = sc.hadoopConfiguration
+    sparkConf.set("spark.cassandra.connection.host", host)
+    val connector = CassandraConnector(sparkConf)
+    val session = new CassandraSession(connector, keySpace)
+    session.execute(deleteKeyspaceCql)
+    session.execute(createKeyspaceCql)
+    f(session)
   }
 }
