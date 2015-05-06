a766c8903064646465ee8ff099202498b24b9222
Eugene Cheipesh <echeipesh@gmail.com>
2015-05-06 10:13:11 -0400

    Upgrade accumulo dependency to 1.6.2 and update deprecated method calls

--- project/Build.scala
+++ project/Build.scala
@@ -371,10 +371,12 @@ object GeotrellisBuild extends Build {
       ),
       libraryDependencies ++=
         Seq(
+          "org.apache.accumulo" % "accumulo-core" % Version.accumulo 
+            exclude("org.jboss.netty", "netty")
+            exclude("org.apache.hadoop", "hadoop-client"),
           "org.apache.spark" %% "spark-core" % Version.spark % "provided",
           "org.apache.hadoop" % "hadoop-client" % Version.hadoop % "provided",
           "com.quantifind" %% "sumac" % "0.3.0",
-          "org.apache.accumulo" % "accumulo-core" % "1.5.2",
           "de.javakaffee" % "kryo-serializers" % "0.27",
           "com.google.uzaygezen" % "uzaygezen-core" % "0.2",
           logging, awsSdkS3,
--- spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloInstance.scala
+++ spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloInstance.scala
@@ -7,21 +7,20 @@ import org.apache.accumulo.core.client._
 import org.apache.accumulo.core.client.mapreduce.{InputFormatBase, AccumuloInputFormat, AccumuloOutputFormat}
 import org.apache.accumulo.core.client.mock.MockInstance
 import org.apache.accumulo.core.client.security.tokens.AuthenticationToken
-import org.apache.accumulo.core.client.mapreduce.lib.util.{ConfiguratorBase => CB}
+import org.apache.accumulo.core.client.mapreduce.{AbstractInputFormat => AIF, AccumuloOutputFormat => AOF}
 import org.apache.accumulo.core.data.{Value, Key, Mutation}
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.SparkContext
 import com.typesafe.config.{ConfigFactory,Config}
+import org.apache.accumulo.core.client.ClientConfiguration
+
 
 trait AccumuloInstance {
   def connector: Connector
   def instanceName: String
-
-  def setAccumuloConfig(job: Job): Unit = setAccumuloConfig(job.getConfiguration)
-  def setAccumuloConfig(sc: SparkContext): Unit = setAccumuloConfig(sc.hadoopConfiguration)
-  def setAccumuloConfig(conf: Configuration): Unit
+  def setAccumuloConfig(job: Job): Unit
 }
 
 object AccumuloInstance {
@@ -39,17 +38,24 @@ case class BaseAccumuloInstance(
   }
   val connector: Connector = instance.getConnector(user, token)
 
-  def setAccumuloConfig(conf: Configuration): Unit = {
+
+  def setAccumuloConfig(job: Job): Unit = {
+    val clientConfig = ClientConfiguration
+      .loadDefault()
+      .withZkHosts(zookeeper)
+      .withInstance(instanceName)
+
+    
     if (instanceName == "fake") {
-      CB.setMockInstance(classOf[AccumuloInputFormat], conf, instanceName)
-      CB.setMockInstance(classOf[AccumuloOutputFormat], conf, instanceName)
+      AIF.setMockInstance(job, instanceName)
+      AOF.setMockInstance(job, instanceName)
     }
     else {
-      CB.setZooKeeperInstance(classOf[AccumuloInputFormat],conf, instanceName, zookeeper)
-      CB.setZooKeeperInstance(classOf[AccumuloOutputFormat],conf, instanceName, zookeeper)
+      AIF.setZooKeeperInstance(job, clientConfig)
+      AOF.setZooKeeperInstance(job, clientConfig)
     }
 
-    CB.setConnectorInfo(classOf[AccumuloInputFormat], conf, user, token)
-    CB.setConnectorInfo(classOf[AccumuloOutputFormat], conf, user, token)
+    AIF.setConnectorInfo(job, user, token)
+    AOF.setConnectorInfo(job, user, token)
   }
 }
