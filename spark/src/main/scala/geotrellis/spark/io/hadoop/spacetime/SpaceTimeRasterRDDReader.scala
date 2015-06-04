d72fc021e49335270f4eebe774e80da71d8dbb48
Eugene Cheipesh <echeipesh@gmail.com>
2015-06-04 14:44:51 -0400

    fix: type-o

--- spark/src/main/scala/geotrellis/spark/io/hadoop/spacetime/SpaceTimeRasterRDDReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/hadoop/spacetime/SpaceTimeRasterRDDReader.scala
@@ -44,7 +44,7 @@ object SpaceTimeRasterRDDReader extends RasterRDDReader[SpaceTimeKey] with Loggi
           classOf[TileWritable]
         )
       } else {
-        val ranges = queryKeyBounds flatMap keyIndex.indexRanges(range)
+        val ranges = queryKeyBounds flatMap keyIndex.indexRanges
         inputConf.setSerialized (FilterMapFileInputFormat.FILTER_INFO_KEY,
           (queryKeyBounds, ranges.toArray))
 
