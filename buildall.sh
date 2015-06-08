0f8ac82fd72ad6b0b5b51155d3690ba3ec28b81d
Eugene Cheipesh <echeipesh@gmail.com>
2015-06-08 16:53:40 -0400

    listing test sbt project individually

--- buildall.sh
+++ buildall.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-projects=("index" "proj4" "vector-test" "raster-test" "geotools" "benchmark" "demo" "vector-benchmark" "engine-test" "spark" "dev" "services" "jetty" "admin" "slick" "gdal")
+./sbt -J-Xmx2G "project index" test 
+./sbt -J-Xmx2G "project proj4" test 
+./sbt -J-Xmx2G "project vector-test" test 
+./sbt -J-Xmx2G "project raster-test" test 
+./sbt -J-Xmx2G "project engine-test" test 
+./sbt -J-Xmx2G "project slick" test:compile 
+./sbt -J-Xmx2G "project gdal" test:compile
+./sbt -J-Xmx2G "project spark" package "project spark" test 
 
-for project in "${projects[@]}"; do
-  echo $project
-  ./sbt -J-Xmx2G "project $project" test 
-done
+./sbt -J-Xmx2G "project geotools" compile 
+./sbt -J-Xmx2G "project benchmark" compile 
+./sbt -J-Xmx2G "project demo" compile 
+./sbt -J-Xmx2G "project dev" compile 
+./sbt -J-Xmx2G "project services" compile 
+./sbt -J-Xmx2G "project jetty" compile 
+./sbt -J-Xmx2G "project admin" compile 
+./sbt -J-Xmx2G "project vector-benchmark" compile
