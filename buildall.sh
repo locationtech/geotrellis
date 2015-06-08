73bcce74574cdb045130b6682ba77cc062d2324b
Eugene Cheipesh <echeipesh@gmail.com>
2015-06-08 15:07:51 -0400

    fix: buildall.sh, building with a for loop to avoid Out of Memory issues

--- buildall.sh
+++ buildall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./sbt -J-Xmx2G  "project index" test "project proj4" test "project vector-test" test "project raster-test" test "project geotools" compile "project benchmark" compile "project demo" compile "project vector-benchmark" compile "project engine-test" test "project spark" package "project spark" test "project spark" test "project dev" compile "project services" compile "project jetty" compile "project admin" compile "project slick" test:compile "project gdal" test:compile
+projects=("index" "proj4" "vector-test" "raster-test" "geotools" "benchmark" "demo" "vector-benchmark" "engine-test" "spark" "dev" "services" "jetty" "admin" "slick" "gdal")
+
+for project in projects; do
+  ./sbt -J-Xmx2G "project $project" test 
+done
