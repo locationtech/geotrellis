4f2fbc91189517b98accb37766a9898f1eb8269c
Eugene Cheipesh <echeipesh@gmail.com>
2015-07-02 18:01:03 -0400

    fix: handling of empty rootPath in s3

--- spark/src/main/scala/geotrellis/spark/io/s3/S3AttributeStore.scala
+++ spark/src/main/scala/geotrellis/spark/io/s3/S3AttributeStore.scala
@@ -30,11 +30,13 @@ class S3AttributeStore(s3Client: S3Client, bucket: String, rootPath: String)
    * It could be remedied by some kind of time-out cache for both read/write in this class.
    */
 
+  def path(parts: String*) = parts.filter(_.nonEmpty).mkString("/")
+
   def attributePath(id: LayerId, attributeName: String): String =
-    s"$rootPath/_attributes/${attributeName}__${id.name}__${id.zoom}.json"
+    path(rootPath, "_attributes", "${attributeName}__${id.name}__${id.zoom}.json")
 
   def attributePrefix(attributeName: String): String =
-    s"$rootPath/_attributes/${attributeName}__"
+    path(rootPath, "_attributes", "${attributeName}__")
 
   private def readKey[T: ReadableWritable](key: String): Option[(LayerId, T)] = {
     val is = s3Client.getObject(bucket, key).getObjectContent()
