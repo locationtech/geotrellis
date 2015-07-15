937edcfd1cd318013fcc51d3fee16c153e4c3661
Eugene Cheipesh <echeipesh@gmail.com>
2015-07-15 12:32:30 -0400

    Revert "base64 encoding for KryoJsonFormat"
    This reverts commit b4ee5e170669643bb066a6fb70f789739595ef2e.

--- spark/src/main/scala/geotrellis/spark/io/json/KryoJsonFormat.scala
+++ spark/src/main/scala/geotrellis/spark/io/json/KryoJsonFormat.scala
@@ -3,27 +3,19 @@ package geotrellis.spark.io.json
 import geotrellis.spark.utils._
 
 import spray.json._
-import scala.reflect._
-import sun.misc.{BASE64Encoder, BASE64Decoder}
+import scala.reflect.ClassTag
 
 /** JsonFormat that uses Kryo to serialize the object and store it in JSON */
 class KryoJsonFormat[T: ClassTag] extends RootJsonFormat[T] {
 
- def write(value: T) =
-   JsObject(
-     "serialized" -> JsString(
-       new BASE64Encoder().encode(
-         KryoSerializer.serialize(value)
-       )
-     )
-   )
+  def write(value: T) =
+    JsObject(
+      "serialized" -> JsString(new String(KryoSerializer.serialize(value).map(_.toChar)))
+    )
 
   def read(value: JsValue): T =
     value.asJsObject.getFields("serialized") match {
-      case Seq(JsString(s)) =>
-        KryoSerializer.deserialize(
-          new BASE64Decoder().decodeBuffer(s)
-        )
+      case Seq(JsString(s)) => KryoSerializer.deserialize(s.toCharArray.map(_.toByte))
       case _ =>
         throw new DeserializationException("${classOf[T].getName} expected.")
     }
