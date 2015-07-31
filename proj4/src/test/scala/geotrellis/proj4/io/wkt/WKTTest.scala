22c60b8c21b9ceb3a5051fec9506ba8d425a68cf
Manuri <amaya.uom@gmail.com>
2015-07-31 23:39:04 +0530

    adding tests to CRSTest.scala

--- proj4/src/test/scala/geotrellis/proj4/io/wkt/WKTTest.scala
+++ proj4/src/test/scala/geotrellis/proj4/io/wkt/WKTTest.scala
@@ -1,6 +1,5 @@
 package geotrellis.proj4.io.wkt
 
-import geotrellis.proj4.CRS
 import org.scalatest.FunSpec
 
 /**
@@ -8,7 +7,6 @@ import org.scalatest.FunSpec
  */
 class WKTTest extends FunSpec {
   it("should return the WKTString corresponding to EPSG:3824") {
-    val crs = CRS.fromName("EPSG:3824")
     val wktString = WKT.fromEPSGCode(3824)
 
     assert(wktString == "GEOGCS[\"TWD97\", DATUM[\"Taiwan Datum 1997\", SPHEROID[\"GRS 1980\", 6378137.0, 298.257222101, AUTHORITY[\"EPSG\",\"7019\"]], TOWGS84[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], AUTHORITY[\"EPSG\",\"1026\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Geodetic longitude\", EAST], AXIS[\"Geodetic latitude\", NORTH], AUTHORITY[\"EPSG\",\"3824\"]]")
@@ -16,17 +14,16 @@ class WKTTest extends FunSpec {
   }
 
   it("should return the WKTString corresponding to EPSG:4326") {
-    val crs = CRS.fromName("EPSG:4326")
     val wktString = WKT.fromEPSGCode(4326)
 
     assert(wktString == "GEOGCS[\"WGS 84\", DATUM[\"World Geodetic System 1984\", SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Geodetic longitude\", EAST], AXIS[\"Geodetic latitude\", NORTH], AUTHORITY[\"EPSG\",\"4326\"]]")
   }
 
   it("should return the WKTString corresponding to EPSG:3857") {
-    val crs = CRS.fromName("EPSG:4326")
     val wktString = WKT.fromEPSGCode(3857)
 
-    assert(wktString == "PROJCS[\"WGS 84 / Pseudo-Mercator\", GEOGCS[\"WGS 84\", DATUM[\"World Geodetic System 1984\", SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Geodetic longitude\", EAST], AXIS[\"Geodetic latitude\", NORTH], AUTHORITY[\"EPSG\",\"4326\"]], PROJECTION[\"Popular Visualisation Pseudo Mercator\", AUTHORITY[\"EPSG\",\"1024\"]], PARAMETER[\"semi_minor\", 6378137.0], PARAMETER[\"latitude_of_origin\", 0.0], PARAMETER[\"central_meridian\", 0.0], PARAMETER[\"scale_factor\", 1.0], PARAMETER[\"false_easting\", 0.0], PARAMETER[\"false_northing\", 0.0], UNIT[\"m\", 1.0], AXIS[\"Easting\", EAST], AXIS[\"Northing\", NORTH], AUTHORITY[\"EPSG\",\"3857\"]]") }
+    assert(wktString == "PROJCS[\"WGS 84 / Pseudo-Mercator\", GEOGCS[\"WGS 84\", DATUM[\"World Geodetic System 1984\", SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]], PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Geodetic longitude\", EAST], AXIS[\"Geodetic latitude\", NORTH], AUTHORITY[\"EPSG\",\"4326\"]], PROJECTION[\"Popular Visualisation Pseudo Mercator\", AUTHORITY[\"EPSG\",\"1024\"]], PARAMETER[\"semi_minor\", 6378137.0], PARAMETER[\"latitude_of_origin\", 0.0], PARAMETER[\"central_meridian\", 0.0], PARAMETER[\"scale_factor\", 1.0], PARAMETER[\"false_easting\", 0.0], PARAMETER[\"false_northing\", 0.0], UNIT[\"m\", 1.0], AXIS[\"Easting\", EAST], AXIS[\"Northing\", NORTH], AUTHORITY[\"EPSG\",\"3857\"]]")
+  }
 
 
   it("should return the EPSG code(3824) of the passed WKT string") {
@@ -54,6 +51,4 @@ class WKTTest extends FunSpec {
     assert(comparisonCode == epsgCodeOfWKT)
 
   }
-
-
 }
