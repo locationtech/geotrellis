07c25dc2b55ea1350a10f59571ceca31f3a9c31f
Manuri <amaya.uom@gmail.com>
2015-07-31 23:41:46 +0530

    using java.util.concurrent.ConcurrentHashMap

--- proj4/src/main/scala/geotrellis/proj4/Memoize.scala
+++ proj4/src/main/scala/geotrellis/proj4/Memoize.scala
@@ -1,17 +1,22 @@
 package geotrellis.proj4
+
 import scala.collection.mutable
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author Manuri Perera
  */
 class Memoize[T, R](f: T => R) extends (T => R) {
-  val map: mutable.Map[T,R] = mutable.Map.empty[T,R]
+  //val map: mutable.Map[T, R] = mutable.Map.empty[T, R]
+  val map: ConcurrentHashMap[T,R] = new ConcurrentHashMap()
+
   def apply(x: T): R = {
     if (map.contains(x)) {
-      map(x)
+      map.get(x)
     }
     else {
       val y = f(x)
-      map += ((x, y))
+      map.put(x,y)
       y
     }
   }
