f9b05fd00cd75fb87a367cbcb5ca70af2e8a11b5
Anuj <apnoob.13@gmail.com>
2015-08-05 08:35:56 +0530

    Writing cloudless image - multiple modes

--- raster/src/main/scala/geotrellis/raster/imagery/CloudRemoval.scala
+++ raster/src/main/scala/geotrellis/raster/imagery/CloudRemoval.scala
@@ -10,19 +10,16 @@ import spire.syntax.cfor._
 
 object Imagery {
 
-  def cloudRemovalSingleBand(images: Seq[Tile]) : Tile = {
+  def cloudRemovalSingleBand(images: Array[Tile]) : Tile = {
     val dummyTile = images(0)
-    println(dummyTile.findMinMax)
+    //println(dummyTile.findMinMax)
     //println(dummyTile.get(510, 140))
-
-    val newTile = dummyTile.map((col, row, x) => images.minBy(y => y.get(col, row)).get(col, row))
-    //println(newTile.get(510, 140))
-    newTile
+    dummyTile.map((col, row, x) => images.minBy(y => y.get(col, row)).get(col, row))
   }
 
-  def cloudRemovalMultiBand(images: Seq[MultiBandTile]): MultiBandTile = {
+  def cloudRemovalMultiBand(images: Array[MultiBandTile]): MultiBandTile = {
 
-    val numBands = images.head.bandCount
+    val numBands = images(0).bandCount
     val numImages = images.length
 
     val cloudlessTiles = new Array[Tile](numBands)
@@ -41,7 +38,7 @@ object Imagery {
   def main(args: Array[String]) : Unit = {
     val dirRed = new File(args(0))
     val dirGreen = new File(args(1))
-    val dirBlue = new File(args(1))
+    val dirBlue = new File(args(2))
 
     val fileListRed = dirRed.listFiles.filter(_.isFile).toList.toArray
     val fileListGreen = dirGreen.listFiles.filter(_.isFile).toList.toArray
@@ -53,46 +50,52 @@ object Imagery {
     val crs = SingleBandGeoTiff(fileListRed(0).toString).crs
     val extent = SingleBandGeoTiff(fileListRed(0).toString).extent
 
-    cfor(1)(_ < numImages, _ + 1) { i =>
+    cfor(0)(_ < numImages, _ + 1) { i =>
       val red = SingleBandGeoTiff(fileListRed(i).toString).tile.rescale(0, 255).convert(TypeByte)
       val green = SingleBandGeoTiff(fileListGreen(i).toString).tile.rescale(0, 255).convert(TypeByte)
       val blue = SingleBandGeoTiff(fileListBlue(i).toString).tile.rescale(0, 255).convert(TypeByte)
 
+//      val red = SingleBandGeoTiff(fileListRed(i).toString).tile.rescale(0, 255)
+//      val green = SingleBandGeoTiff(fileListGreen(i).toString).tile.rescale(0, 255)
+//      val blue = SingleBandGeoTiff(fileListBlue(i).toString).tile.rescale(0, 255)
+
 //      val red = SingleBandGeoTiff(fileListRed(i).toString).tile
 //      val green = SingleBandGeoTiff(fileListGreen(i).toString).tile
 //      val blue = SingleBandGeoTiff(fileListBlue(i).toString).tile
-
       multiBands(i) = ArrayMultiBandTile(Array(red, green, blue))
-
-      val rgb = IntArrayTile(Array.ofDim[Int](512 * 512), 512, 512)
-
-      cfor(0)(_ < 512, _ + 1) { row =>
-        cfor(0)(_ < 512, _ + 1) { col =>
-          var v = 0
-          var i = 0
-          while (v == 0 && i < numImages) {
-            v = {
-              val r = red.get(col, row)
-              val g = green.get(col, row)
-              val b = blue.get(col, row)
-              if (r == 0 && g == 0 && b == 0) 0
-              else {
-                val cr = if (isNoData(r)) 128 else r.toByte & 0xFF
-                val cg = if (isNoData(g)) 128 else g.toByte & 0xFF
-                val cb = if (isNoData(b)) 128 else b.toByte & 0xFF
-
-                (cr << 24) | (cg << 16) | (cb << 8) | 0xFF
-              }
-            }
-            i += 1
-          }
-          rgb.set(col, row, v)
-        }
-      }
-      rgb.renderPng.write("/tmp/image" + i + ".png")
     }
-    //val cloudless = cloudRemovalMultiBand(multiBands)
-    //GeoTiffWriter.write(MultiBandGeoTiff(multiBands(i), extent, crs), "/tmp/image" + i + ".tif")
+
+    //println(multiBands(0).bandCount)
+    val cloudless = cloudRemovalMultiBand(multiBands)
+    val imRows = cloudless.rows
+    val imCols = cloudless.cols
+    val rgb = IntArrayTile(Array.ofDim[Int](imCols * imRows), imCols, imRows)
+
+    cfor(0)(_ < imRows, _ + 1) { row =>
+      cfor(0)(_ < imCols, _ + 1) { col =>
+        var v = 0
+        v = {
+            val r = cloudless.band(0).get(col, row)
+            val g = cloudless.band(1).get(col, row)
+            val b = cloudless.band(2).get(col, row)
+            if (r == 0 && g == 0 && b == 0) 0
+            else {
+              val cr = if (isNoData(r)) 128 else r.toByte & 0xFF
+              val cg = if (isNoData(g)) 128 else g.toByte & 0xFF
+              val cb = if (isNoData(b)) 128 else b.toByte & 0xFF
+
+              (cr << 24) | (cg << 16) | (cb << 8) | 0xFF
+            }
+          }
+        rgb.set(col, row, v)
+      }
+    }
+    rgb.renderPng.write("/tmp/cloudlessimagergb.png")
+
+    GeoTiffWriter.write(SingleBandGeoTiff(cloudless.band(0), extent, crs), "/tmp/cloudlessimagered.tif")
+    GeoTiffWriter.write(SingleBandGeoTiff(cloudless.band(1), extent, crs), "/tmp/cloudlessimagegreen.tif")
+    GeoTiffWriter.write(SingleBandGeoTiff(cloudless.band(2), extent, crs), "/tmp/cloudlessimageblue.tif")
+    GeoTiffWriter.write(MultiBandGeoTiff(cloudless, extent, crs), "/tmp/cloudlessimagergb.tif")
   }
 
   //      val cloudless = cloudRemovalMultiBand(multiBands)
