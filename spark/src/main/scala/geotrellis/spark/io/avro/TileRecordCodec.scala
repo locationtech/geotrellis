d8ce46c9f370068691796dfb53c307a8c7038106
Eugene Cheipesh <echeipesh@gmail.com>
2015-08-05 22:46:09 -0400

    moving out TileRecordCodec to a class

--- spark/src/main/scala/geotrellis/spark/io/avro/TileRecordCodec.scala
+++ spark/src/main/scala/geotrellis/spark/io/avro/TileRecordCodec.scala
@@ -1,8 +1,27 @@
 package geotrellis.spark.io.avro
 
-/**
- * Created by eugene on 8/5/15.
- */
-class TileRecordCodec {
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro._
+import scala.collection.JavaConverters._
 
+class TileRecordCodec[K, V](implicit a: AvroRecordCodec[K], b: AvroRecordCodec[V]) extends AvroRecordCodec[Vector[(K, V)]] {
+  val pairCodec = new TupleCodec[K,V]
+
+  def schema = SchemaBuilder
+    .record("TileRecord").namespace("geotrellis.spark.io")
+    .fields()
+    .name("pairs").`type`().array().items.`type`(pairCodec.schema).noDefault()
+    .endRecord()
+
+  def encode(t: Vector[(K, V)], rec: GenericRecord) = {
+    rec.put("pairs", t.map(pairCodec.encode).asJavaCollection)
+  }
+
+  def decode(rec: GenericRecord) = {
+    rec.get("pairs")
+      .asInstanceOf[java.util.Collection[GenericRecord]]
+      .asScala
+      .map(pairCodec.decode)
+      .toVector
+  }
 }
--- spark/src/main/scala/geotrellis/spark/io/avro/package.scala
+++ spark/src/main/scala/geotrellis/spark/io/avro/package.scala
@@ -2,36 +2,15 @@ package geotrellis.spark.io
 
 import geotrellis.raster.Tile
 import geotrellis.spark.io.avro.TileCodecs._
-import org.apache.avro.SchemaBuilder
-import org.apache.avro.generic.GenericRecord
-import scala.collection.JavaConverters._
 
 package object avro {
-  val tileUnionCodec = new AvroUnionCodec[Tile](ByteArrayTileCodec, FloatArrayTileCodec, DoubleArrayTileCodec, IntArrayTileCodec)
+  val tileUnionCodec = new AvroUnionCodec[Tile](
+    ByteArrayTileCodec,
+    FloatArrayTileCodec,
+    DoubleArrayTileCodec,
+    ShortArrayTileCodec,
+    IntArrayTileCodec)
 
   implicit def tupleCodec[A: AvroRecordCodec, B: AvroRecordCodec] = new TupleCodec[A, B]
-
-  def recordCodec[K, V](implicit keyCodec: AvroRecordCodec[K], valCodec: AvroRecordCodec[V]) = {
-    val pairCodec = tupleCodec[K,V]
-
-    new AvroRecordCodec[Vector[(K, V)]]() {
-      def schema = SchemaBuilder
-        .record("TileRecord").namespace("geotrellis.spark.io")
-        .fields()
-        .name("pairs").`type`().array().items.`type`(pairCodec.schema).noDefault()
-        .endRecord()
-
-      def encode(t: Vector[(K, V)], rec: GenericRecord) = {
-        rec.put("pairs", t.map(pairCodec.encode).asJavaCollection)
-      }
-
-      def decode(rec: GenericRecord) = {
-        rec.get("pairs")
-          .asInstanceOf[java.util.Collection[GenericRecord]]
-          .asScala
-          .map(pairCodec.decode)
-          .toVector
-      }
-    }
-  }
+  def recordCodec[K: AvroRecordCodec, V: AvroRecordCodec] = new TileRecordCodec[K, V]
 }
