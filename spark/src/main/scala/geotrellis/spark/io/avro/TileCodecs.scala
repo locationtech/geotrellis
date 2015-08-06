8a245542bac23c4e75a8848cc6194fd567a639e6
Eugene Cheipesh <echeipesh@gmail.com>
2015-08-05 22:45:39 -0400

    added ShortArrayTileCodec

--- spark/src/main/scala/geotrellis/spark/io/avro/TileCodecs.scala
+++ spark/src/main/scala/geotrellis/spark/io/avro/TileCodecs.scala
@@ -7,6 +7,30 @@ import scala.collection.JavaConverters._
 import java.nio.ByteBuffer
 
 object TileCodecs {
+  implicit object ShortArrayTileCodec extends AvroRecordCodec[ShortArrayTile] {
+    lazy val schema = SchemaBuilder
+      .record("ShortArrayTile").namespace("geotrellis.raster")
+      .fields()
+      .name("cols").`type`().intType().noDefault()
+      .name("rows").`type`().intType().noDefault()
+      .name("cells").`type`().array().items().intType().noDefault()
+      .endRecord()
+
+    def encode(tile: ShortArrayTile, rec: GenericRecord) = {
+      rec.put("cols", tile.cols)
+      rec.put("rows", tile.rows)
+      rec.put("cells", java.util.Arrays.asList(tile.array:_*))
+    }
+
+    def decode(rec: GenericRecord) = {
+      val array  = rec.get("cells")
+        .asInstanceOf[java.util.Collection[Int]]
+        .asScala // notice that Avro does not have native support for Short primitive
+        .map(_.toShort)
+        .toArray
+      new ShortArrayTile(array, rec[Int]("cols"), rec[Int]("rows"))
+    }
+  }
 
   implicit object IntArrayTileCodec extends AvroRecordCodec[IntArrayTile] {
     lazy val schema = SchemaBuilder
@@ -27,8 +51,8 @@ object TileCodecs {
       val array  = rec.get("cells").asInstanceOf[java.util.Collection[Int]].asScala.toArray[Int]
       new IntArrayTile(array, rec[Int]("cols"), rec[Int]("rows"))
     }
-
   }
+
   implicit object FloatArrayTileCodec extends AvroRecordCodec[FloatArrayTile] {
     lazy val schema = SchemaBuilder
       .record("FloatArrayTile").namespace("geotrellis.raster")
---
+++ spark/src/main/scala/geotrellis/spark/io/avro/TileRecordCodec.scala
@@ -0,0 +1,8 @@
+package geotrellis.spark.io.avro
+
+/**
+ * Created by eugene on 8/5/15.
+ */
+class TileRecordCodec {
+
+}
