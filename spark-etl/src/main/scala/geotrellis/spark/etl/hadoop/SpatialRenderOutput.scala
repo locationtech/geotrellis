c24e7e47be96bfa133d8dc51d0c4a102e5d1728f
Eugene Cheipesh <echeipesh@gmail.com>
2015-08-15 02:22:46 -0400

    adjust breaks format for ETL

--- spark-etl/src/main/scala/geotrellis/spark/etl/hadoop/SpatialRenderOutput.scala
+++ spark-etl/src/main/scala/geotrellis/spark/etl/hadoop/SpatialRenderOutput.scala
@@ -1,10 +1,13 @@
 package geotrellis.spark.etl.hadoop
 
+import java.math.BigInteger
+
 import geotrellis.raster.render.ColorBreaks
 import geotrellis.spark.etl.OutputPlugin
 import geotrellis.spark.io.index.KeyIndexMethod
 import geotrellis.spark.{SpatialKey, RasterRDD, LayerId}
 import geotrellis.spark.render._
+import org.apache.hadoop.conf.ConfServlet.BadFormatException
 import scala.reflect._
 
 
@@ -12,21 +15,26 @@ class SpatialRenderOutput extends OutputPlugin {
   def name = "render"
   def key = classTag[SpatialKey]
   def requiredKeys = Array("path", "format")
-  def attributes(props: Map[String, String]) = sys.error("Render plugins do not have AttributesStore")
+  def attributes(props: Map[String, String]) = null
 
   /**
    * Parses into ColorBreaks a string of limits and their colors in hex RGBA
    * Only used for rendering PNGs
    *
-   * @param breaks ex: "23:cc00ccff,30:aa00aaff,120:ff0000ff"
+   * @param breaks ex: "23:cc00ccff;30:aa00aaff;120->ff0000ff"
    * @return
    */
   def parseBreaks(breaks: Option[String]): Option[ColorBreaks] = {
     breaks.map { blob =>
-      val split = blob.split(",").map(_.trim.split(":"))
-      val limits = split.map( pair => Integer.parseInt(pair(0)))
-      val colors = split.map( pair => Integer.parseInt(pair(1), 16))
-      ColorBreaks(limits, colors)
+      try {
+        val split = blob.split(";").map(_.trim.split(":"))
+        val limits = split.map(pair => Integer.parseInt(pair(0)))
+        val colors = split.map(pair => new BigInteger(pair(1), 16).intValue())
+        ColorBreaks(limits, colors)
+      } catch {
+        case e: Exception =>
+          throw new BadFormatException(s"Unable to parse breaks, expected '{limit}:{RGBA};{limit}:{RGBA};...' got: '$blob'")
+      }
     }
   }
   
