f1fecc6ec9323a15591da9e2725ebbefc3a1f433
Eugene Cheipesh <echeipesh@gmail.com>
2015-09-03 16:49:58 -0400

    breaking out MultiBandIngest

---
+++ spark/src/main/scala/geotrellis/spark/ingest/MultiBandIngest.scala
@@ -0,0 +1,41 @@
+package geotrellis.spark.ingest
+
+import geotrellis.spark._
+import geotrellis.spark.tiling._
+import geotrellis.spark.reproject._
+import geotrellis.raster._
+import geotrellis.proj4._
+import org.apache.spark.rdd._
+import org.apache.spark.storage.StorageLevel
+import scala.reflect.ClassTag
+
+object MultiBandIngest{
+  def apply[T: IngestKey: ClassTag, K: SpatialComponent: ClassTag]
+  (sourceTiles: RDD[(T, MultiBandTile)], destCRS: CRS, layoutScheme: LayoutScheme, pyramid: Boolean = false, cacheLevel: StorageLevel = StorageLevel.NONE)
+    (sink: (MultiBandRasterRDD[K], Int) => Unit)
+    (implicit tiler: Tiler[T, K, MultiBandTile]): Unit =
+  {
+    val reprojectedTiles = sourceTiles.reproject(destCRS).cache()
+    val (zoom, rasterMetaData) =
+      RasterMetaData.fromRdd(reprojectedTiles, destCRS, layoutScheme)(_.projectedExtent.extent)
+    val tiledRdd = tiler(sourceTiles, rasterMetaData).cache()
+    val rasterRdd = new MultiBandRasterRDD(tiledRdd, rasterMetaData)
+
+    def buildPyramid(zoom: Int, rdd: MultiBandRasterRDD[K]): List[(Int, MultiBandRasterRDD[K])] = {
+      if (zoom > 1) {
+        rdd.persist(cacheLevel)
+        sink(rdd, zoom)
+        val pyramidLevel@(nextZoom, nextRdd) = Pyramid.up(rdd, layoutScheme, zoom)
+        pyramidLevel :: buildPyramid(nextZoom, nextRdd)
+      } else
+        List((zoom, rdd))
+    }
+
+    if (pyramid)
+      buildPyramid(zoom, rasterRdd)
+        .foreach { case (z, rdd) => rdd.unpersist(true) }
+    else
+      sink(rasterRdd, zoom)
+
+  }
+}
