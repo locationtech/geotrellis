8c1825f78a45c632cb997334f23bd736b700f89b
Eugene Cheipesh <echeipesh@gmail.com>
2015-09-03 14:29:22 -0400

    refactor Pyramid to abstract common `up` method
    MultiBand case may have different metadata type in the future

--- spark/src/main/scala/geotrellis/spark/ingest/Pyramid.scala
+++ spark/src/main/scala/geotrellis/spark/ingest/Pyramid.scala
@@ -10,55 +10,61 @@ import org.apache.spark.rdd._
 import scala.reflect.ClassTag
 
 object Pyramid extends Logging {
-  /**
-   * Functions that require RasterRDD to have a TMS grid dimension to their key
-   */
-  def up[K: SpatialComponent: ClassTag](rdd: RasterRDD[K], layoutScheme: LayoutScheme, zoom: Int): (Int, RasterRDD[K]) = {
-    val metaData = rdd.metaData
-    val LayoutLevel(nextZoom, nextLevel) = layoutScheme.zoomOut(LayoutLevel(zoom, rdd.metaData.layout))
-    val nextMetaData = 
-      RasterMetaData(
-        metaData.cellType,
-        nextLevel,
-        metaData.dataExtent,
-        metaData.crs
-      )
 
-    // Functions for combine step
-    def createTiles(tile: (K, Tile)): Seq[(K, Tile)] =
-      Seq(tile)
-
-    def mergeTiles1(tiles: Seq[(K, Tile)], tile: (K, Tile)): Seq[(K, Tile)] = 
-      tiles :+ tile
-
-    def mergeTiles2(tiles1: Seq[(K, Tile)], tiles2: Seq[(K, Tile)]): Seq[(K, Tile)] =
-      tiles1 ++ tiles2
+  def up[K: SpatialComponent: ClassTag, TileType : MergeView: CellGridPrototypeView](
+    rdd: RDD[(K, TileType)], 
+    sourceLayout: LayoutDefinition, 
+    targetLayout: LayoutDefinition
+  ): RDD[(K, TileType)] = {
   
-    val firstMap: RDD[(K, (K, Tile))] =
+    // Functions for combine step
+    def createTiles(tile: (K, TileType)): Seq[(K, TileType)]                                    = Seq(tile)
+    def mergeTiles1(tiles: Seq[(K, TileType)], tile: (K, TileType)): Seq[(K, TileType)]         = tiles :+ tile
+    def mergeTiles2(tiles1: Seq[(K, TileType)], tiles2: Seq[(K, TileType)]): Seq[(K, TileType)] = tiles1 ++ tiles2
+
+    val nextRdd =
       rdd
-        .map { case (key, tile: Tile) =>
-          val extent = metaData.mapTransform(key)
-          val newSpatialKey = nextMetaData.mapTransform(extent.center)
+        .map { case (key, tile) =>
+          val extent = sourceLayout.mapTransform(key)
+          val newSpatialKey = targetLayout.mapTransform(extent.center)
           (key.updateSpatialComponent(newSpatialKey), (key, tile))
-         }
-
-    val combined =
-      firstMap
+        }
         .combineByKey(createTiles, mergeTiles1, mergeTiles2)
-
-    val nextRdd: RDD[(K, Tile)] =
-        combined.map { case (newKey: K, seq: Seq[(K, Tile)]) =>
-          val newExtent = nextMetaData.mapTransform(newKey)
-          val newTile = ArrayTile.empty(nextMetaData.cellType, nextMetaData.tileLayout.tileCols, nextMetaData.tileLayout.tileRows)
+        .map { case (newKey: K, seq: Seq[(K, TileType)]) =>
+          val newExtent = targetLayout.mapTransform(newKey)
+          val newTile = seq.head._2.prototype(targetLayout.tileLayout.tileCols, targetLayout.tileLayout.tileRows)
 
           for( (oldKey, tile) <- seq) {
-            val oldExtent = metaData.mapTransform(oldKey)
+            val oldExtent = sourceLayout.mapTransform(oldKey)
             newTile.merge(newExtent, oldExtent, tile)
           }
-
-          (newKey, newTile: Tile)
+          (newKey, newTile: TileType)
         }
 
+    nextRdd
+  }
+
+  def up[K: SpatialComponent: ClassTag](rdd: RasterRDD[K], layoutScheme: LayoutScheme, zoom: Int): (Int, RasterRDD[K]) = {
+    val LayoutLevel(nextZoom, nextLayout) = layoutScheme.zoomOut(LayoutLevel(zoom, rdd.metaData.layout))
+    val nextMetaData = RasterMetaData(
+      rdd.metaData.cellType,
+      nextLayout,
+      rdd.metaData.dataExtent,
+      rdd.metaData.crs
+    )
+    val nextRdd = up(rdd, rdd.metaData.layout, nextLayout)
     nextZoom -> new RasterRDD(nextRdd, nextMetaData)
+  }
+
+  def up[K: SpatialComponent: ClassTag](rdd: MultiBandRasterRDD[K], layoutScheme: LayoutScheme, zoom: Int): (Int, MultiBandRasterRDD[K]) = {
+    val LayoutLevel(nextZoom, nextLayout) = layoutScheme.zoomOut(LayoutLevel(zoom, rdd.metaData.layout))
+    val nextMetaData = RasterMetaData(
+      rdd.metaData.cellType,
+      nextLayout,
+      rdd.metaData.dataExtent,
+      rdd.metaData.crs
+    )
+    val nextRdd = up(rdd, rdd.metaData.layout, nextLayout)
+    nextZoom -> new MultiBandRasterRDD(nextRdd, nextMetaData)
   }
 }
