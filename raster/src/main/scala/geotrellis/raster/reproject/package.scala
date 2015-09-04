2b6e3bc3de3e650a68871705e0e81c49682ea816
Eugene Cheipesh <echeipesh@gmail.com>
2015-09-04 10:56:47 -0400

    created ReprojectView type class

--- raster/src/main/scala/geotrellis/raster/reproject/package.scala
+++ raster/src/main/scala/geotrellis/raster/reproject/package.scala
@@ -1,36 +1,28 @@
 package geotrellis.raster
 
-import geotrellis.raster.resample._
 import geotrellis.vector.Extent
-import geotrellis.vector.reproject._
 import geotrellis.proj4._
-
-import spire.syntax.cfor._
 
 package object reproject {
   // Function that takes in coordinates in the first to arrays and fills out
   // the second two arrays with transformed coordinates (srcX, srcY, dstX, dstY)
   type RowTransform = (Array[Double], Array[Double], Array[Double], Array[Double]) => Unit
 
-  implicit class ReprojectExtension(val tile: Tile) {
-    def reproject(extent: Extent, src: CRS, dest: CRS): Raster =
-      reproject(extent, src, dest, ReprojectOptions.DEFAULT)
+  type ReprojectView[TileType] = TileType => ReprojectMethods[TileType]
 
-    def reproject(method: ResampleMethod, extent: Extent, src: CRS, dest: CRS): Raster =
-      reproject(extent, src, dest, ReprojectOptions(method = method))
+  implicit class withTileReprojectMethods(val tile: Tile) extends ReprojectMethods[Tile] {
+    type ReturnType = Raster
 
-    def reproject(extent: Extent, src: CRS, dest: CRS, options: ReprojectOptions): Raster =
+    def reproject(extent: Extent, src: CRS, dest: CRS, options: ReprojectOptions): Raster = {
       Reproject(tile, extent, src, dest, options)
+    }
   }
 
-  implicit class ReprojectMultiBandExtension(val tile: MultiBandTile) {
-    def reproject(extent: Extent, src: CRS, dest: CRS): MultiBandRaster =
-      reproject(extent, src, dest, ReprojectOptions.DEFAULT)
+  implicit class withMultiBandTileReprojectMethods(val tile: MultiBandTile) extends ReprojectMethods[MultiBandTile] {
+    type ReturnType = MultiBandRaster
 
-    def reproject(method: ResampleMethod, extent: Extent, src: CRS, dest: CRS): MultiBandRaster =
-      reproject(extent, src, dest, ReprojectOptions(method = method))
-
-    def reproject(extent: Extent, src: CRS, dest: CRS, options: ReprojectOptions): MultiBandRaster =
+    def reproject(extent: Extent, src: CRS, dest: CRS, options: ReprojectOptions): MultiBandRaster = {
       Reproject(tile, extent, src, dest, options)
+    }
   }
 }
