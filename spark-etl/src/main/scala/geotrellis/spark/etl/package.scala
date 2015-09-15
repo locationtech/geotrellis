8404f1e9b9944bc174c6c1312c865cb633d300f9
Eugene Cheipesh <echeipesh@gmail.com>
2015-09-15 12:33:53 -0400

    tile layout scheme is command line argument in ETL

--- spark-etl/src/main/scala/geotrellis/spark/etl/package.scala
+++ spark-etl/src/main/scala/geotrellis/spark/etl/package.scala
@@ -1,5 +1,8 @@
 package geotrellis.spark
 
+import geotrellis.proj4.CRS
+import geotrellis.spark.tiling.LayoutScheme
+
 package object etl {
   private [etl] def requireKeys(name: String, props: Map[String, String], keys: Seq[String]) = {
     val missing = keys
@@ -7,4 +10,6 @@ package object etl {
       .mkString(", ")
     require(missing.isEmpty, s"$name module missing required settings: $missing")
   }
+
+  type LayoutSchemeProvider = (CRS, Int) => LayoutScheme
 }
