7dadf7aac0a70411038847e69a165e956bae4053
Eugene Cheipesh <echeipesh@gmail.com>
2015-09-25 18:41:53 -0400

    abstract hadoop tile reader
    not performance regression, but not optimal either

--- spark/src/main/scala/geotrellis/spark/io/hadoop/HadoopTileReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/hadoop/HadoopTileReader.scala
@@ -6,7 +6,6 @@ import geotrellis.spark.io._
 import geotrellis.spark.io.index.KeyIndex
 import geotrellis.spark.io.json._
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.MapFile
 import org.apache.spark.SparkContext
 import spray.json.JsonFormat
 import scala.reflect.ClassTag
@@ -14,28 +13,33 @@ import scala.reflect.ClassTag
 class HadoopTileReader[K: JsonFormat: ClassTag, V](
   val attributeStore: HadoopAttributeStore,
   layerId: LayerId)
-(implicit format: HadoopFormat[K, V]) extends Reader[K, V] {
+(implicit format: HadoopFormat[K, V], sc: SparkContext) extends Reader[K, V] {
 
   val catalogConfig = HadoopCatalogConfig.DEFAULT
   val conf = attributeStore.hadoopConfiguration
   val layerMetaData  = attributeStore.cacheRead[HadoopLayerMetaData](layerId, Fields.layerMetaData)
   val keyIndex  = attributeStore.cacheRead[KeyIndex[K]](layerId, Fields.keyIndex)
 
-
-  val mapFilePath: Path = layerMetaData.path.suffix(catalogConfig.SEQFILE_GLOB).getParent
-  val reader = new MapFile.Reader(mapFilePath, conf)
+  val dataPath = layerMetaData.path.suffix(catalogConfig.SEQFILE_GLOB)
+  val inputConf = conf.withInputPath(dataPath)
 
   def read(key: K): V = {
-    val kw = format.kClass.newInstance()
-    val vw = format.vClass.newInstance()
-    kw.set(keyIndex.toIndex(key), key)
-    reader.get(kw, vw)
-    vw.get()
+    // TODO: There must be a way to do this through Readers, which must be faster
+    val keyBounds = KeyBounds[K](key, key)
+    val filterDefinition = (Seq(keyBounds), keyIndex.indexRanges(keyBounds).toArray)
+    inputConf.setSerialized(FilterMapFileInputFormat.FILTER_INFO_KEY, filterDefinition)
+
+    sc.newAPIHadoopRDD(
+      inputConf,
+      format.filteredInputFormatClass,
+      format.kClass,
+      format.vClass
+    ).first._2.get()
   }
 }
 
 object HadoopTileReader {
-  def apply[K: JsonFormat: ClassTag, V](attributeStore: HadoopAttributeStore, id: LayerId)(implicit format: HadoopFormat[K, V]): HadoopTileReader[K, V] =
+  def apply[K: JsonFormat: ClassTag, V](attributeStore: HadoopAttributeStore, id: LayerId)(implicit format: HadoopFormat[K, V], sc: SparkContext): HadoopTileReader[K, V] =
     new HadoopTileReader[K, V](attributeStore, id)
 
   def apply[K: JsonFormat: ClassTag, V](rootPath: Path, id: LayerId)(implicit format: HadoopFormat[K, V], sc: SparkContext): HadoopTileReader[K, V] =
