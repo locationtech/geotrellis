43950bb1d1b1612ec6781edad849e6272a3d4560
Eugene Cheipesh <echeipesh@gmail.com>
2015-10-02 16:46:16 -0400

    fix: token serialization into bytes
    Keeping AccumuloInstance serializable at expense of funky signature

--- spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloInstance.scala
+++ spark/src/main/scala/geotrellis/spark/io/accumulo/AccumuloInstance.scala
@@ -1,18 +1,10 @@
 package geotrellis.spark.io.accumulo
 
-import geotrellis.spark._
-import geotrellis.spark.tiling._
 import org.apache.accumulo.core.client._
-import org.apache.accumulo.core.client.mapreduce.{InputFormatBase, AccumuloInputFormat, AccumuloOutputFormat}
 import org.apache.accumulo.core.client.mock.MockInstance
 import org.apache.accumulo.core.client.security.tokens.AuthenticationToken
 import org.apache.accumulo.core.client.mapreduce.{AbstractInputFormat => AIF, AccumuloOutputFormat => AOF}
-import org.apache.accumulo.core.data.{Value, Key, Mutation}
-import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapreduce.Job
-import org.apache.hadoop.conf.Configuration
-import org.apache.spark.SparkContext
-import com.typesafe.config.{ConfigFactory,Config}
 import org.apache.accumulo.core.client.ClientConfiguration
 
 
@@ -25,15 +17,16 @@ trait AccumuloInstance  extends Serializable {
 object AccumuloInstance {
   def apply(instanceName: String, zookeeper: String, user: String, token: AuthenticationToken): AccumuloInstance = {
     val tokenBytes = AuthenticationToken.AuthenticationTokenSerializer.serialize(token)
-    BaseAccumuloInstance(instanceName, zookeeper, user, tokenBytes)
+    val tokenClass = token.getClass.getCanonicalName
+    BaseAccumuloInstance(instanceName, zookeeper, user, (tokenClass, tokenBytes))
   }
 }
 
 case class BaseAccumuloInstance(
   instanceName: String, zookeeper: String,
-  user: String, tokenBytes: Array[Byte]) extends AccumuloInstance
+  user: String, tokenBytes: (String, Array[Byte])) extends AccumuloInstance
 {
-  @transient lazy val token = AuthenticationToken.AuthenticationTokenSerializer.deserialize("AuthenticationToken", tokenBytes)
+  @transient lazy val token = AuthenticationToken.AuthenticationTokenSerializer.deserialize(tokenBytes._1, tokenBytes._2)
   @transient lazy val instance: Instance = instanceName match {
     case "fake" => new MockInstance("fake") //in-memory only
     case _      => new ZooKeeperInstance(instanceName, zookeeper)
