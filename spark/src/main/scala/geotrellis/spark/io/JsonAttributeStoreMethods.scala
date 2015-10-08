4cb28f784f8f69b2f1dfc298d710449d844b4594
Eugene Cheipesh <echeipesh@gmail.com>
2015-10-08 10:44:19 -0400

    added Warning about attribute migration

--- spark/src/main/scala/geotrellis/spark/io/JsonAttributeStoreMethods.scala
+++ spark/src/main/scala/geotrellis/spark/io/JsonAttributeStoreMethods.scala
@@ -6,6 +6,7 @@ import spray.json.DefaultJsonProtocol._
 import scala.util.Try
 import AttributeStore.Fields
 
+
 /**
  * Internally we are only using AttributeStore[JsonFormat].
  * Knowing the format allows us to abstract over writing and reading layer attributes. Layer attributes are almost
@@ -13,6 +14,11 @@ import AttributeStore.Fields
  *
  * We'd like to be flexible on reading attributes, attempting several fail-overs to account for changes and we'd
  * like to consolidate the writing logic such that attribute stores are as consistent as possible.
+ *
+ * WARNING:
+ * The fail-over feature is a stop-gap until 0.10 release at which point it will be removed.
+ * At that point this class can be used to read all layer attributes for a catalog and write them back,
+ * performing an effective migration.
  */
 class JsonAttributeStoreMethods(attributeStore: AttributeStore[JsonFormat]) {
 
