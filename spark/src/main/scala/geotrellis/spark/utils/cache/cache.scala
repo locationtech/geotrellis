e975f225108f44e506e210dbf374270c7f279a28
Eugene Cheipesh <echeipesh@gmail.com>
2015-10-26 17:14:38 -0400

    removing some cache traits
    cache should be typed on K and V, this untyped version does not appear
    useful or used

--- spark/src/main/scala/geotrellis/spark/utils/cache/cache.scala
+++ spark/src/main/scala/geotrellis/spark/utils/cache/cache.scala
@@ -22,58 +22,6 @@ import scala.collection.mutable.HashMap
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable
 
-/**
- * Trait for a T-keyed, any valued cache.
- */
-trait Cache[T] extends Serializable {
-  /** Lookup the value for key k
-   * @return Some(v) if the value was cached, None otherwise
-   */
-  def lookup[V](k: T):Option[V]
-
-  /** Insert the value v (keyed by k) into the cache
-   * @return true if the value was inserted into the cache
-   */
-  def insert[V](k: T, v: V):Boolean
-
-  /** Remove k from the cache */
-  def remove[V](k: T):Option[V]
-
-  /** Lookup the value for key k
-   * If the value is in the cache, return it.
-   * Otherwise, insert the value v and return it
-   *
-   * Note: This method does not guarantee that v will be in the cache when it returns
-   *
-   * @return the cached value keyed to k or the computed value of v
-   */
-  def getOrInsert[V](k: T, vv: => V):V = lookup(k) match {
-    case Some(v) => v
-    case None => {insert(k, vv); vv}
-  }
-}
-
-/**
- * Simple HashMap backed cache keyed by String and can hold any type.
- */
-class HashCache[T] extends Cache[T] {
-  val cache = new mutable.HashMap[T,Any].empty
-
-  def lookup[V](k: T):Option[V] =
-    cache.get(k) match {
-      case Some(v) => Some(v.asInstanceOf[V])
-      case None => None
-    }
-
-  def remove[V](k: T):Option[V] =
-    cache.remove(k) match {
-      case Some(v) => Some(v.asInstanceOf[V])
-      case None => None
-    }
-
-  def insert[V](k: T, v: V):Boolean = { cache.put(k,v.asInstanceOf[Any]); true }
-}
-
 /** Base trait for a caching strategy
  *
  * K is the cache key
