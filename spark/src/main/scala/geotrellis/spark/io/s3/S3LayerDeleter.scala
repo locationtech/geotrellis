5dafd087a802f4d30e7df069b6c1ccbd70d795aa
Grigory Pomadchin <gr.pomadchin@gmail.com>
2015-12-09 02:18:35 +0300

    weird S3 deleteListing fix

--- spark/src/main/scala/geotrellis/spark/io/s3/S3LayerDeleter.scala
+++ spark/src/main/scala/geotrellis/spark/io/s3/S3LayerDeleter.scala
@@ -1,17 +1,19 @@
 package geotrellis.spark.io.s3
 
+import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion
 import com.amazonaws.services.s3.model.ObjectListing
 import geotrellis.spark.LayerId
 import geotrellis.spark.io._
 import spray.json.JsonFormat
 import spray.json.DefaultJsonProtocol._
+import scala.collection.JavaConversions._
 
 class S3LayerDeleter(attributeStore: AttributeStore[JsonFormat]) extends LayerDeleter[LayerId] {
 
   def getS3Client: () => S3Client = () => S3Client.default
 
   def deleteListing(s3Client: S3Client, bucket: String, listing: ObjectListing): Unit = {
-    s3Client
+    s3Client.deleteObjects(bucket, listing.getObjectSummaries.map { os => new KeyVersion(os.getKey) }.toList)
     if (listing.isTruncated) deleteListing(s3Client, bucket, s3Client.listNextBatchOfObjects(listing))
   }
 
