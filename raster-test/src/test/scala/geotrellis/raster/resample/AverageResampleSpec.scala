2089629e1efb86d1ef7800e2a92c7315cc301ec0
Nathan Zimmerman <npzimmerman@gmail.com>
2015-12-16 12:50:26 -0500

    Fix average tests

--- raster-test/src/test/scala/geotrellis/raster/resample/AverageResampleSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/resample/AverageResampleSpec.scala
@@ -6,7 +6,7 @@ import geotrellis.vector.Extent
 import org.scalatest._
 
 class AverageResampleSpec extends FunSpec with Matchers {
-/*
+
   describe("it should resample to nodata when only nodata in tile") {
 
     it("should, for an integer tile, compute nodata as average") {
@@ -15,9 +15,10 @@ class AverageResampleSpec extends FunSpec with Matchers {
 
       val tile = ArrayTile(Array.fill(cols * rows)(NODATA), cols, rows)
       val extent = Extent(0, 0, cols, rows)
-      val resamp = new AverageResample(tile, extent)
+      val cellsize = CellSize(extent, cols, rows)
+      val resamp = new AverageResample(tile, extent, cellsize)
       for (i <- 0 until cols * 10; j <- 0 until rows * 10)
-        resamp.resample(i / 10.0, j / 10.0) should be (NODATA)
+        resamp.resampleValid(i / 10.0, j / 10.0) should be (NODATA)
     }
 
     it("should, for a double tile, compute nodata as average") {
@@ -26,19 +27,20 @@ class AverageResampleSpec extends FunSpec with Matchers {
 
       val tile = ArrayTile(Array.fill(cols * rows)(Double.NaN), cols, rows)
       val extent = Extent(0, 0, cols, rows)
-      val resamp = new AverageResample(tile, extent)
+      val cellsize = CellSize(extent, cols, rows)
+      val resamp = new AverageResample(tile, extent, cellsize)
       for (i <- 0 until cols * 10; j <- 0 until rows * 10)
-        assert(resamp.resampleDouble(i / 10.0, j / 10.0).isNaN)
+        assert(resamp.resampleDoubleValid(i / 10.0, j / 10.0).isNaN)
     }
 
   }
-*/
-  describe("it should correctly resample to the average") {
 
-    it("should gather all and only the relevant coordinates") {
+  describe("it should correctly resample to the average of a region determined by cellsize") {
+
+    it("should gather all and only the relevant coordinates and correctly resample to their averages for int based tiles") {
       val cols = 4
       val rows = 2
-      val tile = ArrayTile(
+      val tile = IntArrayTile(
         Array(1, 3, 6, 8,
               2, 4, 6, 8),
         cols, rows)
@@ -47,82 +49,50 @@ class AverageResampleSpec extends FunSpec with Matchers {
       val cellsize = CellSize(extent, 2, 1)
       val resamp = new AverageResample(tile, extent, cellsize) {}
       resamp.contributions(0.0, 0.0) should be (Vector((0,0), (0,1), (1,0), (1,1)))
+      resamp.resampleValid(0.0, 0.0) should be (2)  // 10/4 is 2 when rounded to int
+
+      resamp.contributions(3.0, 1.0) should be (Vector((2,0), (2,1), (3,0), (3,1)))
+      resamp.resampleValid(3.0, 1.0) should be (7)  // 28/4 is 7 when rounded to int
+
+      resamp.contributions(2.0, 1.0) should be (Vector((1,0), (1,1), (2,0), (2,1), (3,0), (3,1)))
+      resamp.resampleValid(2.0, 1.0) should be (5)  // 35/6 is 2 when rounded to int
     }
 
-    it("should resample to averages for a region") {
+    it("should gather all and only the relevant coordinates and correctly resample to their averages for float based tiles") {
       val cols = 4
       val rows = 2
-      val tile = ArrayTile(
-        Array(1, 3, 6, 8,
-              2, 4, 6, 8),
+      val tile = DoubleArrayTile(
+        Array(1.0, 3.0, 6.0, 8.0,
+              2.0, 4.0, 6.0, 8.0),
         cols, rows)
 
       val extent = Extent(0, 0, cols, rows)
       val cellsize = CellSize(extent, 2, 1)
       val resamp = new AverageResample(tile, extent, cellsize) {}
-      println(resamp.contributions(0, 0.5), resamp.contributions(2, 0.5))
-      println(resamp.resample(0, 0.5), resamp.resample(2, 0.5))
-      println(tile.resample(extent, Extent(0, 0, 2, 1), Average).asciiDraw)
       resamp.contributions(0.0, 0.0) should be (Vector((0,0), (0,1), (1,0), (1,1)))
+      resamp.resampleDoubleValid(0.0, 0.0) should be (2.5)  // 10/4 is 2.5
+
+      resamp.contributions(3.0, 1.0) should be (Vector((2,0), (2,1), (3,0), (3,1)))
+      resamp.resampleDoubleValid(3.0, 1.0) should be (7)  // 28/4 is 7
+
+      resamp.contributions(2.0, 1.0) should be (Vector((1,0), (1,1), (2,0), (2,1), (3,0), (3,1)))
+      resamp.resampleDoubleValid(2.0, 1.0) should be (5.83 +- 0.01)  // 35/6 is roughly 5.83
     }
 
-    it("should for a integer tile with all values same compute the correct average") {
-      val cols = 4
-      val rows = 1
-      val tile = ArrayTile(
-        Array(1, 3, 6, 8),
-        cols, rows)
+    it("should return the same tile if not resized") {
+      val cols = 100
+      val rows = 100
 
+      val n = cols * rows
+      val arr = List.range(1, n + 1).toArray
+
+      val tile = ArrayTile(arr, cols, rows)
       val extent = Extent(0, 0, cols, rows)
-      val cellsize = CellSize(extent, 2, 1)
+      val cellsize = CellSize(extent, cols, rows)
       val resamp = new AverageResample(tile, extent, cellsize)
-      resamp.resample(0.0, 0.0) should be (2)
-    }
-/*
-    it("should for a double tile with all values same compute the correct average") {
-      val cols = 100
-      val rows = 100
-      val avg = Double.MaxValue / 2
-
-      val tile = ArrayTile(Array.fill(cols * rows)(avg), cols, rows)
-      val extent = Extent(0, 0, cols, rows)
-      val resamp = new AverageResample(tile, extent)
-      for (i <- 0 until cols * 10; j <- 0 until rows * 10)
-        resamp.resampleDouble(i / 10.0, j / 10.0) should be (avg)
+      for (i <- 0 until cols; j <- 0 until rows)
+        resamp.resample(i, j) should be (tile.get(i, j))
     }
 
-    it("should for a integer tile with different values same compute the correct average") {
-      val cols = 100
-      val rows = 100
-
-      val n = cols * rows
-      val arr = List.range(1, n + 1).toArray
-
-      val avg = ((n + 1) / 2.0).round.toInt
-
-      val tile = ArrayTile(arr, cols, rows)
-      val extent = Extent(0, 0, cols, rows)
-      val resamp = new AverageResample(tile, extent)
-      for (i <- 0 until cols * 10; j <- 0 until rows * 10)
-        resamp.resample(i / 10.0, j / 10.0) should be (avg)
-    }
-
-    it("should for a double tile with different values same compute the correct average") {
-      val cols = 100
-      val rows = 100
-
-      val n = cols * rows
-      val arr = List.range(1, n + 1).toArray
-
-      val avg = (n + 1) / 2.0
-
-      val tile = ArrayTile(arr, cols, rows)
-      val extent = Extent(0, 0, cols, rows)
-      val resamp = new AverageResample(tile, extent)
-      for (i <- 0 until cols * 10; j <- 0 until rows * 10)
-        resamp.resampleDouble(i / 10.0, j / 10.0) should be (avg)
-    }
-
-*/
   }
 }
