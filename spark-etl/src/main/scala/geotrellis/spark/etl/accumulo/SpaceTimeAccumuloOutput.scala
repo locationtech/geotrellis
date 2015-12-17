5ae8ed08f181f94c175d72d2be58d24564e0bdea
lossyrob <rdemanuele@gmail.com>
2015-12-17 01:42:39 -0500

    Fixed spark-etl, include in buildall.sh

--- spark-etl/src/main/scala/geotrellis/spark/etl/accumulo/SpaceTimeAccumuloOutput.scala
+++ spark-etl/src/main/scala/geotrellis/spark/etl/accumulo/SpaceTimeAccumuloOutput.scala
@@ -5,8 +5,9 @@ import geotrellis.spark._
 import geotrellis.spark.io.avro.codecs._
 import geotrellis.spark.io.accumulo.AccumuloLayerWriter
 import geotrellis.spark.io.index.KeyIndexMethod
+import geotrellis.spark.io.json._
 
 class SpaceTimeAccumuloOutput extends AccumuloOutput[SpaceTimeKey] {
   def writer(method: KeyIndexMethod[SpaceTimeKey], props: Parameters) =
-    AccumuloLayerWriter[SpaceTimeKey, Tile, RasterRDD](getInstance(props),  props("table"), method)
+    AccumuloLayerWriter[SpaceTimeKey, Tile, RasterMetaData, RasterRDD[SpaceTimeKey]](getInstance(props),  props("table"), method)
 }
