04d79b96caaed45a30cc51dd1a31a0c98cb82fa8
Eugene Cheipesh <echeipesh@gmail.com>
2015-12-30 09:44:09 -0500

    RasterRDDMethods with Serializable

--- spark/src/main/scala/geotrellis/spark/BaseMultiBandRasterRDDMethods.scala
+++ spark/src/main/scala/geotrellis/spark/BaseMultiBandRasterRDDMethods.scala
@@ -3,7 +3,7 @@ package geotrellis.spark
 import geotrellis.raster._
 import org.apache.spark.rdd._
 
-trait BaseMultiBandRasterRDDMethods[K] extends MultiBandRasterRDDMethods[K] {
+trait BaseMultiBandRasterRDDMethods[K] extends MultiBandRasterRDDMethods[K] with Serializable {
   def convert(cellType: CellType): MultiBandRasterRDD[K] =
     rdd.mapValues(_.convert(cellType), rdd.metadata.copy(cellType = cellType))
 }
--- spark/src/main/scala/geotrellis/spark/BaseRasterRDDMethods.scala
+++ spark/src/main/scala/geotrellis/spark/BaseRasterRDDMethods.scala
@@ -3,7 +3,7 @@ package geotrellis.spark
 import geotrellis.raster._
 import org.apache.spark.rdd._
 
-trait BaseRasterRDDMethods[K] extends RasterRDDMethods[K] {
+trait BaseRasterRDDMethods[K] extends RasterRDDMethods[K] with Serializable {
   def convert(cellType: CellType): RasterRDD[K] =
     rasterRDD.mapValues(_.convert(cellType), rasterRDD.metadata.copy(cellType = cellType))
 
