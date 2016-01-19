bba1273ad845431e63c8a7a375ef607646867798
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-01-19 12:56:14 +0300

    Writer extends Function3

--- spark/src/main/scala/geotrellis/spark/io/Writer.scala
+++ spark/src/main/scala/geotrellis/spark/io/Writer.scala
@@ -3,7 +3,8 @@ package geotrellis.spark.io
 import geotrellis.spark.io.index.{KeyIndexMethod, KeyIndex}
 import spray.json.JsonFormat
 
-trait Writer[K, J, V] {
+trait Writer[K, J, V] extends ((K, V, KeyIndexMethod[J]) => Unit) {
   def write(key: K, value: V, keyIndexMethod: KeyIndexMethod[J]): Unit
   def write[I <: KeyIndex[J]: JsonFormat](key: K, value: V, keyIndex: I): Unit
+  def apply(key: K, value: V, keyIndexMethod: KeyIndexMethod[J]): Unit = write(key, value, keyIndexMethod)
 }
