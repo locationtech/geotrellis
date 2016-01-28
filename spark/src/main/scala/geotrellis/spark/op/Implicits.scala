bddb6495d180a5e68940907073045a24ea2f297f
Eugene Cheipesh <echeipesh@gmail.com>
2016-01-28 08:49:24 -0500

    add .combineValues and .updateValues

--- spark/src/main/scala/geotrellis/spark/op/Implicits.scala
+++ spark/src/main/scala/geotrellis/spark/op/Implicits.scala
@@ -1,6 +1,7 @@
 package geotrellis.spark.op
 
 import org.apache.spark.rdd.RDD
+import geotrellis.raster._
 import scala.reflect.ClassTag
 
 object Implicits extends Implicits
@@ -12,5 +13,22 @@ trait Implicits {
   implicit class withCombineTraversableMethods[K: ClassTag, V: ClassTag](rs: Traversable[RDD[(K, V)]]) {
     def combineValues[R: ClassTag](f: Traversable[V] => R): RDD[(K, R)] =
       rs.head.combineValues(rs.tail)(f)
+  }
+
+  implicit class withMapValuesTupleMethods[K: ClassTag, V: ClassTag](val self: RDD[(K, (V, V))]) extends MethodExtensions[RDD[(K, (V, V))]] {
+    // TODO these needs names
+    def combineValues[R: ClassTag](f: (V, V) => R): RDD[(K, R)] =
+      self.mapValues { case (v1, v2) => f(v1, v2) }
+  }
+
+  implicit class withMapValuesOptionMethods[K: ClassTag, V: ClassTag](val self: RDD[(K, (V, Option[V]))]) extends MethodExtensions[RDD[(K, (V, Option[V]))]] {
+    def updateValues(f: (V, V) => V): RDD[(K, V)] =
+      self.mapValues { case (v1, ov2) => 
+        ov2 match {
+          case Some(v2) => f(v1, v2)
+          case None => v1
+        }
+      //ov2.fold(v1)(f(v1, _)) }
+    }
   }
 }
