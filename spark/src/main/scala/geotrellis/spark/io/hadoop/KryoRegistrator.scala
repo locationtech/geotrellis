94034d95ef42ffe1a3df1943db5b9524e8beb8b7
James McClain <jmcclain@azavea.com>
2016-02-05 11:55:36 -0500

    (Most) Registrations Needed for Unit Tests

--- spark/src/main/scala/geotrellis/spark/io/hadoop/KryoRegistrator.scala
+++ spark/src/main/scala/geotrellis/spark/io/hadoop/KryoRegistrator.scala
@@ -51,7 +51,7 @@ class KryoRegistrator extends SparkKryoRegistrator {
     kryo.register(classOf[geotrellis.proj4.CRS])
 
     // de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer.registerSerializers(kryo)
-    import java.util._
+    import java.util.{Arrays, Collections}
     import de.javakaffee.kryoserializers._
 
     kryo.register( Arrays.asList( "" ).getClass, new ArraysAsListSerializer )
@@ -79,9 +79,13 @@ class KryoRegistrator extends SparkKryoRegistrator {
     kryo.register(classOf[Array[com.vividsolutions.jts.geom.Coordinate]])
     kryo.register(classOf[Array[com.vividsolutions.jts.geom.LinearRing]])
     kryo.register(classOf[Array[geotrellis.raster.Tile]])
+    kryo.register(classOf[Array[geotrellis.spark.io.avro.AvroRecordCodec[Any]]])
+    kryo.register(classOf[Array[geotrellis.spark.io.hadoop.SpaceTimeKeyWritable]])
+    kryo.register(classOf[Array[geotrellis.spark.io.hadoop.SpatialKeyWritable]])
     kryo.register(classOf[Array[geotrellis.spark.SpaceTimeKey]])
     kryo.register(classOf[Array[geotrellis.spark.SpatialKey]])
     kryo.register(classOf[Array[geotrellis.vector.Feature[Any,Any]]])
+    kryo.register(classOf[Array[scala.collection.Seq[Any]]])
     kryo.register(classOf[Array[scala.Tuple2[Any,Any]]])
     kryo.register(classOf[Array[scala.Tuple3[Any,Any,Any]]])
     kryo.register(classOf[com.vividsolutions.jts.geom.Coordinate])
@@ -93,8 +97,7 @@ class KryoRegistrator extends SparkKryoRegistrator {
     kryo.register(classOf[com.vividsolutions.jts.geom.Polygon])
     kryo.register(classOf[com.vividsolutions.jts.geom.PrecisionModel])
     kryo.register(classOf[com.vividsolutions.jts.geom.PrecisionModel.Type])
-    kryo.register(classOf[geotrellis.proj4.CRS$$anon$1])
-    kryo.register(classOf[geotrellis.proj4.LatLng$])
+    kryo.register(classOf[geotrellis.raster.ByteArrayTile])
     kryo.register(classOf[geotrellis.raster.CellSize])
     kryo.register(classOf[geotrellis.raster.CroppedTile])
     kryo.register(classOf[geotrellis.raster.GridBounds])
@@ -109,15 +112,24 @@ class KryoRegistrator extends SparkKryoRegistrator {
     kryo.register(classOf[geotrellis.raster.TypeShort$])
     kryo.register(classOf[geotrellis.raster.TypeUByte$])
     kryo.register(classOf[geotrellis.raster.UByteArrayTile])
+    kryo.register(classOf[geotrellis.raster.UShortArrayTile])
     kryo.register(classOf[geotrellis.spark.buffer.BufferSizes])
+    kryo.register(classOf[geotrellis.spark.io.avro.AvroRecordCodec[Any]])
+    kryo.register(classOf[geotrellis.spark.io.avro.AvroUnionCodec[Any]])
     kryo.register(classOf[geotrellis.spark.io.avro.codecs.KeyValueRecordCodec[Any,Any]])
     kryo.register(classOf[geotrellis.spark.io.avro.codecs.TupleCodec[Any,Any]])
+    kryo.register(classOf[geotrellis.spark.io.hadoop.SpaceTimeKeyWritable])
+    kryo.register(classOf[geotrellis.spark.io.hadoop.SpatialKeyWritable])
     kryo.register(classOf[geotrellis.spark.KeyBounds[Any]])
     kryo.register(classOf[geotrellis.spark.SpaceTimeKey])
     kryo.register(classOf[geotrellis.spark.SpatialKey])
     kryo.register(classOf[geotrellis.vector.Feature[Any,Any]])
     kryo.register(classOf[geotrellis.vector.Polygon])
     kryo.register(classOf[java.lang.Class[Any]])
+    kryo.register(classOf[java.util.HashMap[Any,Any]])
+    kryo.register(classOf[java.util.HashSet[Any]])
+    kryo.register(classOf[java.util.LinkedHashMap[Any,Any]])
+    kryo.register(classOf[java.util.LinkedHashSet[Any]])
     kryo.register(classOf[org.apache.accumulo.core.client.BatchWriterConfig])
     kryo.register(classOf[org.apache.accumulo.core.data.Key])
     kryo.register(classOf[org.apache.accumulo.core.data.Value])
@@ -126,11 +138,99 @@ class KryoRegistrator extends SparkKryoRegistrator {
     kryo.register(classOf[org.osgeo.proj4j.datum.Ellipsoid])
     kryo.register(classOf[org.osgeo.proj4j.proj.LambertConformalConicProjection])
     kryo.register(classOf[org.osgeo.proj4j.units.Unit])
-    kryo.register(classOf[scala.collection.immutable.Nil$])
     kryo.register(classOf[scala.collection.mutable.WrappedArray$ofRef])
-    kryo.register(classOf[scala.None$])
-    kryo.register(classOf[scala.reflect.ClassTag$$anon$1])
+    kryo.register(classOf[scala.collection.Seq[Any]])
     kryo.register(classOf[scala.Tuple3[Any,Any,Any]])
+
+    /* Special Handling: Problematic Classes */
+    {
+      val cs = List(
+        "geotrellis.proj4.LatLng$",
+        "geotrellis.spark.EmptyBounds$",
+        "scala.collection.immutable.Nil$",
+        "scala.math.Ordering$Double$",
+        "scala.math.Ordering$Float$",
+        "scala.math.Ordering$Int$",
+        "scala.math.Ordering$Long$",
+        "scala.None$"
+      )
+
+      cs.foreach({ c => kryo.register(java.lang.Class.forName(c)) })
+    }
+
+    /* Special Handling: Avro */
+    {
+      import org.apache.avro.Schema
+
+      val booleanSchema = Schema.create(Schema.Type.BOOLEAN)
+      val bytesSchema = Schema.create(Schema.Type.BYTES)
+      val doubleSchema = Schema.create(Schema.Type.DOUBLE)
+      val floatSchema = Schema.create(Schema.Type.FLOAT)
+      val intSchema = Schema.create(Schema.Type.INT)
+      val longSchema = Schema.create(Schema.Type.LONG)
+      val nullSchema = Schema.create(Schema.Type.NULL)
+      val stringSchema = Schema.create(Schema.Type.STRING)
+
+      val fields = new java.util.ArrayList[Schema.Field]
+      val field = new Schema.Field("a", Schema.create(Schema.Type.NULL), null, null)
+      fields.add(field)
+      val schemas = new java.util.ArrayList[Schema]
+      schemas.add(booleanSchema); schemas.add(bytesSchema)
+      val enums = new java.util.ArrayList[String]
+      enums.add("b"); enums.add("c")
+
+      kryo.register(field.getClass)       // Field
+      kryo.register(field.order.getClass) // Field$order
+      kryo.register(classOf[Schema.Type]) // Schema$Type
+
+      kryo.register(booleanSchema.getClass) // Boolean
+      kryo.register(bytesSchema.getClass)   // Bytes
+      kryo.register(doubleSchema.getClass)  // Double
+      kryo.register(floatSchema.getClass)   // Float
+      kryo.register(intSchema.getClass)     // Int
+      kryo.register(longSchema.getClass)    // Long
+      kryo.register(nullSchema.getClass)    // Null
+      kryo.register(stringSchema.getClass)  // String
+
+      kryo.register(Schema.createArray(nullSchema).getClass)          // Array
+      kryo.register(Schema.createEnum("d", "e", "f", enums).getClass) // Enum
+      kryo.register(Schema.createFixed("g", "h", "i", 1).getClass)    // Fixed
+      kryo.register(Schema.createMap(nullSchema).getClass)            // Map
+      kryo.register(Schema.createRecord(fields).getClass)             // Record
+      kryo.register(Schema.createUnion(schemas).getClass)             // Union
+
+      kryo.register(java.lang.Class.forName("org.apache.avro.Schema$LockableArrayList"))
+      kryo.register(java.lang.Class.forName("org.apache.avro.Schema$Name"))
+    }
+
+    /* Special Handling: Anonymous Objects */
+    {
+      val anonymous = List(
+        "geotrellis.proj4.CRS$$anon$",
+        "geotrellis.spark.io.avro.codecs.KeyCodecs$$anon$",
+        "geotrellis.spark.io.avro.codecs.TileCodecs$$anon$",
+        "geotrellis.spark.SpaceTimeKey$$anonfun$ordering$",
+        "geotrellis.spark.SpatialKey$$anonfun$ordering$",
+        "geotrellis.spark.TemporalKey$$anonfun$ordering$",
+        "scala.math.Ordering$$anon$",
+        "scala.math.Ordering$$anonfun$by$",
+        "scala.reflect.ClassTag$$anon$"
+      )
+
+      // The exact "names" of these anonymous objects may or may not
+      // be known at the time of this file's compilation.  Therefore,
+      // generate guesses at runtime.
+      (1 to 17).foreach({ i =>
+        anonymous.foreach({ anon =>
+          try {
+            kryo.register(Class.forName(anon + i))
+          }
+          catch {
+            case e: java.lang.ClassNotFoundException =>
+          }
+        })
+      })
+    }
 
     UnmodifiableCollectionsSerializer.registerSerializers( kryo )
     SynchronizedCollectionsSerializer.registerSerializers( kryo )
