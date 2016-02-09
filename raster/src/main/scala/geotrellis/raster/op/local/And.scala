48ea140d1102b7a777b77319fc987afae0f2a3da
James McClain <jmcclain@azavea.com>
2016-02-09 15:04:26 -0500

    Remove Excess Whitespace

--- raster/src/main/scala/geotrellis/raster/op/local/And.scala
+++ raster/src/main/scala/geotrellis/raster/op/local/And.scala
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2014 Azavea.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@ import geotrellis.raster._
 
 /**
  * Operation to And values.
- * 
+ *
  * @note        NoData values will cause the results of this operation
  *              to be NODATA.
  * @note        If used with Double typed rasters, the values
--- raster/src/main/scala/geotrellis/raster/op/local/GreaterOrEqual.scala
+++ raster/src/main/scala/geotrellis/raster/op/local/GreaterOrEqual.scala
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2014 Azavea.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +29,7 @@ object GreaterOrEqual extends LocalTileComparatorOp {
     if(isNoData(z1)) { false }
     else {
       if(isNoData(z2)) { false }
-      else { 
+      else {
         if(z1 >= z2) true
         else false
       }
