7ce04a6375b420d11b39d2a171b04586420b33b9
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-12 12:20:30 +0300

    moved type params to etl funcs

--- spark-etl/src/main/scala/geotrellis/spark/etl/Etl.scala
+++ spark-etl/src/main/scala/geotrellis/spark/etl/Etl.scala
@@ -1,7 +1,7 @@
 package geotrellis.spark.etl
 
-import com.typesafe.scalalogging.slf4j.{Logger, LazyLogging}
-import geotrellis.raster.{MultiBandTile, Tile, RasterExtent, CellGrid}
+import com.typesafe.scalalogging.slf4j.Logger
+import geotrellis.raster.{RasterExtent, CellGrid}
 import geotrellis.raster.merge.TileMergeMethods
 import geotrellis.raster.prototype.TilePrototypeMethods
 import geotrellis.raster.reproject._
@@ -22,18 +22,11 @@ object Etl {
   val defaultModules = Array(s3.S3Module, hadoop.HadoopModule, accumulo.AccumuloModule)
 }
 
-case class Etl[
-  I: ProjectedExtentComponent: ? => TilerKeyMethods[I, K],
-  K: SpatialComponent,
-  V <: CellGrid: ? => TileReprojectMethods[V]: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V]
-](args: Seq[String], @transient modules: Seq[TypedModule] = Etl.defaultModules)
- (implicit @transient ti: TypeTag[I], @transient tk: TypeTag[K], @transient tv: TypeTag[V]) {
+case class Etl(args: Seq[String], @transient modules: Seq[TypedModule] = Etl.defaultModules) {
 
   @transient lazy val logger: Logger = Logger(LoggerFactory getLogger getClass.getName)
 
   type M = RasterMetaData
-  implicit def classTagK = ClassTag(tk.mirror.runtimeClass(tk.tpe)).asInstanceOf[ClassTag[K]]
-  implicit def classTagV = ClassTag(tv.mirror.runtimeClass(tv.tpe)).asInstanceOf[ClassTag[V]]
 
   @transient val conf = new EtlConf(args)
 
@@ -51,14 +44,11 @@ case class Etl[
   }
 
   @transient val combinedModule = modules reduce (_ union _)
-    
-  lazy val outputPlugin =
-    combinedModule
-      .findSubclassOf[OutputPlugin[K, V, M]]
-      .find { _.suitableFor(conf.output()) }
-      .getOrElse(sys.error(s"Unable to find output module of type '${conf.output()}'"))
 
-  def load()(implicit sc: SparkContext): RDD[(I, V)] = {
+  def load[
+    I: ProjectedExtentComponent: TypeTag,
+    V <: CellGrid: TypeTag: ? => TileReprojectMethods[V]: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V]
+  ]()(implicit sc: SparkContext): RDD[(I, V)] = {
     val plugin =
       combinedModule
         .findSubclassOf[InputPlugin[I, V]]
@@ -68,9 +58,16 @@ case class Etl[
     plugin(conf.inputProps)
   }
 
-  def reproject(rdd: RDD[(I, V)]): RDD[(I, V)] = rdd.reproject(conf.crs()).persist(conf.cache())
+  def reproject[
+    I: ProjectedExtentComponent,
+    V <: CellGrid: ? => TileReprojectMethods[V]: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V]
+  ](rdd: RDD[(I, V)]): RDD[(I, V)] = rdd.reproject(conf.crs()).persist(conf.cache())
 
-  def tile(rdd: RDD[(I, V)])(implicit sc: SparkContext): (Int, RDD[(K, V)] with Metadata[M]) = {
+  def tile[
+    I: ProjectedExtentComponent: ? => TilerKeyMethods[I, K],
+    K: SpatialComponent: ClassTag,
+    V <: CellGrid: ClassTag: ? => TileReprojectMethods[V]: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V]
+  ](rdd: RDD[(I, V)])(implicit sc: SparkContext): (Int, RDD[(K, V)] with Metadata[M]) = {
     val crs = conf.crs()
     val targetCellType = conf.cellType.get
 
@@ -94,8 +91,19 @@ case class Etl[
     zoom -> ContextRDD(tiles, rasterMetaData)
   }
 
-  def save(id: LayerId, rdd: RDD[(K, V)] with Metadata[M], method: KeyIndexMethod[K]): Unit = {
-    val attributes = outputPlugin.attributes(conf.outputProps)
+  def save[
+    K: SpatialComponent: TypeTag,
+    V <: CellGrid: TypeTag: ? => TileReprojectMethods[V]: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V]
+  ](id: LayerId, rdd: RDD[(K, V)] with Metadata[M], method: KeyIndexMethod[K]): Unit = {
+    implicit def classTagK = ClassTag(typeTag[K].mirror.runtimeClass(typeTag[K].tpe)).asInstanceOf[ClassTag[K]]
+    implicit def classTagV = ClassTag(typeTag[V].mirror.runtimeClass(typeTag[V].tpe)).asInstanceOf[ClassTag[V]]
+
+    val outputPlugin =
+      combinedModule
+        .findSubclassOf[OutputPlugin[K, V, M]]
+        .find { _.suitableFor(conf.output()) }
+        .getOrElse(sys.error(s"Unable to find output module of type '${conf.output()}'"))
+
     def savePyramid(zoom: Int, rdd: RDD[(K, V)] with Metadata[M]): Unit = {
       val currentId = id.copy(zoom = zoom)
       outputPlugin(currentId, rdd, method, conf.outputProps)
