3d1e54ee60c3d961c0219009a2f9a8774d793c6c
James McClain <jmcclain@azavea.com>
2016-02-15 09:24:07 -0500

    Move Some Ingest Tests To geotrellis-gdal

--- spark/src/test/scala/geotrellis/spark/ingest/IngestSpec.scala
+++ spark/src/test/scala/geotrellis/spark/ingest/IngestSpec.scala
@@ -31,45 +31,5 @@ class IngestSpec extends FunSpec
         rdd.count should be (8)
       }
     }
-
-    // ifGdalInstalled {
-    //   val expectedKeys = List(
-    //     SpaceTimeKey(SpatialKey(1,1),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,0),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,1),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,0),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,1),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,1),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,1),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,1),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,1),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(1,0),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(1,0),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,0),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(1,0),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(1,1),TemporalKey(DateTime.parse("2006-01-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(1,1),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,0),TemporalKey(DateTime.parse("2006-02-15T00:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(2,0),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z"))),
-    //     SpaceTimeKey(SpatialKey(0,0),TemporalKey(DateTime.parse("2006-03-16T12:00:00.000Z")))
-    //   )
-
-    //   it("should ingest time-band NetCDF") {
-    //     val source = sc.netCdfRDD(new Path(inputHome, "ipcc-access1-tasmin.nc"))
-    //     Ingest[TemporalProjectedExtent, SpaceTimeKey](source, LatLng, FloatingLayoutScheme(256)){ (rdd, level) =>
-    //       val ingestKeys = rdd.keys.collect()
-    //       info(ingestKeys.toList.toString)
-    //       ingestKeys should contain theSameElementsAs expectedKeys
-    //     }
-    //   }
-
-    //   it("should ingest time-band NetCDF in stages") {
-    //     val source = sc.netCdfRDD(new Path(inputHome, "ipcc-access1-tasmin.nc"))
-    //     val (zoom, rmd) = source.collectMetaData(LatLng, FloatingLayoutScheme(256))
-    //     val tiled = source.cutTiles[SpaceTimeKey](rmd)
-    //     val ingestKeys = tiled.keys.collect()
-    //     ingestKeys should contain theSameElementsAs expectedKeys
-    //   }
-    // }
   }
 }
