4313ceb287fdfed75fa57cd7df83d7e1556cb3c2
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-15 22:19:49 +0300

    fix tests

--- engine-test/src/test/scala/geotrellis/engine/op/elevation/SlopeSpec.scala
+++ engine-test/src/test/scala/geotrellis/engine/op/elevation/SlopeSpec.scala
@@ -65,7 +65,7 @@ class SlopeSpec extends FunSpec with Matchers with TestEngine {
       val (xmax, ymin) = rasterExtentGdal.gridToMap(rg.cols - 2, rg.rows - 2)
 
       val cropExtent = Extent(xmin, ymin, xmax, ymax)
-      val croppedGdal = rg.crop(rasterExtentGdal.extent, cropExtent).convert(TypeDouble)
+      val croppedGdal = rg.crop(rasterExtentGdal.extent, cropExtent).convert(DoubleConstantNoDataCellType)
       val croppedComputed = get(slopeComputed).crop(rasterExtent.extent, cropExtent)
 
       assertEqual(croppedGdal, croppedComputed, 1.0)
