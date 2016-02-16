21d008893222c22d62f77cc330719f5587524aa0
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-16 20:51:01 +0300

    MultibandCombinerSpec refactor

--- raster-test/src/test/scala/geotrellis/raster/MultibandCombinersSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/MultibandCombinersSpec.scala
@@ -5,92 +5,80 @@ import geotrellis.raster.testkit._
 import org.scalatest._
 
 class MultibandCombinersSpec extends FunSuite with RasterMatchers with Matchers {
-  val fillNumber = 99
-  val tile       = IntConstantTile(fillNumber, 2, 2)
+  val tile       = IntConstantTile(99, 3, 3)
+
+  private def mkMultiBandTile(arity: Int) = ArrayMultiBandTile((0 to arity) map (_ => tile))
+
+  private def combineAssert(combined: Tile, arity: Int) = {
+    val expected = IntConstantTile(99 * arity, 3, 3)
+    assert(combined.toArray === expected.toArray)
+  }
 
   test("Multiband combine function test: arity 2") {
     val arity = 2
-    val combined = ArrayMultiBandTile(tile, tile).combine(0, 1) { case (b0, b1) => b0 + b1 }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    val combined = mkMultiBandTile(arity).combine(0, 1) { case (b0, b1) => b0 + b1 }
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 3") {
     val arity = 3
-    val combined = ArrayMultiBandTile(tile, tile, tile).combine(0, 1, 2) { case (b0, b1, b2) => b0 + b1 + b2 }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2) { case (b0, b1, b2) => b0 + b1 + b2 }
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 4") {
     val arity = 4
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile).combine(0, 1, 2, 3) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3) {
       case (b0, b1, b2, b3) => b0 + b1 + b2 + b3
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 5") {
     val arity = 5
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4) {
       case (b0, b1, b2, b3, b4) => b0 + b1 + b2 + b3 + b4
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 6") {
     val arity = 6
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4, 5) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4, 5) {
       case (b0, b1, b2, b3, b4, b5) => b0 + b1 + b2 + b3 + b4 + b5
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 7") {
     val arity = 7
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4, 5, 6) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4, 5, 6) {
       case (b0, b1, b2, b3, b4, b5, b6) => b0 + b1 + b2 + b3 + b4 + b5 + b6
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 8") {
     val arity = 8
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4, 5, 6, 7) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4, 5, 6, 7) {
       case (b0, b1, b2, b3, b4, b5, b6, b7) => b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 9") {
     val arity = 9
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4, 5, 6, 7, 8) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4, 5, 6, 7, 8) {
       case (b0, b1, b2, b3, b4, b5, b6, b7, b8) => b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 
   test("Multiband combine function test: arity 10") {
     val arity = 10
-    val combined = ArrayMultiBandTile(tile, tile, tile, tile, tile, tile, tile, tile, tile, tile).combine(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) {
+    val combined = mkMultiBandTile(arity).combine(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) {
       case (b0, b1, b2, b3, b4, b5, b6, b7, b8, b9) => b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9
     }
-    val expected = IntConstantTile(fillNumber * arity, 2, 2)
-
-    assert(combined.toArray === expected.toArray)
+    combineAssert(combined, arity)
   }
 }
