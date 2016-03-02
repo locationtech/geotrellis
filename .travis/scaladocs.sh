73cdd598680411d4d39990e9319b200dbd05cbb1
lossyrob <rdemanuele@gmail.com>
2016-03-02 16:57:55 -0500

    Added credentials, travis changes.

--- .travis/scaladocs.sh
+++ .travis/scaladocs.sh
@@ -10,16 +10,15 @@ fi
 
 rev=$(git rev-parse --short HEAD)
 
-cd stage/_book
-
 # Build docs
 ./sbt "++2.11.5 unidoc"
 
 # Set up git
-git config --global user.email "azaveadev@azavea.com"
-git config --global user.name "azaveaci"
+# git config --global user.email "azaveadev@azavea.com"
+# git config --global user.name "azaveaci"
 
 # Inside scaladocs from hereon
+rm -rf scaladocs
 git clone https://github.com/geotrellis/scaladocs.git
 rm -rf scaladocs/latest
 mv target/scala-2.11/unidoc scaladocs/latest
