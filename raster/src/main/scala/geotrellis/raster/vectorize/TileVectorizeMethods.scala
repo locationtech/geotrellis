a7b07eb99f26cb5cca947095564373f3a41e05f0
lossyrob <rdemanuele@gmail.com>
2016-03-05 16:26:21 -0500

    Fixed a bug in vectorization.

--- raster/src/main/scala/geotrellis/raster/vectorize/TileVectorizeMethods.scala
+++ raster/src/main/scala/geotrellis/raster/vectorize/TileVectorizeMethods.scala
@@ -4,7 +4,6 @@ import geotrellis.raster._
 import geotrellis.vector._
 import geotrellis.util.MethodExtensions
 
-
 trait VectorizeMethods extends MethodExtensions[Tile] {
   def toVector(extent: Extent, regionConnectivity: Connectivity = FourNeighbors): List[PolygonFeature[Int]] =
     Vectorize(self, extent, regionConnectivity)
