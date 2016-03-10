1f251e4ef0766f3de8051164fcef3e3c0d853983
James McClain <jmcclain@azavea.com>
2016-03-10 14:49:40 -0500

    Supply filterByInstant Method
    This method allows RasterRDDs keyed by SpaceTimeKeys to be filtered by a
    particular instant and for the keys to then be projected to SpatialKeys.

--- spark/src/main/scala/geotrellis/spark/filter/Implicits.scala
+++ spark/src/main/scala/geotrellis/spark/filter/Implicits.scala
@@ -12,4 +12,7 @@ object Implicits extends Implicits
 trait Implicits {
   implicit class withRasterRDDFilterMethods[K: Boundable : ClassTag, V: ClassTag, M](val self: RDD[(K, V)] with Metadata[M])
       extends RasterRDDFilterMethods[K, V, M]
+
+  implicit class withSpaceTimeRasterRDDFilterMethods[K <: SpaceTimeKey : ClassTag, V : ClassTag, M](val self: RDD[(K, V)] with Metadata[M])
+      extends SpaceTimeRasterRDDFilterMethods[K, V, M]
 }
