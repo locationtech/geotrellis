06ebdacbaa8377cfed8f5cca7d1cdb7639dae864
James McClain <jmcclain@azavea.com>
2016-03-14 16:52:40 -0400

    Scaladocs for *Tile.scala
       - ArrayTile.scala
       - ArrayMultibandTile.scala
       - BitArrayTile.scala
       - ByteArrayTile.scala
       - CompositeTile.scala
       - ConstantTile.scala
       - CroppedTile.scala
       - DoubleBasedArrayTile.scala
       - DoubleArrayTile
       - FloatArrayTile.scala
       - IntArrayTile.scala
       - IterableTile.scala
       - IntBasedArrayTile.scala
       - LazyConvertedArrayTile.scala
       - MappableTile.scala
       - MultibandTile.scala
       - MutableArrayTile.scala
       - ShortArrayTile.scala
       - Tile.scala
       - UShortArrayTile.scala
       - UByteArrayTile.scala

--- raster/src/main/scala/geotrellis/raster/MutableArrayTile.scala
+++ raster/src/main/scala/geotrellis/raster/MutableArrayTile.scala
@@ -20,22 +20,62 @@ import geotrellis.vector._
 
 import spire.syntax.cfor._
 
+
 /**
- * MutableArrayTile is an ArrayTile whose cells can be written to
- * (mutated).
- */
+  * [[MutableArrayTile]] is an [[ArrayTile]] whose cells can be
+  * written to (mutated).
+  */
 trait MutableArrayTile extends ArrayTile {
   def mutable = this
 
+  /**
+    * Update the datum at the specified index.
+    *
+    * @param   i  The index of the datum
+    * @param   z  The datum
+    */
   def update(i:Int, z:Int): Unit
+
+  /**
+    * Update the datum at the specified index.
+    *
+    * @param   i  The index of the datum
+    * @param   z  The datum
+    */
   def updateDouble(i:Int, z:Double):Unit
 
+  /**
+    * Set the value of the raster at the given column and row with the
+    * given value.
+    *
+    * @param  col    The column
+    * @param  row    The row
+    * @param  value  The value
+    */
   def set(col:Int, row:Int, value:Int) {
     update(row * cols + col, value)
   }
+
+  /**
+    * Set the value of the raster at the given column and row with the
+    * given value.
+    *
+    * @param  col    The column
+    * @param  row    The row
+    * @param  value  The value
+    */
   def setDouble(col:Int, row:Int, value:Double) {
     updateDouble(row * cols + col, value)
   }
+
+  /**
+    * Paste the given [[Tile]] into the present one starting at the
+    * given column and row offsets.
+    *
+    * @param  colOffset  The column offset
+    * @param  rowOffset  The row offset
+    * @param  update     The source tile
+    */
   def update(colOffset:Int, rowOffset:Int, update: Tile): Unit = {
     if (this.cellType.isFloatingPoint) {
       cfor(0)(_ < update.rows, _ + 1) { r =>
--- raster/src/main/scala/geotrellis/raster/ShortArrayTile.scala
+++ raster/src/main/scala/geotrellis/raster/ShortArrayTile.scala
@@ -2,15 +2,20 @@ package geotrellis.raster
 
 import geotrellis.vector.Extent
 
-import spire.syntax.cfor._
 import java.nio.ByteBuffer
+import spire.syntax.cfor._
+
 
 /**
- * ArrayTile based on Array[Short] (each cell as a Short).
+ * [[ArrayTile]] based on Array[Short] (each cell as a Short).
  */
 abstract class ShortArrayTile(val array: Array[Short], cols: Int, rows: Int)
     extends MutableArrayTile {
 
+  /**
+    * Return an array of bytes representing the data behind this
+    * [[ShortArrayTile]].
+    */
   def toBytes: Array[Byte] = {
     val pixels = new Array[Byte](array.length * cellType.bytes)
     val bytebuff = ByteBuffer.wrap(pixels)
@@ -18,41 +23,163 @@ abstract class ShortArrayTile(val array: Array[Short], cols: Int, rows: Int)
     pixels
   }
 
+  /**
+    * Return a copy of the present [[ShortArrayTile]].
+    *
+    * @return  The copy
+    */
   def copy: ArrayTile = ArrayTile(array.clone, cols, rows)
 }
 
+/**
+  * [[ShortRawArrayTile]] derived from [[ShortArrayTile]].
+  */
 final case class ShortRawArrayTile(arr: Array[Short], val cols: Int, val rows: Int)
     extends ShortArrayTile(arr, cols, rows) {
   val cellType = ShortCellType
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def apply(i: Int): Int = arr(i).toInt
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def applyDouble(i: Int): Double = arr(i).toDouble
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def update(i: Int, z: Int) { arr(i) = z.toShort }
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def updateDouble(i: Int, z: Double) { arr(i) = z.toShort }
 }
 
+/**
+  * [[ShortConstantNoDataArrayTile]] derived from [[ShortArrayTile]].
+  */
 final case class ShortConstantNoDataArrayTile(arr: Array[Short], val cols: Int, val rows: Int)
     extends ShortArrayTile(arr, cols, rows) {
   val cellType = ShortConstantNoDataCellType
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def apply(i: Int): Int = s2i(arr(i))
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def applyDouble(i: Int): Double = s2d(arr(i))
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def update(i: Int, z: Int) = arr(i) = i2s(z)
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def updateDouble(i: Int, z: Double) = arr(i) = d2s(z)
 }
 
+/**
+  * [[ShortUserDefinedNoDataArrayTile]] derived from
+  * [[ShortArrayTile]].
+  */
 final case class ShortUserDefinedNoDataArrayTile(arr: Array[Short], val cols: Int, val rows: Int, val cellType: ShortUserDefinedNoDataCellType)
     extends ShortArrayTile(arr, cols, rows)
        with UserDefinedShortNoDataConversions {
   val userDefinedShortNoDataValue = cellType.noDataValue
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def apply(i: Int): Int = uds2i(arr(i))
+
+  /**
+    * Fetch the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @return     The datum found at the index
+    */
   def applyDouble(i: Int): Double = uds2d(arr(i))
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def update(i: Int, z: Int) { arr(i) = i2uds(z) }
+
+  /**
+    * Update the datum at the given index in the array.
+    *
+    * @param   i  The index
+    * @param   z  The value to place at that index
+    */
   def updateDouble(i: Int, z: Double) { arr(i) = d2uds(z) }
 }
 
+/**
+  * The companion object associated with the [[ShortArrayTile]] type.
+  */
 object ShortArrayTile {
+
+  /**
+    * Create a new [[ShortArrayTile]] from an array of integers, a
+    * number of columns, and a number of rows.
+    *
+    * @param   arr   An array of integer
+    * @param   cols  The number of columns
+    * @param   rows  The number of rows
+    * @return        A new ShortArrayTile
+    */
   def apply(arr: Array[Short], cols: Int, rows: Int): ShortArrayTile =
     apply(arr, cols, rows, ShortConstantNoDataCellType)
 
+  /**
+    * Create a new [[ShortArrayTile]] from an array of integers, a
+    * number of columns, and a number of rows.
+    *
+    * @param   arr       An array of integers
+    * @param   cols      The number of columns
+    * @param   rows      The number of rows
+    * @param   cellType  The type from which to take the NODATA value
+    * @return            A new ShortArrayTile
+    */
   def apply(arr: Array[Short], cols: Int, rows: Int, cellType: ShortCells with NoDataHandling): ShortArrayTile =
     cellType match {
       case ShortCellType =>
@@ -63,9 +190,25 @@ object ShortArrayTile {
         new ShortUserDefinedNoDataArrayTile(arr, cols, rows, udct)
     }
 
+  /**
+    * Produce a [[ShortArrayTile]] of the specified dimensions.
+    *
+    * @param   cols  The number of columns
+    * @param   rows  The number of rows
+    * @return        The new ShortArrayTile
+    */
   def ofDim(cols: Int, rows: Int): ShortArrayTile =
     ofDim(cols, rows, ShortConstantNoDataCellType)
 
+  /**
+    * Produce a [[ShortArrayTile]] of the specified dimensions.  The
+    * NODATA value for the tile is inherited from the given cell type.
+    *
+    * @param   cols      The number of columns
+    * @param   rows      The number of rows
+    * @param   cellType  The cell type from which to derive the NODATA
+    * @return            The new ShortArrayTile
+    */
   def ofDim(cols: Int, rows: Int, cellType: ShortCells with NoDataHandling): ShortArrayTile =
     cellType match {
       case ShortCellType =>
@@ -76,9 +219,25 @@ object ShortArrayTile {
         new ShortUserDefinedNoDataArrayTile(Array.ofDim[Short](cols * rows), cols, rows, udct)
     }
 
+  /**
+    * Produce an empty, new [[ShortArrayTile]].
+    *
+    * @param   cols  The number of columns
+    * @param   rows  The number of rows
+    * @return        The new ShortArrayTile
+    */
   def empty(cols: Int, rows: Int): ShortArrayTile =
     empty(cols, rows, ShortConstantNoDataCellType)
 
+  /**
+    * Produce an empty, new [[ShortArrayTile]].  The NODATA type for
+    * the tile is derived from the given cell type.
+    *
+    * @param   cols      The number of columns
+    * @param   rows      The number of rows
+    * @param   cellType  The cell type from which to derive the NODATA
+    * @return            The new ShortArrayTile
+    */
   def empty(cols: Int, rows: Int, cellType: ShortCells with NoDataHandling): ShortArrayTile =
     cellType match {
       case ShortCellType =>
@@ -89,9 +248,28 @@ object ShortArrayTile {
         fill(nd, cols, rows, cellType)
     }
 
+  /**
+    * Produce a new [[ShortArrayTile]] and fill it with the given value.
+    *
+    * @param   v     the values to fill into the new tile
+    * @param   cols  The number of columns
+    * @param   rows  The number of rows
+    * @return        The new ShortArrayTile
+    */
   def fill(v: Short, cols: Int, rows: Int): ShortArrayTile =
     fill(v, cols, rows, ShortConstantNoDataCellType)
 
+  /**
+    * Produce a new [[ShortArrayTile]] and fill it with the given value.
+    * The NODATA value for the tile is inherited from the given cell
+    * type.
+    *
+    * @param   v         the values to fill into the new tile
+    * @param   cols      The number of columns
+    * @param   rows      The number of rows
+    * @param   cellType  The cell type from which to derive the NODATA
+    * @return            The new ShortArrayTile
+    */
   def fill(v: Short, cols: Int, rows: Int, cellType: ShortCells with NoDataHandling): ShortArrayTile =
     cellType match {
       case ShortCellType =>
@@ -110,9 +288,27 @@ object ShortArrayTile {
     shortArray
   }
 
+  /**
+    * Produce a new [[ShortArrayTile]] from an array of bytes.
+    *
+    * @param   bytes  the data to fill into the new tile
+    * @param   cols   The number of columns
+    * @param   rows   The number of rows
+    * @return         The new ShortArrayTile
+    */
   def fromBytes(bytes: Array[Byte], cols: Int, rows: Int): ShortArrayTile =
     fromBytes(bytes, cols, rows, ShortConstantNoDataCellType)
 
+  /**
+    * Produce a new [[ShortArrayTile]] from an array of bytes.  The
+    * NODATA value for the tile is inherited from the given cell type.
+    *
+    * @param   bytes     the data to fill into the new tile
+    * @param   cols      The number of columns
+    * @param   rows      The number of rows
+    * @param   cellType  The cell type from which to derive the NODATA
+    * @return            The new ShortArrayTile
+    */
   def fromBytes(bytes: Array[Byte], cols: Int, rows: Int, cellType: ShortCells with NoDataHandling): ShortArrayTile =
     cellType match {
       case ShortCellType =>
