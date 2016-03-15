993e4c06a2756ee5f9dbb0ccc75e467d46b425bb
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-03-15 23:08:04 +0300

    unordered insert fix

--- accumulo/src/main/scala/geotrellis/spark/io/accumulo/AccumuloRDDWriter.scala
+++ accumulo/src/main/scala/geotrellis/spark/io/accumulo/AccumuloRDDWriter.scala
@@ -33,8 +33,8 @@ object AccumuloRDDWriter {
         // Map and sort first, so that partitioner is carried over to groupByKey
         raster
           .map { case (key, value) => (encodeKey(key), (key, value)) }
-          .sortByKey()
           .groupByKey()
+          .sortByKey()
       } else {
         // Call groupBy with numPartitions; if called without that argument or a partitioner,
         // groupBy will reuse the partitioner on the parent RDD if it is set, which could be typed
