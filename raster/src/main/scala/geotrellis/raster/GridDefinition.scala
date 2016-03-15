0494906e571a4617f8f44e70354fa24511c17ea9
James McClain <jmcclain@azavea.com>
2016-03-15 15:11:03 -0400

    Scaladocs for GridDefinition.scala

--- raster/src/main/scala/geotrellis/raster/GridDefinition.scala
+++ raster/src/main/scala/geotrellis/raster/GridDefinition.scala
@@ -1,7 +1,9 @@
 package geotrellis.raster
 
 import geotrellis.vector.Extent
+
 import scala.math.{min, max, ceil}
+
 
 trait GridDefinition {
   def extent: Extent
@@ -12,12 +14,14 @@ trait GridDefinition {
 
   def toGridExtent: GridExtent = GridExtent(extent, cellwidth, cellheight)
 
-  /** Creates a RasterExtent out of this GridExtent.
+  /**
+    *  Creates a RasterExtent out of this GridExtent.
     *
-    * @note                      Use with caution: if the number of columns or rows are larger than Int.MaxValue,
-    *                            this will throw an exception. Also, if columns * rows > Int.MaxValue, this will
-    *                            create a RasterExtent for a raster that could not be backed by any of the Array-backed 
-    *                            tile types.
+    * @note Use with caution: if the number of columns or rows are larger than Int.MaxValue,
+    *       this will throw an exception. Also, if columns * rows >
+    *       Int.MaxValue, this will create a RasterExtent for a raster
+    *       that could not be backed by any of the Array-backed tile
+    *       types.
     */
   def toRasterExtent(): RasterExtent = {
     val targetCols = math.round(extent.width / cellwidth).toLong
@@ -33,12 +37,13 @@ trait GridDefinition {
   }
 
   /**
-   * Returns a GridExtent that lines up with this grid' resolution
-   * and grid layout.
-   * i.e., the resulting GridExtent will not have the given extent,
-   * but will have the smallest extent such that the whole of
-   * the given extent is covered, that lines up with the grid.
-   */
+    * Returns a GridExtent that lines up with this grid' resolution
+    * and grid layout.
+    *
+    * For example, the resulting GridExtent will not have the given
+    * extent, but will have the smallest extent such that the whole of
+    * the given extent is covered, that lines up with the grid.
+    */
   def createAlignedGridExtent(targetExtent: Extent): GridExtent = {
     val xmin = extent.xmin + (math.floor((targetExtent.xmin - extent.xmin) / cellwidth) * cellwidth)
     val xmax = extent.xmax - (math.floor((extent.xmax - targetExtent.xmax) / cellwidth) * cellwidth)
@@ -49,23 +54,28 @@ trait GridDefinition {
   }
 
   /**
-   * Returns a RasterExtent that lines up with this RasterExtent's resolution,
-   * and grid layout.
-   * i.e., the resulting RasterExtent will not have the given extent,
-   * but will have the smallest extent such that the whole of
-   * the given extent is covered, that lines up with the grid.
-   */
+    * Returns a RasterExtent that lines up with this RasterExtent's
+    * resolution, and grid layout.
+    *
+    * For example, the resulting RasterExtent will not have the given
+    * extent, but will have the smallest extent such that the whole of
+    * the given extent is covered, that lines up with the grid.
+    */
   def createAlignedRasterExtent(targetExtent: Extent): RasterExtent =
     createAlignedGridExtent(targetExtent).toRasterExtent
 
   /**
-    * Gets the Extent that matches the grid bounds passed in, aligned with this RasterExtent.
-    * 
-    * @param     gridBounds      The extent to get the grid bounds for
-    * @param     clamp          Determines whether or not to clamp the Extent to the
-    *                           extent of this RasterExtent; defaults to true. If true, the
-    *                           returned extent will be contained by this RasterExtent's extent,
-    *                           if false, the Extent returned can be outside of this RasterExtent's extent.
+    * Gets the Extent that matches the grid bounds passed in, aligned
+    * with this RasterExtent.
+    *
+    * The 'clamp' parameter determines whether or not to clamp the
+    * Extent to the extent of this RasterExtent; defaults to true. If
+    * true, the returned extent will be contained by this
+    * RasterExtent's extent, if false, the Extent returned can be
+    * outside of this RasterExtent's extent.
+    *
+    * @param  gridBounds  The extent to get the grid bounds for
+    * @param  clamp       A boolean which controlls the clamping behvior
     */
   def extentFor(gridBounds: GridBounds, clamp: Boolean = true): Extent = {
     val xmin = gridBounds.colMin * cellwidth + extent.xmin
