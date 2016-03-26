441da945e4089f81f1c832e0cc33805de17e6733
lossyrob <rdemanuele@gmail.com>
2016-03-26 11:29:22 -0400

    Allow users to modify PutObjectRequest when saving to S3

--- s3/src/main/scala/geotrellis/spark/io/s3/SaveToS3Methods.scala
+++ s3/src/main/scala/geotrellis/spark/io/s3/SaveToS3Methods.scala
@@ -2,6 +2,7 @@ package geotrellis.spark.io.s3
 
 import geotrellis.util.MethodExtensions
 
+import com.amazonaws.services.s3.model.PutObjectRequest
 import org.apache.spark.rdd.RDD
 
 
@@ -11,7 +12,8 @@ class SaveToS3Methods[K](val self: RDD[(K, Array[Byte])]) extends MethodExtensio
     * Saves each RDD value to an S3 key.
     *
     * @param keyToUri A function from K (a key) to an S3 URI
+    * @param putObjectModifier  Function that will be applied ot S3 PutObjectRequests, so that they can be modified (e.g. to change the ACL settings)
     */
-  def saveToS3(keyToUri: K => String): Unit =
-    SaveToS3(self, keyToUri)
+  def saveToS3(keyToUri: K => String, putObjectModifier: PutObjectRequest => PutObjectRequest = { p => p }): Unit =
+    SaveToS3(self, keyToUri, putObjectModifier)
 }
