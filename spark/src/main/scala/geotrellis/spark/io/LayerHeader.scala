99039830b3841a6406eec7511a6e04b9ed1f5667
Eugene Cheipesh <echeipesh@gmail.com>
2016-04-08 17:17:54 -0400

    Provide JsonFormat for LayerHeader
    this allows reading the base fields of any layer header to determine
    type of saved layers at run time.

--- spark/src/main/scala/geotrellis/spark/io/LayerHeader.scala
+++ spark/src/main/scala/geotrellis/spark/io/LayerHeader.scala
@@ -6,3 +6,26 @@ trait LayerHeader {
   def keyClass: String
   def valueClass: String
 }
+
+object LayerHeader {
+  implicit object LayeHeaderFormat extends RootJsonFormat[LayerHeader] {
+    def write(md: LayerHeader) =
+      JsObject(
+        "format" -> JsString(md.format),
+        "keyClass" -> JsString(md.keyClass),
+        "valueClass" -> JsString(md.valueClass)
+      )
+
+    def read(value: JsValue): LayerHeader =
+      value.asJsObject.getFields("format", "keyClass", "valueClass") match {
+        case Seq(JsString(_format), JsString(_keyClass), JsString(_valueClass)) =>
+          new LayerHeader {
+            val format = _format
+            val keyClass = _keyClass
+            val valueClass = _valueClass
+          }
+        case _ =>
+          throw new DeserializationException(s"LayerHeader expected, got: $value")
+      }
+  }
+}
