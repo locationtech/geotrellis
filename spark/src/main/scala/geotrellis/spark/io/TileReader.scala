3a42ce70f46ce21392256466d99716c1a36463cf
Eugene Cheipesh <echeipesh@gmail.com>
2016-04-08 17:18:18 -0400

    Add docs for layer and tile reading

--- spark/src/main/scala/geotrellis/spark/io/TileReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/TileReader.scala
@@ -8,6 +8,10 @@ import spray.json._
 
 import scala.reflect._
 
+/** A key-value reader producer to read a layer one value at a time.
+ * This interface abstracts over various construction requirements for
+ * constructing a storage back-end specific reader. */
 trait TileReader[ID] {
+  /** Produce a key value reader for a specific layer, prefetching layer metadata once at construction time */
   def reader[K: AvroRecordCodec: JsonFormat: ClassTag, V: AvroRecordCodec](layerId: ID): Reader[K, V]
 }
