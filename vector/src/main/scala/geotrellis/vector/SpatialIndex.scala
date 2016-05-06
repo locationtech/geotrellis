b3acce47693c18c852627c4690e55274931e8fc5
Justin Polchlopek <jpolchlopek@azavea.com>
2016-05-06 15:19:47 -0400

    Added k-nearest neighbor algorithm to SpatialIndex

--- vector/src/main/scala/geotrellis/vector/SpatialIndex.scala
+++ vector/src/main/scala/geotrellis/vector/SpatialIndex.scala
@@ -16,12 +16,15 @@
 
 package geotrellis.vector
 
-import com.vividsolutions.jts.index.strtree.{STRtree, ItemDistance, ItemBoundable}
+import com.vividsolutions.jts.index.strtree.{STRtree, ItemDistance, ItemBoundable, AbstractNode}
 import com.vividsolutions.jts.index.strtree.ItemDistance
 import com.vividsolutions.jts.geom.Coordinate
+import com.vividsolutions.jts.geom.GeometryFactory
 import com.vividsolutions.jts.geom.Envelope
+import com.vividsolutions.jts.operation.distance.DistanceOp
 
 import scala.collection.mutable
+import scala.collection.JavaConversions._
 
 object SpatialIndex {
   def apply(points: Iterable[(Double, Double)]): SpatialIndex[(Double, Double)] = {
@@ -88,6 +91,62 @@ class SpatialIndex[T](val measure: Measure = Measure.Euclidean) extends Serializ
 
   def pointsInExtentAsJavaList(extent: Extent): java.util.List[T] =
     rtree.query(new Envelope(extent.xmin, extent.xmax, extent.ymin, extent.ymax)).asInstanceOf[java.util.List[T]]
+
+  def kNearest(x: Double, y: Double, k: Int): List[T] =
+    kNearest(new Envelope(new Coordinate(x, y)), k)
+
+  def kNearest(pt: (Double, Double), k: Int): List[T] =
+    kNearest(new Envelope(new Coordinate(pt._1, pt._2)), k)
+
+  def kNearest(ex: Extent, k: Int): List[T] = {
+    case class PQitem[A](d: Double, x: A) extends Ordered[PQitem[A]] {
+      import scala.math.Ordered.orderingToOrdered
+      def compare(that: PQitem[A]): Int = { (this.d) compare (that.d) }
+    }
+
+    val gf = new GeometryFactory()
+    val env = ex.jtsEnvelope
+    val pq = (new mutable.PriorityQueue(): mutable.PriorityQueue[PQitem[AbstractNode]]).reverse
+    val best = (new mutable.PriorityQueue(): mutable.PriorityQueue[PQitem[T]])
+
+    def addToBest (elem: PQitem[T]) = {
+      if (best.size < k)
+        best.enqueue(elem)
+      else {
+        if (elem.d < best.head.d) {
+          best.enqueue(elem)
+          best.dequeue()
+        }
+      }
+    }
+    def ibToPQitem (ib: ItemBoundable): PQitem[T] = {
+      PQitem(DistanceOp.distance(gf.toGeometry(env), gf.toGeometry(ib.getBounds.asInstanceOf[Envelope])), ib.getItem.asInstanceOf[T])
+    }
+    def anToPQitem (nd: AbstractNode): PQitem[AbstractNode] = {
+      PQitem(DistanceOp.distance(gf.toGeometry(env), gf.toGeometry(nd.getBounds.asInstanceOf[Envelope])), nd)
+    }
+
+    pq.enqueue(anToPQitem(rtree.getRoot))
+
+    do {
+      val item = pq.dequeue
+
+      if (best.size < k || item.d < best.head.d) {
+        if (item.x.getLevel == 0) {
+          // leaf node
+          item.x.getChildBoundables.map { 
+            ibraw => ibToPQitem(ibraw.asInstanceOf[ItemBoundable]) 
+          }.foreach(addToBest)
+        } else {
+          item.x.getChildBoundables.map {
+            subtree => anToPQitem(subtree.asInstanceOf[AbstractNode])
+          }.foreach(pq.enqueue(_))
+        }
+      }
+    } while (! pq.isEmpty )
+
+    best.toList.map{ _.x }
+  }
 }
 
 object Measure {
