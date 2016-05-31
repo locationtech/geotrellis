ae7027f0d1c057b6565753bc779beabca1e12554
Justin Polchlopek <jpolchlopek@azavea.com>
2016-05-31 12:39:57 -0400

    Revert "Center circle kernel inside tile"
    This reverts commit 98403bbd20d3ddf0a6bf0a9c61761c4a3c6a04da.

--- raster/src/main/scala/geotrellis/raster/mapalgebra/focal/Kernel.scala
+++ raster/src/main/scala/geotrellis/raster/mapalgebra/focal/Kernel.scala
@@ -68,15 +68,14 @@ object Kernel {
 
     val rad2 = rad*rad
 
-    val w = size/2
-    var r = -w
-    var c = -w
-    while(r <= w) {
-      while(c <= w) {
-        output.set(c+w, r+w, if (r * r + c * c <= rad2) 1 else 0)
+    var r = 0
+    var c = 0
+    while(r < size) {
+      while(c < size) {
+        output.set(c, r, if (r * r + c * c < rad2) 1 else 0)
         c += 1
       }
-      c = -w
+      c = 0
       r += 1
     }
 
