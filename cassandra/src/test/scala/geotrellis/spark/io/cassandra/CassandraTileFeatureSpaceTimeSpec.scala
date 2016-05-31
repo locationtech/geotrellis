535fdb1930055d8870f62920c866a9807e2e0509
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-05-31 20:43:28 +0300

    +CassandraTileFeature tests

---
+++ cassandra/src/test/scala/geotrellis/spark/io/cassandra/CassandraTileFeatureSpaceTimeSpec.scala
@@ -0,0 +1,35 @@
+package geotrellis.spark.io.cassandra
+
+import geotrellis.raster.{Tile, TileFeature}
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.testfiles.TestTileFeatureFiles
+
+class CassandraTileFeatureSpaceTimeSpec
+  extends PersistenceSpec[SpaceTimeKey, TileFeature[Tile, Tile], TileLayerMetadata[SpaceTimeKey]]
+    with SpaceTimeKeyIndexMethods
+    with TestEnvironment
+    with CassandraTestEnvironment
+    with TestTileFeatureFiles
+    with CoordinateSpaceTimeTileFeatureSpec
+    with LayerUpdateSpaceTimeTileFeatureSpec {
+
+  lazy val instance       = BaseCassandraInstance(Seq("127.0.0.1"))
+  lazy val attributeStore = try {
+    CassandraAttributeStore(instance)
+  } catch {
+    case e: Exception =>
+      println("A script for setting up the Cassandra environment necessary to run these tests can be found at scripts/cassandraTestDB.sh - requires a working docker setup")
+      throw e
+  }
+
+  lazy val reader    = CassandraLayerReader(attributeStore)
+  lazy val writer    = CassandraLayerWriter(attributeStore, "geotrellis", "tiles")
+  lazy val deleter   = CassandraLayerDeleter(attributeStore)
+  lazy val updater   = CassandraLayerUpdater(attributeStore)
+  lazy val tiles     = CassandraValueReader(attributeStore)
+  lazy val sample    = CoordinateSpaceTime
+  lazy val copier    = CassandraLayerCopier(attributeStore, reader, writer)
+  lazy val reindexer = CassandraLayerReindexer(attributeStore, reader, writer, deleter, copier)
+  lazy val mover     = CassandraLayerMover(copier, deleter)
+}
