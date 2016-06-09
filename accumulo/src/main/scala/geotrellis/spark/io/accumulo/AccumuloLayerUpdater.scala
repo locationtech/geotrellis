0ad9a419e1cda65dfa53a9b1d31ca6b2df42493d
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-06-09 18:38:55 +0300

    fix style

--- accumulo/src/main/scala/geotrellis/spark/io/accumulo/AccumuloLayerUpdater.scala
+++ accumulo/src/main/scala/geotrellis/spark/io/accumulo/AccumuloLayerUpdater.scala
@@ -66,8 +66,8 @@ class AccumuloLayerUpdater(
         .fullOuterJoin(rdd)
         .mapValues {
           case (Some(layerTile), Some(updateTile)) => mergeFunc(layerTile, updateTile)
-          case (Some(layerTile), None) => layerTile
-          case (None, Some(updateTile)) => updateTile
+          case (Some(layerTile), _) => layerTile
+          case (_, Some(updateTile)) => updateTile
           case _ => throw new LayerUpdateError(id, "No value(s) in your existing and new layer RDDs")
         }
 
