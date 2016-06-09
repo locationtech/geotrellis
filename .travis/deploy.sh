e72b50cc508c53fb8e7c0b5449266b34ccb0e858
Rob Emanuele <rdemanuele@gmail.com>
2016-06-09 14:00:27 -0400

    Update deploy.sh
    Removed engine from publish.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,7 +16,6 @@ EOF
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project vector" publish \
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project proj4" publish \
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project raster" publish \
-  && ./sbt "++$TRAVIS_SCALA_VERSION" "project engine" publish \
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project spark" publish \
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project s3" publish \
   && ./sbt "++$TRAVIS_SCALA_VERSION" "project accumulo" publish \
