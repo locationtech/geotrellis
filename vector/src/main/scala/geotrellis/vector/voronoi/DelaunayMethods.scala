8974699fb79a613f324af54cda62a3f8a19fe52e
Justin Polchlopek <jpolchlopek@azavea.com>
2016-06-21 10:27:38 -0400

    Added MethodExtensions and test suite

---
+++ vector/src/main/scala/geotrellis/vector/voronoi/DelaunayMethods.scala
@@ -0,0 +1,8 @@
+package geotrellis.vector.voronoi
+
+import geotrellis.vector.Point
+import geotrellis.util.MethodExtensions
+
+trait DelaunayTriangulationMethods extends MethodExtensions[Traversable[Point]] {
+  def delaunayTriangulation(): Delaunay = { new Delaunay(self.toArray) }
+}
