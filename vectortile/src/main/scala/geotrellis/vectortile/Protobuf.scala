fde8981fe7eb17472affb5cf941face0b7dcd9ac
Colin Woodbury <colingw@gmail.com>
2016-06-28 11:25:05 -0700

    (vt) Added conversion functions between bytes and protobuf `Tile`s

---
+++ vectortile/src/main/scala/geotrellis/vectortile/Protobuf.scala
@@ -0,0 +1,21 @@
+package geotrellis.vectortile
+
+import java.nio.file.{ Files, Paths }
+import vector_tile.vector_tile.Tile
+
+// --- //
+
+// TODO Make this a trait?
+object Protobuf {
+  def decode(bytes: Array[Byte]): Tile = Tile.parseFrom(bytes)
+
+  def encode(tile: Tile): Array[Byte] = tile.toByteArray
+
+  def decodeIO(file: String): Tile = {
+    decode(Files.readAllBytes(Paths.get(file)))
+  }
+
+  def encodeIO(tile: Tile, file: String) = {
+    Files.write(Paths.get(file), encode(tile))
+  }
+}
