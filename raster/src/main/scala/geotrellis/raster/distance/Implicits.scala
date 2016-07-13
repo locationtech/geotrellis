42b10b4182fe6731e3b48a6142af1ca898d12bb8
Justin Polchlopek <jpolchlopek@azavea.com>
2016-07-13 15:24:31 -0400

    Added to method extensions

--- raster/src/main/scala/geotrellis/raster/distance/Implicits.scala
+++ raster/src/main/scala/geotrellis/raster/distance/Implicits.scala
@@ -6,5 +6,6 @@ object Implicits extends Implicits
 
 trait Implicits {
   implicit class withEuclideanDistanceTileMethods(val self: Traversable[Point]) extends EuclideanDistanceTileMethods
-  implicit class withEuclideanDistanceTileArrayMethods(val self: Array[Point]) extends EuclideanDistanceTileMethods
+
+  implicit class withEuclideanDistanceTileArrayMethods(val self: Array[Point]) extends EuclideanDistanceTileArrayMethods
 }
