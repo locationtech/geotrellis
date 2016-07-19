9758242ea932de4648c5bcf1f597512ca1aeceb1
James McClain <jmcclain@azavea.com>
2016-07-19 15:37:04 -0400

    Alternative Vector Join

--- spark/src/main/scala/geotrellis/spark/join/VectorJoin.scala
+++ spark/src/main/scala/geotrellis/spark/join/VectorJoin.scala
@@ -4,11 +4,16 @@ import geotrellis.vector._
 import geotrellis.spark._
 import geotrellis.spark.tiling._
 
+import com.vividsolutions.jts.geom.{Envelope => JtsEnvelope}
+import com.vividsolutions.jts.index.strtree.STRtree
 import org.apache.spark._
 import org.apache.spark.rdd._
+import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
+import scala.collection.JavaConverters._
 import scala.reflect._
+
 
 object VectorJoin {
   /** Map each item to all the layout cells that it intersects with */
@@ -46,4 +51,54 @@ object VectorJoin {
         pred(l: Geometry, r: Geometry)
       }
   }
+
+  def apply[
+    L <% Geometry: ClassTag,
+    R <% Geometry: ClassTag
+  ](
+    left: RDD[L],
+    right: RDD[R],
+    pred: (Geometry, Geometry) => Boolean
+  )(implicit sc: SparkContext): RDD[(L, R)] = {
+
+    // For simplicity, assume that the left RDD has length ≥ that
+    // of the right one.
+    val rtrees = right.mapPartitions({ partition =>
+      val rtree = new STRtree
+
+      partition.foreach({ r =>
+        val Extent(xmin, ymin, xmax, ymax) = r.envelope
+        val envelope = new JtsEnvelope(xmin, xmax, ymin, ymax)
+        rtree.insert(envelope, r)
+      })
+
+      Iterator(rtree)
+    }, preservesPartitioning = true)
+      .zipWithIndex
+      .map({ case (v, k) => (k, v) })
+      .cache
+    val count = rtrees.count.toInt
+
+    // For every partition of the right-hand collection of items, find
+    // an RDD of left-hand items that intersects with some member of
+    // that partition.
+    val rdds = (0 until count).map({ i =>
+      val tree = sc.broadcast(rtrees.lookup(i).head)
+
+      left.flatMap({ l =>
+        val Extent(xmin, ymin, xmax, ymax) = l.envelope
+        val envelope = new JtsEnvelope(xmin, xmax, ymin, ymax)
+
+        tree.value.query(envelope)
+          .asScala
+          .map({ r: Any => r.asInstanceOf[R] })
+          .filter({ r => pred(l, r) })
+          .map({ r => (l, r) })
+      })
+    })
+
+    // Return the results as a single RDD
+    sc.union(rdds)
+  }
+
 }
