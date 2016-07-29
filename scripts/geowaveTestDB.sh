c86a955561bbfef23a5d76ac4f48f09ebd90c159
Nathan Zimmerman <npzimmerman@gmail.com>
2016-07-29 15:01:37 -0400

    Specify path to main in test script

--- scripts/geowaveTestDB.sh
+++ scripts/geowaveTestDB.sh
@@ -2,4 +2,4 @@
 
 wget https://s3.amazonaws.com/geotrellis-test/geowave-minicluster/miniAccumuloCluster-assembly-0.1.0.jar
 
-java -jar miniAccumuloCluster-assembly-0.1.0.jar
+java -cp miniAccumuloCluster-assembly-0.1.0.jar geotrellis.minicluster.Main
