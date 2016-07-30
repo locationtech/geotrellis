0928cc5f29b555b7f3208a230389b370cdc9c95b
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-07-30 19:21:54 +0300

    add reading

--- spark/src/main/scala/geotrellis/spark/io/file/FileCollectionReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/file/FileCollectionReader.scala
@@ -2,7 +2,7 @@ package geotrellis.spark.io.file
 
 import geotrellis.spark._
 import geotrellis.spark.io.avro.codecs.KeyValueRecordCodec
-import geotrellis.spark.io.index.MergeQueue
+import geotrellis.spark.io.index.{IndexRanges, MergeQueue}
 import geotrellis.spark.io.avro.{AvroEncoder, AvroRecordCodec}
 import geotrellis.util.Filesystem
 import org.apache.avro.Schema
@@ -10,6 +10,10 @@ import spire.syntax.cfor._
 
 import scala.collection.mutable
 import java.io.File
+
+import scalaz.concurrent.Task
+import scalaz.concurrent.Future
+import scalaz.stream.{Process, nondeterminism}
 
 object FileCollectionReader {
   def read[K: AvroRecordCodec: Boundable, V: AvroRecordCodec](
@@ -26,11 +30,50 @@ object FileCollectionReader {
     else
       queryKeyBounds.flatMap(decomposeBounds)
 
-    //val bins = IndexRanges.bin(ranges, numPartitions.getOrElse(1))
+    val bins = IndexRanges.bin(ranges, numPartitions.getOrElse(1))
 
     val boundable = implicitly[Boundable[K]]
     val includeKey = (key: K) => KeyBounds.includeKey(queryKeyBounds, key)(boundable)
     val _recordCodec = KeyValueRecordCodec[K, V]
+
+    bins map { partition =>
+      partition map { case (start, end) => Task {
+        val resultPartition = new mutable.ArrayBuffer[Task[Vector[(K, V)]]]()
+
+        cfor(start)(_ <= end, _ + 1) { index => resultPartition += Task {
+          val path = keyPath(index)
+          if (new File(path).exists) {
+            val bytes: Array[Byte] = Filesystem.slurp(path)
+            val recs = AvroEncoder.fromBinary(writerSchema.getOrElse(_recordCodec.schema), bytes)(_recordCodec)
+            if (filterIndexOnly)
+              recs
+            else
+              recs.filter { row => includeKey(row._1) }
+          } else Vector()
+        } }
+
+        Task.gatherUnordered(resultPartition).unsafePerformSync.flatten
+
+      } }
+
+    }
+
+    val requests: Process[Task, (K, V)] =
+      Process.unfold(partition.toIterator){ iter =>
+        if (iter.hasNext) {
+          val recs = iter.next()
+          val key = recs._1
+          val pairs = recs._2.toVector
+          val bytes = AvroEncoder.toBinary(pairs)(_codec)
+          val metadata = new ObjectMetadata()
+          metadata.setContentLength(bytes.length)
+          val is = new ByteArrayInputStream(bytes)
+          val request = putObjectModifier(new PutObjectRequest(bucket, key, is, metadata))
+          Some(request, iter)
+        } else  {
+          None
+        }
+      }
 
     ranges flatMap { case (start, end) =>
       val resultPartition = mutable.ListBuffer[(K, V)]()
@@ -51,30 +94,31 @@ object FileCollectionReader {
 
       resultPartition
     }
+  }
 
-    /*Future.sequence(bins.map { partition: Seq[(Long, Long)] => Future {
-      val resultPartition = mutable.ListBuffer[(K, V)]()
+  /*Future.sequence(bins.map { partition: Seq[(Long, Long)] => Future {
+    val resultPartition = mutable.ListBuffer[(K, V)]()
 
-      for(range <- partition) {
-        val (start, end) = range
-        cfor(start)(_ <= end, _ + 1) { index =>
-          val path = keyPath(index)
-          if(new File(path).exists) {
-            val bytes: Array[Byte] = Filesystem.slurp(path)
-            val recs = AvroEncoder.fromBinary(writerSchema.getOrElse(_recordCodec.schema), bytes)(_recordCodec)
-            resultPartition ++= {
-              if(filterIndexOnly)
-                recs
-              else
-                recs.filter { row => includeKey(row._1) }
-            }
+    for(range <- partition) {
+      val (start, end) = range
+      cfor(start)(_ <= end, _ + 1) { index =>
+        val path = keyPath(index)
+        if(new File(path).exists) {
+          val bytes: Array[Byte] = Filesystem.slurp(path)
+          val recs = AvroEncoder.fromBinary(writerSchema.getOrElse(_recordCodec.schema), bytes)(_recordCodec)
+          resultPartition ++= {
+            if(filterIndexOnly)
+              recs
+            else
+              recs.filter { row => includeKey(row._1) }
           }
         }
       }
+    }
 
-      resultPartition.iterator
-    } })*/
+    resultPartition.iterator
+  } })*/
 
 
-  }
+}
 }
