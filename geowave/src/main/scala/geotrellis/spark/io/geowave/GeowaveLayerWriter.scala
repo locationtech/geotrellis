5dd6e7b51cde9f965c0c8db62fe1b676a0c99972
James McClain <jmcclain@azavea.com>
2016-08-09 12:18:18 -0400

    Changes to GeowaveLayerWriter
    Includes formatting changes, additional comments, GeoWave-API-change
    changes, and removal of unused imports.

--- geowave/src/main/scala/geotrellis/spark/io/geowave/GeowaveLayerWriter.scala
+++ geowave/src/main/scala/geotrellis/spark/io/geowave/GeowaveLayerWriter.scala
@@ -3,68 +3,42 @@ package geotrellis.spark.io.geowave
 import geotrellis.geotools._
 import geotrellis.proj4.LatLng
 import geotrellis.raster._
-import geotrellis.raster.resample.NearestNeighbor
 import geotrellis.spark._
 import geotrellis.spark.io._
 import geotrellis.spark.io.accumulo._
 import geotrellis.spark.io.avro._
 import geotrellis.spark.io.index.KeyIndex
-import geotrellis.spark.io.kryo.KryoRegistrator
 import geotrellis.util._
 import geotrellis.vector.Extent
 
-import com.esotericsoftware.kryo.io.{Input, Output}
-import com.esotericsoftware.kryo.{Kryo, Serializer}
 import com.typesafe.scalalogging.slf4j._
 import mil.nga.giat.geowave.adapter.raster.adapter.merge.RasterTileRowTransform
 import mil.nga.giat.geowave.adapter.raster.adapter.RasterDataAdapter
 import mil.nga.giat.geowave.core.geotime.ingest._
-import mil.nga.giat.geowave.core.index.Persistable
-import mil.nga.giat.geowave.core.index.PersistenceUtils
 import mil.nga.giat.geowave.core.store._
-import mil.nga.giat.geowave.core.store.adapter.statistics.DataStatistics
-import mil.nga.giat.geowave.core.store.adapter.statistics.RowRangeDataStatistics
-import mil.nga.giat.geowave.core.store.adapter.statistics.RowRangeHistogramStatistics
 import mil.nga.giat.geowave.core.store.adapter.statistics.StatsCompositionTool
 import mil.nga.giat.geowave.core.store.data.VisibilityWriter
-import mil.nga.giat.geowave.core.store.memory.DataStoreUtils
 import mil.nga.giat.geowave.core.store.operations.remote.options.DataStorePluginOptions
-import mil.nga.giat.geowave.core.store.query.QueryOptions
+import mil.nga.giat.geowave.core.store.util.DataStoreUtils
 import mil.nga.giat.geowave.datastore.accumulo._
-import mil.nga.giat.geowave.datastore.accumulo.metadata.AccumuloAdapterIndexMappingStore
-import mil.nga.giat.geowave.datastore.accumulo.metadata.AccumuloAdapterStore
-import mil.nga.giat.geowave.datastore.accumulo.metadata.AccumuloDataStatisticsStore
-import mil.nga.giat.geowave.datastore.accumulo.metadata.AccumuloIndexStore
+import mil.nga.giat.geowave.datastore.accumulo.metadata._
 import mil.nga.giat.geowave.datastore.accumulo.operations.config.AccumuloOptions
-import mil.nga.giat.geowave.datastore.accumulo.util.AccumuloKeyValuePair
-import mil.nga.giat.geowave.datastore.accumulo.util.AccumuloKeyValuePairGenerator
+import mil.nga.giat.geowave.datastore.accumulo.util._
 import mil.nga.giat.geowave.datastore.accumulo.util.AccumuloUtils
-import mil.nga.giat.geowave.datastore.accumulo.util.ConnectorPool
-import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputFormat
-import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputKey
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
-import org.apache.accumulo.core.data.Key
-import org.apache.accumulo.core.data.Value
-import org.apache.accumulo.core.iterators.IteratorUtil.IteratorScope
-import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.Writable
-import org.apache.hadoop.mapreduce.Job
-import org.apache.hadoop.mapreduce.task.JobContextImpl
+import org.apache.accumulo.core.data.{ Key, Value }
 import org.apache.spark.rdd.RDD
-import org.apache.spark.SerializableWritable
 import org.apache.spark.SparkContext
 import org.geotools.coverage.grid.GridCoverage2D
 import org.geotools.coverage.processing.CoverageProcessor
-import org.geotools.coverage.processing.operation.Mosaic
-import org.geotools.coverage.processing.operation.Mosaic.GridGeometryPolicy
-import org.geotools.factory.{GeoTools, Hints}
+import org.geotools.factory.{ GeoTools, Hints }
 import org.opengis.coverage.grid.GridCoverage
 import org.opengis.parameter.ParameterValueGroup
 
-import java.io.{DataInputStream, DataOutputStream, File}
+import java.io.{ DataInputStream, DataOutputStream, File }
 import java.util.UUID
 import javax.imageio.ImageIO
-import javax.media.jai.{ImageLayout, JAI}
+import javax.media.jai.{ ImageLayout, JAI }
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
 import scala.reflect._
@@ -116,68 +90,94 @@ object GeowaveLayerWriter extends LazyLogging {
 
     val pluginOptions = new DataStorePluginOptions
     pluginOptions.setFactoryOptions(as.aro)
-    val configOptions = pluginOptions.getFactoryOptionsAsMap
-    val geotrellisKvToGeoWaveKv: Iterable[(K, V)] => Iterable[(Key, Value)] = p => {
-      val gwMetadata = new java.util.HashMap[String, String](); gwMetadata.put("cellType", cellType)
-      configOptions.put(
-        GeoWaveStoreFinder.STORE_HINT_OPTION.getName(),
-        "accumulo")
-      /* Produce mosaic from all of the tiles in this partition */
-      val sources = new java.util.ArrayList[GridCoverage2D]
-      p.map({ case kv => sources.add(geotrellisKvToGeotools(kv)); Unit }).toList
-      val accumuloKvs = ListBuffer[Iterable[(Key, Value)]]()
-      /* Objects for writing into GeoWave */
-      if (sources.size > 0) {
-        val processor = CoverageProcessor.getInstance(GeoTools.getDefaultHints())
-        val param = processor.getOperation("Mosaic").getParameters()
-        val hints = new Hints
-        val imageLayout = new ImageLayout
-        logger.info(s"partition size = ${sources.size}")
-        param.parameter("Sources").setValue(sources)
-        hints.put(JAI.KEY_IMAGE_LAYOUT, imageLayout)
-        imageLayout.setTileHeight(256)
-        imageLayout.setTileWidth(256)
-        val index = (new SpatialDimensionalityTypeProvider.SpatialIndexBuilder).createIndex()
-        val image = processor.doOperation(param, hints).asInstanceOf[GridCoverage2D]
-        val adapter = new RasterDataAdapter(coverageName, gwMetadata, image, 256, false, false, Array.fill[Array[Double]](image.getNumSampleDimensions)(Array(0.0))) // image only used for sample and color metadata, not data, overriding default merge strategy because geotrellis data is already tiled (non-overlapping)
-        for (
-          statsAggregator <- managed(new StatsCompositionTool(new DataStoreStatisticsProvider(
-            adapter,
-            index,
-            true),
-            GeoWaveStoreFinder.createDataStatisticsStore(configOptions)))
-        ) {
-          val kvGen = new AccumuloKeyValuePairGenerator[GridCoverage](
-            adapter,
-            index,
-            statsAggregator,
-            DataStoreUtils.UNCONSTRAINED_VISIBILITY.asInstanceOf[VisibilityWriter[GridCoverage]])
 
-          adapter.convertToIndex(index, image).foreach { x => (accumuloKvs += (kvGen.constructKeyValuePairs(adapter.getAdapterId.getBytes, x).toList.map { kv => (kv.getKey, kv.getValue) })) }
+    val configOptions = pluginOptions.getFactoryOptionsAsMap
+
+    val geotrellisKvToGeoWaveKv: Iterable[(K, V)] => Iterable[(Key, Value)] = { pair =>
+      {
+        val gwMetadata = new java.util.HashMap[String, String](); gwMetadata.put("cellType", cellType)
+        configOptions.put(
+          GeoWaveStoreFinder.STORE_HINT_OPTION.getName(),
+          "accumulo")
+
+        /* Produce mosaic from all of the tiles in this partition */
+        val sources = new java.util.ArrayList[GridCoverage2D]
+        pair.map({ case kv => sources.add(geotrellisKvToGeotools(kv)); Unit }).toList
+        val accumuloKvs = ListBuffer[Iterable[(Key, Value)]]()
+
+        /* Objects for writing into GeoWave */
+        if (sources.size > 0) {
+          val processor = CoverageProcessor.getInstance(GeoTools.getDefaultHints())
+          val param = processor.getOperation("Mosaic").getParameters()
+          val hints = new Hints
+          val imageLayout = new ImageLayout
+          imageLayout.setTileHeight(256)
+          imageLayout.setTileWidth(256)
+
+          logger.info(s"partition size = ${sources.size}")
+          param.parameter("Sources").setValue(sources)
+          hints.put(JAI.KEY_IMAGE_LAYOUT, imageLayout)
+
+          val index = (new SpatialDimensionalityTypeProvider.SpatialIndexBuilder).createIndex()
+          val image = processor.doOperation(param, hints).asInstanceOf[GridCoverage2D]
+          val adapter = new RasterDataAdapter(
+            coverageName,
+            gwMetadata,
+            image,
+            256, false, false,
+            Array.fill[Array[Double]](image.getNumSampleDimensions)(Array(0.0))) // image only used for sample and color metadata, not data, overriding default merge strategy because geotrellis data is already tiled (non-overlapping)
+
+          for (
+            statsAggregator <- managed(new StatsCompositionTool(new DataStoreStatisticsProvider(
+              adapter,
+              index,
+              true),
+              GeoWaveStoreFinder.createDataStatisticsStore(configOptions)))
+          ) {
+            val kvGen = new AccumuloKeyValuePairGenerator[GridCoverage](
+              adapter,
+              index,
+              statsAggregator,
+              DataStoreUtils.UNCONSTRAINED_VISIBILITY.asInstanceOf[VisibilityWriter[GridCoverage]])
+
+            adapter.convertToIndex(index, image).foreach { x => (accumuloKvs += (kvGen.constructKeyValuePairs(adapter.getAdapterId.getBytes, x).toList.map { kv => (kv.getKey, kv.getValue) })) }
+          }
         }
+        accumuloKvs.foldLeft(Iterable[(Key, Value)]())(_ ++ _)
       }
-      accumuloKvs.foldLeft(Iterable[(Key, Value)]())(_ ++ _)
     }
+
     val index = (new SpatialDimensionalityTypeProvider.SpatialIndexBuilder).createIndex()
     val indexName = index.getId.getString
     val tableName = AccumuloUtils.getQualifiedTableName(as.geowaveNamespace, indexName)
 
     val gwMetadata = new java.util.HashMap[String, String](); gwMetadata.put("cellType", cellType)
     val basicOperations = new BasicAccumuloOperations(
-      as.zookeeper,
+      as.zookeepers,
       as.accumuloInstance,
       as.accumuloUser,
       as.accumuloPass,
       as.geowaveNamespace)
-    val adapter = new RasterDataAdapter(coverageName, gwMetadata, image, 256, true, false, Array.fill[Array[Double]](image.getNumSampleDimensions)(Array(0.0)))
-    //make sure adapter and index get written
+    val adapter = new RasterDataAdapter(
+      coverageName,
+      gwMetadata,
+      image,
+      256, true, false,
+      Array.fill[Array[Double]](image.getNumSampleDimensions)(Array(0.0))
+    )
+
+    // make sure adapter get written
     val adapterStore = new AccumuloAdapterStore(basicOperations)
     adapterStore.addAdapter(adapter)
+
+    // make sure index gets written
     val indexStore = new AccumuloIndexStore(basicOperations)
     indexStore.addIndex(index)
-    //make sure adapter and index are associated together in the mapping store
+
+    // make sure adapter and index are associated together in the mapping store
     val mappingStore = new AccumuloAdapterIndexMappingStore(basicOperations)
     mappingStore.addAdapterIndexMapping(new AdapterToIndexMapping(adapter.getAdapterId, Array(index.getId)))
+
     AccumuloUtils.attachRowMergingIterators(
       adapter,
       basicOperations,
@@ -189,17 +189,35 @@ object GeowaveLayerWriter extends LazyLogging {
         .sortBy({ case (k, v) => SpatialKey.keyToTup(k.asInstanceOf[SpatialKey]) })
         .groupBy({ case (k, _) => k.asInstanceOf[SpatialKey]._1 })
         .map(_._2)
-        .mapPartitions(partitions => partitions.map(geotrellisKvToGeoWaveKv)).flatMap(i => i), AccumuloInstance(as.accumuloInstance, as.zookeeper, as.accumuloUser, new PasswordToken(as.accumuloPass)), tableName)
+        .mapPartitions({ partitions => partitions.map(geotrellisKvToGeoWaveKv) })
+        .flatMap(i => i), // first argument to accumuloWriter.write
+      AccumuloInstance(
+        as.accumuloInstance,
+        as.zookeepers,
+        as.accumuloUser,
+        new PasswordToken(as.accumuloPass)
+      ), // second argument
+      tableName // third argument
+    )
 
-    //compact and detach iterator
-    val conn = ConnectorPool.getInstance.getConnector(as.zookeeper,
+    val conn = ConnectorPool.getInstance.getConnector(
+      as.zookeepers,
       as.accumuloInstance,
       as.accumuloUser,
-      as.accumuloPass)
+      as.accumuloPass
+    )
+
+    // compact
     val ops = conn.tableOperations()
     ops.compact(tableName, null, null, true, true)
+
+    // detach iterator
     val iterators = ops.listIterators(tableName)
-    iterators.foreach(kv => if (kv._1.startsWith(RasterTileRowTransform.TRANSFORM_NAME)) { ops.removeIterator(tableName, kv._1, kv._2) })
+    iterators.foreach({ kv =>
+      if (kv._1.startsWith(RasterTileRowTransform.TRANSFORM_NAME)) {
+        ops.removeIterator(tableName, kv._1, kv._2)
+      }
+    })
   }
 }
 
@@ -213,11 +231,12 @@ class GeowaveLayerWriter(
     layerId: LayerId,
     rdd: RDD[(K, V)] with Metadata[M],
     keyIndex: KeyIndex[K]): Unit = {
+
     val LayerId(coverageName, zoom) = layerId
     val specimen = rdd.first
 
     // Perform checks
-    if (zoom > 0) logger.warn("The zoom level is ignored because GeoWave does its own pyramiding")
+    if (zoom > 0) logger.warn("The zoom level is mostly ignored because GeoWave does its own pyramiding")
     specimen._1 match {
       case _: SpatialKey =>
       case _ => throw new Exception(s"Unsupported Key Type: ${implicitly[ClassTag[K]].toString}")
