c16745bc3a2bed121823d8c2082789b59842727c
Colin Woodbury <colingw@gmail.com>
2016-08-16 12:54:15 -0700

    (vt) Fix a merge conflict

--- scripts/publish-snapshot.sh
+++ scripts/publish-snapshot.sh
@@ -3,17 +3,12 @@
 ./sbt "project accumulo" publish \
       "project cassandra" publish \
       "project geotools" publish \
+      "project hbase" publish \
       "project macros" publish \
       "project proj4" publish \
       "project raster" publish \
       "project raster-testkit" publish \
       "project s3" publish \
-<<<<<<< HEAD
-=======
-      "project accumulo" publish \
-      "project cassandra" publish \
-      "project hbase" publish \
->>>>>>> master
       "project shapefile" publish \
       "project spark" publish \
       "project spark-testkit" publish \
