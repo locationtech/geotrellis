25588e5ff04f82fbb65a9cb474a19ebbc7a2b02b
James McClain <jmcclain@azavea.com>
2016-08-27 19:21:00 -0400

    Changes to KryoRegistrator

--- geowave/src/main/scala/geotrellis/spark/io/kryo/GeowaveKryoRegistrator.scala
+++ geowave/src/main/scala/geotrellis/spark/io/kryo/GeowaveKryoRegistrator.scala
@@ -1,29 +1,24 @@
-package geotrellis.spark.io.geowave
+package geotrellis.spark.io.kryo
 
-import geotrellis.spark.io.kryo.KryoRegistrator
-
-import mil.nga.giat.geowave.core.index.Persistable
-import mil.nga.giat.geowave.core.index.PersistenceUtils
-import org.apache.accumulo.core.data.Key
 import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.Serializer
+import mil.nga.giat.geowave.core.index.Persistable
+import mil.nga.giat.geowave.core.index.PersistenceUtils
+import org.apache.accumulo.core.data.Key
 
 
-object GeowaveKryoRegistrator {
-
-  // GeoWave registrator
-  class GeoWaveKryoRegistrator extends KryoRegistrator {
-    override def registerClasses(kryo: Kryo) = {
-      kryo.addDefaultSerializer(classOf[Persistable], new PersistableSerializer())
-      kryo.register(classOf[Key])
-      super.registerClasses(kryo)
-    }
+// GeoWave registrator
+class GeowaveKryoRegistrator extends KryoRegistrator {
+  override def registerClasses(kryo: Kryo) = {
+    kryo.addDefaultSerializer(classOf[Persistable], new PersistableSerializer())
+    kryo.register(classOf[Key])
+    super.registerClasses(kryo)
   }
 
   //Default serializer for any GeoWave Persistable object
-  class PersistableSerializer extends Serializer[Persistable] {
+  private class PersistableSerializer extends Serializer[Persistable] {
     override def write(kryo: Kryo, output: Output, geowaveObj: Persistable): Unit = {
       val bytes = PersistenceUtils.toBinary(geowaveObj)
       output.writeInt(bytes.length)
