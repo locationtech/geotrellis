59ad8c1c4e4fc6f20f795c32986dbee630f5fd03
jbouffard <jbouffard1993@gmail.com>
2016-10-28 18:41:46 -0400

    Added test for reading in BigTiffs

--- raster-test/src/test/scala/geotrellis/raster/io/geotiff/BigTiffSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/io/geotiff/BigTiffSpec.scala
@@ -4,23 +4,88 @@ import geotrellis.util._
 import geotrellis.raster.io.geotiff.reader._
 import geotrellis.raster._
 import geotrellis.raster.testkit._
+import geotrellis.vector.Extent
 
 import org.scalatest._
 
 class BigTiffSpec extends FunSpec with RasterMatchers with GeoTiffTestUtils {
   describe("Reading BigTiffs") {
-    //val path2 = "raster-test/data/geotiff-test-files/bigtiffs/ls8_int32-big.tif"
-    //val path = "raster-test/data/geotiff-test-files/bigtiffs/bit_striped-big.tif"
-    //val path2 = "raster-test/data/geotiff-test-files/bigtiffs/all-ones-big.tif"
-    //val path2 = "raster-test/data/geotiff-test-files/bigtiffs/alaska-big.tif"
-    val path2 = "raster-test/data/geotiff-test-files/bigtiffs/green-1-big.tif"
+    val smallPath = "raster-test/data/geotiff-test-files/ls8_int32.tif"
+    val bigPath = "raster-test/data/geotiff-test-files/bigtiffs/ls8_int32-big.tif"
 
-    val path = "raster-test/data/geotiff-test-files/ls8_int32.tif"
-    val local = LocalBytesStreamer(path2, 25600000)
-    val reader = StreamByteReader(local)
-    println("\nNot Big")
-    println(TiffTagsReader.read(path2))
-    println("\nBig")
-    println(TiffTagsReader.read(reader))
+    val smallPathMulti = "raster-test/data/geotiff-test-files/multi.tif" 
+    val bigPathMulti = "raster-test/data/geotiff-test-files/bigtiffs/multi-big.tif" 
+
+    val chunkSize = 500
+
+    it("should read in the entire SinglebandGeoTiff") {
+      val local = LocalBytesStreamer(bigPath, chunkSize)
+      val reader = StreamByteReader(local)
+      val actual = SinglebandGeoTiff(reader)
+      val expected = SinglebandGeoTiff(smallPath)
+
+      assertEqual(actual, expected)
+    }
+    
+    it("should read in a cropped SinlebandGeoTiff from the edge") {
+      val local = LocalBytesStreamer(bigPath, chunkSize)
+      val reader = StreamByteReader(local)
+      val tiffTags = TiffTagsReader.read(smallPath)
+      val extent = tiffTags.extent
+      val e = Extent(extent.xmin, extent.ymin, extent.xmin + 100, extent.ymin + 100)
+
+      val actual = SinglebandGeoTiff(reader, e)
+      val expected = SinglebandGeoTiff(smallPath, e)
+
+      assertEqual(actual, expected)
+    }
+    
+    it("should read in a cropped SinglebandGeoTiff in the middle") {
+      val local = LocalBytesStreamer(bigPath, chunkSize)
+      val reader = StreamByteReader(local)
+      val tiffTags = TiffTagsReader.read(smallPath)
+      val extent = tiffTags.extent
+      val e = Extent(extent.xmin + 100 , extent.ymin + 100, extent.xmax - 250, extent.ymax - 250)
+
+      val actual = SinglebandGeoTiff(reader, e)
+      val expected = SinglebandGeoTiff(smallPath, e)
+
+      assertEqual(actual, expected)
+    }
+    
+    it("should read in the entire MultibandGeoTiff") {
+      val local = LocalBytesStreamer(bigPathMulti, chunkSize)
+      val reader = StreamByteReader(local)
+      val actual = MultibandGeoTiff(reader)
+      val expected = MultibandGeoTiff(smallPathMulti)
+
+      assertEqual(actual, expected)
+    }
+
+    it("should read in a cropped MultibandGeoTiff from the edge") {
+      val local = LocalBytesStreamer(bigPathMulti, chunkSize)
+      val reader = StreamByteReader(local)
+      val tiffTags = TiffTagsReader.read(smallPathMulti)
+      val extent = tiffTags.extent
+      val e = Extent(extent.xmin, extent.ymin, extent.xmin + 100, extent.ymin + 100)
+
+      val actual = MultibandGeoTiff(reader, e)
+      val expected = MultibandGeoTiff(smallPathMulti, e)
+
+      assertEqual(actual, expected)
+    }
+    
+    it("should read in a cropped MultibandGeoTiff in the middle") {
+      val local = LocalBytesStreamer(bigPathMulti, chunkSize)
+      val reader = StreamByteReader(local)
+      val tiffTags = TiffTagsReader.read(smallPathMulti)
+      val extent = tiffTags.extent
+      val e = Extent(extent.xmin + 100 , extent.ymin + 100, extent.xmax - 250, extent.ymax - 250)
+
+      val actual = MultibandGeoTiff(reader, e)
+      val expected = MultibandGeoTiff(smallPathMulti, e)
+
+      assertEqual(actual, expected)
+    }
   }
 }
