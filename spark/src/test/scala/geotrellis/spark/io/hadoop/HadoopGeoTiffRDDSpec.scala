ba989bea0748ee113ab3752dff56c51ad8cb0cd7
jbouffard <jbouffard1993@gmail.com>
2016-11-01 13:59:14 -0400

    Added TemporalGeoTiff testing in HadoopGeoTiffRDDSpec

--- spark/src/test/scala/geotrellis/spark/io/hadoop/HadoopGeoTiffRDDSpec.scala
+++ spark/src/test/scala/geotrellis/spark/io/hadoop/HadoopGeoTiffRDDSpec.scala
@@ -5,6 +5,7 @@ import geotrellis.raster.testkit.RasterMatchers
 import geotrellis.spark._
 import geotrellis.spark.tiling._
 import geotrellis.spark.testfiles._
+import geotrellis.spark.io.hadoop.formats._
 
 import org.apache.hadoop.fs.Path
 import org.scalatest._
@@ -16,7 +17,8 @@ class HadoopGeoTiffRDDSpec
     with TestEnvironment
     with TestFiles {
   describe("HadoopGeoTiffRDD") {
-    it("should read the same rasters when reading small windows or with no windows, SinglebandGeoTiff") {
+
+    it("should read the same rasters when reading small windows or with no windows, Spatial, SinglebandGeoTiff") {
       val tilesDir = new Path(localFS.getWorkingDirectory, "raster-test/data/one-month-tiles/")
       val source1 = HadoopGeoTiffRDD.spatial(tilesDir)
       val source2 = HadoopGeoTiffRDD.spatial(tilesDir, HadoopGeoTiffRDD.Options(maxTileSize = Some(128)))
@@ -29,10 +31,30 @@ class HadoopGeoTiffRDDSpec
       assertEqual(stitched1, stitched2)
     }
     
-    it("should read the same rasters when reading small windows or with no windows, MultibandGeoTiff") {
+    it("should read the same rasters when reading small windows or with no windows, Spatial, MultibandGeoTiff") {
       val tilesDir = new Path(localFS.getWorkingDirectory, "raster-test/data/geotiff-test-files/3bands/byte/")
       val source1 = HadoopGeoTiffRDD.spatialMultiband(tilesDir)
       val source2 = HadoopGeoTiffRDD.spatialMultiband(tilesDir, HadoopGeoTiffRDD.Options(maxTileSize = Some(128)))
+
+      val (_, md) = source1.collectMetadata[SpatialKey](FloatingLayoutScheme(256))
+
+      val stitched1 = source1.tileToLayout(md).stitch
+      val stitched2 = source2.tileToLayout(md).stitch
+
+      assertEqual(stitched1, stitched2)
+    }
+    
+    it("should read the same rasters when reading small windows or with no windows, Temporal, SinglebandGeoTiff") {
+      val tilesDir = new Path(localFS.getWorkingDirectory, "spark/src/test/resources/nex-geotiff")
+
+      val source1 = HadoopGeoTiffRDD.temporal(tilesDir, HadoopGeoTiffRDD.Options(
+        timeTag = "ISO_TIME",
+        timeFormat = "yyyy-MM-dd'T'HH:mm:ss"))
+
+      val source2 = HadoopGeoTiffRDD.temporal(tilesDir, HadoopGeoTiffRDD.Options(
+        timeTag = "ISO_TIME",
+        timeFormat = "yyyy-MM-dd'T'HH:mm:ss",
+        maxTileSize = Some(128)))
 
       val (_, md) = source1.collectMetadata[SpatialKey](FloatingLayoutScheme(256))
 
