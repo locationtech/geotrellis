b781ebfc1ed6c5ecdf74fcec1d0240cd749e10d8
jbouffard <jbouffard1993@gmail.com>
2016-11-02 16:50:42 -0400

    Added apply methods to S3GeoTiffRDD

--- s3/src/main/scala/geotrellis/spark/io/s3/S3GeoTiffRDD.scala
+++ s3/src/main/scala/geotrellis/spark/io/s3/S3GeoTiffRDD.scala
@@ -16,4 +16,31 @@ import org.apache.spark.rdd.RDD
 
 import spire.syntax.cfor._
 
-object S3GeoTiffRDD
+object S3GeoTiffRDD {
+
+  def apply[K, V](bucket: String, prefix: String, maxTileSize: Option[Int])(implicit client: S3Client, sc: SparkContext): RDD[(K, V)] = ???
+
+  def spatial(bucket: String, prefix: String)(implicit client: S3Client, sc: SparkContext): RDD[(ProjectedExtent, Tile)] =
+    spatial(bucket, prefix, None)
+
+  def spatial(bucket: String, prefix: String, maxTileSize: Option[Int])(implicit client: S3Client, sc: SparkContext): RDD[(ProjectedExtent, Tile)] =
+    apply[ProjectedExtent, Tile](bucket, prefix, maxTileSize)
+  
+  def spatialMultiband(bucket: String, prefix: String)(implicit client: S3Client, sc: SparkContext): RDD[(ProjectedExtent, MultibandTile)] =
+    spatialMultiband(bucket, prefix, None)
+
+  def spatialMultiband(bucket: String, prefix: String, maxTileSize: Option[Int])(implicit client: S3Client, sc: SparkContext): RDD[(ProjectedExtent, MultibandTile)] =
+    apply[ProjectedExtent, MultibandTile](bucket, prefix, maxTileSize)
+  
+  def temporal(bucket: String, prefix: String)(implicit client: S3Client, sc: SparkContext): RDD[(TemporalProjectedExtent, Tile)] =
+    temporal(bucket, prefix, None)
+  
+  def temporal(bucket: String, prefix: String, maxTileSize: Option[Int])(implicit client: S3Client, sc: SparkContext): RDD[(TemporalProjectedExtent, Tile)] =
+    apply[TemporalProjectedExtent, Tile](bucket, prefix, maxTileSize)
+  
+  def temporalMultiband(bucket: String, prefix: String)(implicit client: S3Client, sc: SparkContext): RDD[(TemporalProjectedExtent, MultibandTile)] =
+    temporalMultiband(bucket, prefix, None)
+  
+  def temporalMultiband(bucket: String, prefix: String, maxTileSize: Option[Int])(implicit client: S3Client, sc: SparkContext): RDD[(TemporalProjectedExtent, MultibandTile)] =
+    apply[TemporalProjectedExtent, MultibandTile](bucket, prefix, maxTileSize)
+}
