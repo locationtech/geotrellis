0722e2f84d6874ee2d8056f71dbafdb47d342f00
James McClain <jmcclain@azavea.com>
2016-11-04 10:06:12 -0400

    Loosen Constraints on Histogram Equalization

--- raster/src/main/scala/geotrellis/raster/equalization/SinglebandEqualizationMethods.scala
+++ raster/src/main/scala/geotrellis/raster/equalization/SinglebandEqualizationMethods.scala
@@ -1,6 +1,6 @@
 package geotrellis.raster.equalization
 
-import geotrellis.raster.histogram.StreamingHistogram
+import geotrellis.raster.histogram._
 import geotrellis.raster.Tile
 import geotrellis.util.MethodExtensions
 
@@ -13,7 +13,7 @@ trait SinglebandEqualizationMethods extends MethodExtensions[Tile] {
     *
     * @param  histogram  The histogram of this tile
     */
-  def equalize(histogram: StreamingHistogram): Tile = HistogramEqualization(self, histogram)
+  def equalize[T <: AnyVal](histogram: Histogram[T]): Tile = HistogramEqualization(self, histogram)
 
   /**
     * Equalize the histogram of this [[Tile]].
