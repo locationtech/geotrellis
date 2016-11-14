c7ba5348d09b1842077321db4c8ac86cb540df73
jbouffard <tue68667@temple.edu>
2016-11-14 17:20:02 -0500

    Modified setup so that it'd work when trying to create a package

--- geotrellis-python/setup.py
+++ geotrellis-python/setup.py
@@ -1,18 +1,13 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
-    config = {
-        'description': 'pygeotrellis',
-        'author': 'Jacob Bouffard',
-        'download_url': 'http://github.com/geotrellis/geotrellis',
-        'author_email': 'jbouffard@azavea.com',
-        'version': '0.1',
-        'install_requires': ['py4j'],
-        'packages': ['pygeotrellis'],
-        'scripts': [],
-        'name': 'geotrellis-python'
-    }
-
-    setup(**config)
+setup(
+        description: 'pygeotrellis',
+        author: 'Jacob Bouffard',
+        download_url: 'http://github.com/geotrellis/geotrellis',
+        author_email: 'jbouffard@azavea.com',
+        version: '0.1',
+        install_requires: ['py4j'],
+        packages: ['pygeotrellis'],
+        scripts: [],
+        name: 'pygeotrellis'
+        )
