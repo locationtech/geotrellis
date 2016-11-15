d2bd8d42b384da358fe59363ef892152dbb887d2
jbouffard <tue68667@temple.edu>
2016-11-15 11:37:49 -0500

    Updated test so that it now prints out the Proj4String

--- geotrellis-python/pygeotrellis/test.py
+++ geotrellis-python/pygeotrellis/test.py
@@ -5,10 +5,16 @@ def main():
     jvm = gateway.jvm
     path = "../econic.tif"
 
-    print "\nReading a GeoTiff on the Python side"
-    result = jvm.GeoTiffReader.readSingleband(path)
+    print "\nReading a Proj4String from a SinglebandGeoTiff"
+    singleband_geotiff = jvm.GeoTiffReader.readSingleband(path)
     print "Here is the result of my read"
-    print result
+    crs = singleband_geotiff.crs
+
+    # this won't work
+    # print crs.toProj4String
+
+    # this will work
+    print jvm.crs.toProj4String
 
 if __name__ == "__main__":
     main()
