c8cac4f4538660b35493ba0fadb188a8524a72ef
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-11-22 19:16:11 +0300

    +tileToLayout implicits; publish scripts update
    Signed-off-by: Grigory Pomadchin <gr.pomadchin@gmail.com>

--- scripts/createHeaders.sh
+++ scripts/createHeaders.sh
@@ -7,6 +7,7 @@
       "project geotools" createHeaders test:createHeaders \
       "project geowave" createHeaders test:createHeaders \
       "project hbase" createHeaders  test:createHeaders  \
+      "project pdal" createHeaders test:createHeaders \
       "project proj4" createHeaders test:createHeaders \
       "project raster" createHeaders test:createHeaders \
       "project raster-test" createHeaders test:createHeaders \
