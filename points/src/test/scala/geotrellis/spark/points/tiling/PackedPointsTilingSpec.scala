7b248606b02f2e6e718174157406f006773df430
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-11-24 16:04:33 +0300

    improved tests

--- points/src/test/scala/geotrellis/spark/points/tiling/PackedPointsTilingSpec.scala
+++ points/src/test/scala/geotrellis/spark/points/tiling/PackedPointsTilingSpec.scala
@@ -17,27 +17,26 @@
 package geotrellis.spark.points.tiling
 
 import geotrellis.raster.TileLayout
-import geotrellis.spark.TestEnvironment
+import geotrellis.spark.PointsTestEnvironment
 import geotrellis.spark.points.tiling.Implicits._
 import geotrellis.spark.tiling.LayoutDefinition
 import geotrellis.spark.io.hadoop.HadoopPackedPointsRDD
 import geotrellis.vector.Extent
 
-import org.apache.hadoop.fs.Path
 import org.scalatest._
-
-import java.io.File
 
 class PackedPointsTilingSpec extends FunSpec
   with Matchers
-  with TestEnvironment {
+  with PointsTestEnvironment {
   describe("Points RDD tiling") {
-    val testResources = new File("src/test/resources")
-
     it("should tile RDD of packed points") {
-      val source = HadoopPackedPointsRDD(new Path(s"file://${testResources.getAbsolutePath}/las"))
+      val source = HadoopPackedPointsRDD(lasPath)
       val original = source.take(1).map(_._2).toList.head
-      val ld = LayoutDefinition(Extent(635609.85, 848889.7, 638992.55, 853545.43), TileLayout(5,5,5,5))
+      // that means there can be no more points per "tile" than tileCols * tileRows
+      val ld = LayoutDefinition(
+        Extent(635609.85, 848889.7, 638992.55, 853545.43),
+        TileLayout(layoutCols = 5, layoutRows = 5, tileCols = 10, tileRows = 10)
+      )
       val tiled = withTilerMethods(source).tileToLayout(ld)
       tiled.map(_._2.length).reduce(_ + _) should be (original.length)
       tiled.count() should be (25)
