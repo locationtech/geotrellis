02e8d67ddd9f7d32d300b92773bfeeb844e83873
James McClain <jmcclain@azavea.com>
2016-12-01 19:31:02 -0500

    Filtering Cartesian RDD
    This is largely identical to the CartesianRDD type provided by Spark,
    except that it accepts a predicate which allows non-viable pairs to be
    not generated.  This can reduce computation and communication.
    
    In the case of VectorJoin, items in partitions with disjoint envelopes
    are never generated.

---
+++ spark/src/main/scala/org/apache/spark/rdd/FilteredCartesianRDD.scala
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.rdd
+
+import java.io.{IOException, ObjectOutputStream}
+
+import scala.reflect.ClassTag
+
+import org.apache.spark._
+import org.apache.spark.util.Utils
+
+
+sealed class FilteredCartesianRDD[T: ClassTag, U: ClassTag](
+    sc: SparkContext,
+    val pred: (Int, Int) => Boolean,
+    var rdd1 : RDD[T],
+    var rdd2 : RDD[U])
+  extends RDD[(T, U)](sc, Nil)
+  with Serializable {
+
+  val numPartitionsInRdd2 = rdd2.partitions.length
+
+  override def getPartitions: Array[Partition] = {
+    // create the cross product split
+    val array = new Array[Partition](rdd1.partitions.length * rdd2.partitions.length)
+    for (s1 <- rdd1.partitions; s2 <- rdd2.partitions) {
+      val idx = s1.index * numPartitionsInRdd2 + s2.index
+      array(idx) = new CartesianPartition(idx, rdd1, rdd2, s1.index, s2.index)
+    }
+    array
+  }
+
+  override def getPreferredLocations(split: Partition): Seq[String] = {
+    val currSplit = split.asInstanceOf[CartesianPartition]
+    (rdd1.preferredLocations(currSplit.s1) ++ rdd2.preferredLocations(currSplit.s2)).distinct
+  }
+
+  override def compute(split: Partition, context: TaskContext): Iterator[(T, U)] = {
+    val currSplit = split.asInstanceOf[CartesianPartition]
+    if (pred(currSplit.s1.index, currSplit.s2.index)) {
+      for (x <- rdd1.iterator(currSplit.s1, context);
+           y <- rdd2.iterator(currSplit.s2, context)) yield (x, y)
+    }
+    else Iterator.empty
+  }
+
+  override def getDependencies: Seq[Dependency[_]] = List(
+    new NarrowDependency(rdd1) {
+      def getParents(id: Int): Seq[Int] = List(id / numPartitionsInRdd2)
+    },
+    new NarrowDependency(rdd2) {
+      def getParents(id: Int): Seq[Int] = List(id % numPartitionsInRdd2)
+    }
+  )
+
+  override def clearDependencies() {
+    super.clearDependencies()
+    rdd1 = null
+    rdd2 = null
+  }
+}
