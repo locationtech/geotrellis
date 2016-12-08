98226cf24d6eb8c5f79e89e17e789a5dc7aed9e3
jbouffard <jbouffard@azavea.com>
2016-12-08 14:34:27 -0500

    Changed how bytes are read in for the Int16GeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/Int16GeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/Int16GeoTiffTile.scala
@@ -50,94 +50,23 @@ class Int16GeoTiffTile(
         }
       }
     }
-
     ShortArrayTile(arr, cols, rows, cellType)
   }
 
-
-  def cropStrippedSegmentBytes(
-    segmentBytes: SegmentBytes,
-    segmentLayout: GeoTiffSegmentLayout,
-    bytesPerPixel: Int,
-    windowBounds: GridBounds
-  ): Array[Byte] = {
-    val segmentIds = segmentBytes.intersectingSegments
-    val arr = Array.ofDim[Byte](windowBounds.size * bytesPerPixel)
-    var counter: Int = 0
-    // must traverse in TiffTags order
-    cfor(0)(_ < segmentIds.length, _ + 1) { i =>
-      val segmentId = segmentIds(i)
-      val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-      // TODO: GeoTiffTile saves _lastSegment, so repeated fetched don't refetch
-      // does that mean we have to put this in the super-class or can this be in an object?
-      val segment: Array[Byte] = segmentBytes.getSegment(segmentId)
-
-      val result = windowBounds.intersection(segmentGridBounds).get
-      val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-      val adjStart = intersection.start * bytesPerPixel
-      val adjEnd = intersection.end * bytesPerPixel
-      val adjCols = cols * bytesPerPixel
-      val adjWidth = result.width * bytesPerPixel
-
-      cfor(adjStart)(_ < adjEnd, _ + adjCols) { i =>
-        System.arraycopy(segment, i -  adjStart, arr, counter, adjWidth)
-        counter += adjWidth
-      }
-    }
-    arr
-  }
-
   def crop(gridBounds: GridBounds): MutableArrayTile = {
-    val arr = Array.ofDim[Byte](gridBounds.size * ShortConstantNoDataCellType.bytes)
-    var counter = 0
+    val bytesPerPixel: Int = ShortConstantNoDataCellType.bytes
+    val arr: Array[Byte] =
+      if (segmentLayout.isStriped)
+        readStrippedSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
+      else
+        readTiledSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
 
-    val segments = segmentBytes.intersectingSegments
-
-    if (segmentLayout.isStriped) {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-        val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-        val segment = getSegment(segmentId)
-
-        val result = gridBounds.intersection(segmentGridBounds).get
-        val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-        val adjStart = intersection.start * ShortConstantNoDataCellType.bytes
-        val adjEnd = intersection.end * ShortConstantNoDataCellType.bytes
-        val adjCols = cols * ShortConstantNoDataCellType.bytes
-        val adjWidth = result.width * ShortConstantNoDataCellType.bytes
-
-        cfor(adjStart)(_ < adjEnd, _ + adjCols) { i =>
-          System.arraycopy(segment.bytes, i - adjStart, arr, counter, adjWidth)
-          counter += adjWidth
-        }
-      }
-    } else {
-      cfor(0)(_ < segments.length, _ + 1) {i =>
-        val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-        val segment = getSegment(i)
-        val segmentTransform = segmentLayout.getSegmentTransform(i)
-
-        val result = gridBounds.intersection(segmentGridBounds).get
-        val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-        val adjStart = intersection.start * ShortConstantNoDataCellType.bytes
-        val adjEnd = intersection.end * ShortConstantNoDataCellType.bytes
-        val adjWidth = result.width * ShortConstantNoDataCellType.bytes
-        val adjTileWidth = intersection.tileWidth * ShortConstantNoDataCellType.bytes
-
-        cfor(adjStart)(_ < adjEnd, _ + adjTileWidth) { i =>
-          val col = segmentTransform.indexToCol(i / ShortConstantNoDataCellType.bytes)
-          val row = segmentTransform.indexToRow(i / ShortConstantNoDataCellType.bytes)
-          if (gridBounds.contains(col, row)) {
-            val j = (row - gridBounds.rowMin) * gridBounds.width + (col - gridBounds.colMin)
-            System.arraycopy(segment.bytes, i, arr, j * ShortConstantNoDataCellType.bytes, adjWidth)
-          }
-        }
-      }
-    }
     ShortArrayTile.fromBytes(arr, gridBounds.width, gridBounds.height, cellType)
   }
 
