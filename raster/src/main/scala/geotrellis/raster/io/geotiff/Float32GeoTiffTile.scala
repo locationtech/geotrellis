02d4747c5dec66f494e1ca5a6110988edcb4a538
jbouffard <jbouffard@azavea.com>
2016-12-08 14:35:13 -0500

    Changed how bytes are read in for the Float32GeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/Float32GeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/Float32GeoTiffTile.scala
@@ -41,37 +41,7 @@ class Float32GeoTiffTile(
    * @param  CroppedGeoTiff  The [[WindowedGeoTiff]] of the file
    * @return                 A [[FloatArrayTile]]
    */
-  def mutable: MutableArrayTile = {
-    val arr = Array.ofDim[Byte](cols * rows * FloatConstantNoDataCellType.bytes)
-
-    if(segmentLayout.isStriped) {
-      var i = 0
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment =
-          getSegment(segmentIndex)
-        val size = segment.bytes.size
-        System.arraycopy(segment.bytes, 0, arr, i, size)
-        i += size
-      }
-    } else {
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment =
-          getSegment(segmentIndex)
-
-        val segmentTransform = segmentLayout.getSegmentTransform(segmentIndex)
-        val width = segmentTransform.segmentCols * FloatConstantNoDataCellType.bytes
-        val tileWidth = segmentLayout.tileLayout.tileCols * FloatConstantNoDataCellType.bytes
-
-        cfor(0)(_ < tileWidth * segmentTransform.segmentRows, _ + tileWidth) { i =>
-          val col = segmentTransform.indexToCol(i / FloatConstantNoDataCellType.bytes)
-          val row = segmentTransform.indexToRow(i / FloatConstantNoDataCellType.bytes)
-          val j = ((row * cols) + col) * FloatConstantNoDataCellType.bytes
-          System.arraycopy(segment.bytes, i, arr, j, width)
-        }
-      }
-    }
-    FloatArrayTile.fromBytes(arr, cols, rows, cellType)
-  }
+  def mutable: MutableArrayTile = crop(gridBounds)
 
   /**
    * Reads a windowed area out of a [[GeoTiffTile]] and create a
@@ -81,52 +51,19 @@ class Float32GeoTiffTile(
    * @return                 A [[FloatArrayTile]] that conatins data from the windowed area
    */
   def crop(gridBounds: GridBounds): MutableArrayTile = {
-    val arr = Array.ofDim[Byte](gridBounds.size * FloatConstantNoDataCellType.bytes)
-		val segments = segmentBytes.intersectingSegments
-    var counter = 0
+    val bytesPerPixel: Int = FloatConstantNoDataCellType.bytes
+    val arr: Array[Byte] =
+      if (segmentLayout.isStriped)
+        readStrippedSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
+      else
+        readTiledSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
 
-    if (segmentLayout.isStriped) {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-				val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-				val segment = getSegment(segmentId)
-
-				val result = gridBounds.intersection(segmentGridBounds).get
-				val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-				val adjStart = intersection.start * FloatConstantNoDataCellType.bytes
-				val adjEnd = intersection.end * FloatConstantNoDataCellType.bytes
-				val adjCols = segmentGridBounds.width * FloatConstantNoDataCellType.bytes
-				val adjWidth = result.width * FloatConstantNoDataCellType.bytes
-
-				cfor(adjStart)(_ < adjEnd, _ + adjCols) { i =>
-					System.arraycopy(segment.bytes, i - adjStart, arr, counter, adjWidth)
-					counter += adjWidth
-				}
-      }
-    } else {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-				val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-				val segment = getSegment(segmentId)
-				val segmentTransform = segmentLayout.getSegmentTransform(segmentId)
-
-				val result = gridBounds.intersection(segmentGridBounds).get
-				val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-				val adjStart = intersection.start * FloatConstantNoDataCellType.bytes
-				val adjEnd = intersection.end * FloatConstantNoDataCellType.bytes
-				val adjTileWidth = intersection.tileWidth * FloatConstantNoDataCellType.bytes
-				val adjWidth = result.width * FloatConstantNoDataCellType.bytes
-
-				cfor(adjStart)(_ < adjEnd, _ + adjTileWidth) { i =>
-					val col = segmentTransform.indexToCol(i / FloatConstantNoDataCellType.bytes)
-					val row = segmentTransform.indexToRow(i / FloatConstantNoDataCellType.bytes)
-					val j = (row - gridBounds.rowMin) * gridBounds.width + (col - gridBounds.colMin)
-					System.arraycopy(segment.bytes, i, arr, j * FloatConstantNoDataCellType.bytes, adjWidth)
-				}
-      }
-    }
     FloatArrayTile.fromBytes(arr, gridBounds.width, gridBounds.height, cellType)
   }
 
@@ -141,5 +78,5 @@ class Float32GeoTiffTile(
       case _ =>
         withNoData(None).convert(newCellType)
     }
-  }  
+  }
 }
