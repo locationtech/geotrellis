3e390a92b4eebbeca1f9589911d16ed999d988d4
jbouffard <jbouffard@azavea.com>
2016-12-08 14:34:08 -0500

    Changed how bytes are read in for the UByteGeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/UByteGeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/UByteGeoTiffTile.scala
@@ -34,75 +34,22 @@ class UByteGeoTiffTile(
     case UByteUserDefinedNoDataCellType(nd) => Some(nd)
   }
 
-  def mutable: MutableArrayTile = {
-    val arr = Array.ofDim[Byte](cols * rows)
-
-    if(segmentLayout.isStriped) {
-      var i = 0
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment = getSegment(segmentIndex)
-        val size = segment.bytes.size
-        System.arraycopy(segment.bytes, 0, arr, i, size)
-        i += size
-      }
-    } else {
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment = getSegment(segmentIndex)
-
-        val segmentTransform = segmentLayout.getSegmentTransform(segmentIndex)
-        val width = segmentTransform.segmentCols
-        val tileWidth = segmentLayout.tileLayout.tileCols
-
-        cfor(0)(_ < tileWidth * segmentTransform.segmentRows, _ + tileWidth) { i =>
-          val col = segmentTransform.indexToCol(i)
-          val row = segmentTransform.indexToRow(i)
-          val j = (row * cols) + col
-          System.arraycopy(segment.bytes, i, arr, j, width)
-        }
-      }
-    }
-    UByteArrayTile.fromBytes(arr, cols, rows, cellType)
-  }
+  def mutable: MutableArrayTile = crop(gridBounds)
 
   def crop(gridBounds: GridBounds): MutableArrayTile = {
-    val arr = Array.ofDim[Byte](gridBounds.size)
-    var counter = 0
+    val bytesPerPixel: Int = UByteConstantNoDataCellType.bytes
+    val arr: Array[Byte] =
+      if (segmentLayout.isStriped)
+        readStrippedSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
+      else
+        readTiledSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
 
-    val segments = segmentBytes.intersectingSegments
-
-    if (segmentLayout.isStriped) {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-        val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-        val segment = getSegment(segmentId)
-
-        val result = gridBounds.intersection(segmentGridBounds).get
-        val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-        cfor(intersection.startOffset)(_ < intersection.endOffset, _ + cols) { inRowColMin =>
-          System.arraycopy(segment.bytes, inRowColMin - intersection.startOffset, arr, counter, result.width)
-          counter += result.width
-        }
-      }
-    } else {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-        val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-        val segment = getSegment(segmentId)
-        val segmentTransform = segmentLayout.getSegmentTransform(segmentId)
-        val tileWidth = segmentLayout.tileLayout.tileCols
-
-        val result = gridBounds.intersection(segmentGridBounds).get
-        val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-        cfor(intersection.startOffset)(_ < intersection.endOffset, _ + tileWidth) { i =>
-          val col = segmentTransform.indexToCol(i)
-          val row = segmentTransform.indexToRow(i)
-          val j = (row - gridBounds.rowMin) * gridBounds.width + (col - gridBounds.colMin)
-          System.arraycopy(segment.bytes, i, arr, j, result.width)
-        }
-      }
-    }
     UByteArrayTile.fromBytes(arr, gridBounds.width, gridBounds.height, cellType)
   }
 
