d918773e84a2e9cd5069bef30380c555ca5fe776
jbouffard <jbouffard@azavea.com>
2016-12-08 14:35:22 -0500

    Changed how bytes are read in for the Float64GeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/Float64GeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/Float64GeoTiffTile.scala
@@ -34,84 +34,22 @@ class Float64GeoTiffTile(
     case DoubleUserDefinedNoDataCellType(nd) => Some(nd)
   }
 
-  def mutable: MutableArrayTile = {
-    val arr = Array.ofDim[Byte](cols * rows * DoubleConstantNoDataCellType.bytes)
-
-    if(segmentLayout.isStriped) {
-      var i = 0
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment =
-          getSegment(segmentIndex)
-        val size = segment.bytes.size
-        System.arraycopy(segment.bytes, 0, arr, i, size)
-        i += size
-      }
-    } else {
-      cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
-        val segment =
-          getSegment(segmentIndex)
-
-        val segmentTransform = segmentLayout.getSegmentTransform(segmentIndex)
-        val width = segmentTransform.segmentCols * DoubleConstantNoDataCellType.bytes
-        val tileWidth = segmentLayout.tileLayout.tileCols * DoubleConstantNoDataCellType.bytes
-
-        cfor(0)(_ < tileWidth * segmentTransform.segmentRows, _ + tileWidth) { i =>
-          val col = segmentTransform.indexToCol(i / DoubleConstantNoDataCellType.bytes)
-          val row = segmentTransform.indexToRow(i / DoubleConstantNoDataCellType.bytes)
-          val j = ((row * cols) + col) * DoubleConstantNoDataCellType.bytes
-          System.arraycopy(segment.bytes, i, arr, j, width)
-        }
-      }
-    }
-    DoubleArrayTile.fromBytes(arr, cols, rows, cellType)
-  }
+  def mutable: MutableArrayTile = crop(gridBounds)
 
   def crop(gridBounds: GridBounds): MutableArrayTile = {
-    val arr = Array.ofDim[Byte](gridBounds.size * DoubleConstantNoDataCellType.bytes)
-		val segments = segmentBytes.intersectingSegments
-    var counter = 0
-    if (segmentLayout.isStriped) {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-				val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-				val segment = getSegment(segmentId)
+    val bytesPerPixel: Int = DoubleConstantNoDataCellType.bytes
+    val arr: Array[Byte] =
+      if (segmentLayout.isStriped)
+        readStrippedSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
+      else
+        readTiledSegmentBytes(segmentBytes,
+          segmentLayout,
+          bytesPerPixel,
+          gridBounds)
 
-				val result = gridBounds.intersection(segmentGridBounds).get
-				val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-				val adjStart = intersection.start * DoubleConstantNoDataCellType.bytes
-				val adjEnd = intersection.end * DoubleConstantNoDataCellType.bytes
-				val adjCols = segmentGridBounds.width * DoubleConstantNoDataCellType.bytes
-				val adjWidth = result.width * DoubleConstantNoDataCellType.bytes
-
-				cfor(adjStart)(_ < adjEnd, _ + adjCols) { i =>
-					System.arraycopy(segment.bytes, i - adjStart, arr, counter, adjWidth)
-					counter += adjWidth
-				}
-      }
-    } else {
-      cfor(0)(_ < segments.length, _ + 1) { i =>
-				val segmentId = segments(i)
-        val segmentGridBounds = segmentLayout.getGridBounds(segmentId)
-				val segment = getSegment(segmentId)
-				val segmentTransform = segmentLayout.getSegmentTransform(segmentId)
-
-				val result = gridBounds.intersection(segmentGridBounds).get
-				val intersection = Intersection(segmentGridBounds, result, segmentLayout)
-
-				val adjStart = intersection.start * DoubleConstantNoDataCellType.bytes
-				val adjEnd = intersection.end * DoubleConstantNoDataCellType.bytes
-				val adjTileWidth = intersection.tileWidth * DoubleConstantNoDataCellType.bytes
-				val adjWidth = result.width * DoubleConstantNoDataCellType.bytes
-
-				cfor(adjStart)(_ < adjEnd, _ + adjTileWidth) { i =>
-					val col = segmentTransform.indexToCol(i / DoubleConstantNoDataCellType.bytes)
-					val row = segmentTransform.indexToRow(i / DoubleConstantNoDataCellType.bytes)
-					val j = (row - gridBounds.rowMin) * gridBounds.width + (col - gridBounds.colMin)
-					System.arraycopy(segment.bytes, i, arr, j * DoubleConstantNoDataCellType.bytes, adjWidth)
-				}
-      }
-    }
     DoubleArrayTile.fromBytes(arr, gridBounds.width, gridBounds.height, cellType)
   }
 
