3e87239f2dd15dc5709711e6b0f105cf43857dbe
jbouffard <jbouffard@azavea.com>
2016-12-09 16:36:30 -0500

    Reimplamented how segmentTransform is called in UInt16GeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/UInt16GeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/UInt16GeoTiffTile.scala
@@ -39,7 +39,13 @@ class UInt16GeoTiffTile(
     cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
       val segment =
         getSegment(segmentIndex)
-      val segmentTransform = segmentLayout.getSegmentTransform(segmentIndex)
+
+      val segmentTransform =
+        if (segmentLayout.isStriped)
+          StripedSegmentTransform(segmentIndex, segmentLayout)
+        else
+          TiledSegmentTransform(segmentIndex, segmentLayout)
+
       cfor(0)(_ < segment.size, _ + 1) { i =>
         val col = segmentTransform.indexToCol(i)
         val row = segmentTransform.indexToRow(i)
