0c4a82b5247e01e67bb29157cc6eea97704b50dd
jbouffard <jbouffard@azavea.com>
2016-12-09 16:17:28 -0500

    Removed GridIndexTransform from GeoTiffSegmentLayout
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/GeoTiffSegmentLayout.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/GeoTiffSegmentLayout.scala
@@ -20,24 +20,6 @@ import geotrellis.raster.GridBounds
 import geotrellis.raster.TileLayout
 import scala.collection.mutable.ArrayBuffer
 
-/** Specifically for single band segments. If dealing with multiband segments, you must do the math */
-trait GridIndexTransform {
-  def segmentCols: Int
-  def segmentRows: Int
-
-  /** The col of the source raster that this index represents. Can produce invalid cols */
-  def indexToCol(i: Int): Int
-  /** The row of the source raster that this index represents. Can produce invalid rows */
-  def indexToRow(i: Int): Int
-
-  /** Specific to BitGeoTiffSegment index. The col of the source raster that this index represents. */
-  def bitIndexToCol(i: Int): Int
-  /** Specific to BitGeoTiffSegment index. The row of the source raster that this index represents. */
-  def bitIndexToRow(i: Int): Int
-
-  def gridToIndex(col: Int, row: Int): Int
-}
-
 /**
  * This case class represents how the segments in a given [[GeoTiff]] are arranged.
  *
@@ -113,60 +95,13 @@ case class GeoTiffSegmentLayout(totalCols: Int, totalRows: Int, tileLayout: Tile
     (layoutRow * tileLayout.layoutCols) + layoutCol
   }
 
-  /**
-   * @param segmentIndex: An Int that represents the index of the segment
-   * @return A GridIndexTransform Object
-   */
-  def getSegmentTransform(segmentIndex: Int): GridIndexTransform = {
-    val layoutCol = segmentIndex % tileLayout.layoutCols
-    val layoutRow = segmentIndex / tileLayout.layoutCols
-
-    new GridIndexTransform {
-      val segmentCols =
-        if(layoutCol == tileLayout.layoutCols - 1) {
-          totalCols - ( (tileLayout.layoutCols - 1) * tileLayout.tileCols)
-        } else {
-          tileLayout.tileCols
-        }
-
-      val segmentRows =
-        if(layoutRow == tileLayout.layoutRows - 1) {
-          totalRows - ( (tileLayout.layoutRows - 1) * tileLayout.tileRows)
-        } else {
-          tileLayout.tileRows
-        }
-
-      def indexToCol(i: Int) = {
-        val tileCol = i % tileLayout.tileCols
-        (layoutCol * tileLayout.tileCols) + tileCol
-      }
-
-      def indexToRow(i: Int) = {
-        val tileRow = i / tileLayout.tileCols
-        (layoutRow * tileLayout.tileRows) + tileRow
-      }
-
-      def bitIndexToCol(i: Int) = {
-        val tileCol = i % segmentCols
-        (layoutCol * tileLayout.tileCols) + tileCol
-      }
-
-      def bitIndexToRow(i: Int) = {
-        val tileRow = i / segmentCols
-        (layoutRow * tileLayout.tileRows) + tileRow
-      }
-
-      def gridToIndex(col: Int, row: Int): Int = {
-        val tileCol = col - (layoutCol * tileLayout.tileCols)
-        val tileRow = row - (layoutRow * tileLayout.tileRows)
-        if(isTiled) { tileRow * tileLayout.tileCols + tileCol }
-        else { tileRow * segmentCols + tileCol }
-      }
-    }
-  }
-
   def getGridBounds(segmentIndex: Int, isBit: Boolean = false): GridBounds = {
-    val segmentTransform = getSegmentTransform(segmentIndex)
+    val segmentTransform =
+      if (isStriped)
+        StripedSegmentTransform(segmentIndex, GeoTiffSegmentLayout(totalCols, totalRows, tileLayout, isTiled))
+      else
+        TiledSegmentTransform(segmentIndex, GeoTiffSegmentLayout(totalCols, totalRows, tileLayout, isTiled))
+
     val segmentCols = segmentTransform.segmentCols
     val segmentRows = segmentTransform.segmentRows
 
