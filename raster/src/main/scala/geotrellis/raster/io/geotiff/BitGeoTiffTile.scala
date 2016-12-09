4f78e9b5ac83fb897ff57f7cace4400bbe47e6d7
jbouffard <jbouffard@azavea.com>
2016-12-09 17:22:00 -0500

    Removed the crop method from BitGeoTiffTile
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/io/geotiff/BitGeoTiffTile.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/BitGeoTiffTile.scala
@@ -58,35 +58,6 @@ class BitGeoTiffTile(
    result
   }
 
-  override def crop(gridBounds: GridBounds): MutableArrayTile = {
-    val result = BitArrayTile.empty(gridBounds.width, gridBounds.height)
-
-    val colMin = gridBounds.colMin
-    val rowMin = gridBounds.rowMin
-    val tileCols = segmentLayout.tileLayout.tileCols
-    val tileRows = segmentLayout.tileLayout.tileRows
-
-    cfor(0)(_ < segmentCount, _ + 1) { i =>
-      val segmentTransform = segmentLayout.getSegmentTransform(i)
-
-      val colStart = segmentTransform.bitIndexToCol(0)
-      val rowStart = segmentTransform.bitIndexToRow(0)
-      val colEnd = (colStart + tileCols).min(cols)
-      val rowEnd = (rowStart + tileRows).min(rows)
-
-      if (gridBounds.intersects(GridBounds(colStart, rowStart, colEnd, rowEnd))) {
-        cfor(colStart)(_ < colEnd, _ + 1) { col =>
-          cfor(rowStart)(_ < rowEnd, _ + 1) { row =>
-            if (gridBounds.contains(col, row))
-              result.set(col - colMin, row - rowMin, get(col, row))
-          }
-        }
-      }
-    }
-    result
-  }
-
-
   def withNoData(noDataValue: Option[Double]): BitGeoTiffTile =
     new BitGeoTiffTile(segmentBytes, decompressor, segmentLayout, compression, cellType.withNoData(noDataValue))
 
