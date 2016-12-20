8e07352000b2cf143f64e54eb16911351ea6ddd7
Justin Polchlopek <jpolchlopek@azavea.com>
2016-12-20 13:39:30 -0500

    Minimally functional (but not fully tested) Delaunay triangulator
    Signed-off-by: Justin Polchlopek <jpolchlopek@azavea.com>

---
+++ vector/src/main/scala/geotrellis/vector/triangulation/DelaunayPointSet.scala
@@ -0,0 +1,34 @@
+package geotrellis.vector.triangulation
+
+import com.vividsolutions.jts.geom.Coordinate
+
+import geotrellis.util.Constants.{DOUBLE_EPSILON => EPSILON}
+
+
+trait DelaunayPointSet {
+  def length: Int
+  def getX(i: Int): Double
+  def getY(i: Int): Double
+  def getZ(i: Int): Double
+  def getCoordinate(i: Int): Coordinate = new Coordinate(getX(i), getY(i), getZ(i))
+}
+
+object DelaunayPointSet {
+
+  // implicit def pointCloudToDelaunayPointSet(pointCloud: PointCloud): DelaunayPointSet =
+  //   new DelaunayPointSet {
+  //     def length = pointCloud.length
+  //     def getX(i: Int) = pointCloud.getX(i)
+  //     def getY(i: Int) = pointCloud.getY(i)
+  //     def getZ(i: Int) = pointCloud.getZ(i)
+  //   }
+
+  implicit def lightPointArrayToDelaunayPointSet(points: Array[Coordinate]): DelaunayPointSet =
+    new DelaunayPointSet {
+      def length = points.length
+      def getX(i: Int) = points(i).x
+      def getY(i: Int) = points(i).y
+      def getZ(i: Int) = points(i).z
+      override def getCoordinate(i: Int) = points(i)
+    }
+}
---
+++ vector/src/main/scala/geotrellis/vector/triangulation/DelaunayTriangulation.scala
@@ -0,0 +1,114 @@
+package geotrellis.vector.triangulation
+
+import scala.annotation.tailrec
+
+class DelaunayTriangulation(verts: DelaunayPointSet, navigator: HalfEdgeTable)
+{
+  implicit val nav = navigator
+  implicit val trans = verts.getCoordinate(_)
+
+  val triangles = new TriangleMap
+
+  def distinctPoints(lst: List[Int]): List[Int] = {
+    import verts._
+
+    @tailrec def dpInternal(l: List[Int], acc: List[Int]): List[Int] = {
+      l match {
+        case Nil => acc
+        case i :: Nil => i :: acc
+        case i :: rest => {
+          val chunk = rest.takeWhile{j => getX(j) <= getX(i) + 1e-8}
+          if (chunk.exists { j =>
+            val (xj, yj) = (getX(j), getY(j))
+              val (xi, yi) = (getX(i), getY(i))
+                val dx = xj - xi
+                            val dy = yj - yi
+                            math.sqrt((dx * dx) + (dy * dy)) < 1e-10
+                          }) {
+            dpInternal(rest, acc)
+          } else {
+            dpInternal(rest, i :: acc)
+          }
+        }
+      }
+    }
+
+    dpInternal(lst, Nil).reverse
+  }
+
+  val sortedVs = {
+    import verts._
+    val s =
+      (0 until verts.length).toList.sortWith{
+        (i1,i2) => {
+          val x1 = getX(i1)
+          val x2 = getX(i2)
+          if (x1 < x2) {
+            true
+          } else {
+            if (x1 > x2) {
+
+              false
+            } else {
+              getY(i1) < getY(i2)
+            }
+          }
+        }
+      }
+    println("Finding distinct points")
+    distinctPoints(s)
+    .toArray
+  }
+
+  def triangulate(lo: Int, hi: Int): (Int, Boolean) = {
+    // Implementation follows Guibas and Stolfi, ACM Transations on Graphics, vol. 4(2), 1985, pp. 74--123
+    import nav._
+    import Predicates._
+
+    val n = hi - lo + 1
+
+    n match {
+      case 1 =>
+        throw new IllegalArgumentException("Cannot triangulate a point set of size less than 2")
+
+      case 2 =>
+        val e = createHalfEdges(sortedVs(lo), sortedVs(hi))
+        (e, true)
+
+      case 3 =>
+        val v1 = sortedVs(lo)
+        val v2 = sortedVs(lo + 1)
+        val v3 = sortedVs(lo + 2)
+
+        if (isCCW(v1, v2, v3)) {
+          val e = createHalfEdges(v1, v2, v3)
+          val p = getPrev(e)
+          triangles += (v1, v2, v3) -> p
+          (p, false)
+        } else if(isCCW(v1, v3, v2)) {
+          val e = createHalfEdges(v1, v3, v2)
+          triangles += (v1, v3, v2) -> e
+          (e, false)
+        } else {
+          // Linear case
+          val e1 = createHalfEdges(v1, v2)
+          val e2 = createHalfEdges(v2, v3)
+          val e2Flip = getFlip(e2)
+
+          setNext(e1, e2)
+          setNext(e2Flip, getFlip(e1))
+          (e2Flip, true)
+        }
+
+      case _ => {
+        val med = (hi + lo) / 2
+        var (left, isLeftLinear) = triangulate(lo,med)
+        var (right, isRightLinear) = triangulate(med+1,hi)
+
+        DelaunayStitcher.merge(left, isLeftLinear, right, isRightLinear, triangles)
+      }
+    }
+  }
+
+  val (boundary, isLinear) = triangulate(0, sortedVs.length - 1)
+}
