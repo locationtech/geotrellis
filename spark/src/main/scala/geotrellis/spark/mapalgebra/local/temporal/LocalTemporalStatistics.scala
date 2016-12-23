edc48387e30ebcddcb7162ee0d62708b0dd9443c
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-12-23 21:19:58 +0300

    add local temporal methods
    Signed-off-by: Grigory Pomadchin <gr.pomadchin@gmail.com>

--- spark/src/main/scala/geotrellis/spark/mapalgebra/local/temporal/LocalTemporalStatistics.scala
+++ spark/src/main/scala/geotrellis/spark/mapalgebra/local/temporal/LocalTemporalStatistics.scala
@@ -40,6 +40,14 @@ object LocalTemporalStatistics {
     partitioner: Option[Partitioner] = None): RDD[(K, Tile)] =
     aggregateWithTemporalWindow(rdd, windowSize, unit, start, end, partitioner)(minReduceOp)
 
+  def temporalMin[K: SpatialComponent: TemporalComponent](
+    seq: Seq[(K, Tile)],
+    windowSize: Int,
+    unit: Int,
+    start: ZonedDateTime,
+    end: ZonedDateTime): Seq[(K, Tile)] =
+    aggregateWithTemporalWindow(seq, windowSize, unit, start, end)(minReduceOp)
+
   def temporalMax[K: ClassTag: SpatialComponent: TemporalComponent](
     rdd: RDD[(K, Tile)],
     windowSize: Int,
@@ -48,6 +56,14 @@ object LocalTemporalStatistics {
     end: ZonedDateTime,
     partitioner: Option[Partitioner] = None): RDD[(K, Tile)] =
     aggregateWithTemporalWindow(rdd, windowSize, unit, start, end, partitioner)(maxReduceOp)
+
+  def temporalMax[K: SpatialComponent: TemporalComponent](
+    seq: Seq[(K, Tile)],
+    windowSize: Int,
+    unit: Int,
+    start: ZonedDateTime,
+    end: ZonedDateTime): Seq[(K, Tile)] =
+    aggregateWithTemporalWindow(seq, windowSize, unit, start, end)(maxReduceOp)
 
   def temporalMean[K: ClassTag: SpatialComponent: TemporalComponent](
     rdd: RDD[(K, Tile)],
@@ -58,6 +74,14 @@ object LocalTemporalStatistics {
     partitioner: Option[Partitioner] = None): RDD[(K, Tile)] =
     aggregateWithTemporalWindow(rdd, windowSize, unit, start, end, partitioner)(meanReduceOp)
 
+  def temporalMean[K: SpatialComponent: TemporalComponent](
+    seq: Seq[(K, Tile)],
+    windowSize: Int,
+    unit: Int,
+    start: ZonedDateTime,
+    end: ZonedDateTime): Seq[(K, Tile)] =
+    aggregateWithTemporalWindow(seq, windowSize, unit, start, end)(meanReduceOp)
+
   def temporalVariance[K: ClassTag: SpatialComponent: TemporalComponent](
     rdd: RDD[(K, Tile)],
     windowSize: Int,
@@ -66,6 +90,14 @@ object LocalTemporalStatistics {
     end: ZonedDateTime,
     partitioner: Option[Partitioner] = None): RDD[(K, Tile)] =
     aggregateWithTemporalWindow(rdd, windowSize, unit, start, end, partitioner)(varianceReduceOp)
+
+  def temporalVariance[K: SpatialComponent: TemporalComponent](
+    seq: Seq[(K, Tile)],
+    windowSize: Int,
+    unit: Int,
+    start: ZonedDateTime,
+    end: ZonedDateTime): Seq[(K, Tile)] =
+    aggregateWithTemporalWindow(seq, windowSize, unit, start, end)(varianceReduceOp)
 
   private def aggregateWithTemporalWindow[K: ClassTag: SpatialComponent: TemporalComponent](
     sourceRdd: RDD[(K, Tile)],
@@ -107,6 +139,45 @@ object LocalTemporalStatistics {
 
         (key, tile)
       }
+  }
+
+  private def aggregateWithTemporalWindow[K: SpatialComponent: TemporalComponent](
+    sourceSeq: Seq[(K, Tile)],
+    windowSize: Int,
+    unit: Int,
+    start: ZonedDateTime,
+    end: ZonedDateTime)(reduceOp: Traversable[Tile] => Tile): Seq[(K, Tile)] = {
+    val seq =
+      sourceSeq
+        .map { case (key, tile) =>
+          val SpatialKey(col, row) = key.getComponent[SpatialKey]
+          val time = key.getComponent[TemporalKey].time
+          val startDiff = getDifferenceByUnit(unit, start, time)
+          val endDiff = getDifferenceByUnit(unit, time, end)
+
+          val newKey =
+            if (startDiff < 0 && endDiff < 0) {
+              (-1, col, row)
+            }
+            else {
+              val timeDelimiter = startDiff / windowSize
+              (timeDelimiter, col, row)
+            }
+
+          (newKey, (key, tile))
+        }
+        .filter { case ((i, col, row), _) => i >= 0 }
+
+    seq.groupBy(_._1)
+      .map { case (k, v) => (k, v.map(_._2)) }
+      .map { case (_, iter) =>
+        val (keys, tiles) = iter.unzip
+
+        val key = keys.min(Ordering.by { key: K => key.getComponent[TemporalKey].time })
+        val tile = reduceOp(tiles)
+
+        (key, tile)
+      }.toSeq
   }
 
   private def getDifferenceByUnit(unit: Int, base: ZonedDateTime, time: ZonedDateTime): Int =
