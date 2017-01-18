ecde16e54edd25392c1884147ac9edd2307b2097
jbouffard <jbouffard@azavea.com>
2017-01-18 11:31:23 -0500

    Removed ProjectedExent and TemporalProjectedExtent from ExtentCodec
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- spark/src/main/scala/geotrellis/spark/io/avro/codecs/ExtentCodec.scala
+++ spark/src/main/scala/geotrellis/spark/io/avro/codecs/ExtentCodec.scala
@@ -16,8 +16,6 @@
 
 package geotrellis.spark.io.avro.codecs
 
-import geotrellis.proj4.CRS
-import geotrellis.spark._
 import geotrellis.spark.io.avro._
 import geotrellis.vector._
 
@@ -26,7 +24,7 @@ import org.apache.avro.generic._
 
 // --- //
 
-trait ExtentCodecs {
+trait ExtentCodec {
   implicit def extentCodec = new AvroRecordCodec[Extent] {
     def schema: Schema = SchemaBuilder
       .record("Extent").namespace("geotrellis.spark")
@@ -53,55 +51,5 @@ trait ExtentCodecs {
       )
     }
   }
-
-  implicit def projectedExtentCodec = new AvroRecordCodec[ProjectedExtent] {
-    def schema: Schema = SchemaBuilder
-      .record("ProjectedExtent").namespace("geotrellis.spark")
-      .fields()
-      .name("extent").`type`(extentCodec.schema).noDefault()
-      .name("wkt").`type`().stringType().noDefault()
-      .endRecord()
-
-    def encode(projectedExtent: ProjectedExtent, rec: GenericRecord): Unit = {
-      rec.put("extent", projectedExtent.extent)
-      rec.put("wkt", projectedExtent.crs)
-    }
-
-    def decode(rec: GenericRecord): ProjectedExtent = {
-      val wktString = rec[String]("crs")
-      val crs = CRS.fromWKT(wktString)
-
-      val extent = extentCodec.decode(rec("extent"))
-
-      ProjectedExtent(extent, crs)
-    }
-  }
-
-  implicit def temporalProjectedExtentCodec = new AvroRecordCodec[TemporalProjectedExtent] {
-    def schema: Schema = SchemaBuilder
-      .record("TemporalProjectedExtent").namespace("geotrellis.spark")
-      .fields()
-      .name("extent").`type`(extentCodec.schema).noDefault()
-      .name("wkt").`type`().stringType().noDefault()
-      .name("instant").`type`().longType().noDefault()
-      .endRecord()
-
-    def encode(temporalProjectedExtent: TemporalProjectedExtent, rec: GenericRecord): Unit = {
-      rec.put("extent", temporalProjectedExtent.extent)
-      rec.put("wkt", temporalProjectedExtent.crs)
-      rec.put("instant", temporalProjectedExtent.instant)
-    }
-
-    def decode(rec: GenericRecord): TemporalProjectedExtent = {
-      val instant = rec[Long]("instant")
-      val wktString = rec[String]("wkt")
-      val crs = CRS.fromWKT(wktString)
-
-      val extent = extentCodec.decode(rec("extent"))
-
-      TemporalProjectedExtent(extent, crs, instant)
-    }
-  }
 }
 
-object ExtentCodecs extends ExtentCodecs
