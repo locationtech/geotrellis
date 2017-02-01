2313427bc74e65e16f2e42fcc4252dd7ff1826b1
James McClain <jmcclain@azavea.com>
2017-02-01 16:57:27 -0500

    Simplify CostDistance Implementation

--- raster/src/main/scala/geotrellis/raster/costdistance/CostDistance.scala
+++ raster/src/main/scala/geotrellis/raster/costdistance/CostDistance.scala
@@ -27,225 +27,113 @@ import geotrellis.raster._
   */
 object CostDistance {
 
+  type Cost = (Int, Int, Double, Double) // column, row, friction, cost
+  type Q = PriorityQueue[Cost]
+
+  val q: Q = new PriorityQueue(
+    (1<<11), new java.util.Comparator[Cost] {
+      override def equals(a: Any) = a.equals(this)
+      def compare(a: Cost, b: Cost) = a._4.compareTo(b._4)
+    })
+
+  var cols: Int = -1
+  var rows: Int = -1
+
+  def inTile(col: Int, row: Int): Boolean =
+    ((0 <= col && col < cols) && (0 <= row && row < rows))
+
+  def isPassable(f: Double): Boolean =
+    (isData(f) && 0 <= f)
+
+
   /**
-    * Generate a Cost-Distance raster based on a set of starting points
-    * and a cost raster
+    * Generate a cost-distance raster based on a set of starting
+    * points and a friction raster.  This is an implementation of the
+    * standard algorithm from [1].
     *
-    * @param cost     Cost Tile (Int)
-    * @param points   List of starting points as tuples
+    * 1. Tomlin, Dana.
+    *    "Propagating radial waves of travel cost in a grid."
+    *    International Journal of Geographical Information Science 24.9 (2010): 1391-1413.
     *
-    * @note    Operation will only work with integer typed Cost Tiles (BitCellType, ByteConstantNoDataCellType, ShortConstantNoDataCellType, IntConstantNoDataCellType).
-    *          If a double typed Cost Tile (FloatConstantNoDataCellType, DoubleConstantNoDataCellType) is passed in, those costs will be rounded
-    *          to their floor integer values.
+    * @param  friction  Friction tile
+    * @param  points    List of starting points as tuples
     *
     */
-  def apply(cost: Tile, points: Seq[(Int, Int)]): Tile = {
-    val (cols, rows) = cost.dimensions
-    val output = DoubleArrayTile.empty(cols, rows)
+  def apply(frictionTile: Tile, points: Seq[(Int, Int)]): Tile = {
+    val dims = frictionTile.dimensions
+    cols = dims._1; rows = dims._2
+    val costTile = DoubleArrayTile.empty(cols, rows)
 
-    for((c, r) <- points)
-      output.setDouble(c, r, 0.0)
+    points.foreach({ case (col, row) =>
+      val entry = (col, row, frictionTile.getDouble(col, row), 0.0)
+      q.add(entry)
+    })
+    while (!q.isEmpty) processNext(frictionTile, costTile, q)
 
-    val pqueue = new PriorityQueue(
-        1000, new java.util.Comparator[Cost] {
-          override def equals(a: Any) = a.equals(this)
-          def compare(a: Cost, b: Cost) = a._3.compareTo(b._3)
-        })
-
-
-    for((c, r) <- points) {
-      calcNeighbors(c, r, cost, output, pqueue)
-    }
-
-    while (!pqueue.isEmpty) {
-      val (c, r, v) = pqueue.poll
-      if (v == output.getDouble(c, r)) calcNeighbors(c, r, cost, output, pqueue)
-    }
-
-    output
+    costTile
   }
 
   /**
-    * Predicate: are the given column and row within the bounds of the
-    * [[Tile]]?
+    * Given a location, an instantaneous cost at that neighboring
+    * location (friction), a friction tile, the cost to get to that
+    * location, and the distance from the neighboring pixel, enqueue a
+    * candidate path to this pixel.
     *
-    * @param  c  The column
-    * @param  r  The row
+    * @param  col           The column of the given location
+    * @param  row           The row of the given location
+    * @param  friction1     The instantaneous cost (friction) at the neighboring location
+    * @param  frictionTile  The friction tile
+    * @param  cost          The length of the best-known path from a source to the neighboring location
+    * @param  distance      The distance from the neighboring location to this location
     */
-  private def isValid(c: Int, r: Int, cost: Tile): Boolean =
-    c >= 0 && r >= 0 && c < cost.cols && r < cost.rows
-
-  /**
-    * A class encoding directions.
-    */
-  final class Dir(val dc: Int, val dr: Int) {
-    val diag = !(dc == 0 || dr == 0)
-
-    lazy val cornerOffsets = (dc, dr) match {
-      case (-1, -1) => Array((0, -1), (-1, 0))
-      case ( 1, -1) => Array((0, -1), ( 1, 0))
-      case (-1, 1) => Array((0, 1), (-1, 0))
-      case ( 1, 1) => Array((0, 1), ( 1, 0))
-      case _ => Array[(Int, Int)]()
-    }
-
-    def apply(c: Int, r: Int) = (c + dc, r + dr)
-
-    lazy val unitDistance = if (diag) 1.41421356237 else 1.0
-  }
-
-  val dirs: Array[Dir] = Array(
-    (-1, -1), ( 0, -1), ( 1, -1),
-    (-1, 0),          ( 1, 0),
-    (-1, 1), ( 0, 1), ( 1, 1)).map { case (c, r) => new Dir(c, r) }
-
-
-  /**
-    * Input:
-    * (c, r) => Source cell
-    * (dc, dr) => Delta (direction)
-    * cost => Cost raster
-    * d => C - D output raster
-    *
-    * Output:
-    * List((c, r)) <- list of cells set
-    */
-  private def calcCostCell(c: Int, r: Int, dir: Dir, cost: Tile, d: DoubleArrayTile) = {
-    val cr = dir(c, r)
-
-    if (isValid(cr._1, cr._2, cost)) {
-      val prev = d.getDouble(cr._1, cr._2)
-      if (prev == 0.0) { // This is a source cell, don't override and shortcircuit early
-        None
-      } else {
-        val source = d.getDouble(c, r)
-
-        // Previous value could be NODATA
-        val prevCost = if (isNoData(prev)) java.lang.Double.MAX_VALUE else prev
-
-        var curMinCost = Double.MaxValue
-
-        // Simple solution
-        val baseCostOpt = calcCost(c, r, dir, cost)
-        if (baseCostOpt.isDefined) {
-          curMinCost = source + baseCostOpt.get
-        }
-
-        // Alternative solutions (going around the corner)
-        // Generally you can check diags directly:
-        // +---+---+---+
-        // | a | b | c |
-        // +---+---+---+
-        // | d | e | f |
-        // +---+---+---+
-        // | g | h | i |
-        // +---+---+---+
-        //
-        // For instance, "eg" can be computed directly
-        // but it turns out "eg" can be more expensive
-        // than "edg" or "ehg" so we compute those right
-        // now just in case
-        val cornerOffsets = dir.cornerOffsets
-        val l = cornerOffsets.length
-        var z = 0
-        while(z < l) {
-          val p = cornerOffsets(z)
-          val c1 = p._1 + c
-          val r1 = p._2 + r
-          val cost1 = calcCost(c, r, c1, r1, cost)
-          if (cost1.isDefined) {
-            val cost2 = calcCost(c1, r1, dir(c, r), cost)
-            if (cost2.isDefined) {
-              curMinCost = math.min(curMinCost, source + cost1.get + cost2.get)
-            }
-          }
-          z += 1
-        }
-
-        if (curMinCost == Double.MaxValue) {
-          None // Possible all nodata values
-        } else {
-          if (curMinCost < prevCost) {
-            d.setDouble(cr._1, cr._2, curMinCost)
-
-            Some((cr._1, cr._2, curMinCost))
-          } else {
-            None
-          }
-        }
+  @inline private def enqueueNeighbor(
+    col: Int, row: Int, friction1: Double,
+    frictionTile: Tile, cost: Double,
+    distance: Double = 1.0
+  ): Unit = {
+    // If the location is inside of the tile ...
+    if (inTile(col, row)) {
+      val friction2 = frictionTile.getDouble(col, row)
+      // ... and if the location is passable, enqueue it for future processing
+      if (isPassable(friction2)) {
+        val entry = (col, row, friction2, cost + distance * (friction1 + friction2) / 2.0)
+        q.add(entry)
       }
-    } else {
-      None
     }
   }
 
-  type Cost = (Int, Int, Double)
+  /**
+    * Process the candidate path on the top of the queue.
+    *
+    * @param  frictionTile  The friction tile
+    * @param  costTile      The cost tile
+    * @param  q             The priority queue of candidate paths
+    */
+  private def processNext(frictionTile: Tile, costTile: DoubleArrayTile, q: Q): Unit = {
+    val (col, row, friction1, candidateCost) = q.poll
+    val currentCost =
+      if (inTile(col, row))
+        costTile.getDouble(col, row)
+      else
+        Double.NaN
 
-  private def calcNeighbors(c: Int, r: Int, cost: Tile, d: DoubleArrayTile, p: PriorityQueue[Cost]) {
-    val l = dirs.length
-    var z = 0
+    // If the candidate path is a possible improvement ...
+    if (!isData(currentCost) || candidateCost <= currentCost) {
+      // ... over-write the current cost with the candidate cost
+      if (inTile(col, row)) costTile.setDouble(col, row, candidateCost)
 
-    while(z < l) {
-      val opt = calcCostCell(c, r, dirs(z), cost, d)
-      if (opt.isDefined) {
-        p.add(opt.get)
+      // ... compute candidate costs for neighbors and enqueue them
+      if (isPassable(friction1)) {
+
+        enqueueNeighbor(col-1, row+0, friction1, frictionTile, candidateCost)
+        enqueueNeighbor(col+1, row+0, friction1, frictionTile, candidateCost)
+        enqueueNeighbor(col+0, row+1, friction1, frictionTile, candidateCost)
+        enqueueNeighbor(col+0, row-1, friction1, frictionTile, candidateCost)
+        enqueueNeighbor(col-1, row-1, friction1, frictionTile, candidateCost, math.sqrt(2))
+        enqueueNeighbor(col-1, row+1, friction1, frictionTile, candidateCost, math.sqrt(2))
+        enqueueNeighbor(col+1, row-1, friction1, frictionTile, candidateCost, math.sqrt(2))
+        enqueueNeighbor(col+1, row+1, friction1, frictionTile, candidateCost, math.sqrt(2))
       }
-      z += 1
     }
   }
-
-  private def factor(c: Int, r: Int, c1: Int, r1: Int) = if (c == c1 || r == r1) 1.0 else 1.41421356237
-
-  private def safeGet(c: Int, r: Int, cost: Tile): IOption = IOption(cost.get(c, r))
-
-  private def calcCost(c: Int, r: Int, c2: Int, r2: Int, cost: Tile): DOption = {
-    val cost1 = safeGet(c, r, cost)
-    val cost2 = safeGet(c2, r2, cost)
-
-    if (cost1.isDefined && cost2.isDefined) {
-      DOption(factor(c, r, c2, r2) * (cost1.get + cost2.get) / 2.0)
-    } else {
-      DOption.None
-    }
-  }
-
-  private def calcCost(c: Int, r: Int, cr2: (Int, Int), cost: Tile): DOption =
-    calcCost(c, r, cr2._1, cr2._2, cost)
-
-  private def calcCost(c: Int, r: Int, dir: Dir, cost: Tile): DOption =
-    calcCost(c, r, dir(c, r), cost)
-}
-
-/**
-  * Represents an optional integer using 'Tile NODATA' as a flag
-  */
-private [costdistance]
-class IOption(val v: Int) extends AnyVal {
-  def map(f: Int => Int) = if (isDefined) new IOption(f(v)) else this
-  def flatMap(f: Int => IOption) = if (isDefined) f(v) else this
-  def isDefined = isData(v)
-  def get = if (isDefined) v else sys.error("Get called on NODATA")
-}
-
-/**
-  * Represents an optional integer using 'Double.NaN' as a flag
-  */
-private [costdistance]
-class DOption(val v: Double) extends AnyVal {
-  def map(f: Double => Double) = if (isDefined) new DOption(f(v)) else this
-  def flatMap(f: Double => DOption) = if (isDefined) f(v) else this
-  def isDefined = isData(v)
-  def get = if (isDefined) v else sys.error("Get called on NaN")
-}
-
-private [costdistance]
-object IOption {
-  val None = new IOption(NODATA)
-  def apply(v: Int) = new IOption(v)
-}
-
-private [costdistance]
-object DOption {
-  val None = new DOption(Double.NaN)
-  def apply(v: Double) = new DOption(v)
 }
