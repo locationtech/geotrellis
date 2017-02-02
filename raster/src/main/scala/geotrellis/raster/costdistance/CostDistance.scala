d078755b2dcce450bce5fc10d1728961cc14439f
James McClain <jmcclain@azavea.com>
2017-02-02 09:06:19 -0500

    Make Thread Safe

--- raster/src/main/scala/geotrellis/raster/costdistance/CostDistance.scala
+++ raster/src/main/scala/geotrellis/raster/costdistance/CostDistance.scala
@@ -30,22 +30,6 @@ object CostDistance {
   type Cost = (Int, Int, Double, Double) // column, row, friction, cost
   type Q = PriorityQueue[Cost]
 
-  val q: Q = new PriorityQueue(
-    (1<<12), new java.util.Comparator[Cost] {
-      override def equals(a: Any) = a.equals(this)
-      def compare(a: Cost, b: Cost) = a._4.compareTo(b._4)
-    })
-
-  var cols: Int = -1
-  var rows: Int = -1
-
-  def inTile(col: Int, row: Int): Boolean =
-    ((0 <= col && col < cols) && (0 <= row && row < rows))
-
-  def isPassable(f: Double): Boolean =
-    (isData(f) && 0 <= f)
-
-
   /**
     * Generate a cost-distance raster based on a set of starting
     * points and a friction raster.  This is an implementation of the
@@ -60,80 +44,90 @@ object CostDistance {
     *
     */
   def apply(frictionTile: Tile, points: Seq[(Int, Int)]): Tile = {
-    val dims = frictionTile.dimensions
-    cols = dims._1; rows = dims._2
+    val cols = frictionTile.cols
+    val rows = frictionTile.rows
     val costTile = DoubleArrayTile.empty(cols, rows)
+    val q: Q = new PriorityQueue(
+      (cols*2 + rows*2), new java.util.Comparator[Cost] {
+        override def equals(a: Any) = a.equals(this)
+        def compare(a: Cost, b: Cost) = a._4.compareTo(b._4)
+      })
+
+    def inTile(col: Int, row: Int): Boolean =
+      ((0 <= col && col < cols) && (0 <= row && row < rows))
+
+    def isPassable(f: Double): Boolean =
+      (isData(f) && 0 <= f)
+
+    /**
+      * Given a location, an instantaneous cost at that neighboring
+      * location (friction), the cost to get to the neighboring
+      * location, and the distance from the neighboring pixel to this
+      * pixel, enqueue a candidate path to the present pixel.
+      *
+      * @param  col           The column of the given location
+      * @param  row           The row of the given location
+      * @param  friction1     The instantaneous cost (friction) at the neighboring location
+      * @param  cost          The length of the best-known path from a source to the neighboring location
+      * @param  distance      The distance from the neighboring location to this location
+      */
+    @inline def enqueueNeighbor(
+      col: Int, row: Int, friction1: Double,
+      cost: Double,
+      distance: Double = 1.0
+    ): Unit = {
+      // If the location is inside of the tile ...
+      if (inTile(col, row)) {
+        val friction2 = frictionTile.getDouble(col, row)
+        // ... and if the location is passable, enqueue it for future processing
+        if (isPassable(friction2)) {
+          val entry = (col, row, friction2, cost + distance * (friction1 + friction2) / 2.0)
+          q.add(entry)
+        }
+      }
+    }
+
+    /**
+      * Process the candidate path on the top of the queue.
+      *
+      * @param  frictionTile  The friction tile
+      * @param  costTile      The cost tile
+      * @param  q             The priority queue of candidate paths
+      */
+    def processNext(): Unit = {
+      val (col, row, friction1, candidateCost) = q.poll
+      val currentCost =
+        if (inTile(col, row))
+          costTile.getDouble(col, row)
+        else
+          Double.NaN
+
+      // If the candidate path is a possible improvement ...
+      if (!isData(currentCost) || candidateCost <= currentCost) {
+
+        // ... over-write the current cost with the candidate cost
+        if (inTile(col, row)) costTile.setDouble(col, row, candidateCost)
+
+        // ... compute candidate costs for neighbors and enqueue them
+        if (isPassable(friction1)) {
+          enqueueNeighbor(col-1, row+0, friction1, candidateCost)
+          enqueueNeighbor(col+1, row+0, friction1, candidateCost)
+          enqueueNeighbor(col+0, row+1, friction1, candidateCost)
+          enqueueNeighbor(col+0, row-1, friction1, candidateCost)
+          enqueueNeighbor(col-1, row-1, friction1, candidateCost, math.sqrt(2))
+          enqueueNeighbor(col-1, row+1, friction1, candidateCost, math.sqrt(2))
+          enqueueNeighbor(col+1, row-1, friction1, candidateCost, math.sqrt(2))
+          enqueueNeighbor(col+1, row+1, friction1, candidateCost, math.sqrt(2))
+        }
+      }
+    }
 
     points.foreach({ case (col, row) =>
       val entry = (col, row, frictionTile.getDouble(col, row), 0.0)
       q.add(entry)
     })
-    while (!q.isEmpty) processNext(frictionTile, costTile, q)
+    while (!q.isEmpty) processNext
 
     costTile
-  }
-
-  /**
-    * Given a location, an instantaneous cost at that neighboring
-    * location (friction), a friction tile, the cost to get to the
-    * neighboring location, and the distance from the neighboring
-    * pixel, enqueue a candidate path to the present pixel.
-    *
-    * @param  col           The column of the given location
-    * @param  row           The row of the given location
-    * @param  friction1     The instantaneous cost (friction) at the neighboring location
-    * @param  frictionTile  The friction tile
-    * @param  cost          The length of the best-known path from a source to the neighboring location
-    * @param  distance      The distance from the neighboring location to this location
-    */
-  @inline private def enqueueNeighbor(
-    col: Int, row: Int, friction1: Double,
-    frictionTile: Tile, cost: Double,
-    distance: Double = 1.0
-  ): Unit = {
-    // If the location is inside of the tile ...
-    if (inTile(col, row)) {
-      val friction2 = frictionTile.getDouble(col, row)
-      // ... and if the location is passable, enqueue it for future processing
-      if (isPassable(friction2)) {
-        val entry = (col, row, friction2, cost + distance * (friction1 + friction2) / 2.0)
-        q.add(entry)
-      }
-    }
-  }
-
-  /**
-    * Process the candidate path on the top of the queue.
-    *
-    * @param  frictionTile  The friction tile
-    * @param  costTile      The cost tile
-    * @param  q             The priority queue of candidate paths
-    */
-  private def processNext(frictionTile: Tile, costTile: DoubleArrayTile, q: Q): Unit = {
-    val (col, row, friction1, candidateCost) = q.poll
-    val currentCost =
-      if (inTile(col, row))
-        costTile.getDouble(col, row)
-      else
-        Double.NaN
-
-    // If the candidate path is a possible improvement ...
-    if (!isData(currentCost) || candidateCost <= currentCost) {
-
-      // ... over-write the current cost with the candidate cost
-      if (inTile(col, row)) costTile.setDouble(col, row, candidateCost)
-
-      // ... compute candidate costs for neighbors and enqueue them
-      if (isPassable(friction1)) {
-        enqueueNeighbor(col-1, row+0, friction1, frictionTile, candidateCost)
-        enqueueNeighbor(col+1, row+0, friction1, frictionTile, candidateCost)
-        enqueueNeighbor(col+0, row+1, friction1, frictionTile, candidateCost)
-        enqueueNeighbor(col+0, row-1, friction1, frictionTile, candidateCost)
-        enqueueNeighbor(col-1, row-1, friction1, frictionTile, candidateCost, math.sqrt(2))
-        enqueueNeighbor(col-1, row+1, friction1, frictionTile, candidateCost, math.sqrt(2))
-        enqueueNeighbor(col+1, row-1, friction1, frictionTile, candidateCost, math.sqrt(2))
-        enqueueNeighbor(col+1, row+1, friction1, frictionTile, candidateCost, math.sqrt(2))
-      }
-    }
   }
 }
