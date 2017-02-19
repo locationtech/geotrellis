5af54219df4518642725d7e9580f9f719d0c2ba2
James McClain <jmcclain@azavea.com>
2017-02-18 19:38:37 -0500

    Add Extension Methods

--- spark/src/test/scala/geotrellis/spark/costdistance/IterativeCostDistance.scala
+++ spark/src/test/scala/geotrellis/spark/costdistance/IterativeCostDistance.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Azavea
+ * Copyright 2017 Azavea
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
