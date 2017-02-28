4322640365c27400659b7133e4daeb2d224bb1f8
James McClain <jmcclain@azavea.com>
2017-02-28 14:44:57 -0500

    Add Rudamentary Propagation Capability

--- raster/src/main/scala/geotrellis/raster/viewshed/R2Viewshed.scala
+++ raster/src/main/scala/geotrellis/raster/viewshed/R2Viewshed.scala
@@ -37,55 +37,64 @@ object R2Viewshed extends Serializable {
   sealed case class DirectedSegment(
     startCol: Int, startRow: Int,
     endCol: Int, endRow: Int,
-    theta: Double
+    m: Double
   )
 
-  sealed case class Ray(theta: Double, alpha: Double)
+  sealed case class Ray(m: Double, alpha: Double)
+
+  type EdgeCallback = (Ray => Unit)
 
   object RayComparator extends Comparator[Ray] {
     def compare(left: Ray, right: Ray): Int =
-      if (left.theta < right.theta) -1
-      else if (left.theta > right.theta) +1
+      if (left.m < right.m) -1
+      else if (left.m > right.m) +1
       else 0
   }
 
-  def apply(tile: Tile, col: Int, row: Int): Tile =
-    R2Viewshed.apply(tile, col, row, 0)
-
-  def apply(
-    tile: Tile,
-    startCol: Int, startRow: Int, height: Double,
-    resolution: Double = 1.0,
-    from: From = FromInside,
-    rays: Array[Ray] = null
-  ): Tile = {
+  def apply(tile: Tile, col: Int, row: Int): Tile = {
     val cols = tile.cols
     val rows = tile.rows
+    val viewshedTile = ArrayTile.empty(IntCellType, cols, rows)
+
+    R2Viewshed.compute(
+      tile, viewshedTile,
+      col, row, 0, 1.0,
+      FromInside, null, null,
+      { _ => }
+    )
+    viewshedTile
+  }
+
+  def compute(
+    elevationTile: Tile, viewshedTile: MutableArrayTile,
+    startCol: Int, startRow: Int, height: Double, resolution: Double,
+    from: From, left: Array[Ray], right: Array[Ray],
+    edgeCallback: EdgeCallback
+  ): Tile = {
+    val cols = elevationTile.cols
+    val rows = elevationTile.rows
     val re = RasterExtent(Extent(0, 0, cols, rows), cols, rows)
-    val viewshed = ArrayTile.empty(IntCellType, cols, rows)
     val inTile: Boolean = (0 <= startCol && startCol < cols && 0 <= startRow && startRow <= rows)
     val viewHeight =
-      if (inTile) tile.getDouble(startCol, startRow) + height
+      if (inTile) elevationTile.getDouble(startCol, startRow) + height
       else height
-    var theta: Double = 0.0
+    var m: Double = 0.0
     var alpha: Double = 0.0
-
-    @inline def slopeToTheta(slope: Double): Double =
-      math.atan(slope)
 
     def clipRayNorthSouth(newStartRow: Int)(endCol: Int, endRow: Int): Option[DirectedSegment] = {
       if (startCol == endCol) {
-        if (startRow >= endRow)
-          Some(DirectedSegment(startCol, newStartRow, endCol, endRow, -Math.PI/2))
+        if (newStartRow == endRow) None
+        else if (startRow >= endRow)
+          Some(DirectedSegment(startCol, newStartRow, endCol, endRow, Double.NegativeInfinity))
         else
-          Some(DirectedSegment(startCol, newStartRow, endCol, endRow, Math.PI/2))
+          Some(DirectedSegment(startCol, newStartRow, endCol, endRow, Double.PositiveInfinity))
       }
       else {
         val m = (startRow - endRow).toDouble / (startCol - endCol)
         val newStartCol = math.round(((newStartRow - startRow) / m) + startCol).toInt
         if (newStartCol == endCol && newStartRow == endRow) None
         else if (0 <= newStartCol && newStartCol < cols)
-          Some(DirectedSegment(newStartCol, newStartRow, endCol, endRow, slopeToTheta(m)))
+          Some(DirectedSegment(newStartCol, newStartRow, endCol, endRow, m))
         else None
       }
     }
@@ -97,14 +106,14 @@ object R2Viewshed extends Serializable {
         val newStartRow = math.round(m * (newStartCol - startCol) + startRow).toInt
         if (newStartCol == endCol && newStartRow == endRow) None
         else if (0 <= newStartRow && newStartRow < rows)
-          Some(DirectedSegment(newStartCol, newStartRow, endCol, endRow, slopeToTheta(m)))
+          Some(DirectedSegment(newStartCol, newStartRow, endCol, endRow, m))
         else None
       }
     }
 
     def clipRayInside(endCol: Int, endRow: Int): Option[DirectedSegment] = {
       val m = (startRow - endRow).toDouble / (startCol - endCol)
-      Some(DirectedSegment(startCol, startRow, endCol, endRow, slopeToTheta(m)))
+      Some(DirectedSegment(startCol, startRow, endCol, endRow, m))
     }
 
     val clipRay: ((Int, Int) => Option[DirectedSegment]) =
@@ -118,33 +127,35 @@ object R2Viewshed extends Serializable {
           else throw new Exception("Cannot be both inside and outside")
       }
 
-    val thetaToAlpha: (Double => Double) =
+    val slopeToAlpha: (Double => Double) =
       from match {
         case FromInside => { _ => -Math.PI }
-        case _ => { theta: Double =>
-          val index = binarySearch(rays, Ray(theta, Double.NaN), RayComparator)
-          if (index >= 0) rays(index).alpha
+        case _ => { m: Double =>
+          val array = if (m < 0) left; else right
+          val index = binarySearch(array, Ray(math.abs(m), Double.NaN), RayComparator)
+
+          if (index >= 0) array(index).alpha
           else {
             val place = -1 - index
-            if (place == rays.length) rays.last.alpha
-            else rays(place).alpha // XXX interpolate
+            if (place == array.length) array.last.alpha
+            else array(place).alpha // XXX interpolate
           }
         }
       }
 
     def callback(col: Int, row: Int) = {
       if (col == startCol && row == startRow) { // starting point
-        viewshed.setDouble(col, row, 1)
+        viewshedTile.setDouble(col, row, 1)
       }
       else { // any other point
         val deltax = startCol-col
         val deltay = startRow-row
         val distance = math.sqrt(deltax*deltax + deltay*deltay) * resolution
-        val angle = math.atan((tile.getDouble(col, row) - viewHeight) / distance)
+        val angle = math.atan((elevationTile.getDouble(col, row) - viewHeight) / distance)
 
         if (alpha <= angle) {
           alpha = angle
-          viewshed.setDouble(col, row, 1)
+          viewshedTile.setDouble(col, row, 1)
         }
       }
     }
@@ -152,37 +163,57 @@ object R2Viewshed extends Serializable {
     Range(0, cols) // North
       .flatMap({ col => clipRay(col, 0) })
       .foreach({ seg =>
-        theta = seg.theta
-        alpha = thetaToAlpha(theta)
+        m = seg.m
+        alpha = slopeToAlpha(m)
+        // println(s"NORTH ($startCol, $startRow) $seg $alpha")
         Rasterizer.foreachCellInGridLine(
-          seg.startCol, seg.startRow, seg.endCol, seg.endRow, null, re, false)(callback)
+          seg.startCol, seg.startRow,
+          seg.endCol, seg.endRow,
+          null, re, false
+        )(callback)
+        edgeCallback(Ray(m, alpha))
       })
     Range(0, rows) // East
       .flatMap({ row => clipRay(cols-1, row) })
       .foreach({ seg =>
-        theta = seg.theta
-        alpha = thetaToAlpha(theta)
+        m = seg.m
+        alpha = slopeToAlpha(m)
+        // println(s"EAST ($startCol, $startRow) $seg $alpha")
         Rasterizer.foreachCellInGridLine(
-          seg.startCol, seg.startRow, seg.endCol, seg.endRow, null, re, false)(callback)
+          seg.startCol, seg.startRow,
+          seg.endCol, seg.endRow,
+          null, re, false
+        )(callback)
+        edgeCallback(Ray(m, alpha))
       })
     Range(0, cols) // South
       .flatMap({ col => clipRay(col, rows-1) })
       .foreach({ seg =>
-        theta = seg.theta
-        alpha = thetaToAlpha(theta)
+        m = seg.m
+        alpha = slopeToAlpha(m)
+        // println(s"SOUTH ($startCol, $startRow) $seg $alpha")
         Rasterizer.foreachCellInGridLine(
-          seg.startCol, seg.startRow, seg.endCol, seg.endRow, null, re, false)(callback)
+          seg.startCol, seg.startRow,
+          seg.endCol, seg.endRow,
+          null, re, false
+        )(callback)
+        edgeCallback(Ray(m, alpha))
       })
     Range(0, rows) // West
       .flatMap({ row => clipRay(0, row) })
       .foreach({ seg =>
-        theta = seg.theta
-        alpha = thetaToAlpha(theta)
+        m = seg.m
+        alpha = slopeToAlpha(m)
+        // println(s"WEST ($startCol, $startRow) $seg $alpha")
         Rasterizer.foreachCellInGridLine(
-          seg.startCol, seg.startRow, seg.endCol, seg.endRow, null, re, false)(callback)
+          seg.startCol, seg.startRow,
+          seg.endCol, seg.endRow,
+          null, re, false
+        )(callback)
+        edgeCallback(Ray(m, alpha))
       })
 
-    viewshed
+    viewshedTile
   }
 
 }
