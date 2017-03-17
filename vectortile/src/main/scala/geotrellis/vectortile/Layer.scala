1beba41323e7415949b1ed55d33c5102b466760f
Colin Woodbury <colingw@gmail.com>
2017-03-17 14:23:26 -0700

    (vt) Remove `StrictLayer.empty`
    - It turned out to be unnecessary.

--- vectortile/src/main/scala/geotrellis/vectortile/Layer.scala
+++ vectortile/src/main/scala/geotrellis/vectortile/Layer.scala
@@ -145,13 +145,6 @@ case class StrictLayer(
   multiPolygons: Seq[Feature[MultiPolygon, Map[String, Value]]]
 ) extends Layer
 
-object StrictLayer {
-  /** A [[StrictLayer]] with no Geometries. */
-  def empty(name: String, tileExtent: Extent): StrictLayer = StrictLayer(
-    name, 4096, 2, tileExtent, Seq.empty, Seq.empty, Seq.empty, Seq.empty, Seq.empty, Seq.empty
-  )
-}
-
 /**
   * A [[Layer]] decoded from Protobuf data. All of its Features are decoded
   * lazily, making for very fast extraction of single features/geometries.
