4932379a6ced60eea39aef1806bec727abd13412
Grigory Pomadchin <gr.pomadchin@gmail.com>
2017-03-27 13:23:37 +0300

    add tests and copyright headers
    Signed-off-by: Grigory Pomadchin <gr.pomadchin@gmail.com>

--- raster/src/main/scala/geotrellis/raster/transform/TransformTileMethods.scala
+++ raster/src/main/scala/geotrellis/raster/transform/TransformTileMethods.scala
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2016 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package geotrellis.raster.transform
 
 import geotrellis.raster._
 import spire.syntax.cfor._
 
 trait TransformTileMethods extends TransformMethods[Tile] {
+  /**
+    * Rotate [[Tile]] n times by 90 degrees in the counter-clockwise direction.
+    * @param n   number of times to rotate tile
+    */
   def rotate90(n: Int = 1): Tile = {
     val (rows, cols) = self.rows -> self.cols
     if (n % 4 == 0) self
@@ -25,24 +45,6 @@ trait TransformTileMethods extends TransformMethods[Tile] {
       tile
     } else if (n % 3 == 0) {
       val tile = ArrayTile.alloc(self.cellType, self.rows, self.cols)
-
-      if (!self.cellType.isFloatingPoint) {
-        cfor(0)(_ < cols, _ + 1) { col =>
-          cfor(0)(_ < rows, _ + 1) { row =>
-            tile.set(rows - 1 - row, col, self.get(col, row))
-          }
-        }
-      } else {
-        cfor(0)(_ < cols, _ + 1) { col =>
-          cfor(0)(_ < rows, _ + 1) { row =>
-            tile.setDouble(rows - 1 - row, col, self.getDouble(col, row))
-          }
-        }
-      }
-
-      tile
-    } else {
-      val tile = ArrayTile.alloc(self.cellType, self.rows, self.cols)
       if (!self.cellType.isFloatingPoint) {
         cfor(0)(_ < cols, _ + 1) { col =>
           cfor(0)(_ < rows, _ + 1) { row =>
@@ -56,7 +58,22 @@ trait TransformTileMethods extends TransformMethods[Tile] {
           }
         }
       }
-
+      tile
+    } else {
+      val tile = ArrayTile.alloc(self.cellType, self.rows, self.cols)
+      if (!self.cellType.isFloatingPoint) {
+        cfor(0)(_ < cols, _ + 1) { col =>
+          cfor(0)(_ < rows, _ + 1) { row =>
+            tile.set(rows - 1 - row, col, self.get(col, row))
+          }
+        }
+      } else {
+        cfor(0)(_ < cols, _ + 1) { col =>
+          cfor(0)(_ < rows, _ + 1) { row =>
+            tile.setDouble(rows - 1 - row, col, self.getDouble(col, row))
+          }
+        }
+      }
       tile
     }
   }
@@ -105,15 +122,5 @@ trait TransformTileMethods extends TransformMethods[Tile] {
     }
 
     tile
-  }
-
-  def zscore: Tile = {
-    if(!self.cellType.isFloatingPoint) {
-      val stats = self.statistics.getOrElse(sys.error("No stats for a tile."))
-      self.mapIfSet(z => ((z - stats.mean) / stats.stddev).toInt)
-    } else {
-      val stats = self.statisticsDouble.getOrElse(sys.error("No stats for a tile."))
-      self.mapIfSetDouble(z => ((z - stats.mean) / stats.stddev).toInt)
-    }
   }
 }
