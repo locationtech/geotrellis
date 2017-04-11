4ed38a32007122e0b28b5636e8784a876a99f039
jpolchlo <jpolchlopek@azavea.com>
2017-04-11 14:58:41 -0400

    Generalize VoronoiDiagram internals to handle any cell-extent pairing
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- vector/src/main/scala/geotrellis/vector/voronoi/VoronoiDiagram.scala
+++ vector/src/main/scala/geotrellis/vector/voronoi/VoronoiDiagram.scala
@@ -3,16 +3,20 @@ package geotrellis.vector.voronoi
 import com.vividsolutions.jts.geom.Coordinate
 import org.apache.commons.math3.linear._
 
-import geotrellis.util.Constants.DOUBLE_EPSILON
+import geotrellis.util.Constants.{DOUBLE_EPSILON => EPSILON}
 import geotrellis.vector._
 import geotrellis.vector.triangulation._
 
-import scala.collection.mutable.Map
-import scala.math.{abs,sqrt}
+import scala.collection.mutable.{ListBuffer, Map}
+import scala.math.{abs, max, sqrt}
+import spire.syntax.cfor._
 
 object VoronoiDiagram {
 
-  def apply(pts: Array[Coordinate], extent: Extent) = new VoronoiDiagram(DelaunayTriangulation(pts), extent)
+  def apply(pts: Array[Coordinate], extent: Extent, debug: Boolean = false) = {
+    val dt = DelaunayTriangulation(pts, debug=debug)
+    new VoronoiDiagram(dt, extent)
+  }
 
   object V2 {
     def apply(x: Double, y: Double) = { new V2(MatrixUtils.createRealVector(Array(x, y))) }
@@ -27,7 +31,7 @@ object VoronoiDiagram {
     def length() = { sqrt(v dotProduct v) }
     def normalize() = {
       val len2 = this dot this
-      if (abs(len2) < 1e-16)
+      if (abs(len2) > 1e-16)
         this * (1/sqrt(len2))
       else
         V2(0,0)
@@ -199,8 +203,60 @@ object VoronoiDiagram {
         // Boundary edge (reverse ray from infinity)
         case (None, Some(c)) => l += ReverseRay(c, (V2(verts(getDest(e))) - V2(verts(getSrc(e)))).rot90CCW)
         // Normal edge
-        case (Some(c), Some(d)) if (c.distance(d) > DOUBLE_EPSILON) => l += BoundedRay(c, V2(d) - V2(c))
+        case (Some(c), Some(d)) if (c.distance(d) > EPSILON) => l += BoundedRay(c, V2(d) - V2(c))
       }
+      e = rotCCWDest(e)
+    } while (e != incidentEdge)
+    l
+  }
+
+  private def cellBoundsNew(het: HalfEdgeTable, verts: Int => Coordinate, extent: Extent)(incidentEdge: Int): Seq[CellBound] = {
+    import het._
+
+    var e = incidentEdge
+    val l = collection.mutable.ListBuffer.empty[CellBound]
+    do {
+      val origin = V2(verts(getDest(e)))
+      val vplus = V2(verts(getSrc(rotCCWDest(e)))) - origin
+      val v = V2(verts(getSrc(e))) - origin
+      val vminus = V2(verts(getSrc(rotCWDest(e)))) - origin
+
+      val xplus = origin + vplus * 0.5
+      val x = origin + v * 0.5
+      val xminus = origin + vminus * 0.5
+      val norm = v.rot90CCW.normalize
+
+      val aplus = - ((x - xplus).dot(vplus)) / (norm dot vplus)
+      val aminus = - ((x - xminus).dot(vminus)) / (norm dot vminus)
+
+      if (abs(norm dot vplus) < EPSILON) {
+        if (abs(norm dot vminus) < EPSILON) {
+          // Linear triangulation; corresponding cell edge is an infinite line
+          l ++= Seq(ReverseRay(x.toCoord, norm * (-1)), Ray(x.toCoord, norm))
+        } else {
+          // On boundary; next "face center" is point at infinity
+          l += Ray((x + norm * aminus).toCoord, norm)
+        }
+      } else if (abs(norm dot vminus) < EPSILON) {
+        // On boundary; previous "face center" is point at infinity
+        ReverseRay((x + norm * aplus).toCoord, norm * (-1))
+      } else if (abs(aplus - aminus) > EPSILON) {
+        if (aplus > aminus) {
+          // "Normal case"; cell bound is line segment
+          l += BoundedRay((x + norm * aminus).toCoord, norm * (aplus - aminus))
+        } else {
+          if (RobustPredicates.isCCW(x.x, x.y, origin.x, origin.y, xminus.x, xminus.y)) {
+            // On boundary; next "face center" is point at infinity
+            l += Ray((x + norm * aminus).toCoord, norm)
+          } else {
+            // On boundary; previous "face center" is point at infinity
+            l += ReverseRay((x + norm * aplus).toCoord, norm * (-1))
+          }
+        }
+      } else {
+        // Degenerate line segment (equal endpoints); skip
+      }
+
       e = rotCCWDest(e)
     } while (e != incidentEdge)
     l
@@ -223,32 +279,112 @@ object VoronoiDiagram {
     accum
   }
 
-  private def polygonalCell(het: HalfEdgeTable, boundEs: collection.mutable.Set[Int], verts: Int => Coordinate, extent: Extent)(incidentEdge: Int): Polygon = {
-    val cell = cellBounds(het, boundEs, verts, extent)(incidentEdge)
-    var i = 0
-    val accum = collection.mutable.ListBuffer.empty[Coordinate]
+  def cellExtentIntersection(cell: Seq[CellBound], extent: Extent) = {
+    val Extent(xmin, ymin, xmax, ymax) = extent
+    val expts = ListBuffer((xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)).map{ case (x, y) => new Coordinate(x, y) }
 
-    while (i < cell.length) {
-      val lrei = lastRayExtentIntersection(cell(i), extent)
-      i = i + 1
-      if (lrei != None) {
-        val (cellVert, vertLoc) = lrei.get
-        accum += cellVert
-        if (vertLoc != INTERIOR) {
-          var frei = firstRayExtentIntersection(cell(i % cell.length), extent)
-          while(frei == None) {
-            i = i + 1
-            frei = firstRayExtentIntersection(cell(i % cell.length), extent)
-          }
-          accum ++= infill(lrei.get, frei.get, extent)
+    def clipToCellBound(poly: ListBuffer[Coordinate], bound: CellBound): ListBuffer[Coordinate] = {
+      val n = poly.length
+      val result = ListBuffer.empty[Coordinate]
+
+      // println(s"current corners: ${poly}")
+
+      def isContained(c: Coordinate) = {
+        val (base, dir) = bound match {
+          case BoundedRay(base, dir) => (base, dir)
+          case Ray(base, dir) => (base, dir)
+          case ReverseRay(base, dir) => (base, dir * (-1))
+        }
+
+        ! RobustPredicates.isCCW(base.x + dir.x, base.y + dir.y, base.x, base.y, c.x, c.y)
+      }
+
+      def crossing(ca: Coordinate, cb: Coordinate) = {
+        val (base, dir) = bound match {
+          case BoundedRay(base, dir) => (V2(base), dir)
+          case Ray(base, dir) => (V2(base), dir)
+          case ReverseRay(base, dir) => (V2(base), dir)
+        }
+        val norm = dir.rot90CCW
+        val a = V2(ca)
+        val v = V2(cb) - a
+        val alpha = (base - a).dot(norm) / (v dot norm)
+
+        (a + v * alpha).toCoord
+      }
+
+      val contained = poly.map(isContained(_))
+
+      cfor(0)(_ < n, _ + 1) { i =>
+        val v3 = poly(i)
+        val v4 = poly((i + 1) % n)
+
+        // println(s"  edge from $v3 to $v4")
+
+        (contained(i), contained((i + 1) % n)) match {
+          case (true, true)   => 
+            // println("case 1")
+            result += v3
+          case (true, false)  => 
+            // println("case 2")
+            result ++= Seq(v3, crossing(v3, v4))
+          case (false, true)  => 
+            // println("case 3")
+            result += crossing(v3, v4)
+          case (false, false) => 
+            // println("case 4")
+            ()
         }
       }
+
+      result
     }
 
-    accum += accum.head
-    Polygon(accum.map(Point.jtsCoord2Point(_)))
+    val clippedCorners = cell.foldLeft(expts)(clipToCellBound)
+
+    if (clippedCorners isEmpty) {
+      None
+    } else {
+      clippedCorners += clippedCorners.head
+      Some(Polygon(clippedCorners.map(Point.jtsCoord2Point(_))))
+    }
   }
 
+  private def polygonalCell(het: HalfEdgeTable, verts: Int => Coordinate, extent: Extent, debug: Boolean)(incidentEdge: Int): Option[Polygon] = {
+    val cell = cellBoundsNew(het, verts, extent)(incidentEdge)
+    if (debug) {
+      println(s"Cell boundary: $cell")
+    }
+
+    // var i = 0
+    // val accum = collection.mutable.ListBuffer.empty[Coordinate]
+
+    // while (i < cell.length) {
+    //   val lrei = lastRayExtentIntersection(cell(i), extent)
+    //   i = i + 1
+    //   if (lrei != None) {
+    //     val (cellVert, vertLoc) = lrei.get
+    //     accum += cellVert
+    //     if (vertLoc != INTERIOR) {
+    //       var frei = firstRayExtentIntersection(cell(i % cell.length), extent)
+    //       while(frei == None) {
+    //         i = i + 1
+    //         frei = firstRayExtentIntersection(cell(i % cell.length), extent)
+    //       }
+    //       accum ++= infill(lrei.get, frei.get, extent)
+    //     }
+    //   }
+    // }
+
+    // accum += accum.head
+    // val result = Polygon(accum.map(Point.jtsCoord2Point(_)))
+    val cei = cellExtentIntersection(cell, extent)
+
+    // println(s"cellExtentIntersection: ${cei}")
+    // println(s"polygonalCell: $result")
+
+    cei
+  }
 }
 
 /**
@@ -259,15 +395,15 @@ class VoronoiDiagram(val dt: DelaunayTriangulation, val extent: Extent) {
   import VoronoiDiagram._
 
   val pointSet = dt.pointSet
-  private val boundEs = collection.mutable.Set.empty[Int]
-  dt.halfEdgeTable.foreachInLoop(dt.boundary){ e => boundEs += e }
+  // private val boundEs = collection.mutable.Set.empty[Int]
+  // dt.halfEdgeTable.foreachInLoop(dt.boundary){ e => boundEs += e }
 
   /**
    * A method to generate the Voronoi cell corresponding to the point in verts(i).  Note that if
    * verts(i) is not distinct, this function may raise an exception.
    */
-  def voronoiCell(i: Int): Polygon = {
-    polygonalCell(dt.halfEdgeTable, boundEs, pointSet.getCoordinate(_), extent)(dt.halfEdgeTable.edgeIncidentTo(i))
+  def voronoiCell(i: Int): Option[Polygon] = {
+    polygonalCell(dt.halfEdgeTable, pointSet.getCoordinate(_), extent, dt.debug)(dt.halfEdgeTable.edgeIncidentTo(i))
   }
 
   /**
@@ -275,7 +411,7 @@ class VoronoiDiagram(val dt: DelaunayTriangulation, val extent: Extent) {
    * distinct vector of verts.
    */
   def voronoiCells(): Seq[Polygon] = {
-    dt.halfEdgeTable.allVertices.toSeq.map(voronoiCell(_))
+    dt.halfEdgeTable.allVertices.toSeq.flatMap(voronoiCell(_))
   }
 
   /**
@@ -283,7 +419,12 @@ class VoronoiDiagram(val dt: DelaunayTriangulation, val extent: Extent) {
    * corresponding polygonal regions.
    */
   def voronoiCellsWithPoints(): Seq[(Polygon, Coordinate)] = {
-    dt.halfEdgeTable.allVertices.toSeq.map{ i:Int => (voronoiCell(i), dt.pointSet.getCoordinate(i)) }
+    dt.halfEdgeTable.allVertices.toSeq.flatMap{ i: Int => 
+      voronoiCell(i) match { 
+        case None => None
+        case Some(poly) => Some(poly, dt.pointSet.getCoordinate(i)) 
+      }
+    }
   }
 
 }
