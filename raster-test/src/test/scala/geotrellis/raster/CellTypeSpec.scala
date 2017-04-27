74f9e72de4ff3c79a4a665250bff8d06b7f522cb
Simeon H.K. fitch <fitch@astraea.io>
2017-04-27 15:10:25 -0400

    Initial scaffolding to more extensively test string serialization of user defined CellTypes.
    Signed-off-by: Simeon H.K. fitch <fitch@astraea.io>

--- raster-test/src/test/scala/geotrellis/raster/CellTypeSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/CellTypeSpec.scala
@@ -18,7 +18,23 @@ package geotrellis.raster
 
 import org.scalatest._
 
-class CellTypeSpec extends FunSpec with Matchers {
+class CellTypeSpec extends FunSpec with Matchers with Inspectors {
+  def roundTrip(ct: CellType) {
+    withClue("fromName"){
+      // Updated behavior.
+      val str = ct.name
+      val ctp = CellType.fromName(str)
+      ctp should be (ct)
+    }
+    withClue("fromString"){
+      // Tests backward compatibility.
+      val str = ct.toString
+      //noinspection ScalaDeprecation
+      val ctp = CellType.fromString(str)
+      ctp should be (ct)
+    }
+  }
+
   describe("CellType") {
     it("should union cells correctly under various circumstance") {
       ShortCellType.union(IntCellType) should be (IntCellType)
@@ -35,20 +51,7 @@ class CellTypeSpec extends FunSpec with Matchers {
       IntCellType.intersect(FloatCellType) should be (IntCellType)
       FloatCellType.intersect(IntCellType) should be (IntCellType)
     }
-    def roundTrip(ct: CellType) {
-      {
-        // Updated behavior.
-        val str = ct.name
-        val ctp = CellType.fromName(str)
-        ctp should be (ct)
-      }
-      {
-        // Tests backward compatibility.
-        val str = ct.toString
-        val ctp = CellType.fromString(str)
-        ctp should be (ct)
-      }
-    }
+
 
     it("should serialize float64ud123") {
       roundTrip(DoubleUserDefinedNoDataCellType(123))
@@ -128,4 +131,115 @@ class CellTypeSpec extends FunSpec with Matchers {
     }
 
   }
+
+  describe("CellType Bounds checking") {
+
+    //    it("should serialize uint8ud with various ND values") {
+    //
+    //      val ndVals = Seq(0, 1, Byte.MaxValue/2, Byte.MaxValue - 1, Byte.MaxValue, Byte.MaxValue * 2)
+    //      forEvery(ndVals) { nd ⇒
+    //        roundTrip(UByteUserDefinedNoDataCellType(nd.toByte))
+    //      }
+    //    }
+    //
+    //    it("should serialize int8ud with various ND values") {
+    //
+    //      val ndVals = Seq(0, 1, 127, 128, 255)
+    //      forEvery(ndVals) { nd ⇒
+    //        roundTrip(ByteUserDefinedNoDataCellType(nd.toByte))
+    //      }
+    //    }
+
+
+
+    implicit val doubleAsIntegral = scala.math.Numeric.DoubleAsIfIntegral
+    implicit val floatAsIntegral = scala.math.Numeric.FloatAsIfIntegral
+
+
+    it("should handle encoding no data types across valid bounds") {
+      forEvery(CellDef.all) { cellDef ⇒
+        val cd = cellDef.asInstanceOf[CellDef[AnyVal]]
+        forEvery(cd.testPoints) { nd ⇒
+          val ct = cd(nd)
+          assert(cd.toCode(nd) === ct.name)
+          roundTrip(ct)
+        }
+      }
+    }
+
+    abstract class RangeAlgebra[T: Integral] {
+      protected val alg = implicitly[Integral[T]]
+      import alg._
+      val one = alg.one
+      val twice =  one + one
+      val half = one / twice
+    }
+
+    case class TestRange[Encoding: Integral](min: Encoding, max: Encoding) extends RangeAlgebra[Encoding]{
+      import alg._
+      def width = max - min
+      def middle = width * half
+    }
+
+    abstract class CellDef[T: Integral] extends RangeAlgebra[T] {
+      import alg._
+      type Encoding = T
+      val rng: TestRange[Encoding]
+      val baseCode: String
+      def apply(noData: Encoding): CellType with UserDefinedNoData[_]
+      def toCode(noData: Encoding): String = {
+        s"${baseCode}ud${noData}"
+      }
+      def testPoints = Seq(
+        rng.min, rng.min + one, rng.middle - one, rng.middle, rng.middle + one, rng.max - one, rng.max
+      )
+      override def toString = getClass.getSimpleName
+    }
+    object CellDef {
+      val all = Seq(UByteDef, ByteDef, UShortDef, ShortDef, IntDef, FloatDef, DoubleDef)
+    }
+
+    object UByteDef extends CellDef[Short] {
+      val baseCode = "uint8"
+      def apply(noData: Short) = UByteUserDefinedNoDataCellType(noData.toByte)
+      val rng = TestRange(0.toShort, (Byte.MaxValue * 2).toShort)
+    }
+
+    object ByteDef extends CellDef[Byte] {
+      val baseCode = "int8"
+      def apply(noData: Byte) = ByteUserDefinedNoDataCellType(noData.toByte)
+      val rng = TestRange(Byte.MinValue, Byte.MaxValue)
+    }
+
+    object UShortDef extends CellDef[Int] {
+      val baseCode = "uint16"
+      def apply(noData: Int) = UShortUserDefinedNoDataCellType(noData.toShort)
+      val rng = TestRange(0, Short.MaxValue * 2)
+    }
+
+    object ShortDef extends CellDef[Short] {
+      val baseCode = "int16"
+      def apply(noData: Short) = ShortUserDefinedNoDataCellType(noData)
+      val rng = TestRange(Short.MinValue, Short.MaxValue)
+    }
+
+    object IntDef extends CellDef[Int] {
+      val baseCode = "int32"
+      def apply(noData: Int) = IntUserDefinedNoDataCellType(noData)
+      val rng = TestRange(Int.MinValue, Int.MaxValue)
+    }
+
+    object FloatDef extends CellDef[Float] {
+      val baseCode = "float32"
+      def apply(noData: Float) = FloatUserDefinedNoDataCellType(noData)
+      val rng = TestRange(Float.MinValue, Float.MaxValue)
+    }
+
+    object DoubleDef extends CellDef[Double] {
+      val baseCode = "float64"
+      def apply(noData: Double) = DoubleUserDefinedNoDataCellType(noData)
+      val rng = TestRange(Double.MinValue, Double.MaxValue)
+    }
+  }
+
 }
