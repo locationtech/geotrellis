6d32ec987fc779998c1ea631676a80e37bc54f7f
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-06-05 18:12:36 +0300

    Update test HBase
    Signed-off-by: Grisha Pomadchin <gr.pomadchin@gmail.com>

--- project/Version.scala
+++ project/Version.scala
@@ -22,7 +22,7 @@ object Version {
   val monocle     = "1.4.0"
   val accumulo    = "1.7.2"
   val cassandra   = "3.1.4"
-  val hbase       = "1.3.0"
+  val hbase       = "1.3.1"
   val geomesa     = "1.2.7.3"
   lazy val hadoop = Environment.hadoopVersion
   lazy val spark  = Environment.sparkVersion
