9c214d55c4a394fc9c1863538316d02f92b899d7
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-06-09 20:58:10 +0300

    Bump up HBase test DB
    Signed-off-by: Grisha Pomadchin <gr.pomadchin@gmail.com>

--- .travis/hbase-install.sh
+++ .travis/hbase-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ ! -f $HOME/downloads/hbase-1.3.0-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-1.3.0-bin.tar.gz http://www-eu.apache.org/dist/hbase/1.3.0/hbase-1.3.0-bin.tar.gz; fi
-sudo mv $HOME/downloads/hbase-1.3.0-bin.tar.gz hbase-1.3.0-bin.tar.gz && tar xzf hbase-1.3.0-bin.tar.gz
-sudo rm -f hbase-1.3.0/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-1.3.0/conf
-sudo hbase-1.3.0/bin/start-hbase.sh
+if [ ! -f $HOME/downloads/hbase-1.3.1-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-1.3.1-bin.tar.gz http://www-eu.apache.org/dist/hbase/1.3.1/hbase-1.3.1-bin.tar.gz; fi
+sudo mv $HOME/downloads/hbase-1.3.1-bin.tar.gz hbase-1.3.1-bin.tar.gz && tar xzf hbase-1.3.1-bin.tar.gz
+sudo rm -f hbase-1.3.1/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-1.3.1/conf
+sudo hbase-1.3.1/bin/start-hbase.sh
