ece08b31323e8f94c64dee4bf4dc0ce0130bb9d0
James McClain <jmcclain@azavea.com>
2017-06-15 18:18:52 -0400

    Extend Key Length in KeyIndex.scala
    The range length is still expressed as a `Double`, the maximum bin size
    is still a `Long`.
    
    Signed-off-by: James McClain <jmcclain@azavea.com>

--- spark/src/main/scala/geotrellis/spark/io/index/KeyIndex.scala
+++ spark/src/main/scala/geotrellis/spark/io/index/KeyIndex.scala
@@ -21,37 +21,42 @@ import geotrellis.spark.KeyBounds
 trait KeyIndex[K] extends Serializable {
   /** Some(keybounds) if the indexed space is bounded; None if it is unbounded */
   def keyBounds: KeyBounds[K]
-  def toIndex(key: K): Long
-  def indexRanges(keyRange: (K, K)): Seq[(Long, Long)]
+  def toIndex(key: K): BigInt
+  def indexRanges(keyRange: (K, K)): Seq[(BigInt, BigInt)]
 }
 
 object KeyIndex {
   /**
-   * Mapping KeyBounds of Extent to SFC ranges will often result in a set of non-contigrious ranges.
-   * The indices excluded by these ranges should not be included in breaks calculation as they will never be seen.
+   * Mapping KeyBounds of Extent to SFC ranges will often result in a
+   * set of non-contigrious ranges.  The indices excluded by these
+   * ranges should not be included in breaks calculation as they will
+   * never be seen.
    */
-  def breaks[K](kb: KeyBounds[K], ki: KeyIndex[K], count: Int): Vector[Long] = {
+  def breaks[K](kb: KeyBounds[K], ki: KeyIndex[K], count: Int): Vector[BigInt] = {
     breaks(ki.indexRanges(kb), count)
   }
 
   /**
-    * Divide the space covered by ranges as evenly as possible by providing break points from the ranges.
-    * All break points will be from the ranges given and never from spaces between the ranges.
+    * Divide the space covered by ranges as evenly as possible by
+    * providing break points from the ranges.  All break points will
+    * be from the ranges given and never from spaces between the
+    * ranges.
     *
     * @param ranges  sorted list of tuples which represent non-negative, non-intersecting ranges.
     * @param count   desired number of break points
     */
-  def breaks(ranges: Seq[(Long, Long)], count: Int): Vector[Long] = {
+  def breaks(ranges: Seq[(BigInt, BigInt)], count: Int): Vector[BigInt] = {
     require(count > 0, "breaks count must be at least one")
-    def len(r: (Long, Long)) = r._2 - r._1 + 1l
-    val total: Double = ranges.foldLeft(0l)(_ + len(_))
-    val maxBinSize =  math.max(math.ceil(total / (count+1)), 1).toLong
+    def len(r: (BigInt, BigInt)): Double = (r._2 - r._1).toDouble + 1
+    val total: Double = ranges.foldLeft(0.0)(_ + len(_))
+    val maxBinSize: Long =  math.max(math.ceil(total /count+1), 1).toLong
 
-    def take(range: (Long, Long), count: Long): Long = {
+    def take(range: (BigInt, BigInt), count: Long): Long = {
       if (len(range) >= count) count
-      else len(range)
+      else len(range).toLong
     }
-    ranges.foldLeft((Vector.empty[Long], maxBinSize)) { case ((_breaks, _roomLeft), range) =>
+
+    ranges.foldLeft((Vector.empty[BigInt], maxBinSize)) { case ((_breaks, _roomLeft), range) =>
       var breaks = _breaks
       var roomLeft = _roomLeft
       var remainder = range
