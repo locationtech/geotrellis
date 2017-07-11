dcbb412d94d954235ca2915cc2db7a4df1ae201c
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-07-11 18:55:29 +0300

    Use SPI for writers

--- spark-pipeline/src/main/scala/geotrellis/spark/pipeline/json/Write.scala
+++ spark-pipeline/src/main/scala/geotrellis/spark/pipeline/json/Write.scala
@@ -3,15 +3,13 @@ package geotrellis.spark.pipeline.json
 import geotrellis.raster.CellGrid
 import geotrellis.raster.merge.TileMergeMethods
 import geotrellis.raster.prototype.TilePrototypeMethods
-import geotrellis.spark.{Bounds, LayerId, Metadata, SpatialComponent, TileLayerMetadata}
+import geotrellis.spark._
 import geotrellis.spark.io.LayerWriter
 import geotrellis.spark.io.avro.AvroRecordCodec
-import geotrellis.spark.io.file.FileLayerWriter
-import geotrellis.spark.io.hadoop.HadoopLayerWriter
 import geotrellis.spark.pyramid.Pyramid
 import geotrellis.spark.tiling.{LayoutDefinition, LayoutScheme}
 import geotrellis.util.{Component, GetComponent}
-import org.apache.hadoop.fs.Path
+
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import spray.json.JsonFormat
@@ -27,16 +25,15 @@ trait Write extends PipelineExpr {
   val keyIndexMethod: PipelineKeyIndexMethod
   val scheme: Either[LayoutScheme, LayoutDefinition]
 
-  def writer: LayerWriter[LayerId]
+  def writer = LayerWriter(uri)
 
   def eval[
     K: SpatialComponent : AvroRecordCodec : JsonFormat : ClassTag,
     V <: CellGrid : AvroRecordCodec : ClassTag: ? => TileMergeMethods[V]: ? => TilePrototypeMethods[V],
     M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]: JsonFormat : GetComponent[?, Bounds[K]]
   ](tuple: (Int, RDD[(K, V)] with Metadata[M])) = {
+    lazy val _writer = writer
     def savePyramid(zoom: Int, rdd: RDD[(K, V)] with Metadata[M]): Unit = {
-      val currentId = LayerId(name, zoom)
-
       scheme match {
         case Left(s) =>
           if (pyramid && zoom >= 1) {
@@ -50,7 +47,7 @@ trait Write extends PipelineExpr {
     }
 
     val (zoom, rdd) = tuple
-    writer.write(LayerId(name, zoom), rdd, keyIndexMethod.getKeyIndexMethod[K])
+    _writer.write(LayerId(name, zoom), rdd, keyIndexMethod.getKeyIndexMethod[K])
     savePyramid(zoom, rdd)
     tuple
   }
@@ -65,9 +62,7 @@ case class WriteFile(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-) extends Write {
-  lazy val writer = FileLayerWriter(uri)
-}
+) extends Write
 
 case class WriteHadoop(
   `type`: String,
@@ -78,9 +73,7 @@ case class WriteHadoop(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-)(implicit sc: SparkContext) extends Write {
-  lazy val writer = HadoopLayerWriter(new Path(uri))
-}
+)(implicit sc: SparkContext) extends Write
 
 case class WriteS3(
   `type`: String,
@@ -91,9 +84,7 @@ case class WriteS3(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-) extends Write {
-  lazy val writer = ???
-}
+) extends Write
 
 case class WriteAccumulo(
   `type`: String,
@@ -104,9 +95,7 @@ case class WriteAccumulo(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-) extends Write {
-  lazy val writer = ???
-}
+) extends Write
 
 case class WriteCassandra(
   `type`: String,
@@ -117,9 +106,7 @@ case class WriteCassandra(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-) extends Write {
-  lazy val writer = ???
-}
+) extends Write
 
 case class WriteHBase(
   `type`: String,
@@ -130,6 +117,4 @@ case class WriteHBase(
   keyIndexMethod: PipelineKeyIndexMethod,
   scheme: Either[LayoutScheme, LayoutDefinition],
   maxZoom: Option[Int] = None
-) extends Write {
-  lazy val writer = ???
-}
+) extends Write
