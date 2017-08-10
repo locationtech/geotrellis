c0b480d785b7508efe6a15ca88b059afa7caf161
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-08-10 20:19:38 +0300

    Move RealWorld type into a separate file

---
+++ spark-pipeline/src/main/scala/geotrellis/spark/pipeline/ast/RealWorld.scala
@@ -0,0 +1,20 @@
+package geotrellis.spark.pipeline.ast
+
+import geotrellis.spark.pipeline.json.PipelineExpr
+
+import io.circe.Json
+import org.apache.spark.SparkContext
+
+sealed trait RealWorld
+
+object RealWorld {
+  /** RealWorld node has nothing implemented */
+  def instance = new Node[RealWorld] {
+    def get(implicit sc: SparkContext): RealWorld =
+      throw new UnsupportedOperationException("get function is not supported by a RealWorld node")
+    def arg: PipelineExpr =
+      throw new UnsupportedOperationException("arg function is not supported by a RealWorld node")
+    def asJson: List[Json] =
+      throw new UnsupportedOperationException("asJson function is not supported by a RealWorld node")
+  }
+}
