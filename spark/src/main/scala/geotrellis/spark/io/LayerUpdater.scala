90488a8bf94742f5db352aec210a3edc42f732a1
James McClain <jmcclain@azavea.com>
2017-09-06 14:20:32 -0400

    Remove Protected Methods in LayerUpdater

--- spark/src/main/scala/geotrellis/spark/io/LayerUpdater.scala
+++ spark/src/main/scala/geotrellis/spark/io/LayerUpdater.scala
@@ -32,17 +32,18 @@ import spray.json._
 import scala.reflect.ClassTag
 
 abstract class LayerUpdater[ID] {
-  protected def _overwrite[
-    K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
-    V: AvroRecordCodec: ClassTag,
-    M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
-  ](id: ID, rdd: RDD[(K, V)] with Metadata[M], keyBounds: KeyBounds[K]): Unit
 
-  protected def _update[
-    K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
-    V: AvroRecordCodec: ClassTag,
-    M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
-  ](id: ID, rdd: RDD[(K, V)] with Metadata[M], keyBounds: KeyBounds[K], mergeFunc: (V, V) => V): Unit
+  // protected def _overwrite[
+  //   K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
+  //   V: AvroRecordCodec: ClassTag,
+  //   M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
+  // ](id: ID, rdd: RDD[(K, V)] with Metadata[M], keyBounds: KeyBounds[K]): Unit
+
+  // protected def _update[
+  //   K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
+  //   V: AvroRecordCodec: ClassTag,
+  //   M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
+  // ](id: ID, rdd: RDD[(K, V)] with Metadata[M], keyBounds: KeyBounds[K], mergeFunc: (V, V) => V): Unit
 
   protected def schemaHasChanged[K: AvroRecordCodec, V: AvroRecordCodec](writerSchema: Schema): Boolean = {
     val codec  = KeyValueRecordCodec[K, V]
@@ -54,37 +55,40 @@ abstract class LayerUpdater[ID] {
     K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
     V: AvroRecordCodec: ClassTag,
     M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
-  ](id: ID, rdd: RDD[(K, V)] with Metadata[M], mergeFunc: (V, V) => V): Unit =
-    rdd.metadata.getComponent[Bounds[K]] match {
-      case keyBounds: KeyBounds[K] =>
-        _update(id, rdd, keyBounds, mergeFunc)
-      case EmptyBounds =>
-        throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
-    }
+  ](id: ID, rdd: RDD[(K, V)] with Metadata[M], mergeFunc: (V, V) => V): Unit
+  // =
+  //   rdd.metadata.getComponent[Bounds[K]] match {
+  //     case keyBounds: KeyBounds[K] =>
+  //       _update(id, rdd, keyBounds, mergeFunc)
+  //     case EmptyBounds =>
+  //       throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
+  //   }
 
   def update[
     K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
     V: AvroRecordCodec: ClassTag,
     M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
-  ](id: ID, rdd: RDD[(K, V)] with Metadata[M]): Unit =
-    rdd.metadata.getComponent[Bounds[K]] match {
-      case keyBounds: KeyBounds[K] =>
-        // By default, we want the updating tile to replace the existing tile.
-        val mergeFunc: (V, V) => V = { (existing, updating) => updating }
-        _update(id, rdd, keyBounds, mergeFunc)
-      case EmptyBounds =>
-        throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
-    }
+  ](id: ID, rdd: RDD[(K, V)] with Metadata[M]): Unit
+  // =
+  //   rdd.metadata.getComponent[Bounds[K]] match {
+  //     case keyBounds: KeyBounds[K] =>
+  //       // By default, we want the updating tile to replace the existing tile.
+  //       val mergeFunc: (V, V) => V = { (existing, updating) => updating }
+  //       _update(id, rdd, keyBounds, mergeFunc)
+  //     case EmptyBounds =>
+  //       throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
+  //   }
 
   def overwrite[
     K: AvroRecordCodec: Boundable: JsonFormat: ClassTag,
     V: AvroRecordCodec: ClassTag,
     M: JsonFormat: GetComponent[?, Bounds[K]]: Mergable
-  ](id: ID, rdd: RDD[(K, V)] with Metadata[M]): Unit =
-    rdd.metadata.getComponent[Bounds[K]] match {
-      case keyBounds: KeyBounds[K] =>
-        _overwrite(id, rdd, keyBounds)
-      case EmptyBounds =>
-        throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
-    }
+  ](id: ID, rdd: RDD[(K, V)] with Metadata[M]): Unit
+  // =
+  //   rdd.metadata.getComponent[Bounds[K]] match {
+  //     case keyBounds: KeyBounds[K] =>
+  //       _overwrite(id, rdd, keyBounds)
+  //     case EmptyBounds =>
+  //       throw new EmptyBoundsError(s"Cannot update layer $id with a layer with empty bounds.")
+  //   }
 }
