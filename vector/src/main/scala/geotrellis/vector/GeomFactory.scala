bab0b37c6dd9ef3dd5016c90c841122758cfc580
jpolchlo <jpolchlopek@azavea.com>
2017-09-14 16:35:08 -0400

    Fix link in comment to point to Wayback Machine version of dead link
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- vector/src/main/scala/geotrellis/vector/GeomFactory.scala
+++ vector/src/main/scala/geotrellis/vector/GeomFactory.scala
@@ -37,7 +37,7 @@ private[vector] object GeomFactory {
 
   val factory = new geom.GeometryFactory(precisionModel)
 
-  // 12 digits is maximum to avoid [[TopologyException]], see http://tsusiatsoftware.net/jts/jts-faq/jts-faq.html#D9
+  // 12 digits is maximum to avoid [[TopologyException]], see https://web.archive.org/web/20160226031453/http://tsusiatsoftware.net/jts/jts-faq/jts-faq.html#D9
   lazy val simplifier = new GeometryPrecisionReducer(new PrecisionModel(1e12))
 
 }
