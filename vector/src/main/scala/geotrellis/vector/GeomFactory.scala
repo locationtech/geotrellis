3b8c9fafb217e82659f079c25f61f827a7703983
jpolchlo <jpolchlopek@azavea.com>
2017-09-14 16:32:32 -0400

    Fix error in geotrellis.vector.GeomFactory.simplifier to take 12 digits of precision, not use a scale factor of 12
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- vector/src/main/scala/geotrellis/vector/GeomFactory.scala
+++ vector/src/main/scala/geotrellis/vector/GeomFactory.scala
@@ -38,6 +38,6 @@ private[vector] object GeomFactory {
   val factory = new geom.GeometryFactory(precisionModel)
 
   // 12 digits is maximum to avoid [[TopologyException]], see http://tsusiatsoftware.net/jts/jts-faq/jts-faq.html#D9
-  lazy val simplifier = new GeometryPrecisionReducer(new PrecisionModel(12))
+  lazy val simplifier = new GeometryPrecisionReducer(new PrecisionModel(1e12))
 
 }
