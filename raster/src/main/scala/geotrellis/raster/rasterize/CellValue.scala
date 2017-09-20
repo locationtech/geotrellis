6c7227f4265c4cf860f399b8de5199482792fb6b
Nathan Zimmerman <npzimmerman@gmail.com>
2017-09-20 16:45:37 -0400

    Add extra (optional) parameter to rasterizeRDD for zindex celltype

--- raster/src/main/scala/geotrellis/raster/rasterize/CellValue.scala
+++ raster/src/main/scala/geotrellis/raster/rasterize/CellValue.scala
@@ -3,5 +3,5 @@ package geotrellis.raster.rasterize
 import geotrellis.raster.CellType
 
 
-/** Cell value with its zindex and celltype to be used by the rasterizer. */
-case class CellValue(value: Double, zindex: Double, celltype: CellType)
+/** Cell value with its zindex to be used by the rasterizer. */
+case class CellValue(value: Double, zindex: Double)
