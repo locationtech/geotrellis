4e6885cacf4287427c4791c85320d779f6b91cf9
jpolchlo <jpolchlopek@azavea.com>
2017-09-21 16:46:25 -0400

    Add tests and make them pass
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- spark/src/main/scala/geotrellis/spark/regrid/Regrid.scala
+++ spark/src/main/scala/geotrellis/spark/regrid/Regrid.scala
@@ -18,6 +18,7 @@ object Regrid {
 
   private case class Interval[N : Ordering](start: N, end: N) {
     val ord = implicitly[Ordering[N]]
+    assert(ord.compare(start, end) < 1)
     // let the interval be start to end, inclusive
     def intersect(that: Interval[N]) = {
         Interval(if (ord.compare(this.start, that.start) > 0) this.start else that.start, 
@@ -52,24 +53,26 @@ object Regrid {
       // the new extent of the resized tiles
       val nld = LayoutDefinition(
         Extent(
-          oldEx.xmin, 
-          oldEx.ymax - cellH * tl.layoutRows * oldH, 
-          oldEx.xmin + cellW * tl.layoutCols * oldW, 
-          oldEx.ymax), 
+          oldEx.xmin,
+          oldEx.ymax - cellH * ntl.layoutRows * tileRows,
+          oldEx.xmin + cellW * ntl.layoutCols * tileCols,
+          oldEx.ymax),
         ntl)
+      // println(s"Created new LayoutDefinition with ${nld.extent} and $ntl from ${ld.extent} and ${ld.tileLayout}")
 
       val bounds =
         md.getComponent[Bounds[K]] match {
           case KeyBounds(minKey, maxKey) =>
-            val newMinKey = {
-              val sk = nld.mapTransform(ld.mapTransform(minKey.getComponent[SpatialKey]).northWest)
-              minKey.setComponent[SpatialKey](sk)
-            }
-            val newMaxKey = {
-              val sk = nld.mapTransform(ld.mapTransform(maxKey.getComponent[SpatialKey]).southEast)
-              maxKey.setComponent[SpatialKey](sk)
-            }
-            KeyBounds(newMinKey, newMaxKey)
+            val ulSK = minKey.getComponent[SpatialKey]
+            val lrSK = maxKey.getComponent[SpatialKey]
+            val pxXrange = Interval(ulSK._1 * oldW.toLong, (lrSK._1 + 1) * oldW.toLong - 1)
+            val pxYrange = Interval(ulSK._2 * oldH.toLong, (lrSK._2 + 1) * oldH.toLong - 1)
+
+            val newMinKey = SpatialKey((pxXrange.start / tileCols).toInt, (pxYrange.start / tileRows).toInt)
+            val newMaxKey = SpatialKey((pxXrange.end / tileCols).toInt, (pxYrange.end / tileRows).toInt)
+
+            // println(s"In bounds calculation: $minKey became $newMinKey and $maxKey became $newMaxKey")
+            KeyBounds(minKey.setComponent[SpatialKey](newMinKey), maxKey.setComponent[SpatialKey](newMaxKey))
           case EmptyBounds =>
             EmptyBounds
         }
@@ -91,19 +94,27 @@ object Regrid {
             val tileEx = ld.mapTransform(key)
             val newBounds = nld.mapTransform(tileEx)
 
+            val xBounds = (oldXstart / tileCols).toInt to (oldXrange.end.toDouble / tileCols).toInt
+            val yBounds = (oldYstart / tileRows).toInt to (oldYrange.end.toDouble / tileRows).toInt
+
+            // println(s"For $key:\n\tIntersects $newBounds in new layout (x: $xBounds, y: $yBounds) (pixel ranges, x=$oldXrange, y=$oldYrange)")
+
             for (
-              x <- newBounds.colMin to newBounds.colMax ;
+              // x <- newBounds.colMin to newBounds.colMax ;
+              x <- xBounds ;
               newXrange = {
                 val newXstart = x * tileCols.toLong
                 Interval(newXstart, newXstart + tileCols - 1)
               } ;
-              y <- newBounds.rowMin to newBounds.rowMax ;
+              // y <- newBounds.rowMin to newBounds.rowMax ;
+              y <- yBounds ;
               newYrange = {
                 val newYstart = y * tileRows.toLong
                 Interval(newYstart, newYstart + tileRows - 1)
               }
             ) yield {
               val newKey = key.setComponent[SpatialKey](SpatialKey(x, y))
+
               val xSpan: Interval[Long] = oldXrange intersect newXrange
               val ySpan: Interval[Long] = oldYrange intersect newYrange
               newKey -> 
@@ -119,126 +130,6 @@ object Regrid {
           .mapValues { tiles => implicitly[Stitcher[V]].stitch(tiles, tileCols, tileRows) }
 
       ContextRDD(tiled, newMd)
-
-      // val ceil = { x: Double => math.ceil(x).toInt }
-
-      // val tl = ld.tileLayout
-      // val oldTileCols = tl.tileCols
-      // val oldTileRows = tl.tileRows
-      // if(tileSize > oldTileSize) {
-      //   val r = tileSize / oldTileSize
-      //   val ntl = TileLayout(ceil(tl.layoutCols / r.toDouble), ceil(tl.layoutRows / r.toDouble), tileSize, tileSize)
-      //   val layoutDefinition =
-      //     LayoutDefinition(
-      //       ld.extent,
-      //       ntl
-      //     )
-
-      //   val bounds =
-      //     md.getComponent[Bounds[K]] match {
-      //       case KeyBounds(minKey, maxKey) =>
-      //         val newMinKey = {
-      //           val SpatialKey(minKeyCol, minKeyRow) = minKey.getComponent[SpatialKey]
-      //           val sk = SpatialKey(minKeyCol / r, minKeyRow / r)
-      //           minKey.setComponent[SpatialKey](sk)
-      //         }
-      //         val newMaxKey = {
-      //           val SpatialKey(maxKeyCol, maxKeyRow) = maxKey.getComponent[SpatialKey]
-      //           val sk = SpatialKey(ceil(maxKeyCol / r.toDouble), ceil(maxKeyRow / r.toDouble))
-      //           maxKey.setComponent[SpatialKey](sk)
-      //         }
-      //         KeyBounds(newMinKey, newMaxKey)
-      //       case EmptyBounds =>
-      //         EmptyBounds
-      //     }
-
-      //   val newMd =
-      //     md.setComponent[LayoutDefinition](layoutDefinition)
-      //       .setComponent[Bounds[K]](bounds)
-
-      //   val tiled =
-      //     layer
-      //       .map { case (key, tile) =>
-      //              val sk = key.getComponent[SpatialKey]
-      //              val newKey = key.setComponent[SpatialKey](SpatialKey(sk.col / r, sk.row / r))
-      //                                                       (newKey, (key, tile))
-      //            }
-      //   .groupByKey
-      //   .map { case (key, tiles) =>
-      //          val pieces =
-      //            tiles.map { case (oldKey, oldTile) =>
-      //                        val SpatialKey(oldCol, oldRow) = oldKey.getComponent[SpatialKey]
-      //                        val updateCol = (oldCol % r) * oldTileSize
-      //                        val updateRow = (oldRow % r) * oldTileSize
-      //                        (oldTile, (updateCol, updateRow))
-      //                      }
-      //          val newTile =
-      //              implicitly[Stitcher[V]].stitch(pieces, tileSize, tileSize)
-
-      //          (key, newTile)
-      //        }
-
-      //   ContextRDD(tiled, newMd)
-      // } else {
-      //   val r = oldTileSize / tileSize
-      //   val ntl = TileLayout(tl.layoutCols * r, tl.layoutRows * r, tileSize, tileSize)
-      //   val layoutDefinition =
-      //     LayoutDefinition(
-      //       ld.extent,
-      //       ntl
-      //     )
-
-      //   val bounds =
-      //     md.getComponent[Bounds[K]] match {
-      //       case KeyBounds(minKey, maxKey) =>
-      //         val newMinKey = {
-      //           val SpatialKey(minKeyCol, minKeyRow) = minKey.getComponent[SpatialKey]
-      //           val sk = SpatialKey(minKeyCol * r, minKeyRow * r)
-      //           minKey.setComponent[SpatialKey](sk)
-      //         }
-      //         val newMaxKey = {
-      //           val SpatialKey(maxKeyCol, maxKeyRow) = maxKey.getComponent[SpatialKey]
-      //           val sk = SpatialKey((maxKeyCol + 1) * r - 1, (maxKeyRow + 1) * r - 1)
-      //           maxKey.setComponent[SpatialKey](sk)
-      //         }
-      //         KeyBounds(newMinKey, newMaxKey)
-      //       case EmptyBounds =>
-      //         EmptyBounds
-      //     }
-
-      //   val newMd =
-      //     md.setComponent[LayoutDefinition](layoutDefinition)
-      //       .setComponent[Bounds[K]](bounds)
-
-      //   val layoutCols = r
-      //   val layoutRows = r
-
-      //   val tiled =
-      //     layer.flatMap { case (key, tile) =>
-      //                     val result = mutable.ListBuffer[(K, V)]()
-      //                     val thisKey = key.getComponent[SpatialKey]
-      //                     for(layoutRow <- 0 until layoutRows) {
-      //                       for(layoutCol <- 0 until layoutCols) {
-      //                         val sk = SpatialKey(thisKey.col * layoutCols + layoutCol, thisKey.row * layoutRows + layoutRow)
-      //                         val newTile =
-      //                           tile.crop(
-      //                             GridBounds(
-      //                               layoutCol * tileSize,
-      //                               layoutRow * tileSize,
-      //                               (layoutCol + 1) * tileSize - 1,
-      //                               (layoutRow + 1) * tileSize - 1
-      //                             )
-      //                           )
-
-      //                         result += ((key.setComponent[SpatialKey](sk), newTile))
-      //                       }
-      //                     }
-
-      //                     result
-      //                   }
-
-      //   ContextRDD(tiled, newMd)
-      // }
     }
   }
 
---
+++ spark/src/test/scala/geotrellis/spark/regrid/Regrid.scala
@@ -0,0 +1,69 @@
+package geotrellis.spark.regrid
+
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.raster.testkit._
+import geotrellis.spark._
+import geotrellis.spark.testkit._
+import geotrellis.spark.tiling._
+import geotrellis.vector._
+
+import org.scalatest._
+
+class RegridSpec extends FunSpec with TestEnvironment with RasterMatchers {
+
+  val simpleLayer = {
+    val tiles = 
+      for ( x <- 0 to 3 ;
+            y <- 0 to 2
+      ) yield {
+        val tile = IntArrayTile.ofDim(32, 32)
+        (SpatialKey(x, y), tile.map{ (tx, ty, _) => math.max(tx + 32 * x, ty + 32 * y) })
+      }
+    val rdd = sc.parallelize(tiles)
+    val ex = Extent(0,0,12.8,9.6)
+    val ld = LayoutDefinition(GridExtent(ex, 0.1, 0.1), 32, 32)
+    val md = TileLayerMetadata[SpatialKey](IntConstantNoDataCellType,
+                                           ld,
+                                           ex,
+                                           LatLng,
+                                           KeyBounds[SpatialKey](SpatialKey(0,0), SpatialKey(3,2)))
+    ContextRDD(rdd, md)
+  }
+
+  describe("Regridding") {
+    it("should allow chipping into smaller tiles") {
+      val newLayer = Regrid.regrid(simpleLayer, 16, 16)
+
+      import geotrellis.raster.render._
+      val cm = ColorMap((0 to 128).toArray, ColorRamps.Plasma)
+      newLayer.stitch.tile.renderPng(cm).write("regrid_16.png")
+
+      assert(simpleLayer.stitch.dimensions == newLayer.stitch.dimensions)
+      assertEqual(simpleLayer.stitch, newLayer.stitch)
+    }
+
+    it("should allow joining into larger tiles") {
+      val newLayer = Regrid.regrid(simpleLayer, 64, 64)
+
+      import geotrellis.raster.render._
+      val cm = ColorMap((0 to 128).toArray, ColorRamps.Plasma)
+      newLayer.stitch.tile.renderPng(cm).write("regrid_64.png")
+
+      assert(newLayer.stitch.dimensions == (128, 128))
+      assertEqual(simpleLayer.stitch, newLayer.stitch.tile.crop(0,0,127,95))
+    }
+
+    it("should allow breaking into non-square tiles") {
+      val newLayer = Regrid.regrid(simpleLayer, 50, 25)
+
+      import geotrellis.raster.render._
+      val cm = ColorMap((0 to 128).toArray, ColorRamps.Plasma)
+      newLayer.stitch.tile.renderPng(cm).write("regrid_50_25.png")
+
+      assert(newLayer.stitch.dimensions == (150, 100))
+      assertEqual(simpleLayer.stitch, newLayer.stitch.tile.crop(0,0,127,95))
+    }
+  }
+
+}
