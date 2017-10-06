e28cfaa26d7694d5fd7696e11e51d11f0edfceb8
jpolchlo <jpolchlopek@azavea.com>
2017-10-06 14:01:23 -0400

    Include copyright headers; add helpful (?) assert message for future debugging
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- spark/src/main/scala/geotrellis/spark/regrid/Regrid.scala
+++ spark/src/main/scala/geotrellis/spark/regrid/Regrid.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package geotrellis.spark.regrid
 
 import org.apache.spark.rdd.RDD
@@ -17,7 +33,7 @@ object Regrid {
 
   private case class Interval[N : Ordering](start: N, end: N) {
     val ord = implicitly[Ordering[N]]
-    assert(ord.compare(start, end) < 1)
+    assert(ord.compare(start, end) < 1, "Row/col intervals must begin before they end")
 
     // let the interval be start to end, inclusive
     def intersect(that: Interval[N]) = {
