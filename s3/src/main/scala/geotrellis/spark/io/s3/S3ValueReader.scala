0ed40f0951e2d391268cac8f8cf7e9f5180283c5
James McClain <jmcclain@azavea.com>
2017-10-09 14:18:59 -0400

    Change `val` to `def` in S3ValueReader

--- s3/src/main/scala/geotrellis/spark/io/s3/S3ValueReader.scala
+++ s3/src/main/scala/geotrellis/spark/io/s3/S3ValueReader.scala
@@ -36,7 +36,7 @@ class S3ValueReader(
   val attributeStore: AttributeStore
 ) extends OverzoomingValueReader {
 
-  val s3Client: S3Client = S3Client.DEFAULT
+  def s3Client: S3Client = S3Client.DEFAULT
 
   def reader[K: AvroRecordCodec: JsonFormat: ClassTag, V: AvroRecordCodec](layerId: LayerId): Reader[K, V] = new Reader[K, V] {
     val header = attributeStore.readHeader[S3LayerHeader](layerId)
