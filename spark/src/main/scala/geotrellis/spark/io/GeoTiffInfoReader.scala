72f141b3b7e9010e72adc3ca39ae8623d3fcf29b
Eugene Cheipesh <echeipesh@gmail.com>
2017-11-14 09:56:38 -0500

    Partition GeoTiff segments to minimize partition overlap

--- spark/src/main/scala/geotrellis/spark/io/GeoTiffInfoReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/GeoTiffInfoReader.scala
@@ -82,15 +82,7 @@ private [geotrellis] trait GeoTiffInfoReader extends LazyLogging {
       val segCols = md.segmentLayout.tileLayout.tileCols
       val segRows = md.segmentLayout.tileLayout.tileRows
       val cellType = md.cellType
-      val depth = {
-        cellType match {
-          case BitCellType | ByteCellType | UByteCellType | ByteConstantNoDataCellType | ByteUserDefinedNoDataCellType(_) | UByteConstantNoDataCellType | UByteUserDefinedNoDataCellType(_) => 1
-          case ShortCellType | UShortCellType | ShortConstantNoDataCellType | ShortUserDefinedNoDataCellType(_) | UShortConstantNoDataCellType | UShortUserDefinedNoDataCellType(_) => 2
-          case IntCellType | IntConstantNoDataCellType | IntUserDefinedNoDataCellType(_) => 4
-          case FloatCellType | FloatConstantNoDataCellType | FloatUserDefinedNoDataCellType(_) => 4
-          case DoubleCellType | DoubleConstantNoDataCellType | DoubleUserDefinedNoDataCellType(_) => 8
-        }
-      }
+
       val fileWindows =
         geometry match {
           case Some(geometry) =>
@@ -101,33 +93,8 @@ private [geotrellis] trait GeoTiffInfoReader extends LazyLogging {
             RasterReader.listWindows(cols, rows, maxSize, segCols, segRows)
         }
 
-      var currentBytes: Long = 0
-      val currentPartition = mutable.ArrayBuffer.empty[GridBounds]
-      val allPartitions = mutable.ArrayBuffer.empty[Array[GridBounds]]
-
-      fileWindows.foreach({ gb =>
-        val windowBytes = gb.sizeLong * depth
-
-        // Add the window to the present partition
-        if (currentBytes + windowBytes <= partitionBytes) {
-          currentPartition.append(gb)
-          currentBytes += windowBytes
-        }
-        // The window is small enough to fit into some partition,
-        // but not this partition; start a new partition.
-        else if ((currentBytes + windowBytes > partitionBytes) && (windowBytes < partitionBytes)) {
-          allPartitions.append(currentPartition.toArray)
-          currentPartition.clear
-          currentPartition.append(gb)
-          currentBytes = windowBytes
-        }
-        // The window is too large to fit into any partition.
-        else {
-          allPartitions.append(Array(gb))
-        }
-      })
-      allPartitions.append(currentPartition.toArray)
-      allPartitions.toArray.map({ array => (uri, array) })
+      val partitions = md.segmentLayout.partitionWindowsBySegments(fileWindows, partitionBytes / cellType.bytes)
+      partitions.map({ windows => (uri, windows)})
     })
   }
 }
