f7ae4213d52a652f28a1711d5553f85a7b0d4dda
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-11-17 17:02:12 +0300

    Construct GeoTiffs from segments

--- spark/src/main/scala/geotrellis/spark/cog/Implicits.scala
+++ spark/src/main/scala/geotrellis/spark/cog/Implicits.scala
@@ -6,6 +6,8 @@ import geotrellis.raster.io.geotiff._
 import geotrellis.spark._
 import geotrellis.spark.tiling._
 
+import spire.syntax.cfor._
+
 object Implicits extends Implicits
 
 trait Implicits {
@@ -43,6 +45,146 @@ trait Implicits {
         Tags.empty,
         options = options,
         overviews.map(_.asInstanceOf[MultibandGeoTiff])
+      )
+    }
+  }
+
+  implicit class withSinglebandGeoTiffSegmentConstructMethods(val self: Iterable[(SpatialKey, Tile)]) extends GeoTiffSegmentConstructMethods[Tile] {
+    def toGeoTiff[K](
+      nextLayout: LayoutDefinition,
+      md: TileLayerMetadata[K],
+      options: GeoTiffOptions,
+      overviews: List[GeoTiff[Tile]] = Nil
+    ): SinglebandGeoTiff = {
+      val geoTiffTile: GeoTiffTile = {
+        val (layoutCols, layoutRows) = gb.width * nextLayout.tileCols -> gb.height * nextLayout.tileRows
+
+        val segmentLayout = GeoTiffSegmentLayout(layoutCols, layoutRows, options.storageMethod, BandInterleave, BandType.forCellType(md.cellType))
+
+        val segmentCount = segmentLayout.tileLayout.layoutCols * segmentLayout.tileLayout.layoutRows
+        val compressor = options.compression.createCompressor(segmentCount)
+
+        val segments: Map[Int, Array[Byte]] =
+          self
+            .map { case (spatialKey, tile) =>
+              val updateCol = (spatialKey.col - gb.colMin) * md.tileLayout.tileCols
+              val updateRow = (spatialKey.row - gb.rowMin) * md.tileLayout.tileRows
+              val index = segmentLayout.getSegmentIndex(updateCol, updateRow)
+
+              index -> compressor.compress(tile.toBytes, index)
+            }
+            .toMap
+
+        val segmentBytes = Array.ofDim[Array[Byte]](segmentCount)
+        cfor(0)(_ < segmentCount, _ + 1) { i =>
+          segmentBytes(i) = segments.getOrElse(i, compressor.compress(Array[Byte](), i))
+        }
+
+        GeoTiffTile(new ArraySegmentBytes(segmentBytes), compressor.createDecompressor, segmentLayout, options.compression, md.cellType)
+      }
+
+      val re = RasterExtent(nextLayout.mapTransform(gb), geoTiffTile.cols, geoTiffTile.rows)
+      val gridBounds = re.gridBoundsFor(md.extent, clamp = false)
+
+      SinglebandGeoTiff(
+        geoTiffTile.crop(gridBounds),
+        md.extent,
+        md.crs,
+        Tags.empty,
+        options = options,
+        overviews = overviews.map(_.asInstanceOf[SinglebandGeoTiff])
+      )
+    }
+  }
+
+  implicit class withMultibandGeoTiffSegmentConstructMethods(val self: Iterable[(SpatialKey, MultibandTile)]) extends GeoTiffSegmentConstructMethods[MultibandTile] {
+    def toGeoTiff[K](
+      nextLayout: LayoutDefinition,
+      md: TileLayerMetadata[K],
+      options: GeoTiffOptions,
+      overviews: List[GeoTiff[MultibandTile]] = Nil
+    ): MultibandGeoTiff = {
+      val geoTiffTile: GeoTiffMultibandTile = {
+        val bandCount = self.head._2.bandCount
+
+        // TODO: Handle band interleave construction.
+        val (layoutCols, layoutRows) = gb.width * nextLayout.tileCols -> gb.height * nextLayout.tileRows
+
+        val segmentLayout = GeoTiffSegmentLayout(layoutCols, layoutRows, options.storageMethod, PixelInterleave, BandType.forCellType(md.cellType))
+
+        val segmentCount = segmentLayout.tileLayout.layoutCols * segmentLayout.tileLayout.layoutRows
+        val compressor = options.compression.createCompressor(segmentCount)
+
+        val segmentBytes = Array.ofDim[Array[Byte]](segmentCount)
+        val segmentTiles = Array.ofDim[Array[Tile]](segmentCount)
+
+        segmentLayout.interleaveMethod match {
+          case PixelInterleave => {
+            cfor(0)(_ < bandCount, _ + 1) { bandIndex =>
+              val bandTiles =
+                self
+                  .map { case (spatialKey, tile) =>
+                    val updateCol = (spatialKey.col - gb.colMin) * md.tileLayout.tileCols
+                    val updateRow = (spatialKey.row - gb.rowMin) * md.tileLayout.tileRows
+                    val index = segmentLayout.getSegmentIndex(updateCol, updateRow)
+
+                    index -> tile.band(bandIndex)
+                  }.toMap
+
+              cfor(0)(_ < segmentCount, _ + 1) { segmentIndex =>
+                val bandTile = bandTiles.getOrElse(segmentIndex, ArrayTile.empty(md.cellType, md.tileLayout.tileCols, md.tileLayout.tileRows))
+                if (bandIndex == 0) {
+                  segmentTiles(segmentIndex) = Array.ofDim[Tile](bandCount)
+                }
+                segmentTiles(segmentIndex)(bandIndex) = bandTile
+              }
+            }
+
+            val byteCount = md.cellType.bytes
+
+            cfor(0)(_ < segmentCount, _ + 1) { i =>
+              val tiles = segmentTiles(i)
+              val cols = tiles(0).cols
+              val rows = tiles(0).rows
+              val segBytes = Array.ofDim[Byte](cols * rows * bandCount * byteCount)
+
+              val tileBytes = Array.ofDim[Array[Byte]](bandCount)
+              cfor(0)(_ < bandCount, _ + 1) { b =>
+                tileBytes(b) = tiles(b).toBytes
+              }
+
+              var segmentIndex = 0
+              cfor(0)(_ < cols * rows, _ + 1) { cellIndex =>
+                cfor(0)(_ < bandCount, _ + 1) { bandIndex =>
+                  cfor(0)(_ < byteCount, _ + 1) { b =>
+                    val bytes = tileBytes(bandIndex)
+                    segBytes(segmentIndex) = bytes(cellIndex * byteCount + b)
+                    segmentIndex += 1
+                  }
+                }
+              }
+
+              segmentBytes(i) = compressor.compress(segBytes, i)
+            }
+          }
+
+          case BandInterleave =>
+            throw new Exception("Band interleave construction is not supported yet.")
+        }
+
+        GeoTiffMultibandTile(new ArraySegmentBytes(segmentBytes), compressor.createDecompressor, segmentLayout, options.compression, bandCount, md.cellType)
+      }
+
+      val re = RasterExtent(nextLayout.mapTransform(gb), geoTiffTile.cols, geoTiffTile.rows)
+      val gridBounds = re.gridBoundsFor(md.extent, clamp = false)
+
+      MultibandGeoTiff(
+        geoTiffTile.crop(gridBounds),
+        md.extent,
+        md.crs,
+        Tags.empty,
+        options = options,
+        overviews = overviews.map(_.asInstanceOf[MultibandGeoTiff])
       )
     }
   }
--- spark/src/test/scala/geotrellis/spark/cog/COGLayerSpec.scala
+++ spark/src/test/scala/geotrellis/spark/cog/COGLayerSpec.scala
@@ -39,7 +39,42 @@ class COGLayerSpec extends FunSpec
   with Matchers
   with TestEnvironment {
   describe("COGLayer") {
-    it("should write GeoTrellis COG Layer") {
+  it("should write GeoTrellis stitch based COGLayer") {
+    val source = sc.hadoopGeoTiffRDD(new Path("file:///Users/daunnc/subversions/git/github/pomadchin/geotrellis/raster/data/geotiff-test-files/reproject/cea.tif"))
+    val list: ListBuffer[(Int, TileLayerRDD[SpatialKey])] = ListBuffer()
+    val layoutScheme = ZoomedLayoutScheme(LatLng, 512)
+
+    Ingest[ProjectedExtent, SpatialKey](source, LatLng, layoutScheme) { (rdd, zoom) =>
+      list += zoom -> rdd
+    }
+
+    val (zoom, layer) = list.head
+    val index: ZSpatialKeyIndex = new ZSpatialKeyIndex(layer.metadata.bounds match {
+      case kb: KeyBounds[SpatialKey] => kb
+      case _ => null
+    })
+
+    //COGLayer.withStitch(layer)(zoom, 7, layoutScheme).collect().head
+
+    val cogs = COGLayerS(layer)(zoom, 7, layoutScheme)
+    COGLayer.write(cogs)(index, new URI("file:///tmp/write"))
+
+    val (key, tiff: GeoTiff[Tile]) = cogs.collect().head
+    val sfc = index.toIndex(key)
+
+    val tiff2 =
+      GeoTiff(layer.stitch, layer.metadata.mapTransform(layer.metadata.gridBounds), LatLng)
+        .copy(options = tiff.options)
+
+    GeoTiffWriter.write(tiff2.crop(layer.metadata.extent), "/tmp/testo2.tif", optimizedOrder = true)
+
+    //GeoTiffWriter.write(tiff, s"/tmp/${sfc}_base.tif", optimizedOrder = true)
+    //GeoTiffWriter.write(tiff.overviews(0), s"/tmp/${sfc}_0.tif", optimizedOrder = true)
+    //GeoTiffWriter.write(tiff.overviews(1), s"/tmp/${sfc}_1.tif", optimizedOrder = true)
+    //GeoTiffWriter.write(tiff.overviews(2), s"/tmp/${sfc}_2.tif", optimizedOrder = true)
+  }
+
+    it("should write GeoTrellis COGLayer") {
       val source = sc.hadoopGeoTiffRDD(new Path("file:///Users/daunnc/subversions/git/github/pomadchin/geotrellis/raster/data/geotiff-test-files/reproject/cea.tif"))
       val list: ListBuffer[(Int, TileLayerRDD[SpatialKey])] = ListBuffer()
       val layoutScheme = ZoomedLayoutScheme(LatLng, 512)
@@ -56,17 +91,17 @@ class COGLayerSpec extends FunSpec
 
       //COGLayer.withStitch(layer)(zoom, 7, layoutScheme).collect().head
 
-      val cogs = COGLayer.withStitch(layer)(zoom, 10, layoutScheme)
-      COGLayer.write(cogs)(index, new URI("file:///tmp/write"))
+      val cogs = COGLayer(layer)(zoom, 7, layoutScheme)
+      COGLayer.write(cogs)(index, new URI("file:///tmp/write2"))
 
-      val (key, tiff: GeoTiff[Tile]) = cogs.collect().head
-      val sfc = index.toIndex(key)
+      //val (key, tiff: GeoTiff[Tile]) = cogs.collect().head
+      /*val sfc = index.toIndex(key)
 
       val tiff2 =
         GeoTiff(layer.stitch, layer.metadata.mapTransform(layer.metadata.gridBounds), LatLng)
           .copy(options = tiff.options)
 
-      GeoTiffWriter.write(tiff2.crop(layer.metadata.extent), "/tmp/testo2.tif", optimizedOrder = true)
+      GeoTiffWriter.write(tiff2.crop(layer.metadata.extent), "/tmp/testo2.tif", optimizedOrder = true)*/
 
       //GeoTiffWriter.write(tiff, s"/tmp/${sfc}_base.tif", optimizedOrder = true)
       //GeoTiffWriter.write(tiff.overviews(0), s"/tmp/${sfc}_0.tif", optimizedOrder = true)
