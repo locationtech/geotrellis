2dfd838b72a011ea766838bb7b84f0427ddfb6a7
jpolchlo <jpolchlopek@azavea.com>
2017-11-29 09:51:47 -0500

    Add rasterizer-based raster reprojection code
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

---
+++ raster/src/test/scala/geotrellis/raster/reproject/RasterizeReprojectSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package geotrellis.raster.reproject
+
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.raster.resample._
+import geotrellis.raster.testkit._
+import geotrellis.vector._
+
+import org.scalatest._
+
+class RasterizeReprojectSpec extends FunSpec
+    with RasterMatchers {
+
+  describe("Rasterizing reprojection") {
+    it("should fill in only valid region") {
+      val coloRaster = ProjectedRaster.apply(Raster[Tile](IntArrayTile.fill(1,700,400), Extent(-109,37,-102,41)), LatLng)
+      val destRegion = coloRaster.projectedExtent.reprojectAsPolygon(ConusAlbers, 0.005)
+      val destRE = ProjectedRasterExtent(destRegion.envelope, ConusAlbers, 1000, 800)
+      val reprojected = coloRaster.rasterizeReproject(destRE, NearestNeighbor, IntConstantNoDataCellType)
+      val trans = Proj4Transform(ConusAlbers, LatLng)
+
+      // Must ignore narrow strip near boundary of tile (resampler issues)
+      val ex = coloRaster.extent.buffer(-1e-3)
+      var valid = true
+
+      reprojected.tile.foreach{ (px, py, v) =>
+        val (x, y) = reprojected.rasterExtent.gridToMap(px, py)
+        val (tx, ty) = trans(x, y)
+        val dist = ex.distance(Point(tx, ty))
+        if (dist >= 1e-3) {
+          valid = valid && (v != 1)
+        } else if (dist == 0) {
+          valid = valid && (v == 1)
+        }
+      }
+
+      valid should be (true)
+    }
+  }
+
+}
