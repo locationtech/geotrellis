c9421c0fb2bb72f4ebe28fb51a8f9f80069b8927
jpolchlo <jpolchlopek@azavea.com>
2018-03-22 13:59:55 -0400

    Improved handling of geometry collections and added test suite
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- vector/src/main/scala/geotrellis/vector/util/Intersection.scala
+++ vector/src/main/scala/geotrellis/vector/util/Intersection.scala
@@ -19,26 +19,33 @@ import geotrellis.vector._
 
 object Intersection {
 
+  def unpackGeometryCollection(gc: GeometryCollection): Seq[Geometry] =
+    gc.geometryCollections.flatMap(unpackGeometryCollection(_)) ++ gc.geometries.filter(!_.isInstanceOf[GeometryCollection])
+
+  private def polysFromGC(gc: GeometryCollection): Seq[Polygon] =
+    unpackGeometryCollection(gc).flatMap{ g => g match {
+      case p: Polygon => Seq(p)
+      case mp: MultiPolygon => mp.polygons
+      case _ => Seq.empty
+    }}
+
   private def polyGeomIntersection(poly: Polygon, geom: Geometry): Seq[Polygon] = {
     geom match {
       case poly2: Polygon => poly2.intersection(poly) match {
         case PolygonResult(p) => Seq(p)
         case MultiPolygonResult(mp) => mp.polygons.toSeq
-        case GeometryCollectionResult(gc) =>
-          gc.multiPolygons.flatMap{ mp => mp.polygons.toSeq } ++ gc.polygons
+        case GeometryCollectionResult(gc) => polysFromGC(gc)
         case _ => Seq.empty[Polygon]
       }
       case mp: MultiPolygon => mp.intersection(poly) match {
         case PolygonResult(p) => Seq(p)
         case MultiPolygonResult(mp) => mp.polygons.toSeq
-        case GeometryCollectionResult(gc) =>
-          gc.multiPolygons.flatMap{ mp => mp.polygons.toSeq } ++ gc.polygons
+        case GeometryCollectionResult(gc) => polysFromGC(gc)
         case _ => Seq.empty[Polygon]
       }
       case gc: GeometryCollection => gc.intersection(poly) match {
         // In testing, GeometryCollection.intersection only seems to return GeometryCollectionResult
-        case GeometryCollectionResult(res) =>
-          res.multiPolygons.flatMap{ mp => mp.polygons.toSeq } ++ res.polygons
+        case GeometryCollectionResult(res) => polysFromGC(res)
         case _ => Seq.empty[Polygon]
       }
       case _ =>
@@ -52,8 +59,7 @@ object Intersection {
     a match {
       case poly: Polygon => polyGeomIntersection(poly, b)
       case multipoly: MultiPolygon => multipoly.polygons.toSeq.flatMap{ p => polyGeomIntersection(p, b) }
-      case gc: GeometryCollection =>
-        (gc.multiPolygons.flatMap(_.polygons) ++ gc.polygons).flatMap{ p => polyGeomIntersection(p, b) }
+      case gc: GeometryCollection => polysFromGC(gc).flatMap{ p => polyGeomIntersection(p, b) }
       case _ => Seq.empty[Polygon] // Both participants must contain polygonal regions
     }
   }
