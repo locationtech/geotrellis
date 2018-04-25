979da3946da2210d88a8cd42c0f0683b9b2b3659
jpolchlo <jpolchlopek@azavea.com>
2018-04-25 10:08:58 -0400

    Add docstring for availableZoomLevels
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- spark/src/main/scala/geotrellis/spark/io/AttributeStore.scala
+++ spark/src/main/scala/geotrellis/spark/io/AttributeStore.scala
@@ -44,7 +44,13 @@ trait AttributeStore extends  AttributeCaching with LayerAttributeStore {
    * catalogs with large numbers of layers can be queried efficiently.
    */
   def layersWithZoomLevels: Map[String, Seq[Int]] = layerIds.groupBy(_.name).mapValues(_.map(_.zoom))
-  def availableZoomLevels(layerName: String) = layersWithZoomLevels(layerName)
+
+  /** Return a sequence of available zoom levels for a named layer.
+   *
+   * This function should be re-implemented by AttributeStore subclasses so that
+   * catalogs with large numbers of layers can be queried efficiently.
+   */
+  def availableZoomLevels(layerName: String): Seq[Int] = layersWithZoomLevels(layerName)
 
   def copy(from: LayerId, to: LayerId): Unit =
     copy(from, to, availableAttributes(from))
