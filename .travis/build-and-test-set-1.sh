1d32f5f484388fe085c4f099d2f0ed6fb049953c
Grigory Pomadchin <gr.pomadchin@gmail.com>
2018-05-18 01:01:53 +0300

    An attempt to fix travis timouts

--- .travis/build-and-test-set-1.sh
+++ .travis/build-and-test-set-1.sh
@@ -4,7 +4,6 @@
   "project proj4" test \
   "project geotools" test \
   "project shapefile" test \
-  "project spark" test \
-  "project accumulo" test \
-  "project cassandra" test \
   "project doc-examples" compile || { exit 1; }
+./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project spark" test || { exit 1; }
+./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project accumulo" test "project cassandra" test || { exit 1; }
