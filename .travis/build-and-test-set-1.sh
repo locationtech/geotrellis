290945abefbde1a9bcb37d21c3da46c5b7ebc1c8
Grigory Pomadchin <gr.pomadchin@gmail.com>
2018-05-17 18:45:05 +0300

    Use a single sbt start instead of multiple to reduce CI run time

--- .travis/build-and-test-set-1.sh
+++ .travis/build-and-test-set-1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project proj4" test || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project geotools" test || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project shapefile" test || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project spark" test  || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project spark-pipeline" test  || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project spark-etl" test  || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project accumulo" test  || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project cassandra" test  || { exit 1; }
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" "project doc-examples" compile  || { exit 1; }
+./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" \
+  "project proj4" test \
+  "project geotools" test \
+  "project shapefile" test \
+  "project spark" test \
+  "project spark-pipeline" test \
+  "project spark-etl" test \
+  "project accumulo" test \
+  "project cassandra" test \
+  "project doc-examples" compile || { exit 1; }
