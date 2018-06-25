afd60ff5c0005e6c72397c286bdc09209aad9423
lossyrob <rdemanuele@gmail.com>
2018-06-25 15:44:29 -0400

    Added example of rasterizing polygons with count

--- doc-examples/src/main/scala/geotrellis/doc/examples/raster/RasterExamples.scala
+++ doc-examples/src/main/scala/geotrellis/doc/examples/raster/RasterExamples.scala
@@ -35,4 +35,25 @@ object RasterExamples {
       IntArrayTile(array, cols, rows)
     }
   }
+
+  def `Counting polygon features in raster cells (rasterize operation)`: Unit = {
+    import geotrellis.raster._
+    import geotrellis.vector._
+
+    def countPolygon(features: Seq[PolygonFeature[Int]], rasterExtent: RasterExtent): MultibandTile = {
+      // Assumes the polygon features have 5 classes
+
+      val (cols, rows) = (rasterExtent.cols, rasterExtent.rows)
+      val bands = Array.fill(5) { IntArrayTile.ofDim(cols, rows) }
+      for(Feature(polygon, classId) <- features) {
+        val targetBand = bands(classId)
+        // Use the 'foreach' method exension to rasterize the polygon over the raster extent.
+        rasterExtent.foreach(polygon) { (col, row) =>
+          targetBand.set(col, row, targetBand.get(col, row) + 1)
+        }
+      }
+
+      MultibandTile(bands)
+    }
+  }
 }
