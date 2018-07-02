ecfd6d78962af33c0c8c20aba0e831a531d17247
Grigory Pomadchin <gr.pomadchin@gmail.com>
2018-07-02 22:43:07 +0300

    Speedup travis builds

---
+++ .travis/build-and-test-set-3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" \
+  "project spark" test \
+  "project cassandra" test \
+  "project spark-pipeline" test \
+  "project spark-etl" test || { exit 1; }
