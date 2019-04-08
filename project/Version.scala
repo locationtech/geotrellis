5d997524fd579ebbc62f43b77ddb812d27a58bc0
Eugene Cheipesh <echeipesh@gmail.com>
2019-04-08 14:13:41 -0400

    Version 3.0.0-M2

--- project/Version.scala
+++ project/Version.scala
@@ -15,7 +15,7 @@
  */
 
 object Version {
-  val geotrellis  = "3.0.0-M1.1"
+  val geotrellis  = "3.0.0-M2"
   val scala       = "2.11.12"
   val crossScala  = Seq(scala, "2.12.7")
   val geotools    = "21.0"
