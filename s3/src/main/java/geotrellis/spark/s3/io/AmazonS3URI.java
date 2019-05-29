3ee21256bac4106aa08b8f28921f115aa37913ae
moradology <npzimmerman@gmail.com>
2019-05-29 09:44:56 -0400

    Use geotrellis namespace for aws uri

--- s3/src/main/java/geotrellis/spark/s3/io/AmazonS3URI.java
+++ s3/src/main/java/geotrellis/spark/s3/io/AmazonS3URI.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.amazonaws.services.s3;
+package geotrellis.spark.io.s3;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
