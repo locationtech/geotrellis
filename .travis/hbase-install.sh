47b5fe7ac98b7af4da99016eeb75726376e836dd
CloudNiner <andy@finkmiller.com>
2019-06-14 11:26:16 -0400

    Bump travis hbase to 2.1.5
    Signed-off-by: CloudNiner <andy@finkmiller.com>

--- .travis/hbase-install.sh
+++ .travis/hbase-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ ! -f $HOME/downloads/hbase-2.1.4-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-2.1.4-bin.tar.gz http://www-us.apache.org/dist/hbase/2.1.4/hbase-2.1.4-bin.tar.gz; fi
-sudo mv $HOME/downloads/hbase-2.1.4-bin.tar.gz hbase-2.1.4-bin.tar.gz && tar xzf hbase-2.1.4-bin.tar.gz
-sudo rm -f hbase-2.1.4/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-2.1.4/conf
-sudo hbase-2.1.4/bin/start-hbase.sh
+if [ ! -f $HOME/downloads/hbase-2.1.5-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-2.1.5-bin.tar.gz http://www-us.apache.org/dist/hbase/2.1.5/hbase-2.1.5-bin.tar.gz; fi
+sudo mv $HOME/downloads/hbase-2.1.5-bin.tar.gz hbase-2.1.5-bin.tar.gz && tar xzf hbase-2.1.5-bin.tar.gz
+sudo rm -f hbase-2.1.5/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-2.1.5/conf
+sudo hbase-2.1.5/bin/start-hbase.sh
