f6ac12cd78de9dcc2cf9b0ce65011d0783ea4b21
Grigory <gr.pomadchin@gmail.com>
2019-06-18 21:37:30 +0300

    Fix locationtech deployment scripts, remove unsued code (#3001)

--- layers/src/main/scala/geotrellis/layers/hadoop/package.scala
+++
@@ -1,9 +0,0 @@
-package geotrellis.layers
-
-import org.apache.hadoop.fs.Path
-
-package object hadoop extends Implicits {
-  final val SCHEMES: Array[String] = Array("hdfs", "hdfs+file", "s3n", "s3a", "wasb", "wasbs")
-
-  implicit def stringToPath(path: String): Path = new Path(path)
-}
