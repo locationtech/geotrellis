991d1281d45c453aceac5d3cd87915236980e2ac
Jacob Bouffard <jbouffard@azavea.com>
2019-07-09 09:05:52 -0400

    Impoved the docstrings for ZFactorCalculator
    Signed-off-by: Jacob Bouffard <jbouffard@azavea.com>

--- raster/src/main/scala/geotrellis/raster/mapalgebra/focal/ZFactorCalculator.scala
+++ raster/src/main/scala/geotrellis/raster/mapalgebra/focal/ZFactorCalculator.scala
@@ -7,6 +7,9 @@ import org.apache.commons.math3.analysis.interpolation._
 
 /** Produces a ZFactor for a given point using the prescribed
  *  conversion method.
+ *
+ *  @param produceZFactor A function that takes a latitude point and
+ *    converts it to its corresponding ZFactor.
  */
 class ZFactorCalculator(produceZFactor: Double => Double) extends Serializable {
   /** Produces the associated ZFactor for the given location.
@@ -26,16 +29,16 @@ class ZFactorCalculator(produceZFactor: Double => Double) extends Serializable {
 }
 
 /** When creating a ZFactorCalculator, the projection of the target
- *  Tiles needs to be taken into account. If the Tiles are in
- *  LatLng, then the conversion between Latitude and ZFactor is
- *  already known. Otherwise, one will need to supply the
- *  transformations required to produce the ZFactor.
+ *  Tile(s) needs to be taken into account. If the Tiles are in
+ *  LatLng, then the conversion between Latitude and ZFactor can
+ *  already be calculated. Otherwise, one will need to supply the
+ *  transformation required to produce the ZFactor.
  */
 object ZFactorCalculator {
   final val LAT_LNG_FEET_AT_EQUATOR = 365217.6
   final val LAT_LNG_METERS_AT_EQUATOR = 11320
 
-  /** Creates a [[ZFactorCalculator]] specifically for layers that are in
+  /** Creates a [[ZFactorCalculator]] specifically for Tiles that are in
    *  LatLng.
    *
    *  @param unit The [[TileUnit]] type that the Tiles are in.
@@ -48,17 +51,18 @@ object ZFactorCalculator {
         ZFactorCalculator((lat: Double) => 1 / (LAT_LNG_METERS_AT_EQUATOR * math.cos(math.toRadians(lat))))
     }
 
-  /** Creates a [[ZFactorCalculator]] from user provided information.
+  /** Creates a [[ZFactorCalculator]] which uses linear interplation to calculate
+   *  ZFactors. The linear interploation itself is derived from the values given.
    *
-   *  @param mappedLats A Map that maps latitudes to ZFactors. It is not required
-   *    to supply a ZFactor for every latitude intersected by the layer. Rather,
+   *  @param mappedLatitudes A Map that maps latitudes to ZFactors. It is not required
+   *    to supply a ZFactor for every latitude intersected by the Tile(s). Rather,
    *    based on the values given, a linear interpolation will be created and
    *    any latitude not mapped will have its associated ZFactor derived from
    *    that interpolation.
    */
-  def createCalculator(mappedLats: Map[Double, Double]): ZFactorCalculator = {
+  def createCalculator(mappedLatitudess: Map[Double, Double]): ZFactorCalculator = {
     val interp = new LinearInterpolator()
-    val spline = interp.interpolate(mappedLats.keys.toArray, mappedLats.values.toArray)
+    val spline = interp.interpolate(mappedLatitudes.keys.toArray, mappedLatitudes.values.toArray)
 
     ZFactorCalculator((lat: Double) => spline.value(lat))
   }
