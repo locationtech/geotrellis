84b36c56186512de97e55f4b0393cc5e22fd8b4d
Grigory <gr.pomadchin@gmail.com>
2019-08-26 14:46:24 -0400

    Fix SaveToS3 methods serialization (#3060)

--- s3-spark/src/main/scala/geotrellis/spark/store/s3/Implicits.scala
+++ s3-spark/src/main/scala/geotrellis/spark/store/s3/Implicits.scala
@@ -21,6 +21,6 @@ import org.apache.spark.rdd.RDD
 
 object Implicits extends Implicits
 
-trait Implicits {
+trait Implicits extends Serializable {
   implicit class withSaveToS3Methods[K](rdd: RDD[(K, Array[Byte])]) extends SaveToS3Methods(rdd)
 }
