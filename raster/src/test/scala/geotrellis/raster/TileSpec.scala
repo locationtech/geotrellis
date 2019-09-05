d67a1b769a3c42d28d7060de63ba93920ab7f996
CloudNiner <andy@finkmiller.com>
2019-09-05 15:55:40 -0400

    Backport np helper functions to geotrellis.util.np
    - linspace: https://docs.scipy.org/doc/numpy/reference/generated/numpy.linspace.html
    - percentile: https://docs.scipy.org/doc/numpy/reference/generated/numpy.percentile.html
    
    Also adds implicit helper methods for Tile.percentile

--- raster/src/test/scala/geotrellis/raster/TileSpec.scala
+++ raster/src/test/scala/geotrellis/raster/TileSpec.scala
@@ -383,4 +383,20 @@ class TileSpec extends FunSpec
                                  2, 3, 4, 2))
     }
   }
+
+  describe("percentile") {
+    it("should construct a numpy percentile for a Tile") {
+      val x = ArrayTile((0 until 8).map(_ * 0.5).toArray, 7, 1)
+
+      x.percentile(0) shouldBe 0d
+      x.percentile(100) shouldBe 3.5
+      x.percentile(50) shouldBe 1.75
+
+      val xmutable = x.mutable
+      xmutable.setDouble(1, 0, NODATA)
+      val xn = xmutable.toArrayTile
+
+      xn.percentile(0) shouldBe NODATA
+    }
+  }
 }
---
+++ util/src/test/scala/geotrellis/util/np/PercentileSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package geotrellis.util.np
+
+import org.scalatest._
+
+class PercentileSpec extends FunSpec with Matchers {
+  // https://github.com/numpy/numpy/blob/b57957c639ca7c96c328003cc2436a06f8ecf9db/numpy/lib/tests/test_function_base.py#L2508
+  describe("percentile should behave like numpy.percentile") {
+    it("test basic") {
+      val x = (0 until 8).map(_ * 0.5).toArray
+
+      percentile(x, 0) shouldBe 0d
+      percentile(x, 100) shouldBe 3.5
+      percentile(x, 50) shouldBe 1.75
+    }
+  }
+}
