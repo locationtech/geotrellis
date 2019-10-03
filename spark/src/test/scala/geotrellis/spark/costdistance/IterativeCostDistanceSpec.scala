12599ec919efd5ac74c77a84c655390be9712586
jmtaysom <james.taysom@gmail.com>
2019-10-03 09:56:24 -0400

    updated cost distance tests for compute resolution
    Signed-off-by: jmtaysom <james.taysom@gmail.com>

--- spark/src/test/scala/geotrellis/spark/costdistance/IterativeCostDistanceSpec.scala
+++ spark/src/test/scala/geotrellis/spark/costdistance/IterativeCostDistanceSpec.scala
@@ -104,7 +104,7 @@ class IterativeCostDistanceSpec extends FunSpec
       val resolution = IterativeCostDistance.computeResolution(rdd3)
       val hops = (up.getDouble(2,3) - down.getDouble(2,3)) / resolution
 
-      hops should be (5.0)
+      hops should be (5.0 +- 1e-10)
     }
 
     it("Should propogate down") {
@@ -114,7 +114,7 @@ class IterativeCostDistanceSpec extends FunSpec
       val resolution = IterativeCostDistance.computeResolution(rdd3)
       val hops = (up.getDouble(2,1) - down.getDouble(2,1)) / resolution
 
-      hops should be (5.0)
+      hops should be (5.0 +- 1e-10)
     }
 
   }
