ac0bf5fd26a7ef405bd84f458650fe35bc1fee30
Eugene Cheipesh <echeipesh@gmail.com>
2019-10-04 11:58:19 -0400

    hbase 2.1.6 for tests

--- .travis/hbase-install.sh
+++ .travis/hbase-install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-if [ ! -f $HOME/downloads/hbase-2.1.5-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-2.1.5-bin.tar.gz http://www-us.apache.org/dist/hbase/2.1.5/hbase-2.1.5-bin.tar.gz; fi
-sudo mv $HOME/downloads/hbase-2.1.5-bin.tar.gz hbase-2.1.5-bin.tar.gz && tar xzf hbase-2.1.5-bin.tar.gz
-sudo rm -f hbase-2.1.5/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-2.1.5/conf
-sudo hbase-2.1.5/bin/start-hbase.sh
-
+if [ ! -f $HOME/downloads/hbase-2.1.6-bin.tar.gz ]; then sudo wget -O $HOME/downloads/hbase-2.1.6-bin.tar.gz http://www-us.apache.org/dist/hbase/2.1.6/hbase-2.1.6-bin.tar.gz; fi
+sudo mv $HOME/downloads/hbase-2.1.6-bin.tar.gz hbase-2.1.6-bin.tar.gz && tar xzf hbase-2.1.6-bin.tar.gz
+sudo rm -f hbase-2.1.6/conf/hbase-site.xml && sudo mv .travis/hbase/hbase-site.xml hbase-2.1.6/conf
+sudo hbase-2.1.6/bin/start-hbase.sh
