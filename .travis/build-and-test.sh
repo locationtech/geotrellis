8bd12a9f371e1718d6415a81c7cb137cd5b3c546
CloudNiner <andy@finkmiller.com>
2019-10-07 13:12:05 -0400

    Run scaladoc in separate RUN_SET
    To avoid build runtime limits

--- .travis/build-and-test.sh
+++ .travis/build-and-test.sh
@@ -14,11 +14,13 @@ elif [ $RUN_SET = "2" ]; then
     else
         .travis/build-set-2.sh;
     fi
-else
+elif [ $RUN_SET = "3" ]; then
     echo "RUNNING SET 3";
     if [ `echo $TRAVIS_SCALA_VERSION | cut -f1-2 -d "."` = "2.11" ]; then
         .travis/build-and-test-set-3.sh;
     else
         .travis/build-set-3.sh;
     fi
+else
+    echo "Skipping RUN_SET = $RUN_SET in build-and-test.sh."
 fi
