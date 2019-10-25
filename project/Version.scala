8f70df3274b67cdabc12d79412a85480476f7599
Eugene Cheipesh <echeipesh@gmail.com>
2019-10-25 19:53:06 -0400

    Version 3.1.0

--- project/Version.scala
+++ project/Version.scala
@@ -15,7 +15,7 @@
  */
 
 object Version {
-  val geotrellis  = "3.0.0" + Environment.versionSuffix
+  val geotrellis  = "3.1.0" + Environment.versionSuffix
   val scala       = "2.11.12"
   val crossScala  = Seq(scala, "2.12.7")
   val geotools    = "21.2"
@@ -28,10 +28,10 @@ object Version {
   val geowave     = "0.9.3"
   val circe       = "0.11.1"
   val previousVersion = "2.2.0"
-  
+
   val gdal     = "2.4.0"
   val gdalWarp = "33.61199eb"
-  
+
   lazy val hadoop = Environment.hadoopVersion
   lazy val spark  = Environment.sparkVersion
 }
