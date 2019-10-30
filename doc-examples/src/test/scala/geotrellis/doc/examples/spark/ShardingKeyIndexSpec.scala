5b88b8dca2b547ceea1607ef7b6b2c0871fdda6b
Eugene Cheipesh <echeipesh@gmail.com>
2019-10-30 10:21:31 -0400

    Fix more deprecation warnings

--- doc-examples/src/test/scala/geotrellis/doc/examples/spark/ShardingKeyIndexSpec.scala
+++ doc-examples/src/test/scala/geotrellis/doc/examples/spark/ShardingKeyIndexSpec.scala
@@ -64,17 +64,13 @@ class ShardingKeyIndexSpec extends FunSpec with Matchers {
     it("Z-Space Isomorphism") {
       val index: KeyIndex[SpatialKey] = new ShardingKeyIndex(zspace, 5)
 
-      index.asJson.as[KeyIndex[SpatialKey]] match {
-        case Left(err) => fail(err.message)
-      }
+      index.asJson.as[KeyIndex[SpatialKey]].toTry.get
     }
 
     it("Z-Time Isomorphism") {
       val index: KeyIndex[SpaceTimeKey] = new ShardingKeyIndex(zspaceTime, 5)
 
-      index.asJson.as[KeyIndex[SpaceTimeKey]] match {
-        case Left(err) => fail(err.message)
-      }
+      index.asJson.as[KeyIndex[SpaceTimeKey]].toTry.get
     }
 
     it("H-Space Isomorphism") {
