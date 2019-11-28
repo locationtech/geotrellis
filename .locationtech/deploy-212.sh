c2919b7c81657953c3333eb4b274951a8bf51b60
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-11-27 23:07:49 -0500

    Fix publish scripts

--- .locationtech/deploy-212.sh
+++ .locationtech/deploy-212.sh
@@ -3,28 +3,27 @@
  set -e
  set -x
 
- # there is no geomesa project
- ./sbt -212 "project macros" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project vector" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project proj4" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project raster" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project spark-pipeline" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project s3" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project s3-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project accumulo" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project accumulo-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project hbase" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project hbase-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project cassandra" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project cassandra-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project geotools" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project shapefile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project layer" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project store" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project util" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project vectortile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project raster-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project vector-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project spark-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -212 "project gdal" publish -no-colors -J-Drelease=locationtech
+ ./sbt "project macros" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project vector" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project proj4" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project raster" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project spark" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project spark-pipeline" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project s3" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project s3-spark" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project accumulo" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project accumulo-spark" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project hbase" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project hbase-spark" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project cassandra" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project cassandra-spark" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project geotools" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project shapefile" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project layer" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project store" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project util" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project vectortile" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project raster-testkit" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project vector-testkit" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project spark-testkit" publish -no-colors -J-Drelease=locationtech \
+   && ./sbt "project gdal" publish -no-colors -J-Drelease=locationtech
--- publish/publish-to-sonatype-212.sh
+++ publish/publish-to-sonatype-212.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 # Publish to sonatype for all supported scala version 2.12
 
- # there is no geomesa project
- ./sbt -212 "project macros" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project vector" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project proj4" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project raster" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project spark" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project spark-pipeline" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project s3" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project s3-spark" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project accumulo" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project accumulo-spark" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project hbase" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project hbase-spark" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project cassandra" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project cassandra-spark" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project geotools" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project shapefile" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project layer" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project store" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project util" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project vectortile" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project raster-testkit" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project vector-testkit" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project spark-testkit" publishSigned -no-colors -J-Drelease=sonatype \
-   && ./sbt -212 "project gdal" publishSigned -no-colors -J-Drelease=sonatype
+ ./sbt "project macros" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project vector" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project proj4" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project raster" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project spark" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project spark-pipeline" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project s3" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project s3-spark" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project accumulo" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project accumulo-spark" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project hbase" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project hbase-spark" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project cassandra" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project cassandra-spark" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project geotools" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project shapefile" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project layer" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project store" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project util" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project vectortile" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project raster-testkit" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project vector-testkit" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project spark-testkit" publishSigned -no-colors -J-Drelease=sonatype \
+   && ./sbt "project gdal" publishSigned -no-colors -J-Drelease=sonatype
