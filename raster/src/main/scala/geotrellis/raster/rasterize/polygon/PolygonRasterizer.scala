c1005244a6d1f16d0a299510d2336170d565bbe5
peter.macleod <psmacleod@gmail.com>
2020-02-27 09:43:57 -0800

    PixelIsArea rasterizer sometimes fills in concavities because the edges are not sorted by intersecting X.
    Signed-off-by: peter.macleod <psmacleod@gmail.com>

--- raster/src/main/scala/geotrellis/raster/rasterize/polygon/PolygonRasterizer.scala
+++ raster/src/main/scala/geotrellis/raster/rasterize/polygon/PolygonRasterizer.scala
@@ -318,11 +318,16 @@ object PolygonRasterizer {
           )}
       })
 
-    interactions
+    interactions.map { edge: Segment =>
+      val topX = lineAxisIntersection(edge, top)._1
+      val bottomX = lineAxisIntersection(edge, bot)._1
+      val sortX = if (topX != Double.NegativeInfinity) topX
+      else if (bottomX != Double.NegativeInfinity) bottomX
+      else edge._1
+      (sortX, topX, bottomX, edge)
+    }
       .sortWith(_._1 < _._1)
-      .foreach({ edge =>
-        val topIntervalX = lineAxisIntersection(edge, top)._1
-        val botIntervalX = lineAxisIntersection(edge, bot)._1
+      .foreach({ case (_: Double, topIntervalX: Double, botIntervalX: Double, edge: Segment) =>
         val touchesTop = (topIntervalX != Double.NegativeInfinity)
         val touchesBot = (botIntervalX != Double.NegativeInfinity)
 
