7d894bd01b7c3693e6f094c396cf73a2050d4796
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-04-02 10:28:57 -0400

    Refactor the selectIndexByProximity function to use fold instead of a search

--- raster/src/test/scala/geotrellis/raster/io/geotiff/OverviewStrategySpec.scala
+++ raster/src/test/scala/geotrellis/raster/io/geotiff/OverviewStrategySpec.scala
@@ -15,58 +15,72 @@ class OverviewStrategySpec extends FunSpec with Matchers {
   describe("Auto") {
     it("auto0 should select the closest resolution") {
       val strategy = Auto(0)
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
       selected should be (2)
     }
     it("auto1 should select the one after the closest") {
       val strategy = Auto(1)
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
       selected should be (3)
     }
     it("auto2 should select the one after the one after closest") {
       val strategy = Auto(2)
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
       selected should be (4)
     }
     it ("should select the last overview if out of bounds high") {
       val strategy = Auto(8)
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(1,1), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(1,1), strategy)
       selected should be (4)
+    }
+
+    it("should select the base overview if the list is not sorted") {
+      val strategy = Auto(0)
+      val selected = OverviewStrategy.selectOverview(availableResolutions.reverse, CellSize(45,45), strategy)
+      selected should be (0)
     }
   }
   describe("Level") {
     it("should select the nth overview") {
       val strategy = Level(2)
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      selected should be (2)
+    }
+
+    it("should select the nth overview if the list is not sorted") {
+      val strategy = Level(2)
+      val selected = OverviewStrategy.selectOverview(availableResolutions.reverse, CellSize(5,5), strategy)
       selected should be (2)
     }
   }
   describe("Base") {
+    val strategy = Base
     it("should select the highest/base resolution overview") {
-      val strategy = Base
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(5,5), strategy)
       selected should be (0)
+    }
+
+    it("hould select the highest/base resolution overview if the list is not sorted") {
+      val selected = OverviewStrategy.selectOverview(availableResolutions.reverse, CellSize(5,5), strategy)
+      selected should be (4)
     }
   }
   describe("AutoHigherResolution") {
     val strategy = AutoHigherResolution
 
     it("should select the nearest overview - without rounding down") {
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(7,7), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(7,7), strategy)
       selected should be (2)
     }
 
     it("should select the best matching overview if out of bounds") {
-      val selected =
-        OverviewStrategy.selectOverview(availableResolutions, CellSize(32,32), strategy)
+      val selected = OverviewStrategy.selectOverview(availableResolutions, CellSize(32,32), strategy)
       selected should be (4)
+    }
+
+    it("should select the base overview if the list is not sorted") {
+      val selected = OverviewStrategy.selectOverview(availableResolutions.reverse, CellSize(32,32), strategy)
+      selected should be (0)
     }
   }
 }
