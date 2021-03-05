a334a5ce1911c3bbcedf5d24711dcc79cf6a20da
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-03-05 11:48:34 -0500

    Add the new JenkinsCI credentials location

--- project/Settings.scala
+++ project/Settings.scala
@@ -87,7 +87,10 @@ object Settings {
       }
     },
 
-    credentials ++= List(Path.userHome / ".ivy2" / ".credentials").filter(_.asFile.canRead).map(Credentials(_)),
+    credentials ++= List(
+      Path.userHome / ".ivy2" / ".credentials",
+      Path.userHome / ".sbt" / ".credentials"
+    ).filter(_.asFile.canRead).map(Credentials(_)),
 
     addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.2" cross CrossVersion.full),
     addCompilerPlugin("org.scalamacros" %% "paradise" % "2.1.1" cross CrossVersion.full),
