fa88aeddab0849cdb8f830d3747554bc7adceb1f
Grigory <gr.pomadchin@gmail.com>
2021-04-21 08:00:44 -0400

    Cleanup build files (#3386)

--- project/Settings.scala
+++ project/Settings.scala
@@ -283,8 +283,6 @@ object Settings {
       geotoolsReferencing,
       geotoolsMetadata,
       geotoolsOpengis,
-      imageioExtUtilities,
-      jtUtilities,
       geotoolsGeoTiff % Test,
       geotoolsShapefile % Test,
       scalatest % Test
