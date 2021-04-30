cfad50146a8e71600290e1a7778758c4a0909018
Grigory <gr.pomadchin@gmail.com>
2021-04-30 12:34:49 -0400

    Cleanup benchmarks (#3387)
    * Bump GT version up
    
    * Remove GeoTrellisFlightRecordingProfile

--- bench/src/main/scala/geotrellis/bench/package.scala
+++ bench/src/main/scala/geotrellis/bench/package.scala
@@ -17,13 +17,11 @@
 package geotrellis
 
 import geotrellis.raster.io.geotiff.SinglebandGeoTiff
+import scala.reflect.ClassTag
 
-/**
- * Module utilities. Originally from geotrellis-benchmark
- */
 package object bench {
   /** Sugar for building arrays using a per-cell init function */
-  def init[A: Manifest](size: Int)(init: => A) = {
+  def init[A: ClassTag](size: Int)(init: => A): Array[A] = {
     val data = Array.ofDim[A](size)
     for (i <- 0 until size) data(i) = init
     data
