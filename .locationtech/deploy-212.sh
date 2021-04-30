166d545183e4e4d516f36aca9b62d0a3297f1531
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-04-30 13:20:05 -0400

    Update Locationtech deploy scripts

--- .locationtech/deploy-212.sh
+++ .locationtech/deploy-212.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 
- set -e
- set -x
+set -e
+set -x
 
- ./sbt "project macros" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project vector" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project proj4" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project raster" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project spark-pipeline" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project s3" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project s3-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project accumulo" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project accumulo-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project hbase" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project hbase-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project cassandra" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project cassandra-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project geowave" publish -no-colors -J-Drelease=locationtech
-   && ./sbt "project geotools" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project shapefile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project layer" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project store" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project util" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project vectortile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project raster-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project vector-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project spark-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt "project gdal" publish -no-colors -J-Drelease=locationtech
+./sbt -213 publish -no-colors -J-Drelease=locationtech
--- .locationtech/deploy-213.sh
+++ .locationtech/deploy-213.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 
- set -e
- set -x
+set -e
+set -x
 
- ./sbt -213 "project macros" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project vector" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project proj4" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project raster" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project spark-pipeline" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project s3" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project s3-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project accumulo" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project accumulo-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project hbase" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project hbase-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project cassandra" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project cassandra-spark" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project geotools" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project shapefile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project layer" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project store" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project util" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project vectortile" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project raster-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project vector-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project spark-testkit" publish -no-colors -J-Drelease=locationtech \
-   && ./sbt -213 "project gdal" publish -no-colors -J-Drelease=locationtech
+./sbt -213 publish -no-colors -J-Drelease=locationtech
