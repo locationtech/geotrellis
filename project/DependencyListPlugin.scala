82913c604cfda7a8818c9d89c0a421da38cd6ea7
Grigory Pomadchin <gr.pomadchin@gmail.com>
2022-11-03 22:29:48 -0400

    Add the new CQs instructions and filing automation description

---
+++ project/DependencyListPlugin.scala
@@ -0,0 +1,64 @@
+import sbt._
+import sbt.Keys._
+import sbt.io.IO
+import sbt.plugins.DependencyTreeKeys.dependencyList
+import sbt.plugins.DependencyTreeSettings.targetFileAndForceParser
+import sbt.plugins.MiniDependencyTreeKeys.{asString, toFile}
+import java.io.File
+
+/** Usage example: dependencyListGT/toFile target/dependencies-list.txt */
+object DependencyListPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object Keys {
+    val dependencyListGT       = inputKey[Unit]("Execute dependencyList GeoTrellis command")
+    val dependencyListGTAppend = settingKey[Boolean]("dependencyList GeoTrellis command append mode: true by default").withRank(KeyRanks.Invisible)
+    val dependencyListGTIgnore = settingKey[Seq[String]]("dependencyList GeoTrellis command ignored dependencies: skips dependencies containing ignored strings in the output file").withRank(KeyRanks.Invisible)
+  }
+
+  val autoImport = Keys
+  import autoImport._
+
+  override def projectSettings = renderingTaskSettingsAppend(dependencyListGT)
+
+  def renderingTaskSettingsAppend(key: InputKey[Unit]) =
+    Seq(
+      dependencyListGTAppend := true,
+      dependencyListGTIgnore := Seq("locationtech"),
+      key := {
+        val s = streams.value
+        val str = (Compile / dependencyList / asString).value
+        s.log.info(str)
+      },
+      key / toFile := {
+        val (targetFile, force) = targetFileAndForceParser.parsed
+        val list =
+          (Compile / dependencyList / asString)
+            .value
+            .split('\n')
+            .filterNot(str => dependencyListGTIgnore.value.exists(str.contains))
+            .mkString("\n")
+
+        writeToFileAppend(key.key.label, list, targetFile, force, streams.value, dependencyListGTAppend.value)
+      },
+    )
+
+  def writeToFileAppend(
+    what: String,
+    data: String,
+    targetFile: File,
+    force: Boolean,
+    streams: TaskStreams,
+    append: Boolean
+  ): File =
+    if (targetFile.exists && !force && !append) {
+      throw new RuntimeException(
+        s"Target file for $what already exists at ${targetFile.getAbsolutePath}. Use '-f' to override"
+      )
+    } else {
+      IO.write(targetFile, data, IO.utf8, append)
+
+      streams.log.info(s"Wrote $what to '$targetFile'")
+      targetFile
+    }
+}
