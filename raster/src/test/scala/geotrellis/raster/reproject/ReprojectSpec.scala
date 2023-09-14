c35b32ff868603a46c263ade14132b6607d77e2a
Grigory Pomadchin <gr.pomadchin@gmail.com>
2023-09-13 22:04:20 -0400

    Fix JDK11 raster tests

--- raster/src/test/scala/geotrellis/raster/reproject/ReprojectSpec.scala
+++ raster/src/test/scala/geotrellis/raster/reproject/ReprojectSpec.scala
@@ -294,7 +294,13 @@ class ReprojectSpec extends AnyFunSpec
       val raster = geoTiff.raster.reproject(geoTiff.crs, WebMercator)
 
       geoTiff.crs.toProj4String should be ("+proj=cea +lat_ts=33.75 +lon_0=-117.333333333333 +x_0=0.0 +y_0=0.0 +datum=NAD27 +units=m ")
-      raster.extent should be (Extent(-1.3095719172012957E7, 3983866.9277966353, -1.305868719072902E7, 4021260.5495227976))
+
+      // raster.extent should be (Extent(-1.3095719172012957E7, 3983866.9277966353, -1.305868719072902E7, 4021260.5495227976))
+      raster.extent.xmin should be (-1.3095719172012957E7 +- 1e-7)
+      raster.extent.ymin should be (3983866.9277966353 +- 1e-7)
+      raster.extent.xmax should be (-1.305868719072902E7 +- 1e-7)
+      raster.extent.ymax should be (4021260.5495227976 +- 1e-7)
+
       raster.dimensions shouldBe Dimensions(512, 517)
     }
   }
