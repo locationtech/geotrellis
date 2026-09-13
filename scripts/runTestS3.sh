7f462f8d1727af7577dfeddbcc8678837aa61fcd
Grigory Pomadchin <gr.pomadchin@gmail.com>
2026-09-13 09:45:26 -0400

    minio => silo rename

--- scripts/runTestS3.sh
+++ scripts/runTestS3.sh
@@ -4,5 +4,5 @@
 docker run -d --restart=always \
     -p 9091:9000 \
     -e MINIO_ROOT_USER=minio -e MINIO_ROOT_PASSWORD=password \
-    pgsty/minio:RELEASE.2026-09-03T13-18-01Z \
+    pgsty/silo:RELEASE.2026-09-03T13-18-01Z \
     server /data
